af941e1cb8bfb868213b2e6c1242daaca72d02fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:03:59 +0000

    Add package hi-spike/0.0.9/hi_spike-0.0.9.tar.gz

---
+++ packages/hi-spike/hi_spike-0.0.9.tar.gz/hi_spike-0.0.9/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hi_spike',
+    version='0.0.9',
+    author='deng1fan',
+    author_email='dengyifan@iie.ac.cn',
+    url='https://github.com/deng1fan',
+    description=u'工具包，包含 Redis 安装、基于 Redis 的GPU 任务队列管理等功能',
+    long_description=open("README.md", "r", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'nvitop',
+        'redis',
+        'rich',
+        'psutil',
+        'jsonlines',
+        'setproctitle',
+        'traceback'
+    ],
+    exclude=["*.tests", "*.tests.*", "tests"],
+    include_package_data=True,
+    python_requires='>=3.6',
+    keywords=['gpu', 'queuer', 'redis'],
+    entry_points = {
+        'console_scripts' : [
+            # 这一行是安装到命令行运行的关键
+            'spike = spike.cmd_utils:main',
+            'run = spike.run_exp_plan:run'
+        ]
+    }
+)
