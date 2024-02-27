7ec0735bfa97d20572ac46f6e944397e370b82fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:04:30 +0000

    Add package accelbyte-py-sdk/0.61.0/accelbyte_py_sdk-0.61.0-py2.py3-none-any.whl

---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.61.0-py2.py3-none-any.whl/accelbyte_py_sdk/ext/gametelemetry.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+# Analytics Game Telemetry
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+from .utils import randomize
+
+from ..api.gametelemetry.models import HTTPValidationError
+from ..api.gametelemetry.models import TelemetryBody
+from ..api.gametelemetry.models import ValidationError
+
+
+def create_http_validation_error_example() -> HTTPValidationError:
+    instance = HTTPValidationError()
+    instance.detail = [create_validation_error_example()]
+    return instance
+
+
+def create_telemetry_body_example() -> TelemetryBody:
+    instance = TelemetryBody()
+    instance.event_name = randomize()
+    instance.event_namespace = randomize("slug")
+    instance.payload = {randomize(): randomize()}
+    instance.client_timestamp = randomize("date")
+    instance.event_id = randomize()
+    instance.event_timestamp = randomize("date")
+    return instance
+
+
+def create_validation_error_example() -> ValidationError:
+    instance = ValidationError()
+    instance.loc = [randomize()]
+    instance.msg = randomize()
+    instance.type_ = randomize()
+    return instance
