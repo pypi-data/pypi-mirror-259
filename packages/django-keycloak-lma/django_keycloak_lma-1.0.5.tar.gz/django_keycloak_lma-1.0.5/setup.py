41cd25f1d687a42e066d20300adc0701fa1a9bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:03:45 +0000

    Add package django-keycloak-lma/1.0.5/django_keycloak_lma-1.0.5.tar.gz

---
+++ packages/django-keycloak-lma/django_keycloak_lma-1.0.5.tar.gz/django_keycloak_lma-1.0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='django_keycloak_lma',
+      version='1.0.5',
+      description='Modified version of the package django_keycloak (0.1.1)',
+      long_description='Modified version of the package django_keycloak (0.1.1)',
+      author='Vyacheslav Konovalov',
+      packages=find_packages(),
+      install_requires=[
+            'djangorestframework',
+            'python-keycloak-client',
+      ],
+      include_package_data=True,
+      zip_safe=False)
