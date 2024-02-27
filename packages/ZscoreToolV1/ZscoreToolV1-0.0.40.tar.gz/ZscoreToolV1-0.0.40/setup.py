12eb06fa1c6c8cbf0273a4af975d2ffc336a7acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:03:39 +0000

    Add package ZscoreToolV1/0.0.40/ZscoreToolV1-0.0.40.tar.gz

---
+++ packages/ZscoreToolV1/ZscoreToolV1-0.0.40.tar.gz/ZscoreToolV1-0.0.40/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.40' 
+DESCRIPTION = 'Test python package'
+LONG_DESCRIPTION = 'Just testing'
+
+# Setting up
+setup(
+        name="ZscoreToolV1", 
+        version=VERSION,
+        author="Emma Gerrits",
+        author_email="emma.gerrits@ki.se",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['numpy', 'pandas', 'scanpy', 'umap',
+                          'scipy', 'tqdm', 'adjustText', 'scikit-learn', 'matplotlib',
+                          'tifffile', 'Pillow'],
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+            'Operating System :: POSIX :: Linux',
+        ]
+)
