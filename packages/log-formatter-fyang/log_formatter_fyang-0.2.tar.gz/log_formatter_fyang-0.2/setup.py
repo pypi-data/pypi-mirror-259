7800ee505cab6606adab011d88bdecc3fc9bb0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:04:00 +0000

    Add package log-formatter-fyang/0.2/log_formatter_fyang-0.2.tar.gz

---
+++ packages/log-formatter-fyang/log_formatter_fyang-0.2.tar.gz/log_formatter_fyang-0.2/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='log_formatter_fyang',
+    version='0.2',
+    description='log formatting utility',
+    license="MIT",
+    author="ocean",
+    packages=find_packages(),
+    long_description="A simple log formatting utility for Python projects",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='logging log formatting',
+    python_requires='>=3.7',
+    install_requires=[
+        'requests',
+        'numpy',
+	'huluwa_art',
+    ]
+)
+
