41f33ba8a71372c044759f39af22d44e2f0951db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:03:11 +0000

    Add package fake-bge-module-latest/20240227/fake_bge_module_latest-20240227-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20240227-py3-none-any.whl/bl_keymap_utils/__init__.py
@@ -0,0 +1,9 @@
+import sys
+import typing
+from . import io
+from . import platform_helpers
+from . import keymap_hierarchy
+from . import keymap_from_toolbar
+from . import versioning
+
+GenericType = typing.TypeVar("GenericType")
