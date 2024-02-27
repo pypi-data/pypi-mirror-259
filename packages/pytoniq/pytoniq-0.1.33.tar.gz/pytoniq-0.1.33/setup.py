16f24bb7e9075d3c625b6b4469722ff68a769a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:03:43 +0000

    Add package pytoniq/0.1.33/pytoniq-0.1.33.tar.gz

---
+++ packages/pytoniq/pytoniq-0.1.33.tar.gz/pytoniq-0.1.33/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytoniq",
+    version="0.1.33",
+    author="Maksim Kurbatov",
+    author_email="cyrbatoff@gmail.com",
+    description="TON Blockchain SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages('.', exclude=['tests', 'examples']),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries",
+    ],
+    url="https://github.com/yungwine/pytoniq",
+    python_requires='>=3.9',
+    py_modules=["pytoniq"],
+    install_requires=[
+        "pytoniq-core>=0.1.31",
+        "requests>=2.31.0",
+        "setuptools>=65.5.1",
+    ]
+)
