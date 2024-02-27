1d494ff76de4cfcf18d2dc575084118696d19628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:03:50 +0000

    Add package truecasingIT/1.0.5/truecasingIT-1.0.5.tar.gz

---
+++ packages/truecasingIT/truecasingIT-1.0.5.tar.gz/truecasingIT-1.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name="truecasingIT",
+	version="1.0.5",
+	author="Simon Hengchen",
+	author_email="simon@iguanodon.ai",
+	description="This package allows you to truecase Italian.",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+    url = "https://github.com/iguanodon-ai/truecasingIT",
+	classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages = setuptools.find_packages(where="."),
+    python_requires = ">=3.6",
+    include_package_data=True
+)
