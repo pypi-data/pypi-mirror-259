343b93dc00619bffe655ff4cc829baf33b3d5541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:04:10 +0000

    Add package aell/0.0.1/aell-0.0.1-py3-none-any.whl

---
+++ packages/aell/aell-0.0.1-py3-none-any.whl/aell/llm/api_general.py
@@ -0,0 +1,43 @@
+import http.client
+import json
+
+
+class InterfaceAPI:
+    def __init__(self, api_endpoint, api_key, model_LLM, debug_mode):
+        self.api_endpoint = api_endpoint
+        self.api_key = api_key
+        self.model_LLM = model_LLM
+        self.debug_mode = debug_mode
+
+    def get_response(self, prompt_content):
+        payload_explanation = json.dumps(
+            {
+                "model": self.model_LLM,
+                "messages": [
+                    # {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": prompt_content}
+                ],
+            }
+        )
+
+        headers = {
+            "Authorization": "Bearer " + self.api_key,
+            "User-Agent": "Apifox/1.0.0 (https://apifox.com)",
+            "Content-Type": "application/json",
+            "x-api2d-no-cache": 1,
+        }
+
+        while True:
+            try:
+                conn = http.client.HTTPSConnection(self.api_endpoint)
+                conn.request("POST", "/v1/chat/completions", payload_explanation, headers)
+                res = conn.getresponse()
+                data = res.read()
+                json_data = json.loads(data)
+                response = json_data["choices"][0]["message"]["content"]
+                break
+            except:
+                print("Error in API. Restarting the process...")
+                continue
+
+        return response
