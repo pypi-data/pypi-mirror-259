f965ceb6df09263532b7c860d58b0d870a592dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:03:53 +0000

    Add package simple-tool-kit-ui/0.0.2/simple-tool-kit-ui-0.0.2.tar.gz

---
+++ packages/simple-tool-kit-ui/simple-tool-kit-ui-0.0.2.tar.gz/simple-tool-kit-ui-0.0.2/src/tool_kit/summary_widget.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import QVBoxLayout, QPushButton
+
+from tool_kit.base_widgets import CustomGrouBox
+
+
+class SummaryWidget(CustomGrouBox):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        help_page_bt = QPushButton('Help Page')
+        layout.addWidget(help_page_bt)
