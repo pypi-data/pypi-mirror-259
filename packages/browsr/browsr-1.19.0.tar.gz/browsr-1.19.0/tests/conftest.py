d9fc8979720586bb8470f39850553c2f661e80f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:03:53 +0000

    Add package browsr/1.19.0/browsr-1.19.0.tar.gz

---
+++ packages/browsr/browsr-1.19.0.tar.gz/browsr-1.19.0/tests/conftest.py
@@ -0,0 +1,75 @@
+"""
+Pytest Fixtures Shared Across all Unit Tests
+"""
+
+import pathlib
+from typing import Any, Dict, List
+
+import pyperclip
+import pytest
+from click.testing import CliRunner
+from textual_universal_directorytree import GitHubPath
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    """
+    Return a CliRunner object
+    """
+    return CliRunner()
+
+
+@pytest.fixture
+def repo_dir() -> pathlib.Path:
+    """
+    Return the path to the repository root
+    """
+    return pathlib.Path(__file__).parent.parent.resolve()
+
+
+@pytest.fixture
+def screenshot_dir(repo_dir: pathlib.Path) -> pathlib.Path:
+    """
+    Return the path to the screenshot directory
+    """
+    return repo_dir / "tests" / "screenshots"
+
+
+@pytest.fixture
+def github_release_path() -> GitHubPath:
+    """
+    Return the path to the Github Release
+    """
+    release = "v1.6.0"
+    uri = f"github://juftin:browsr@{release}"
+    return GitHubPath(uri)
+
+
+@pytest.fixture(autouse=True)
+def copy_supported(monkeypatch: pytest.MonkeyPatch) -> None:
+    """
+    Override _copy_supported
+    """
+    monkeypatch.setattr(
+        pyperclip, "determine_clipboard", lambda: (lambda: True, lambda: True)
+    )
+
+
+@pytest.fixture(scope="module")
+def vcr_config() -> Dict[str, List[Any]]:
+    """
+    VCR Cassette Privacy Enforcer
+
+    This fixture ensures the API Credentials are obfuscated
+
+    Returns
+    -------
+    Dict[str, list]:
+    """
+    return {
+        "filter_headers": [("authorization", "XXXXXXXXXX")],
+        "filter_query_parameters": [("user", "XXXXXXXXXX"), ("token", "XXXXXXXXXX")],
+    }
+
+
+cassette = pytest.mark.vcr(scope="module")
---
+++ packages/browsr/browsr-1.19.0.tar.gz/browsr-1.19.0/tests/test_screenshots.py
@@ -0,0 +1,80 @@
+"""
+Screenshot Testing Using Cassettes!
+"""
+
+import pathlib
+from textwrap import dedent
+from typing import Callable, Tuple
+
+import pytest
+from textual_universal_directorytree import GitHubPath
+
+from tests.conftest import cassette
+
+
+@pytest.fixture
+def app_file() -> str:
+    file_content = """
+    from browsr.browsr import Browsr
+    from browsr.base import TextualAppContext
+
+    file_path = "{file_path}"
+    context = TextualAppContext(file_path=file_path)
+    app = Browsr(config_object=context)
+    """
+    return dedent(file_content).strip()
+
+
+@pytest.fixture
+def terminal_size() -> Tuple[int, int]:
+    return 160, 48
+
+
+@cassette
+def test_github_screenshot(
+    snap_compare: Callable[..., bool],
+    tmp_path: pathlib.Path,
+    app_file: str,
+    github_release_path: GitHubPath,
+    terminal_size: Tuple[int, int],
+) -> None:
+    """
+    Snapshot a release of this repo
+    """
+    app_path = tmp_path / "app.py"
+    app_path.write_text(app_file.format(file_path=str(github_release_path)))
+    assert snap_compare(app_path=app_path, terminal_size=terminal_size)
+
+
+@cassette
+def test_github_screenshot_license(
+    snap_compare: Callable[..., bool],
+    tmp_path: pathlib.Path,
+    app_file: str,
+    github_release_path: GitHubPath,
+    terminal_size: Tuple[int, int],
+) -> None:
+    """
+    Snapshot the LICENSE file
+    """
+    file_path = str(github_release_path / "LICENSE")
+    app_path = tmp_path / "app.py"
+    app_path.write_text(app_file.format(file_path=file_path))
+    assert snap_compare(app_path=app_path, terminal_size=terminal_size)
+
+
+@cassette
+def test_mkdocs_screenshot(
+    snap_compare: Callable[..., bool],
+    tmp_path: pathlib.Path,
+    app_file: str,
+    terminal_size: Tuple[int, int],
+    github_release_path: GitHubPath,
+) -> None:
+    """
+    Snapshot the pyproject.toml file
+    """
+    file_path = str(github_release_path / "mkdocs.yaml")
+    app_path = tmp_path / "app.py"
+    app_path.write_text(app_file.format(file_path=file_path))
+    assert snap_compare(app_path=app_path, terminal_size=terminal_size)
