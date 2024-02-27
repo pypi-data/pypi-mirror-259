42b8ad717b0cdcbb33305c1ec5ef79728131f392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:03:40 +0000

    Add package ingestr/0.1.0/ingestr-0.1.0.tar.gz

---
+++ packages/ingestr/ingestr-0.1.0.tar.gz/ingestr-0.1.0/ingestr/src/factory.py
@@ -0,0 +1,92 @@
+from typing import Protocol
+from urllib.parse import urlparse
+
+from ingestr.src.destinations import (
+    BigQueryDestination,
+    DatabricksDestination,
+    DuckDBDestination,
+    MsSQLDestination,
+    PostgresDestination,
+    RedshiftDestination,
+    SnowflakeDestination,
+)
+from ingestr.src.sources import SqlSource
+
+SQL_SOURCE_SCHEMES = [
+    "bigquery",
+    "duckdb",
+    "mssql",
+    "mysql",
+    "mysql+pymysql",
+    "postgres",
+    "postgresql",
+    "redshift",
+    "redshift+psycopg2",
+    "snowflake",
+    "sqlite",
+]
+
+
+class SourceProtocol(Protocol):
+    def dlt_source(self, uri: str, table: str, **kwargs):
+        pass
+
+
+class DestinationProtocol(Protocol):
+    def dlt_dest(self, uri: str, **kwargs):
+        pass
+
+    def dlt_run_params(self, uri: str, table: str, **kwargs) -> dict:
+        pass
+
+
+def parse_scheme_from_uri(uri: str) -> str:
+    parsed = urlparse(uri)
+    if parsed.scheme != "":
+        return parsed.scheme
+
+    uri_parts = uri.split("://")
+    if len(uri_parts) > 1:
+        return uri_parts[0]
+
+    raise ValueError(f"Could not parse scheme from uri: {uri}")
+
+
+class SourceDestinationFactory:
+    source_scheme: str
+    destination_scheme: str
+
+    def __init__(self, source_uri: str, destination_uri: str):
+        self.source_uri = source_uri
+        source_fields = urlparse(source_uri)
+        self.source_scheme = source_fields.scheme
+
+        self.destination_uri = destination_uri
+        self.destination_scheme = parse_scheme_from_uri(destination_uri)
+
+    def get_source(self) -> SourceProtocol:
+        if self.source_scheme in SQL_SOURCE_SCHEMES:
+            return SqlSource()
+        else:
+            raise ValueError(f"Unsupported source scheme: {self.source_scheme}")
+
+    def get_destination(self) -> DestinationProtocol:
+        match: dict[str, DestinationProtocol] = {
+            "bigquery": BigQueryDestination(),
+            "databricks": DatabricksDestination(),
+            "duckdb": DuckDBDestination(),
+            "mssql": MsSQLDestination(),
+            "postgres": PostgresDestination(),
+            "postgresql": PostgresDestination(),
+            "redshift": RedshiftDestination(),
+            "redshift+psycopg2": RedshiftDestination(),
+            "redshift+redshift_connector": RedshiftDestination(),
+            "snowflake": SnowflakeDestination(),
+        }
+
+        if self.destination_scheme in match:
+            return match[self.destination_scheme]
+        else:
+            raise ValueError(
+                f"Unsupported destination scheme: {self.destination_scheme}"
+            )
