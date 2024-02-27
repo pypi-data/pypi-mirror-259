3d45bf9290e78a515303b520536c0a6c84d6488f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:03:37 +0000

    Add package pyotp-cli/0.1.2/pyotp_cli-0.1.2.tar.gz

---
+++ packages/pyotp-cli/pyotp_cli-0.1.2.tar.gz/pyotp_cli-0.1.2/pyotp_cli/cli_hotp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Author : Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
+Date   : 2024-02-26
+Purpose: Get OTP Using HOTP
+"""
+
+import argparse
+from pathlib import Path
+
+
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Get OTP Using HOTP",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("secret", metavar="SECRET", help="Secret")
+    parser.add_argument("count", metavar="COUNT", type=int, help="Counter value")
+    parser.add_argument(
+        "-d",
+        "--digits",
+        metavar="DIGITS",
+        type=int,
+        default=6,
+        help="Number of digits",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    from pyotp_cli.utils import get_hotp
+
+    otp = get_hotp(args.secret, args.count, args.digits)
+    print(otp, end="")
+
+
+if __name__ == "__main__":
+    main()
