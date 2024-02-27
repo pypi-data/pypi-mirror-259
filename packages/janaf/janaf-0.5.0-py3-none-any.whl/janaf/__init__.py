5d2bd77db424f61e1e7ed8b27d0ce651966c5ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:04:04 +0000

    Add package janaf/0.5.0/janaf-0.5.0-py3-none-any.whl

---
+++ packages/janaf/janaf-0.5.0-py3-none-any.whl/janaf/__init__.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import polars as pl
+
+from .index import search
+from .table import Table
+
+__version__ = "0.5.0"
+
+
+def __getattr__(name):
+    from .index import db
+
+    if name == "db":
+        return db()
+    msg = f"module '{__name__}' has no attribute '{name}'"
+    raise AttributeError(msg)
+
+
+db: pl.DataFrame
