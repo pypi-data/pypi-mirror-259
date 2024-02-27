f4857c66cf5ec4a38ec026c24baea47347d8c23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:03:35 +0000

    Add package lyzr/0.1.25/lyzr-0.1.25.tar.gz

---
+++ packages/lyzr/lyzr-0.1.25.tar.gz/lyzr-0.1.25/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lyzr",
+    version="0.1.25",
+    author="lyzr",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8.1, <3.12",
+    install_requires=[
+        "asyncio",
+        "nest_asyncio",
+        "openai==1.3.4",
+        "litellm==1.2.0",
+        "llama-index==0.9.4",
+        "langchain==0.0.339",
+        "python-dotenv>=1.0.0",
+        "beautifulsoup4==4.12.2",
+        "pandas==2.0.2",
+        "matplotlib==3.8.2",
+        "weaviate-client==3.25.3",
+        "llmsherpa"
+    ],
+)
