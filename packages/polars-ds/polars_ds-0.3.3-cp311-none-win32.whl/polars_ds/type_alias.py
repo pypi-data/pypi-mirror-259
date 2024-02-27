9a47da03f414c0800a6e5d77e9710ee92f286cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:04:05 +0000

    Add package polars-ds/0.3.3/polars_ds-0.3.3-cp311-none-win32.whl

---
+++ packages/polars-ds/polars_ds-0.3.3-cp311-none-win32.whl/polars_ds/type_alias.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+from typing import Literal
+import sys
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:  # 3.9, 3.8
+    from typing_extensions import TypeAlias
+
+
+DetrendMethod: TypeAlias = Literal["linear", "mean"]
+Alternative: TypeAlias = Literal["two-sided", "less", "greater"]
+ROCAUCStrategy: TypeAlias = Literal["macro", "weighted"]
+Distance: TypeAlias = Literal["l1", "l2", "inf", "h", "cosine", "haversine"]
+ConvMode: TypeAlias = Literal["same", "left", "right", "full", "valid"]
