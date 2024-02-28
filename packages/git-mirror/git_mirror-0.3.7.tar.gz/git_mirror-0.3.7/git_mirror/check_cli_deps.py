20ac461f12a8567330969490bb2b9eb373419897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 06:03:43 +0000

    Add package git-mirror/0.3.7/git_mirror-0.3.7.tar.gz

---
+++ packages/git-mirror/git_mirror-0.3.7.tar.gz/git_mirror-0.3.7/git_mirror/check_cli_deps.py
@@ -0,0 +1,33 @@
+import cli_tool_audit as cta
+import cli_tool_audit.models as models
+
+from git_mirror.ui import console_with_theme
+
+console = console_with_theme()
+
+
+def check_tool_availability():
+    """
+    This function is used to check the availability of the 3rd party cli tools
+    """
+    git_version = models.CliToolConfig(
+        name="git",
+        version=">=1.7.*",
+        schema=models.SchemaType.SEMVER,
+    )
+    poetry_version = models.CliToolConfig(
+        name="poetry",
+        version="*",
+        schema=models.SchemaType.SEMVER,
+    )
+    to_check = {
+        "git": git_version,
+        "poetry": poetry_version,
+    }
+    results = cta.process_tools(to_check, no_cache=True, disable_progress_bar=True)
+    for result in results:
+        console.print(f"{result.tool}: available {result.is_available}, compatible {result.is_compatible}")
+
+
+if __name__ == "__main__":
+    check_tool_availability()
