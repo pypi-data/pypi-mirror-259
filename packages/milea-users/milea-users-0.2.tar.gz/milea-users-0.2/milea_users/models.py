1180182f53641490becd0dac3dbe08054411f66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:03:32 +0000

    Add package milea-users/0.2/milea-users-0.2.tar.gz

---
+++ packages/milea-users/milea-users-0.2.tar.gz/milea-users-0.2/milea_users/models.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group as BaseGroup
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Group(BaseGroup):
+    class Meta:
+        proxy = True
+        verbose_name = _("group")
+        verbose_name_plural = _("groups")
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name) if self.last_name else self.email
