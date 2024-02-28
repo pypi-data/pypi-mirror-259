475c09bf3e10833e169e063ff23a8b8bd40f6268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 14:04:14 +0000

    Add package bzutil/1.0.8/bzutil-1.0.8.tar.gz

---
+++ packages/bzutil/bzutil-1.0.8.tar.gz/bzutil-1.0.8/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   setup.py
+@Describe :  
+@Contact :   mrbingzhao@qq.com
+@License :   (C)Copyright 2023/11/7, Liugroup-NLPR-CASIA
+
+@Modify Time        @Author       @Version    @Desciption
+----------------   -----------   ---------    -----------
+2023/11/7 下午1:55   liubingzhao      1.0           ml
+'''
+from setuptools import setup, find_packages
+
+setup(
+    name='bzutil',
+    version='1.0.8',
+    author='mrbingzhao',
+    author_email='mrbingzhao@qq.com',
+    description='Integrates a number of commonly used Python functions, methods, algorithms, and other toolkits for easy reuse.',
+    packages=find_packages(exclude=["*.tests"]),
+    install_requires=[''],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Topic :: Software Development :: Libraries",
+    ],
+    python_requires=">=3.6",
+)
