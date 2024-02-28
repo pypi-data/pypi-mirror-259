29e04f4720da11cfdb0fa345a685ccd9a5398bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:03:44 +0000

    Add package odoo14-addon-ssi-partner/14.0.1.3.0/odoo14_addon_ssi_partner-14.0.1.3.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-partner/odoo14_addon_ssi_partner-14.0.1.3.0-py3-none-any.whl/odoo/addons/ssi_partner/models/res_partner.py
@@ -0,0 +1,14 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    ownership_type_id = fields.Many2one(
+        string="Ownership Type",
+        comodel_name="company_ownership_type",
+    )
