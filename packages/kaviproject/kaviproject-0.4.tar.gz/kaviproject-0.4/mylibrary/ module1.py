404fe3171b616cb49508944cee050d3d6e472712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:04:06 +0000

    Add package kaviproject/0.4/kaviproject-0.4.tar.gz

---
+++ packages/kaviproject/kaviproject-0.4.tar.gz/kaviproject-0.4/mylibrary/ module1.py
@@ -0,0 +1,15 @@
+def function1():
+    print("This is function 1")
+
+def leanth(val):
+    ''''
+    #leanth~
+    over all leanth get use the fuction mecure the leanth
+    '''
+    count=0
+    for i in val:
+        count=count+1
+    return count
+
+def function2():
+    print("This is function 2")
