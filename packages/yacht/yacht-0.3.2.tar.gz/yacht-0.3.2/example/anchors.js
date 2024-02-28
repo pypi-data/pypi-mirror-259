703dfe481b84ff4deb1bc65ed2a66e0c06e85f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:18 +0000

    Add package yacht/0.3.2/yacht-0.3.2.tar.gz

---
+++ packages/yacht/yacht-0.3.2.tar.gz/yacht-0.3.2/example/anchors.js
@@ -0,0 +1,59 @@
+
+class Anchor {
+    constructor(element, name, args, target){
+        this.element = element;
+        this.name = name;
+        this.args = args;
+        this.target = target;
+    }       
+    send() {
+        const xhr = new XMLHttpRequest();
+        console.log(`http://${window.location.host}/anchor/${this.name}`)
+        xhr.open("POST", `http://${window.location.host}/anchor/${this.name}`);
+        xhr.send(JSON.stringify(this.args));
+        xhr.responseType = "text";
+        xhr.onload = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                if(this.target) this.element[this.target] = xhr.responseText;
+            } else {
+                console.log(`Error: ${xhr.status}`);
+                console.log(`happended on anchor: ${this}`);
+            }
+        };
+    }
+}
+class Anchors {
+    anchors = [];
+    pollingTargets = [];
+    pollingDelay = 500;
+    pollingInterval = null;
+    bindAnchor(id, name, args, target, isPolling, trigger) {
+        const anchor = new Anchor(document.getElementById(id), name, args, target)
+        this.anchors.push(anchor);
+        if (isPolling) this.pollingTargets.push(anchor);
+        console.log("new anchortrigger", trigger);
+        if(trigger) {
+            const previousFunction = document.getElementById(id)[trigger];
+            document.getElementById(id)[trigger] = function() {
+                if(previousFunction) previousFunction();    
+                anchor.send();
+            }
+        }
+    }
+    request(harb) {
+        harbour.pollingTargets.forEach(anchor => {
+            anchor.send();            
+        });
+    }
+    startPolling() {
+        this.pollingInterval = setInterval(function() {harbour.request()}, this.pollingDelay);
+    }
+}
+
+var harbour = undefined;
+(function () {
+    harbour = new Anchors();
+    document.addEventListener("DOMContentLoaded", function(event) {
+        harbour.startPolling();
+    }); 
+}())
