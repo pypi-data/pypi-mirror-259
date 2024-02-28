4e7bb48c668e17adb8f8ca1ec94a874407fc8bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:03:59 +0000

    Add package aspectreat/1.0.6a1/aspectreat-1.0.6a1-py3-none-any.whl

---
+++ packages/aspectreat/aspectreat-1.0.6a1-py3-none-any.whl/aspectreat/main.py
@@ -0,0 +1,7 @@
+# -*- coding = utf-8 -*-
+from .aspectreat_pcs.et import np as nps
+from .aspectreat_pcs.et import aspects as aspects
+from .aspectreat_pcs.et import aspectPerson_s as aspect_person_s
+from .aspectreat_pcs.et import aspectPerson_nk as aspectPerson_nk
+from .aspectreat_pcs.et import aspects_kl as aspects_kl
+from .aspectreat_pcs.et import sln as sln
---
+++ packages/aspectreat/aspectreat-1.0.6a1-py3-none-any.whl/aspectreat/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.4.6 (trial), 000000, 2024-02-28T09:50:03.958204
+from .pyarmor_runtime import __pyarmor__
