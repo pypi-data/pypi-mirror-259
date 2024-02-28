d1415ecfaeb141eced8118d1b4c0a21f458f9f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 05:03:23 +0000

    Add package aisdb/1.7.0/aisdb-1.7.0-cp312-cp312-macosx_10_13_x86_64.whl

---
+++ packages/aisdb/aisdb-1.7.0-cp312-cp312-macosx_10_13_x86_64.whl/aisdb/tests/test_00_decode.py
@@ -0,0 +1,36 @@
+import os
+from datetime import datetime
+
+from aisdb.database.dbconn import DBConn
+from aisdb.database.decoder import decode_msgs
+
+
+def test_decode_1day(tmpdir):
+    dbpath = os.path.join(tmpdir, 'test_decode_1day.db')
+    print("\n ---> ", dbpath)
+    testingdata_nm4 = os.path.join(os.path.dirname(__file__), 'testdata',
+                                   'test_data_20211101.nm4')
+    testingdata_csv = os.path.join(os.path.dirname(__file__), 'testdata',
+                                   'test_data_20210701.csv')
+    testingdata_gz = os.path.join(os.path.dirname(__file__), 'testdata',
+                                  'test_data_20211101.nm4.gz')
+    testingdata_zip = os.path.join(os.path.dirname(__file__), 'testdata',
+                                   'test_data_20211101.nm4.zip')
+    with DBConn(dbpath) as dbconn:
+        filepaths = [
+            testingdata_nm4, testingdata_csv, testingdata_gz, testingdata_zip
+        ]
+        dt = datetime.now()
+        decode_msgs(filepaths=filepaths,
+                    dbconn=dbconn,
+                    source='TESTING',
+                    vacuum=True)
+        decode_msgs(filepaths=filepaths,
+                    dbconn=dbconn,
+                    source='TESTING',
+                    vacuum=dbpath + '.vacuum')
+        delta = datetime.now() - dt
+        print(
+            f'sqlite total parse and insert time: {delta.total_seconds():.2f}s'
+        )
+
