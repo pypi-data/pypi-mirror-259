16a7fb4642c06aa9f6967c1a3eeb38549b2175b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 14:04:55 +0000

    Add package roboflex/0.1.32/roboflex-0.1.32.tar.gz

---
+++ packages/roboflex/roboflex-0.1.32.tar.gz/roboflex-0.1.32/include/roboflex_core/core_nodes/core_nodes.h
@@ -0,0 +1,27 @@
+#ifndef ROBOFLEX_CORE_NODES__H
+#define ROBOFLEX_CORE_NODES__H
+
+// frp style: lambda support
+#include "roboflex_core/core_nodes/map_fun.h"
+#include "roboflex_core/core_nodes/callback_fun.h"
+#include "roboflex_core/core_nodes/filter_fun.h"
+#include "roboflex_core/core_nodes/take.h"
+#include "roboflex_core/core_nodes/null.h"
+
+// queuing
+#include "roboflex_core/core_nodes/last_one.h"
+
+// various utilities
+#include "roboflex_core/core_nodes/every_n.h"
+#include "roboflex_core/core_nodes/frequency_generator.h"
+#include "roboflex_core/core_nodes/message_printer.h"
+#include "roboflex_core/core_nodes/metrics.h"
+
+// fast message record and playback
+#include "roboflex_core/core_nodes/universal_data_saver.h"
+#include "roboflex_core/core_nodes/universal_data_player.h"
+
+// super useful - can perform profiling, graph re-writing, more.
+#include "roboflex_core/core_nodes/graph_root.h"
+
+#endif // ROBOFLEX_CORE_NODES__H
