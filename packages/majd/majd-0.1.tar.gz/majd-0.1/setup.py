75e155a7015f7d817b7c36d981df77454d9f4a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:08 +0000

    Add package majd/0.1/majd-0.1.tar.gz

---
+++ packages/majd/majd-0.1.tar.gz/majd-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup (
+    name = 'majd',
+    version = '0.1',
+    author = 'majd',
+    description = 'simple Python library for dragging files ',
+    packages = setuptools.find_packages(),
+    classifiers =[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
