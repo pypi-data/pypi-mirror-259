6cf53b4c3abceb68d52a34704bef824b4c65d27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:04:06 +0000

    Add package pygpt-net/2.1.0/pygpt_net-2.1.0-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.1.0-py3-none-any.whl/pygpt_net/provider/loaders/file_csv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.02.27 22:00:00                  #
+# ================================================== #
+
+from llama_index.core.readers.base import BaseReader
+
+from .hub.simple_csv.base import SimpleCSVReader
+from .base import BaseLoader
+
+
+class Loader(BaseLoader):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.id = "csv"
+        self.name = "CSV files"
+        self.extensions = ["csv"]
+        self.type = ["file"]
+
+    def get(self) -> BaseReader:
+        """
+        Get reader instance
+
+        :return: Data reader instance
+        """
+        return SimpleCSVReader()
