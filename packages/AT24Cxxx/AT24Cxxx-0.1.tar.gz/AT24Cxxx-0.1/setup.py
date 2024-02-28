a9f9195ae25a9646c0079c4b1dd72c5b6acff92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:20 +0000

    Add package AT24Cxxx/0.1/AT24Cxxx-0.1.tar.gz

---
+++ packages/AT24Cxxx/AT24Cxxx-0.1.tar.gz/AT24Cxxx-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AT24Cxxx',
+    version='0.1',
+    description='A simple Python library for comunication with AT24Cxxx I2C EEPROM IC. This library is intended to be used in RasPi PC.',
+    author='Frantisek Beranek',
+    packages=find_packages(),
+)
