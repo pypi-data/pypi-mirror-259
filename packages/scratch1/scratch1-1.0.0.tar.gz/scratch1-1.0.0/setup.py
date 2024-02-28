160547e003a68c02045683b81990b03433188e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:03:48 +0000

    Add package scratch1/1.0.0/scratch1-1.0.0.tar.gz

---
+++ packages/scratch1/scratch1-1.0.0.tar.gz/scratch1-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'scratch1',
+    version = '1.0.0',
+    py_modules = ['scratch'],
+    author = 'the anonymous',
+    author_email= 'hfpython@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'a simple printer of nested lists',
+)
