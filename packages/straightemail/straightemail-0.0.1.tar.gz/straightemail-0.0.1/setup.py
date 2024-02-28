162327a5ab1d4dca0c98699cf2a187e341e0e1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:13 +0000

    Add package straightemail/0.0.1/straightemail-0.0.1.tar.gz

---
+++ packages/straightemail/straightemail-0.0.1.tar.gz/straightemail-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='straightemail',
+    version='0.0.1',
+    author="Shreejeet Sahay",
+    packages=find_packages(),
+    install_requires=[
+    ]
+    )
