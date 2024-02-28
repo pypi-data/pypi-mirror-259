577c9acd5001745dacea5849fa3daba1d9a3b32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:04:09 +0000

    Add package tasksqwerty/0.1/tasksqwerty-0.1.tar.gz

---
+++ packages/tasksqwerty/tasksqwerty-0.1.tar.gz/tasksqwerty-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='tasksqwerty',
+    packages=['tasksqwerty'],
+    description='test todo list',
+    version='0.1',
+    url='http://github.com/perfectsmile/tasks',
+    author='jzeedyt',
+    author_email='jzeedyt@gmail.com',
+    keywords=['pip','todolist','example'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+    )
