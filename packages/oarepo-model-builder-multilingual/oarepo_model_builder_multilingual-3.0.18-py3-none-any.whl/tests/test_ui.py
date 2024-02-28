40da18ffe8dbc33ed23ff8e27c9d3d35bbaf0186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:03:32 +0000

    Add package oarepo-model-builder-multilingual/3.0.18/oarepo_model_builder_multilingual-3.0.18-py3-none-any.whl

---
+++ packages/oarepo-model-builder-multilingual/oarepo_model_builder_multilingual-3.0.18-py3-none-any.whl/tests/test_ui.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+from oarepo_model_builder.entrypoints import create_builder_from_entrypoints, load_model
+
+from tests.mock_filesystem import MockFilesystem
+
+
+def test_validity():
+    schema = load_model(
+        "test.yaml",
+        model_content={
+            "settings": {
+                "supported-langs": {
+                    "en": {
+                        "text": {"analyzer": "czech"},
+                        "sort": {"type": "icu_collation_keyword"},
+                    }
+                }
+            },
+            "record": {
+                "use": "invenio",
+                "module": {"qualified": "test"},
+                "properties": {"a": {"type": "multilingual", "ui": {}}},
+            },
+        },
+        isort=False,
+        black=False,
+        autoflake=False,
+    )
+
+    filesystem = MockFilesystem()
+    builder = create_builder_from_entrypoints(filesystem=filesystem)
+
+    builder.build(schema, "record", ["record"], "")
