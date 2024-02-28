be5029710a942eff6c157acaee94544d7f954ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:13 +0000

    Add package LBBNorm/6.6.2/LBBNorm-6.6.2.tar.gz

---
+++ packages/LBBNorm/LBBNorm-6.6.2.tar.gz/LBBNorm-6.6.2/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import sys
+
+is_windows = sys.platform.startswith('win')
+
+
+required_packages = ['numpy', 'tensorflow','opencv-python','spams-bin',
+                     'openslide-python','scikit-image','scipy','torch',
+                     'torchvision', 'dominate', 'visdom', 'pillow', 'visdom',
+                     'imageio', 'tqdm', 'lmdb', 'staintools', 'fitter'
+                     ]
+
+if is_windows:
+    required_packages.append('spams-bin') 
+else:
+    required_packages.append('spams') 
+setup(
+    
+    name='LBBNorm',
+    version='6.6.2',
+    packages=find_packages('src'),
+    author='Laboratory of Systems Biology and Bioinformatics (LBB)',
+    author_email='amasoudin@ut.ac.ir',
+    description='Empower Your Tomorrow, Conquer the Future!',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    package_dir={'': 'src'},
+    classifiers=[
+        'Development Status :: 4 - Beta', 
+        
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Information Technology',
+        
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+    ],
+
+    keywords='Healthcare Bio-Informatics',
+
+    install_requires= required_packages,
+
+)
