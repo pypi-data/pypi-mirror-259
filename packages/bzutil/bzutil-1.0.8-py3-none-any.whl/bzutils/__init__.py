e9a62625844922f04f5c28a46eed9ae6f7658029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 14:04:14 +0000

    Add package bzutil/1.0.8/bzutil-1.0.8-py3-none-any.whl

---
+++ packages/bzutil/bzutil-1.0.8-py3-none-any.whl/bzutils/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Describe :  
+@Contact :   mrbingzhao@qq.com
+@License :   (C)Copyright 2023/11/7, Liugroup-NLPR-CASIA
+
+@Modify Time        @Author       @Version    @Desciption
+----------------   -----------   ---------    -----------
+2023/11/7 下午5:07   liubingzhao      1.0           ml
+'''
