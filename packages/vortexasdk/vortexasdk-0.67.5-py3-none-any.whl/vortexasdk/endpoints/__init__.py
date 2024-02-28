3265495a3ad12c4316cffd393ab7aae33c95a8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:03:50 +0000

    Add package vortexasdk/0.67.5/vortexasdk-0.67.5-py3-none-any.whl

---
+++ packages/vortexasdk/vortexasdk-0.67.5-py3-none-any.whl/vortexasdk/endpoints/__init__.py
@@ -0,0 +1,49 @@
+"""Vortexa endpoints."""
+from vortexasdk.endpoints.asset_tanks import AssetTanks
+from vortexasdk.endpoints.attributes import Attributes
+from vortexasdk.endpoints.cargo_movements import CargoMovements
+from vortexasdk.endpoints.cargo_timeseries import CargoTimeSeries
+from vortexasdk.endpoints.corporations import Corporations
+from vortexasdk.endpoints.geographies import Geographies
+from vortexasdk.endpoints.products import Products
+from vortexasdk.endpoints.vessels import Vessels
+from vortexasdk.endpoints.storage_terminals import StorageTerminals
+from vortexasdk.endpoints.vessel_availability_breakdown import (
+    VesselAvailabilityBreakdown,
+)
+from vortexasdk.endpoints.vessel_availability_timeseries import (
+    VesselAvailabilityTimeseries,
+)
+from vortexasdk.endpoints.vessel_availability_search import (
+    VesselAvailabilitySearch,
+)
+
+from vortexasdk.endpoints.eia_forecasts import EIAForecasts
+from vortexasdk.endpoints.origin_breakdown import OriginBreakdown
+from vortexasdk.endpoints.destination_breakdown import DestinationBreakdown
+from vortexasdk.endpoints.movement_status_breakdown import (
+    MovementStatusBreakdown,
+)
+from vortexasdk.endpoints.vessel_class_breakdown import VesselClassBreakdown
+from vortexasdk.endpoints.product_breakdown import ProductBreakdown
+from vortexasdk.endpoints.parent_product_breakdown import (
+    ParentProductBreakdown,
+)
+from vortexasdk.endpoints.onshore_inventories_timeseries import (
+    OnshoreInventoriesTimeseries,
+)
+from vortexasdk.endpoints.onshore_inventories_search import (
+    OnshoreInventoriesSearch,
+)
+from vortexasdk.endpoints.freight_pricing_search import FreightPricingSearch
+from vortexasdk.endpoints.freight_pricing_timeseries import (
+    FreightPricingTimeseries,
+)
+from vortexasdk.endpoints.voyages_timeseries import VoyagesTimeseries
+from vortexasdk.endpoints.voyages_congestion_breakdown import (
+    VoyagesCongestionBreakdown,
+)
+from vortexasdk.endpoints.voyages_top_hits import VoyagesTopHits
+from vortexasdk.endpoints.voyages_search_enriched import VoyagesSearchEnriched
+from vortexasdk.endpoints.fixtures import Fixtures
+from vortexasdk.endpoints.vessel_summary import VesselSummary
