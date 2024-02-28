d9be0244a60f93efa9b6b8cead6d87619a0b4aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:04:13 +0000

    Add package eburger/1.0.3/eburger-1.0.3-py3-none-any.whl

---
+++ packages/eburger/eburger-1.0.3-py3-none-any.whl/eburger/settings.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+
+project_root = Path.cwd()
+outputs_dir = Path.cwd() / ".eburger"
+excluded_dirs = ["test", "script"]
+excluded_contracts = [
+    "@openzeppelin",
+    "openzeppelin-contracts",
+    "@uniswap",
+    "@gnosis.pm",
+    "pendle",
+    "solmate",
+    "solady",
+    "forge-std",
+    "ds-test",
+]
+
+templates_directories = [Path(os.path.dirname(os.path.abspath(__file__)), "templates")]
+if __package__ is None or __package__ == "":
+    templates_directories = [Path.cwd() / "eburger" / "templates"]
