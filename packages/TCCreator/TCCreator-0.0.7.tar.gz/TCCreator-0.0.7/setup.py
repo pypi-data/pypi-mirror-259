e3938df8fa47a8391a57cd4b1a03fd2048fb91cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:04:02 +0000

    Add package TCCreator/0.0.7/TCCreator-0.0.7.tar.gz

---
+++ packages/TCCreator/TCCreator-0.0.7.tar.gz/TCCreator-0.0.7/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  8 14:55:19 2023
+
+@author: QianYang
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="TCCreator",
+    version="0.0.7",
+    packages=find_packages(),
+    DESCRIPTION = 'Creat TC with anotated CRF and metadata file',
+)
