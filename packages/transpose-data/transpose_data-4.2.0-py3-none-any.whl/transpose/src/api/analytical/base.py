734ee886140a864ea26a9c28740780eff3690a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:04:15 +0000

    Add package transpose-data/4.2.0/transpose_data-4.2.0-py3-none-any.whl

---
+++ packages/transpose-data/transpose_data-4.2.0-py3-none-any.whl/transpose/src/api/analytical/base.py
@@ -0,0 +1,31 @@
+from ....src.util.client import post_api_request
+
+
+class Analytical():
+    def __init__(self, base_class) -> None:
+        self.super = base_class
+
+    # Performs the given SQL query
+    # https://docs.transpose.io/sql/parameters/
+    def query(
+        self,
+        sql_query: str,
+        parameters: dict = None,
+        return_df: bool = False
+    ) -> dict:
+
+        parameters = {} if parameters is None else parameters
+
+        url = "https://api.transpose.io/sql/analytical"
+        body = {
+            'sql': sql_query,
+            'parameters': parameters
+        }
+
+        return post_api_request(
+            url=url,
+            api_key=self.super.api_key,
+            body=body,
+            return_df=return_df,
+            verbose=self.super.verbose
+        )
