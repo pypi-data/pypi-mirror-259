423508426fc354add53cb3f0e8c4dc8c25a36ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 05:03:35 +0000

    Add package cat-data-tools/0.7.1/cat_data_tools-0.7.1-py3-none-any.whl

---
+++ packages/cat-data-tools/cat_data_tools-0.7.1-py3-none-any.whl/cat_data_tools/__init__.py
@@ -0,0 +1,8 @@
+"""A template Python module"""
+
+__version__ = "0.7.1"
+from .adapters import *  # noqa
+from .captures_list import *  # noqa
+from .cli import *  # noqa
+from .filter_data_between_years import *  # noqa
+from .filter_data_by_month import *  # noqa
