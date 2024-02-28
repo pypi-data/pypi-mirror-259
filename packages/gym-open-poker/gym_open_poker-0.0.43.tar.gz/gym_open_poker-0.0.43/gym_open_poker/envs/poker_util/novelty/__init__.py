cb7c1ff752ee96a0d1baf13565c25602beedd10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:04:16 +0000

    Add package gym-open-poker/0.0.43/gym_open_poker-0.0.43.tar.gz

---
+++ packages/gym-open-poker/gym_open_poker-0.0.43.tar.gz/gym_open_poker-0.0.43/gym_open_poker/envs/poker_util/novelty/__init__.py
@@ -0,0 +1,40 @@
+from .action1 import Action1
+from .card_dist_high import CardDistHigh
+from .card_dist_low import CardDistLow
+from .card1 import Card1
+from .environment1 import Environment1
+from .environment2 import Environment2
+from .agent1 import Agent1
+from .agent2 import Agent2
+from .agent3 import Agent3
+from .event1 import Event1
+
+# from .rule1 import Rule1
+from .rule2 import Rule2
+from .rule3 import Rule3
+from .event2 import Event2
+from .rule4 import Rule4
+from .card2 import Card2
+from .card3 import Card3
+
+NOVELTY_LIST = [cls for cls in locals().values() if isinstance(cls, type)]
+# NOVELTY_LIST.append("RANDOM")
+
+__all__ = [
+    "Action1",
+    "CardDistHigh",
+    "CardDistLow",
+    "Card1",
+    "Environment1",
+    "Environment2",
+    "Agent1",
+    "Agent2",
+    "Agent3",
+    "Event1",
+    "Rule2",
+    "Rule3",
+    "Event2",
+    "Rule4",
+    "Card2",
+    "Card3",
+]
---
+++ packages/gym-open-poker/gym_open_poker-0.0.43.tar.gz/gym_open_poker-0.0.43/gym_open_poker/envs/poker_util/novelty_generator.py
@@ -0,0 +1,64 @@
+from gym_open_poker.envs.poker_util.novelty import NOVELTY_LIST
+import random
+import logging
+
+logger = logging.getLogger("gym_open_poker.envs.poker_util.logging_info.novelty_generator")
+
+
+class NoveltyGenerator:
+    def __init__(self):
+        self._novelties = [novelty.__name__ for novelty in NOVELTY_LIST]
+        self._novelties.append("RANDOM")
+
+    def get_support_novelties(self):
+        return self._novelties
+
+    def inject(self, env, novelty_list: list[str]):
+        """
+        Args:
+            env(gym.Env): the original environment object create by gym.make
+            novelty_list(list[str]): a novlety_list assigned by user in config file
+        Return:
+            novel_env(gym.Env): the environment object with novelty injected
+        """
+        novelty_module = __import__("novelty")
+
+        if "RANDOM" in novelty_list:
+            # we randomly inject one of the novelty in NOVELTY_LIST
+            novelty_string = random.choice([novelty_string for novelty_string in self._novelties])
+            novelty = getattr(novelty_module, novelty_string, None)
+            self._logging(novelty)
+            novel_env = novelty(env)
+
+        else:
+            # check if all novelies in novelty_list in self._novelties
+            for novelty_string in novelty_list:
+                if novelty_string not in self._novelties:
+                    raise ValueError(
+                        f"{novelty_string} is not supported, please use NoveltyGenerator.get_support_novelties() to \
+                            check supported novelties."
+                    )
+
+            # inject novelties
+            novelty_index = 1
+            for novelty_string in novelty_list:
+                novelty = getattr(novelty_module, novelty_string, None)
+                self._logging(novelty, novelty_index)
+                novel_env = novelty(env)
+                novelty_index += 1
+
+        return novel_env
+
+    def _logging(self, novelty, novelty_index=None):
+        """
+        Args:
+            novelty(gym.Wrapper): An wrapper object with doc string
+            novelty_index(int): an novelty index to record in log
+        """
+        logger.debug("-------------------")
+        if novelty_index is not None:
+            logger.debug(f"{str(novelty_index)} NOVELTY INJECTING:")
+        else:
+            logger.debug("NOVELTY INJECTING:")
+        logger.debug(novelty.__doc__)
+        logger.debug("-------------------")
