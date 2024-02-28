2040334868330bb686b5672b4d47ce123c6b3553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:04:23 +0000

    Add package y-tal/0.13.3/y-tal-0.13.3.tar.gz

---
+++ packages/y-tal/y-tal-0.13.3.tar.gz/y-tal-0.13.3/tal/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from . import io, plot, reconstruct
+from . import enums, callbacks
+from tal.config import set_resources, ResourcesConfig as resources
+
+__version__ = '0.13.3'
