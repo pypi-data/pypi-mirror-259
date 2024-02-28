d4e1ec4cefac713d6ee785c7996d630e78ceb438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:04 +0000

    Add package NexR-qc/0.0.2/NexR_qc-0.0.2.tar.gz

---
+++ packages/NexR-qc/NexR_qc-0.0.2.tar.gz/NexR_qc-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="NexR_qc",
+    version="0.0.2",
+    description="PYPI tutorial package creation written by NexR-qc",
+    author="mata.lee",
+    author_email="ldh3810@gmail.com",
+    url="https://github.com/mata-1223/Quality-Check-Module",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "openpyxl",
+    ],
+    packages=find_packages(exclude=[]),
+    keywords=["qc", "NexR", "mata.lee", "QualityCheck", "python", "python tutorial", "pypi"],
+    python_requires=">=3.6",
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
