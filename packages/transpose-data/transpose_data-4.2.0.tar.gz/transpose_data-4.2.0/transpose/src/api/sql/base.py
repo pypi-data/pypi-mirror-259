627c33fb26b2ca797bf736a2686cf797bdc58d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:04:15 +0000

    Add package transpose-data/4.2.0/transpose_data-4.2.0.tar.gz

---
+++ packages/transpose-data/transpose_data-4.2.0.tar.gz/transpose_data-4.2.0/transpose/src/api/sql/base.py
@@ -0,0 +1,39 @@
+from ....src.util.client import get_api_request, post_api_request
+
+
+class SQL():
+    def __init__(self, base_class) -> None:
+        self.super = base_class
+
+    # Performs the given SQL query
+    # https://docs.transpose.io/sql/parameters/
+    def query(
+        self,
+        sql_query: str,
+        parameters: dict = {},
+        return_df: bool = False
+    ) -> dict:
+
+        url = "https://api.transpose.io/sql"
+        body = {
+            'sql': sql_query,
+            'parameters': parameters
+        }
+
+        return post_api_request(
+            url=url,
+            api_key=self.super.api_key,
+            body=body,
+            return_df=return_df,
+            verbose=self.super.verbose
+        )
+
+    # Gets the schema from the Transpose API
+    def schema(self) -> dict:
+
+        url = "https://api.transpose.io/get-schema"
+        return get_api_request(
+            url=url,
+            api_key=self.super.api_key,
+            verbose=self.super.verbose
+        )
