f1e43fd410bc38af694ab4aa2396a21cc5b2e0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:16 +0000

    Add package hqq/0.1.4/hqq-0.1.4.tar.gz

---
+++ packages/hqq/hqq-0.1.4.tar.gz/hqq-0.1.4/hqq/engine/hf.py
@@ -0,0 +1,64 @@
+import transformers, json, torch
+from typing import Dict
+
+_HQQ_REGISTRY = {}
+
+from ..models.hf.llama import LlamaHQQ
+_HQQ_REGISTRY['LlamaForCausalLM'] = LlamaHQQ
+
+from hqq.models.hf.mixtral import MixtralHQQ
+_HQQ_REGISTRY['MixtralForCausalLM'] = MixtralHQQ
+
+from hqq.models.hf.phi import PhiHQQ
+_HQQ_REGISTRY['PhiForCausalLM'] = PhiHQQ
+
+from hqq.models.hf.mistral import MistralHQQ
+_HQQ_REGISTRY['MistralForCausalLM'] = MistralHQQ
+
+#Alias 
+AutoTokenizer = transformers.AutoTokenizer
+
+#Used to call super() on classmethods
+_Parent = transformers.AutoModelForCausalLM
+
+from .base import HQQWrapper
+
+class HQQModelForCausalLM(_Parent, HQQWrapper):
+	_HQQ_REGISTRY = _HQQ_REGISTRY
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		cls._make_quantizable(model, quantized=False)
+
+	@classmethod
+	def _make_quantizable(cls, model, quantized):
+		model.hqq_quantized  = quantized
+		model.arch_key       = model.config.architectures[0] 
+		model.quantize_model = lambda quant_config, compute_dtype=torch.float16: cls.quantize_model_(model=model, quant_config=quant_config, compute_dtype=compute_dtype)
+		model.save_quantized = lambda save_dir: cls.save_quantized_(model=model, save_dir=save_dir)
+		model.cuda           = lambda *args, **kwargs: model if(quantized) else model.cuda
+		model.to             = lambda *args, **kwargs: model if(quantized) else model.to
+		model.float          = lambda *args, **kwargs: model if(quantized) else model.float
+		model.half           = lambda *args, **kwargs: model if(quantized) else model.half
+		model.base_class     = cls._get_hqq_class(model)
+
+	#Force loading the model on CPU and unquantized 
+	@classmethod
+	def _validate_params(cls, params:Dict):
+		for p in ['load_in_4bit', 'load_in_8bit']: #ignore these
+			if(p in params):
+				params[p] = False 
+		params['device_map'] = None
+
+	@classmethod
+	def from_pretrained(cls, *args, **kwargs):
+		cls._validate_params(kwargs)
+		model = super(_Parent, cls).from_pretrained(*args, **kwargs)
+		cls._make_quantizable(model, quantized=False)
+		return model
+
+	@classmethod
+	def _get_arch_key_from_save_dir(cls, save_dir:str):
+		config = transformers.AutoConfig.from_pretrained(save_dir)
+		return config.architectures[0]
+
---
+++ packages/hqq/hqq-0.1.4.tar.gz/hqq-0.1.4/hqq/models/base.py
@@ -0,0 +1,216 @@
+#Written by Dr. Hicham Badri @Mobius Labs GmbH - 2023
+#####################################################
+import torch 
+import gc, os, copy
+from tqdm import tqdm 
+from abc import abstractmethod
+
+from huggingface_hub import snapshot_download
+from ..core.quantize import HQQLinear
+
+#Cleanup GPU vram
+def cleanup():
+	torch.cuda.empty_cache()
+	gc.collect()
+
+#Make sure file paths end with '/'
+def fix_path(path):
+	if(len(path)==0): return path
+	return path + '/' if (path[-1]!='/') else path 
+
+#Base patching class. Patching defines how nn.Linear and other layers are replaced via a patching function. 
+class BasePatch():
+	#Override these OR override the main patch_model() function
+	############################################
+	#This method iterates through layers of the model that are NOT nn.Linear and processes them via new_nodule = patch_fct(module, params)
+	@classmethod
+	def patch_nonlinearlayers(cls, model, patch_fct, verbose=True):
+		pass
+
+	#This method iterates through layers of the model that are nn.Linear and processes them via new_nodule = patch_fct(module, params)
+	@classmethod
+	def patch_linearlayers(cls, base_model, patch_fct, patch_params, verbose=True):
+		pass 
+	############################################
+	#These tags are used to specfiy parameters of the patching in  patch_linearlayers()
+	@classmethod
+	def get_linear_tags(cls):
+		return []
+	
+	#Autmatically name modules. This is very important to save/load the weights 
+	@classmethod
+	def autoname_modules(cls, model):
+		for name, module in model.named_modules():
+			module.name = name
+
+	#Freeze all layers
+	@classmethod
+	def freeze_model(cls, model):
+		for param in model.parameters():
+			param.requires_grad = False
+		try:
+			for param in model.model.parameters():
+				param.requires_grad = False
+		except:
+			pass
+
+	#Main patching function
+	@classmethod
+	def patch_model(cls, model, patch_nonlinear_fct, patch_linear_fct, patch_params, verbose=True):
+		model.eval()
+		cls.freeze_model(model)
+		cls.patch_nonlinearlayers(model, patch_nonlinear_fct, verbose=verbose)
+		cls.patch_linearlayers(model, patch_linear_fct, patch_params, verbose=verbose)
+		cls.autoname_modules(model)
+		cleanup()
+
+
+class BaseHQQModel:
+	#Override these
+	############################################
+	#This method creates and empty model based on the specfied architecture
+	@abstractmethod
+	def create_model(self):
+		pass
+
+	#This method saves the model architecture only without inculding the weights (for example to a config.json)
+	@abstractmethod	
+	def cache_model(cls, model, save_dir):
+		pass
+	############################################
+
+	@classmethod
+	def get_config_file(cls, save_dir):
+		return fix_path(save_dir) + 'config.json'
+
+	@classmethod
+	def get_weight_file(cls, save_dir):
+		return fix_path(save_dir) + 'qmodel.pt'    
+
+	@classmethod
+	def get_ignore_layers(cls, model):
+		return []
+
+	#Save weights to disk
+	@classmethod
+	def save_weights(cls, weights, save_dir):
+		torch.save(weights, cls.get_weight_file(save_dir))
+
+	#Load weights from disk
+	@classmethod
+	def load_weights(cls, save_dir, map_location=None):
+		return torch.load(cls.get_weight_file(save_dir), map_location=map_location)
+
+	#Main function to quantize a model. Basically goes through the linear layers specfied in the patching function and replaces them with HQQLinear
+	@classmethod
+	def quantize_model(cls, model, quant_config, compute_dtype=torch.float16):
+		#Use the same quantization config for all linear layers. Use None to skip quantizing a specfic layer.
+		if(True in [(key in cls.get_linear_tags()) for key in quant_config.keys()]): 
+			#If the user doesn't specify a key from get_linear_tags, the layer is not quantized via (key, None)
+			patch_params = dict([(key, None) for key in cls.get_linear_tags()])
+			patch_params.update(quant_config)
+		else:
+			#Same quant_config for all layers
+			patch_params = dict([(k, quant_config) for k in cls.get_linear_tags()])
+
+		#We replace the nn.Linear layers with HQQLinear
+		def _patch_linear(linear_layer, quant_config):
+			return HQQLinear(linear_layer, quant_config, compute_dtype=compute_dtype) if (quant_config is not None) else linear_layer.to(compute_dtype).cuda()
+
+		cls.patch_model(model, lambda l: l.to(compute_dtype).cuda(), _patch_linear, patch_params)
+
+	#Prepares model weights by iterating through modules. It might some parameters that are NOT modules like model.param1
+	@classmethod
+	def serialize_weights(cls, model, verbose):
+		weights     = {}
+		ignore_keys = cls.get_ignore_layers(model)
+		for name, module in model.named_modules():
+			if(name in ignore_keys): continue
+			try:
+				state_dict = module.state_dict()
+				if(len(state_dict)>0): 
+					weights[name] = dict(state_dict)
+			except Exception as error:
+				if(verbose): 
+					print('Skipping', name)
+
+		return weights
+
+	#Main function to save a quantized model
+	@classmethod
+	def save_quantized(cls, model, save_dir, verbose=False):
+		#Save config
+		cls.cache_model(model, save_dir)
+
+		#Serialization
+		weights = cls.serialize_weights(model, verbose=verbose)
+
+		#Save
+		cls.save_weights(weights, save_dir)
+
+	@classmethod
+	def try_snapshot_download(cls, save_dir_or_hub, cache_dir=''):
+		save_dir = fix_path(cache_dir) + save_dir_or_hub
+
+		if(os.path.exists(save_dir)==False):
+			save_dir = snapshot_download(repo_id=save_dir_or_hub, cache_dir=cache_dir)
+			save_dir = fix_path(save_dir)
+
+		#Check 
+		if(os.path.exists(cls.get_weight_file(save_dir))==False):
+			raise Exception('Weight file missing. Check your cache directory.')
+		if(os.path.exists(cls.get_config_file(save_dir))==False):
+			raise Exception('Config file missing. Check your cache directory.')
+
+		return save_dir
+
+
+	#This method is specfically designed in case we need to load some weights that are not part of any module
+	@classmethod
+	def post_module_load(cls, model, weights):
+		pass
+
+	#Main function to load an HQQ quantized model from either HF hub or locally
+	@classmethod
+	def from_quantized(cls, save_dir_or_hub, compute_dtype=torch.float16, cache_dir=''):
+		#Get directory path
+		save_dir = cls.try_snapshot_download(save_dir_or_hub, cache_dir)
+
+		#Load model from config
+		model = cls.create_model(save_dir)
+
+		#Name the layers
+		cls.autoname_modules(model) 
+
+		#Load weights
+		try:
+			weights = cls.load_weights(save_dir)
+		except Exception as error:
+			print("Failed to load the weights", error)
+			return
+		
+		#load_state_dict() doesn't work with modules initialized with init_empty_weights(), so we need to do this manually
+		@torch.no_grad()
+		def _load_module(module, params=None):
+			if(module.name not in weights): 
+				return module.to(compute_dtype).cuda()
+
+			state_dict = weights[module.name]
+			if(('W_q' in state_dict) and ('meta' in state_dict)):
+				module = HQQLinear(linear_layer=None, quant_config=None, compute_dtype=compute_dtype)
+				module.load_state_dict(state_dict)
+			else:
+				for key in state_dict:
+					setattr(module, key, torch.nn.Parameter(state_dict[key], requires_grad=False))
+
+			return module 
+
+		#Load modules
+		cls.patch_model(model, _load_module, _load_module, dict([(k, None) for k in cls.get_linear_tags()]))
+		#Load other weights that are not part of any module
+		cls.post_module_load(model, weights) 
+		
+		return model
+
+
+
