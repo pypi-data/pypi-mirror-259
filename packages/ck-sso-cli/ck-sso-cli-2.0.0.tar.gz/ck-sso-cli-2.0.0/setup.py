e518e9a686ae115a5c70d42b5c4b7f053a949895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:03:54 +0000

    Add package ck-sso-cli/2.0.0/ck-sso-cli-2.0.0.tar.gz

---
+++ packages/ck-sso-cli/ck-sso-cli-2.0.0.tar.gz/ck-sso-cli-2.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+exec(open(os.path.join(here, 'ckssocli/version.py')).read())
+
+
+setup(
+    name='ck-sso-cli',
+    version=__version__,
+    description='Provides a wrapper for AWS IAM Identity Center authentication to an External AWS Account',
+    packages=find_packages(),
+    license='Apache License 2.0',
+    author='Aditya Ajay',
+    author_email='aditya.ajay@tothenew.com',
+    url='https://www.cloudkeeper.ai/',
+    entry_points={
+        'console_scripts': [
+            'ck-sso-cli=ckssocli.ck_sso_cli:main',
+        ],
+    },
+    install_requires=[
+        'boto3'
+        ]
+)
+
+# export PATH="/home/aditya/.local/bin:$PATH"
