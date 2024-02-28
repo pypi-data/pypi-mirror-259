007871864ca91b49519076e34bd94787468135a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 05:04:31 +0000

    Add package Oasys.THIS/21.0.0b8/Oasys.THIS-21.0.0b8.tar.gz

---
+++ packages/Oasys.THIS/Oasys.THIS-21.0.0b8.tar.gz/Oasys.THIS-21.0.0b8/src/Oasys/THIS/unitsystem.py
@@ -0,0 +1,31 @@
+import Oasys.gRPC
+
+
+# Metaclass for static properties and constants
+class UnitSystemType(type):
+    _consts = {'U1', 'U2', 'U3', 'U4', 'U5', 'U6'}
+
+    def __getattr__(cls, name):
+        if name in UnitSystemType._consts:
+            return Oasys.THIS._connection.classGetter(cls.__name__, name)
+
+        raise AttributeError
+
+
+class UnitSystem(Oasys.gRPC.OasysItem, metaclass=UnitSystemType):
+
+
+    def __del__(self):
+        if not Oasys.THIS._connection:
+            return
+
+        Oasys.THIS._connection.destructor(self.__class__.__name__, self._handle)
+
+
+    def __getattr__(self, name):
+        raise AttributeError
+
+
+    def __setattr__(self, name, value):
+# Set the property locally
+        self.__dict__[name] = value
