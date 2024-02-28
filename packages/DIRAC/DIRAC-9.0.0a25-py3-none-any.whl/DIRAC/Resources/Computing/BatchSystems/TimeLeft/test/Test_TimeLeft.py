b0cec3dbdd1aa22668ab7f2a45b8991492735371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:03:40 +0000

    Add package DIRAC/9.0.0a25/DIRAC-9.0.0a25-py3-none-any.whl

---
+++ packages/DIRAC/DIRAC-9.0.0a25-py3-none-any.whl/DIRAC/Resources/Computing/BatchSystems/TimeLeft/test/Test_TimeLeft.py
@@ -0,0 +1,68 @@
+""" Test TimeLeft utility
+"""
+from DIRAC import S_OK
+from DIRAC.Resources.Computing.BatchSystems.TimeLeft.TimeLeft import TimeLeft
+
+
+def test_cpuPowerNotDefined(mocker):
+    """Test cpuPower not defined"""
+    mocker.patch("DIRAC.gConfig.getSections", return_value={"Type": "SLURM", "JobID": "12345", "Parameters": {}})
+
+    tl = TimeLeft()
+    res = tl.getTimeLeft()
+    assert not res["OK"]
+    assert "/LocalSite/CPUNormalizationFactor not defined" in res["Message"]
+
+
+def test_batchSystemNotDefined(mocker):
+    """Test batch system not defined"""
+    mocker.patch("DIRAC.gConfig.getSections", return_value={})
+
+    tl = TimeLeft()
+    tl.cpuPower = 10
+    res = tl.getTimeLeft()
+    assert not res["OK"]
+    assert "Current batch system is not supported" in res["Message"]
+
+
+def test_getScaledCPU(mocker):
+    """Test getScaledCPU()"""
+    mocker.patch(
+        "DIRAC.Resources.Computing.BatchSystems.TimeLeft.SLURMResourceUsage.runCommand",
+        return_value=S_OK("19283,9000,10,900,30:00"),
+    )
+    mocker.patch("DIRAC.gConfig.getSections", return_value={"Type": "SLURM", "JobID": "12345", "Parameters": {}})
+
+    tl = TimeLeft()
+
+    # Test 1: no normalization
+    res = tl.getScaledCPU()
+    assert res == 0
+
+    # Test 2: normalization
+    tl.cpuPower = 5.0
+    res = tl.getScaledCPU()
+    assert res == 45000
+
+
+def test_getTimeLeft(mocker):
+    """Test getTimeLeft()"""
+    mocker.patch(
+        "DIRAC.Resources.Computing.BatchSystems.TimeLeft.SLURMResourceUsage.runCommand",
+        return_value=S_OK("19283,9000,10,900,30:00"),
+    )
+    mocker.patch("DIRAC.gConfig.getSections", return_value={"Type": "SLURM", "JobID": "12345", "Parameters": {}})
+
+    tl = TimeLeft()
+
+    # Test 1: CPU power = 10
+    tl.cpuPower = 10.0
+    res = tl.getTimeLeft()
+    assert res["OK"]
+    assert res["Value"] == 9000
+
+    # Test 2: CPU power = 15
+    tl.cpuPower = 15.0
+    res = tl.getTimeLeft()
+    assert res["OK"]
+    assert res["Value"] == 13500
