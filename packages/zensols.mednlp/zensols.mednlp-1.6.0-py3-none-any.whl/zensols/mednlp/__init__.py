e731e6780646a404185840e9f12349c96b51b95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 14:04:40 +0000

    Add package zensols.mednlp/1.6.0/zensols.mednlp-1.6.0-py3-none-any.whl

---
+++ packages/zensols.mednlp/zensols.mednlp-1.6.0-py3-none-any.whl/zensols/mednlp/__init__.py
@@ -0,0 +1,8 @@
+from .domain import *
+from .uts import UTSError, NoResultsError, AuthenticationError, UTSClient
+from .resource import *
+from .tok import *
+from .lib import *
+from .parser import *
+from .app import *
+from .cli import *
