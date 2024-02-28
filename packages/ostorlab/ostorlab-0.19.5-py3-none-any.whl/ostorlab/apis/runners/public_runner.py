61e851639c48936ba617616527ed54903d8faa11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:03:57 +0000

    Add package ostorlab/0.19.5/ostorlab-0.19.5-py3-none-any.whl

---
+++ packages/ostorlab/ostorlab-0.19.5-py3-none-any.whl/ostorlab/apis/runners/public_runner.py
@@ -0,0 +1,65 @@
+"""Handles the public API calls.
+
+    Typical usage example:
+    public_runner = PublicAPIRunner()
+    public_runner.execute()
+"""
+
+from typing import Dict, Any, Optional
+
+import requests
+
+from ostorlab.apis import request as api_request
+from ostorlab.apis.runners import runner
+
+PUBLIC_GRAPHQL_ENDPOINT = "https://api.ostorlab.co/apis/public_graphql"
+
+
+class PublicAPIRunner(runner.APIRunner):
+    """Responsible for the public API calls, and preparing the responses."""
+
+    @property
+    def endpoint(self) -> str:
+        """API endpoint."""
+        return PUBLIC_GRAPHQL_ENDPOINT
+
+    def execute(self, request: api_request.APIRequest) -> Dict[str, Any]:
+        """Executes a request using the Public GraphQL API.
+
+        Args:
+            request: The request to be executed
+
+        Raises:
+            ResponseError: When the API returns an error
+
+        Returns:
+            The API response
+        """
+        response = self._sent_request(request)
+        if response.status_code != 200:
+            raise runner.ResponseError(
+                f'Response status code is {response.status_code}: {response.content.decode(errors="ignore")}'
+            )
+        data: Dict[str, Any] = response.json()
+        errors = data.get("errors")
+        if errors is not None and isinstance(errors, list):
+            error = errors[0].get("message")
+            raise runner.ResponseError(f"Response errors: {error}")
+        else:
+            return data
+
+    def _sent_request(
+        self, request: api_request.APIRequest, headers: Optional[Dict[str, str]] = None
+    ) -> requests.Response:
+        """Sends an API request."""
+        if self._proxy is not None:
+            proxy = {"https": self._proxy}
+        else:
+            proxy = None
+        return requests.post(
+            self.endpoint,
+            data=request.data,
+            proxies=proxy,
+            verify=self._verify,
+            timeout=runner.REQUEST_TIMEOUT,
+        )
