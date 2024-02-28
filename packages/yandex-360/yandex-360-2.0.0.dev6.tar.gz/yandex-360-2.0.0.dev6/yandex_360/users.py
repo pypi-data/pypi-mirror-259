2ce39eab7790835efba7922f9005b5f3125b0a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:02:54 +0000

    Add package yandex-360/2.0.0.dev6/yandex-360-2.0.0.dev6.tar.gz

---
+++ packages/yandex-360/yandex-360-2.0.0.dev6.tar.gz/yandex-360-2.0.0.dev6/yandex_360/users.py
@@ -0,0 +1,32 @@
+"""Модуль функций для работы с сотрудниками. Просмотр, создание, изменение и удаление.
+
+.. note::
+    **Права доступа для работы с данными сотрудников:**
+
+    **directory:read_users** — просмотр;
+    **directory:write_users** — просмотр и изменение.
+
+"""
+
+from jreq.jreq import safe_request
+import json
+
+def show_users(token, orgID, url=None):
+    """Функция Возвращает список сотрудников с постраничной навигацией
+
+    :param token: :term:`Яндекс токен приложения`
+    :type token: str
+    :param orgID: :term:`ID организации в Яндекс 360`
+    :type orgID: str
+    :param url: :term:`Ключи разбивки на страницы`
+    :type url: str or None
+    :return: результат запроса
+    :rtype: dict
+
+    """
+
+    urll = f'https://api360.yandex.net/directory/v1/org/{orgID}/users/?{url}'
+    print(urll)
+    headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
+	
+    return safe_request('get', urll, headers)
