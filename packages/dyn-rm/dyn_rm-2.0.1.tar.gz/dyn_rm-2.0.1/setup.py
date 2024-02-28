608a59e3c6e0a1b6991191c392e31302ff2306fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 05:03:23 +0000

    Add package dyn-rm/2.0.1/dyn_rm-2.0.1.tar.gz

---
+++ packages/dyn-rm/dyn_rm-2.0.1.tar.gz/dyn_rm-2.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dyn_rm',
+    version='2.0.1',
+    description='A Modular Dynamic Resource Manager',
+    author='Dominik Huber',
+    author_email='domi.huber@tum.de',
+    url='https://gitlab.inria.fr/dynres/dyn-procs/dyn_rm',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your package requires
+        'pypmix',
+        'asyncio',
+        'pyyaml',
+        'numpy'
+    ],
+)
