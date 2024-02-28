f996bf3e2515a9ac026934935837e3d70cca8cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 05:04:30 +0000

    Add package squeaky/0.6.0/squeaky-0.6.0.tar.gz

---
+++ packages/squeaky/squeaky-0.6.0.tar.gz/squeaky-0.6.0/squeaky/__init__.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+
+import sys
+import nbformat
+import subprocess
+
+from .passes.cell_ids import add_missing_cell_ids
+from .passes.metadata import clean_metadata
+from .passes.svg import clean_svgs
+from .passes.svg_id import clean_svg_ids
+from .passes.empty_cells import clean_empty_cells
+from .passes.trailing_whitespace import clean_trailing_whitespace
+from .tools import parse_args
+
+
+def clean_notebook(notebook: nbformat.NotebookNode) -> tuple[nbformat.NotebookNode, list[str]]:
+    """
+    Clean notebook using squeaky
+
+    Args:
+        notebook: The nbformat.NotebookNode to clean.
+
+    Returns:
+        notebook: The cleaned nbformat.NotebookNode.
+        problems: A list of problem descriptions (empty if no problems found).
+    """
+    problems = []
+    for fn in [
+        add_missing_cell_ids,
+        clean_metadata,
+        clean_svgs,
+        clean_svg_ids,
+        clean_trailing_whitespace,
+        clean_empty_cells,
+    ]:
+        notebook, msg = fn(notebook)
+        if msg is not None:
+            problems.append(msg)
+    return notebook, problems
+
+
+def squeaky_clean_hook(model, **_):
+    """
+    To run automatically when saving Jupyter notebooks.
+    """
+    if model["type"] != "notebook":
+        return
+    if model["content"]["nbformat"] != 4:
+        return
+    model["content"], _ = clean_notebook(
+        nbformat.from_dict(model["content"])
+    )
+
+
+def squeaky_cli():
+    """
+    To run from the command line
+    """
+    switches, filepaths = parse_args(sys.argv)
+    check = "--check" in switches
+    no_advice = "--no-advice" in switches
+
+    num_unclean = 0
+    for path in filepaths:
+        print(f"\033[1m{path}\033[0m")
+        notebook = nbformat.read(path, 4)
+        cleaned_notebook, problems = clean_notebook(notebook)
+        if problems == []:
+            continue
+        num_unclean += 1
+        if check:
+            [print(f"❌ {msg.capitalize()}") for msg in problems]
+            continue
+        [print(f"✅ Fixed {msg}") for msg in problems]
+        nbformat.write(cleaned_notebook, path)
+
+    if num_unclean > 0:
+        print("━" * 35)
+        if check:
+            if not no_advice:
+                print(
+                    f"Problems in {num_unclean} notebook"
+                    f"{'s' if num_unclean != 1 else ''}; to fix, run"
+                    "\n\n  squeaky path/to/notebook.ipynb\n"
+                )
+            sys.exit(2)
+        print(f"Modified {num_unclean} notebook{'s' if num_unclean != 1 else ''}")
+        print("━" * 35)
+    print("✨ All clean ✨")
---
+++ packages/squeaky/squeaky-0.6.0.tar.gz/squeaky-0.6.0/test/test.py
@@ -0,0 +1,79 @@
+import unittest
+import tempfile
+from pathlib import Path
+import nbformat
+import sys
+from io import StringIO
+from unittest.mock import patch
+
+from squeaky import squeaky_cli
+from squeaky import clean_notebook as clean_notebook_fn
+
+
+# set up
+class ExampleNotebooks:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.clean_notebook = nbformat.read("test/example-notebooks/clean.ipynb", 4)
+        self.clean_tempfile_path = Path(tempfile.gettempdir(), "squeaky-unittest-clean.ipynb")
+        nbformat.write(self.clean_notebook, self.clean_tempfile_path)
+
+        self.dirty_notebook = nbformat.read("test/example-notebooks/dirty.ipynb", 4)
+        self.dirty_tempfile_path = Path(tempfile.gettempdir(), "squeaky-unittest-dirty.ipynb")
+        nbformat.write(self.dirty_notebook, self.dirty_tempfile_path)
+
+examples = ExampleNotebooks()
+
+class TestAPI(unittest.TestCase):
+    def test_clean_notebooks(self):
+        new_notebook, problems = clean_notebook_fn(examples.dirty_notebook)
+        assert len(problems) == 5
+        assert new_notebook == examples.clean_notebook
+        examples.reset()
+
+
+class TestCLI(unittest.TestCase):
+    @patch("sys.stdout", StringIO())
+    @patch("sys.argv", ["squeaky", str(examples.dirty_tempfile_path), "--check"])
+    def test_check_flag_does_not_modify(self):
+        with self.assertRaises(SystemExit) as context:
+            squeaky_cli()
+        self.assertEqual(context.exception.code, 2)
+        self.assertEqual(
+            nbformat.read(examples.dirty_tempfile_path, 4),
+            examples.dirty_notebook
+        )
+        examples.reset()
+
+    @patch("sys.stdout", StringIO())
+    @patch("sys.argv", ["squeaky", str(examples.dirty_tempfile_path)])
+    def test_command_modifies(self):
+        squeaky_cli()
+        with open(examples.dirty_tempfile_path) as f:
+            self.assertEqual(
+                nbformat.read(f, 4),
+                examples.clean_notebook
+            )
+        examples.reset()
+
+    @patch("sys.stdout", new_callable=StringIO)
+    @patch(
+        "sys.argv",
+        ["squeaky", str(examples.dirty_tempfile_path), "--check", "--no-advice"],
+    )
+    def test_no_advice_flag_works(self, mock_stdout):
+        with self.assertRaises(SystemExit) as context:
+            squeaky_cli()
+        self.assertEqual(
+            mock_stdout.getvalue().strip().split("\n")[-1],
+            "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━",
+        )
+        examples.reset()
+
+
+if __name__ == "__main__":
+    unittest.main(buffer=True)
+    os.remove(clean_tempfile_path)
+    os.remove(dirty_tempfile_path)
