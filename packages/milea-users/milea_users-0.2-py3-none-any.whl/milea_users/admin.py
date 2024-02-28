1a16d3e8ed1b3fd2f1a4361a0486b2d7b3f969a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:03:32 +0000

    Add package milea-users/0.2/milea_users-0.2-py3-none-any.whl

---
+++ packages/milea-users/milea_users-0.2-py3-none-any.whl/milea_users/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group as BaseGroup
+from django.utils.translation import gettext_lazy as _
+
+from milea_users.models import Group, User
+
+# Remove the Default Group from the Default App and Register the Proxy
+admin.site.unregister(BaseGroup)
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin):
+    pass
+
+# Register New User Admin
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'last_login', 'is_active', 'is_staff')
+    search_fields = ('email',)
+    readonly_fields = ['last_login', 'date_joined']
+    ordering = ('id',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_("Personal info"), {'fields': (('first_name', 'last_name'),)}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {'fields': (('last_login', 'date_joined'),)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2')}),
+        (_("Personal info"), {'fields': (('first_name', 'last_name'),)}),
+    )
