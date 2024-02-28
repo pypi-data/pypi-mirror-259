a38363ad64c5f78e08ace8f857535a061e8df3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:02 +0000

    Add package pydmd/1.0.0/pydmd-1.0.0.tar.gz

---
+++ packages/pydmd/pydmd-1.0.0.tar.gz/pydmd-1.0.0/pydmd/meta.py
@@ -0,0 +1,21 @@
+__all__ = [
+    "__project__",
+    "__title__",
+    "__author__",
+    "__copyright__",
+    "__license__",
+    "__version__",
+    "__mail__",
+    "__maintainer__",
+    "__status__",
+]
+
+__project__ = "PyDMD"
+__title__ = "pydmd"
+__author__ = "Nicola Demo, Marco Tezzele, Francesco Andreuzzi, Sara Ichinaga"
+__copyright__ = "Copyright 2017-2024, PyDMD contributors"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__mail__ = "pydmd.info@gmail.com"
+__maintainer__ = __author__
+__status__ = "Stable"
