5dfafb26a3f8bc90fb10af3655035904a438f517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:04:24 +0000

    Add package machine-learning-evaluation/1.1.2/machine_learning_evaluation-1.1.2.tar.gz

---
+++ packages/machine-learning-evaluation/machine_learning_evaluation-1.1.2.tar.gz/machine_learning_evaluation-1.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='machine_learning_evaluation',
+    version='1.1.2',
+    author='Jay Dhotre',
+    author_email='jaydhotre524@gmail.com',
+    description='A library for evaluating machine learning models.',
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-learn>=0.24.0',
+        'pandas',
+        'numpy',
+        'imblearn'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ]
+)
