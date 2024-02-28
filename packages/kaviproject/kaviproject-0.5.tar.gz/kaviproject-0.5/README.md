b96a8e153b4bca6b394438201ef92825cbb71174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:04:14 +0000

    Add package kaviproject/0.5/kaviproject-0.5.tar.gz

---
+++ packages/kaviproject/kaviproject-0.5.tar.gz/kaviproject-0.5/README.md
@@ -0,0 +1,18 @@
+# MyLibrary
+
+A brief description of what your library does and why it's useful.
+
+## Installation
+
+You can install MyLibrary using pip:
+
+```bash
+pip install kaviproject==0.4
+```
+## Usage
+```python
+import mylibrary from leanth
+def print_hi():
+    a=[2,4,34,54,64]
+    print(leanth(a))
+```
---
+++ packages/kaviproject/kaviproject-0.5.tar.gz/kaviproject-0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='kaviproject',
+    version='0.5',
+    packages=find_packages(),
+    author='Kavi',
+    author_email='arasansk54@gmail.com',
+    description='A description of your library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kaviarasan2720/my_library',
+    license='MIT',
+    install_requires=[
+        # List of dependencies
+    ],
+)
