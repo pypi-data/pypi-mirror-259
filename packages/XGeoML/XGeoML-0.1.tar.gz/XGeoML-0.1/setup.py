1509fe80b8708737e0add57bcc1eda25e08a6a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 14:04:55 +0000

    Add package XGeoML/0.1/XGeoML-0.1.tar.gz

---
+++ packages/XGeoML/XGeoML-0.1.tar.gz/XGeoML-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='XGeoML',
+    version='0.1',
+    packages=find_packages(),
+    description='A ensemble framework for explainable geospatial machine Learning models',
+    author='Lingbo Liu',
+    author_email='lingbo.liu@whu.edu.cn',
+    license="MIT",
+    install_requires=[
+        'sklearn',
+        'numpy',
+        'scipy',
+        'joblib',
+        'pandas',
+        'lime',
+        'shap',
+        'tqdm', 
+    ],
+)
