1dc1d8580aefc46336523ba8cf50e01424c7ef0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 06:03:46 +0000

    Add package SilverStreamingApplicationPoC/1.8/SilverStreamingApplicationPoC-1.8-py3-none-any.whl

---
+++ packages/SilverStreamingApplicationPoC/SilverStreamingApplicationPoC-1.8-py3-none-any.whl/com/phida/main/sparksession.py
@@ -0,0 +1,31 @@
+#from pyspark.sql import SparkSession
+
+#from com.phida.main import logging
+
+#spark = (SparkSession.builder
+#         .getOrCreate())
+
+#logger = logging.Log4j(spark)
+
+###############Above lines are commented and Below lines are added by Shilton#########
+import pyspark
+import base64
+from pyspark.sql import SparkSession
+from com.phida.main import logging
+
+with open("/mnt/secrets/azure-secret.txt", "rb") as file:
+    storage_account_key = file.read().strip()
+
+decoded_key = base64.b64decode(storage_account_key).decode("utf-8")
+
+#This 'sparkAppName' is substituted by the arguments section of the SparkApplication.
+spark = SparkSession.builder \
+    .appName(sparkAppName) \
+    .config('spark.jars.packages', 'org.apache.hadoop:hadoop-azure:3.3.1') \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .config("fs.azure.account.auth.type.eabase.dfs.core.windows.net", "SharedKey") \
+    .config("fs.azure.account.key.eabase.dfs.core.windows.net", decoded_key) \
+    .getOrCreate()
+
+logger = logging.Log4j(spark)
