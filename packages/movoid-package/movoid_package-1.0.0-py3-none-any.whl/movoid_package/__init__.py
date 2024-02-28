d4a5bf04db11f003c1d0fda2870bf416af9ad7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 05:03:52 +0000

    Add package movoid-package/1.0.0/movoid_package-1.0.0-py3-none-any.whl

---
+++ packages/movoid-package/movoid_package-1.0.0-py3-none-any.whl/movoid_package/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/2/28 0:07
+# Description   : 
+"""
+from .importing import import_object, import_module
