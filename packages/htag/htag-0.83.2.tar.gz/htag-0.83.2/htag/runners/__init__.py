1c6a40b984f41a0d76595180b8d28eb8fa794b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 05:03:18 +0000

    Add package htag/0.83.2/htag-0.83.2.tar.gz

---
+++ packages/htag/htag-0.83.2.tar.gz/htag-0.83.2/htag/runners/__init__.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2022 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htag
+# #############################################################################
+
+class _RunnerNotFunctionnal:
+    def __init__(self,*tagClass:type):
+        raise ERROR
+
+
+# mono instance
+from .browserhttp import BrowserHTTP
+
+# multi instances                                                   !!! CAN UPDATE !!!
+from .pyscript import PyScript
+
+
+try:
+    # mono instance
+    from .browserstarlettehttp import BrowserStarletteHTTP
+except Exception as err:
+    ERROR=err
+    class BrowserStarletteHTTP(_RunnerNotFunctionnal): pass
+
+try:
+    # mono instance # !                                             !! CAN UPDATE !!!
+    from .browserstarlettews import BrowserStarletteWS
+except Exception as err:
+    ERROR=err
+    class BrowserStarletteWS(_RunnerNotFunctionnal): pass
+
+try:
+    # mono instance #                                               !!! CAN UPDATE !!!
+    from .devapp import DevApp                                      # special dev
+except Exception as err:
+    ERROR=err
+    class DevApp(_RunnerNotFunctionnal): pass
+
+try:
+    # mono instance (just one pywebview ;-)
+    from .pywebview import PyWebView
+except Exception as err:
+    ERROR=err
+    class PyWebWiew(_RunnerNotFunctionnal): pass
+
+try:
+    # mono instance (just one chrome app instance)                  !!! CAN UPDATE !!!
+    from .chromeapp import ChromeApp
+except Exception as err:
+    ERROR=err
+    class ChromeApp(_RunnerNotFunctionnal): pass
+
+
+try:
+    # mono instance (just one chrome app instance)                  !!! CAN UPDATE !!!
+    from .winapp import WinApp
+except Exception as err:
+    ERROR=err
+    class WinApp(_RunnerNotFunctionnal): pass
+
+try:
+    # mono instance (just one android app instance)
+    from .androidapp import AndroidApp
+except Exception as err:
+    ERROR=err
+    class AndroidApp(_RunnerNotFunctionnal): pass
+
+try:
+    # mono instance
+    from .browsertornadohttp import BrowserTornadoHTTP
+except Exception as err:
+    ERROR=err
+    class BrowserTornadoHTTP(_RunnerNotFunctionnal): pass
+
+from .runner import Runner,HTTPResponse
