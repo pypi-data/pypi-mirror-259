1c656937f47795beede54a8b323082a846cff091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:04:09 +0000

    Add package release-testing-Ninniet5670/0.1.0/release_testing_ninniet5670-0.1.0.tar.gz

---
+++ packages/release-testing-Ninniet5670/release_testing_ninniet5670-0.1.0.tar.gz/release_testing_ninniet5670-0.1.0/src/testing/test.py
@@ -0,0 +1,8 @@
+def hello_testing():
+    print('Hello, Testing!')
+
+def love_Izabela():
+    print('I love you my love, you are everything to me Sz Izabela')
+
+if __name__ == '__main__':
+    love_Izabela()
