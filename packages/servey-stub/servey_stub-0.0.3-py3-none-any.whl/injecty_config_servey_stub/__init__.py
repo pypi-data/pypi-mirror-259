fba5121e311f048c79331b630b90609df1ebbf98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 14:04:31 +0000

    Add package servey-stub/0.0.3/servey_stub-0.0.3-py3-none-any.whl

---
+++ packages/servey-stub/servey_stub-0.0.3-py3-none-any.whl/injecty_config_servey_stub/__init__.py
@@ -0,0 +1,31 @@
+from injecty import InjectyContext
+
+from servey_stub.type_definition.dataclass_type_definition_factory import (
+    DataclassTypeDefinitionFactory,
+)
+from servey_stub.type_definition.enum_type_definition_factory import (
+    EnumTypeDefinitionFactory,
+)
+from servey_stub.type_definition.generic_type_definition_factory import (
+    GenericTypeDefinitionFactory,
+)
+from servey_stub.type_definition.primitive_type_definition_factory import (
+    PrimitiveTypeDefinitionFactory,
+)
+from servey_stub.type_definition.type_definition_factory_abc import (
+    TypeDefinitionFactoryABC,
+)
+
+priority = 100
+
+
+def configure(context: InjectyContext):
+    context.register_impls(
+        TypeDefinitionFactoryABC,
+        [
+            DataclassTypeDefinitionFactory,
+            PrimitiveTypeDefinitionFactory,
+            EnumTypeDefinitionFactory,
+            GenericTypeDefinitionFactory,
+        ],
+    )
