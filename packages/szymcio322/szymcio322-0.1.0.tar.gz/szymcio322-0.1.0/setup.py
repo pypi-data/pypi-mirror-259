d78286ecea6bf94e5338a0863076cd60ffbfc865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 06:04:33 +0000

    Add package szymcio322/0.1.0/szymcio322-0.1.0.tar.gz

---
+++ packages/szymcio322/szymcio322-0.1.0.tar.gz/szymcio322-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="szymcio322",
+    version="0.1.0",
+    description="A simple Python package for testing",
+    long_description="test",
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
