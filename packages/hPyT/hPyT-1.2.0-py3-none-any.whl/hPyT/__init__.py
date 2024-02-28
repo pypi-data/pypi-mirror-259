1cd9b86a2cbb533b335ad17dba55dc245fc31bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 06:03:58 +0000

    Add package hPyT/1.2.0/hPyT-1.2.0-py3-none-any.whl

---
+++ packages/hPyT/hPyT-1.2.0-py3-none-any.whl/hPyT/__init__.py
@@ -0,0 +1,27 @@
+"""
+hPyT - Hide Python Titlebar
+Author - zingzy
+version - 1.2.0
+License - MIT
+Homepage - https://github.com/zingzy/hPyT
+"""
+
+from .hPyT import title_bar
+from .hPyT import maximize_minimize_button
+from .hPyT import all_stuffs
+from .hPyT import maximize_button
+from .hPyT import minimize_button
+from .hPyT import opacity
+from .hPyT import window_flash
+
+__all__ = [
+    'title_bar',
+    'maximize_minimize_button',
+    'all_stuffs',
+    'maximize_button',
+    'minimize_button',
+    'opacity',
+    'window_flash',
+]
+
+__version__ = '1.2.0'
