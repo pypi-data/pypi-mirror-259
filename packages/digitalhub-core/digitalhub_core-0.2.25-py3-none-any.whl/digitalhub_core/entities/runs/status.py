28256115ef6247da3a6c585daec1bd9bb80320cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:03:40 +0000

    Add package digitalhub-core/0.2.25/digitalhub_core-0.2.25-py3-none-any.whl

---
+++ packages/digitalhub-core/digitalhub_core-0.2.25-py3-none-any.whl/digitalhub_core/entities/runs/status.py
@@ -0,0 +1,12 @@
+"""
+RunStatus class module.
+"""
+from __future__ import annotations
+
+from digitalhub_core.entities._base.status import Status
+
+
+class RunStatus(Status):
+    """
+    Status class for run entities.
+    """
