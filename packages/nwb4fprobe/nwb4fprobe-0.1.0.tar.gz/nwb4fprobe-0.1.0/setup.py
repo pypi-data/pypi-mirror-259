292fbcee58ccb2b29a2b424a5b7c818a3be2c36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:04:12 +0000

    Add package nwb4fprobe/0.1.0/nwb4fprobe-0.1.0.tar.gz

---
+++ packages/nwb4fprobe/nwb4fprobe-0.1.0.tar.gz/nwb4fprobe-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nwb4fprobe',
+    version='0.1.0',
+    url='https://github.com/sachuriga283/quality_metrix.git',
+    author='sachuriga283',
+    author_email='sachuriga.sachuriga@ntnu.no',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=[],
+)
