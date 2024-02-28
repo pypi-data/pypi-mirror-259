5c70911a5fce6822eda6db83ad891310014821fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:04:27 +0000

    Add package caerp-oidc-client/2024.1.1/caerp_oidc_client-2024.1.1.tar.gz

---
+++ packages/caerp-oidc-client/caerp_oidc_client-2024.1.1.tar.gz/caerp_oidc_client-2024.1.1/src/caerp_oidc_client/__init__.py
@@ -0,0 +1,8 @@
+import logging
+from .models import *
+logger = logging.getLogger(f"endi.{__name__}")
+
+def includeme(config):
+    logger.debug("Including caerp_oidc_client views")
+    config.include(".views")
+    
