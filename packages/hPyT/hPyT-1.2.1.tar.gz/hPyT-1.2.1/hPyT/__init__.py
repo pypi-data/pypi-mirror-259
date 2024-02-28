00cf7e31d56e905dc71fcb57947182391d9dcb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:03:51 +0000

    Add package hPyT/1.2.1/hPyT-1.2.1.tar.gz

---
+++ packages/hPyT/hPyT-1.2.1.tar.gz/hPyT-1.2.1/hPyT/__init__.py
@@ -0,0 +1,39 @@
+"""
+hPyT - Hack Python Titlebar
+Author - zingzy
+version - 1.2.1
+License - MIT
+Homepage - https://github.com/zingzy/hPyT
+"""
+
+from .hPyT import title_bar
+from .hPyT import maximize_minimize_button
+from .hPyT import all_stuffs
+from .hPyT import maximize_button
+from .hPyT import minimize_button
+from .hPyT import opacity
+from .hPyT import window_flash
+from .hPyT import title_bar_color
+from .hPyT import title_bar_text_color
+from .hPyT import rainbow_title_bar
+from .hPyT import window_frame
+from .hPyT import window_animation
+from .hPyT import title_text
+
+__all__ = [
+    'title_bar',
+    'maximize_minimize_button',
+    'all_stuffs',
+    'maximize_button',
+    'minimize_button',
+    'opacity',
+    'window_flash',
+    'title_bar_color',
+    'title_bar_text_color',
+    'rainbow_title_bar',
+    'window_frame',
+    'window_animation',
+    'title_text'
+]
+
+__version__ = '1.2.1'
