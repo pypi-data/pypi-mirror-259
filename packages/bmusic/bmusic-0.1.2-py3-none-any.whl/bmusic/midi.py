97cdb1df553d9f7976b63c48241a4efdd76d3f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 14:04:53 +0000

    Add package bmusic/0.1.2/bmusic-0.1.2-py3-none-any.whl

---
+++ packages/bmusic/bmusic-0.1.2-py3-none-any.whl/bmusic/midi.py
@@ -0,0 +1,239 @@
+"""
+"note" refers to a pitch, represented as an integer in MIDI.
+For example, there are 88 notes on a piano.
+
+"message" refers to an instance of a note being played.
+For example, this MIDI file has 100 messages,
+using a total of 10 different notes.
+"""
+
+from typing import Generator, Optional, Sequence
+
+import bpy
+import mido
+
+__all__ = (
+    "Message",
+    "MessageList",
+    "parse_midi",
+    "split_chords",
+)
+
+
+class Message:
+    """
+    Message with absolute start and end time in frames.
+    Also supports linked-list like behavior within track.
+    """
+
+    note: int
+    velocity: int
+    start: float
+    end: float
+
+    def __init__(self, note, velocity, start, end, msglist: Optional["MessageList"] = None, index: Optional[int] = None):
+        """
+        :param msglist: MessageList object this message belongs to.
+        :param index: Index of this message in the track. Allows for linked-list behavior.
+        """
+        self.note = note
+        self.velocity = velocity
+        self.start = start
+        self.end = end
+
+        self.msglist = msglist
+        self.index = index
+
+    def __repr__(self):
+        return (f"bmusic.Message(note={self.note}, velocity={self.velocity}, "
+                f"start={self.start}, end={self.end})")
+
+    def copy(self) -> "Message":
+        return Message(self.note, self.velocity, self.start, self.end, self.msglist, self.index)
+
+    def diff(self, other: "Message") -> float:
+        """
+        Calculate difference in frames between this message and another's start.
+        Positive if this message starts after other.
+        """
+        return self.start - other.start
+
+    def next(self) -> Optional["Message"]:
+        """
+        Get next message in msglist.
+        """
+        assert self.index is not None
+        assert self.msglist is not None
+
+        ind = self.index + 1
+        if ind >= len(self.msglist._messages):
+            return None
+        return self.msglist._messages[ind]
+
+    def next_start(self, fallback: float = 1e9) -> float:
+        """
+        Timestamp of next message's start.
+
+        :param fallback: Value to return if no next message.
+        """
+        return self.next().start if self.next() else fallback
+
+    def prev(self) -> Optional["Message"]:
+        """
+        Get previous message in msglist.
+        """
+        assert self.index is not None
+        assert self.msglist is not None
+
+        ind = self.index - 1
+        if ind < 0:
+            return None
+        return self.msglist._messages[ind]
+
+    def prev_start(self, fallback: float = -1e9) -> float:
+        """
+        Timestamp of previous message's start.
+
+        :param fallback: Value to return if no previous message.
+        """
+        return self.prev().start if self.prev() else fallback
+
+    def prev_end(self, fallback: float = -1e9) -> float:
+        """
+        Timestamp of previous message's end.
+
+        :param fallback: Value to return if no previous message.
+        """
+        return self.prev().end if self.prev() else fallback
+
+
+class MessageList:
+    """
+    Sequence of messages sorted by start time.
+
+    A frozen class; that is, cannot be modified after creation.
+
+    TODO this shouldn't be frozen. It should be possible to add and remove messages.
+    """
+
+    def __init__(self, messages: Optional[Sequence[Message]] = None):
+        """
+        Initialize from message.
+        Creates copies of each message.
+        Also associates each message with self.
+        """
+        if messages is None:
+            messages = []
+
+        self._messages = []
+        for msg in messages:
+            msg = msg.copy()
+            msg.msglist = self
+            msg.index = len(self._messages)
+            self._messages.append(msg)
+
+        self._messages.sort(key=lambda n: n.start)
+        self._noteset = sorted(set(n.note for n in self._messages))
+
+    def __iter__(self) -> Generator[Message, None, None]:
+        yield from self._messages
+
+    def __len__(self):
+        return len(self._messages)
+
+    def __getitem__(self, item):
+        return self._messages[item]
+
+    def __repr__(self):
+        return f"bmusic.MessageList({self._messages})"
+
+    def noteset(self):
+        """
+        Set of all notes used in this track.
+        """
+        return self._noteset
+
+    def duration(self) -> float:
+        """
+        Duration, in frames, between first msg's start and last msg's end.
+        """
+        return self._messages[-1].end - self._messages[0].start
+    
+    def filter_notes(self, notes: Sequence[int]) -> "MessageList":
+        """
+        Only keep messages with note in the given list.
+        """
+        return MessageList([n for n in self._messages if n.note in notes])
+
+    def split_notes(self):
+        """
+        Split into multiple tracks, each containing only one note.
+        """
+        for note in self.noteset():
+            yield MessageList([n for n in self._messages if n.note == note])
+
+
+def parse_midi(path: str, offset: float = 0, fps: Optional[float] = None) -> MessageList:
+    """
+    Parse MIDI from a file.
+
+    :param path: Path to midi file.
+    :param offset: Duration in frames of first message's start.
+    :param fps: Frames per second (None for blender fps)
+    """
+    notes = []
+
+    if fps is None:
+        fps = bpy.context.scene.render.fps
+    midi = mido.MidiFile(path)
+
+    # State of the last key press of note i.
+    starts = [0] * 1000
+    vels = [0] * 1000
+
+    frame = 0
+    started = False
+    for msg in midi:
+        if started:
+            frame += msg.time * fps
+
+        if msg.type.startswith("note_"):
+            started = True
+            note = msg.note
+            vel = msg.velocity if msg.type == "note_on" else 0
+            if vel == 0:
+                n = Message(note, vels[note], starts[note]+offset, frame+offset)
+                notes.append(n)
+            else:
+                starts[note] = frame
+                vels[note] = vel
+
+    return MessageList(notes)
+
+
+def split_chords(midi: MessageList, threshold: float) -> list[list[Message]]:
+    """
+    Split a MIDI track into chords --- messages that play at the same time (or within a threshold of each other).
+
+    Messages less than threshold apart will be combined into one chord.
+
+    Threshold units are whatever given MessageList units are; most likely frames.
+    """
+    chords = []
+
+    last_time = None
+    chord = []
+    for msg in midi:
+        if last_time is None:
+            chord.append(msg)
+        elif abs(msg.start - last_time) < threshold:
+            chord.append(msg)
+        else:
+            chords.append(chord)
+            chord = [msg]
+        last_time = msg.start
+
+    if chord:
+        chords.append(chord)
+
+    return chords
---
+++ packages/bmusic/bmusic-0.1.2-py3-none-any.whl/bmusic/proc/intensity.py
@@ -0,0 +1,173 @@
+"""
+Procedures relating to intensity of something.
+e.g. light, piano key's depression, string's vibration, etc.
+"""
+
+__all__ = (
+    "Intensity",
+    "IntensityOnOff",
+    "IntensityFade",
+)
+
+from typing import Callable
+
+import bpy
+
+from ..affix import *
+from ..anim import *
+from ..midi import *
+from .procedure import ForEachProc
+from .. import utils
+
+
+class Intensity(ForEachProc):
+    """
+    Base intensity procedure.
+
+    Parameters:
+
+    animkey
+        Animation key with the following keys:
+
+            - basis: Resting (intensity 0) position.
+            - on: Playing (intensity max) position.
+    """
+
+    animkey: AnimKey
+
+
+class IntensityOnOff(Intensity):
+    """
+    Turns on when a note starts and off when it ends.
+
+    :Keyframe types:
+
+        - JITTER: Resting (intensity = 0).
+        - BREAKDOWN: Playing.
+
+    Parameters:
+
+    duration
+        Time, in seconds, to spend interpolating between states.
+
+    vector_handles
+        Whether to use vector handles (no easing in or out).
+    """
+
+    duration: float = 0.05
+    vector_handles: bool = False
+
+    def animate(self):
+        duration = self.duration * bpy.context.scene.render.fps
+        handle = "VECTOR" if self.vector_handles else "AUTO_CLAMPED"
+
+        msgs = compute_affixes(self.midi, max_prefix=duration, max_suffix=duration, suffix_after_end=True)
+        for msg in msgs:
+            intensity = self.get_intensity(msg)
+
+            # For brevity, make a list and call func later.
+            # List of (frame, value, key_type)
+            keys = []
+
+            # Initial resting position
+            keys.append((msg.start-msg.prefix, 0, "JITTER"))
+            # Playing through note
+            keys.append((msg.start, intensity, "BREAKDOWN"))
+            keys.append((msg.end, intensity, "BREAKDOWN"))
+            # Resting after note
+            if msg.suffix >= duration:
+                # Only if suffix is full length; otherwise, the next note will take care of it.
+                keys.append((msg.end+msg.suffix, 0, "JITTER"))
+
+            for frame, value, type in keys:
+                self.animkey.animate(frame, handle=handle, type=type, on=value)
+
+
+class IntensityFade(Intensity):
+    """
+    Turns on when a note starts and fades away.
+
+    :Keyframe types:
+
+        - JITTER: Resting (intensity = 0).
+        - BREAKDOWN: Fading.
+        - EXTREME: Max intensity.
+
+    Parameters:
+
+    fade_func
+        Function that curves linear time to fade intensity.
+        Takes parameters ``(t,)`` (time in seconds since note start) and returns
+        between 0 and 1, where 0 is off and 1 is max intensity. There are a few
+        predefined functions ``bmusic.utils``
+
+    start_time
+        Time, in seconds, from off to initial on.
+
+    key_interval
+        Interval in seconds for decay keyframes. Avoids unneccessary
+        keyframing on every frame.
+
+    off_thres
+        Threshold for intensity to be considered off. Keyframing will stop
+        at this point. If using a custom ``fade_func`` that is NOT monotonically decreasing,
+        set this to ``0`` (or negative).
+
+    max_len
+        Maximum length of note, in seconds. Keyframing will stop after this
+        time, even if the note is still playing.
+
+    note_end
+        Whether to cut intensity to 0 at end of note. If false, the note will
+        keep fading until next play, even if the note was released.
+    """
+
+    @staticmethod
+    def _default_fade_func(t):
+        return utils.EXPONENTIAL(1.5, t)
+
+    fade_func: Callable[[float], float] = _default_fade_func
+    start_time: float = 0.05
+    key_interval: float = 0.3
+    off_thres: float = 0.001
+    max_len: float = 60
+    note_end: bool = True
+
+    def animate(self):
+        fps = bpy.context.scene.render.fps
+        start_time = self.start_time * fps
+        key_interval = self.key_interval * fps
+        max_len = self.max_len * fps
+
+        msgs = compute_affixes(self.midi, max_prefix=start_time, max_suffix=max_len, suffix_after_end=False, split=0)
+        last_value = 0
+        for msg in msgs:
+            intensity = self.get_intensity(msg)
+
+            # Start
+            self.animkey.animate(msg.start-msg.prefix, on=last_value, handle="VECTOR", type="BREAKDOWN")
+            self.animkey.animate(msg.start, on=intensity, handle="VECTOR", type="EXTREME")
+
+            # Fading
+            frame = msg.start
+            added_key = False
+            while True:
+                frame += key_interval
+                if frame > msg.start + msg.suffix:
+                    break
+                added_key = True
+
+                last_value = intensity * self.fade_func((frame - msg.start) / fps)
+
+                if self.off_thres > 0 and last_value < self.off_thres:
+                    # Keyframe off
+                    self.animkey.animate(frame, on=0, handle="VECTOR", type="JITTER")
+                    last_value = 0
+                    break
+
+                self.animkey.animate(frame, on=last_value, handle="VECTOR", type="BREAKDOWN")
+
+            if not added_key:
+                # Suffix was too short for ``key_interval``.
+                # Here, we set ``last_value`` to the appropriate value, and the next iteration will key it for us.
+                last_value = intensity * self.fade_func(msg.suffix / fps)
