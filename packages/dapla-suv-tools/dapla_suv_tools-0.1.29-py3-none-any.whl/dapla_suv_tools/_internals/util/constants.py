25589073f393391877ba3fc4bdd4d58a5e91e3ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:03:57 +0000

    Add package dapla-suv-tools/0.1.29/dapla_suv_tools-0.1.29-py3-none-any.whl

---
+++ packages/dapla-suv-tools/dapla_suv_tools-0.1.29-py3-none-any.whl/dapla_suv_tools/_internals/util/constants.py
@@ -0,0 +1,12 @@
+OPERATION_OK = "OK"
+OPERATION_ERROR = "ERROR"
+
+LOG_DIAGNOSTIC = "diag"
+LOG_INFO = "info"
+LOG_WARNING = "warn"
+LOG_ERROR = "error"
+
+LOG_LEVELS = [LOG_DIAGNOSTIC, LOG_INFO, LOG_WARNING, LOG_ERROR]
+
+PERIODE_PATH = "/skjemadata/periode"
+SKJEMA_PATH = "/skjemadata/skjema"
