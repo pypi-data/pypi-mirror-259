3ac64c171c1d72ef238deda87e26a8dd3ed0a827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:03:12 +0000

    Add package ssave/1.0.5/ssave-1.0.5-py3-none-any.whl

---
+++ packages/ssave/ssave-1.0.5-py3-none-any.whl/ssave/__init__.py
@@ -0,0 +1,34 @@
+import sys
+import configparser
+
+config = configparser.ConfigParser()
+print("SCOS Save Utility is Included.")
+
+def read(direct, variable):
+    print(config[direct][variable])
+
+def write(direct, variable, change):
+    config[direct][variable] = change
+
+def sopen(file):
+    try:
+        open(file).read()
+    except:
+        print("File not found or not specified.")
+    else:
+        try:
+            config.read(file)
+        except:
+            print("File does not exist or something else happened.")
+
+try:
+    print(sys.argv[1])
+except:
+    print("File not found or not specified.")
+else:
+    try:
+        config.read(sys.argv[1])
+    except:
+        print("File does not exist or something else happened.")
+    else:
+        config.read(sys.argv[1])
