0db277dbe01dd7065173dc077e47af95d3550c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:03:55 +0000

    Add package ostorlab/0.19.6/ostorlab-0.19.6.tar.gz

---
+++ packages/ostorlab/ostorlab-0.19.6.tar.gz/ostorlab-0.19.6/src/ostorlab/cli/auth/__init__.py
@@ -0,0 +1,6 @@
+"""Module for the root command: auth."""
+from ostorlab.cli.auth import auth
+from ostorlab.cli.auth import login
+from ostorlab.cli.auth import revoke
+
+__all__ = ["auth", "login", "revoke"]
