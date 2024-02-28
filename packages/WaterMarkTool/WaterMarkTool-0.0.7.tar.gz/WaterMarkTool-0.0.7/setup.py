339441b3da3cf1ed8c99a122f927835fca76efd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 06:04:36 +0000

    Add package WaterMarkTool/0.0.7/WaterMarkTool-0.0.7.tar.gz

---
+++ packages/WaterMarkTool/WaterMarkTool-0.0.7.tar.gz/WaterMarkTool-0.0.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+name="WaterMarkTool",
+author='GamingGGa',
+author_email='GabrielGaming1443@gmail.com',
+maintainer='GamingGGa',
+version="0.0.7",
+description="A little tool to add water mark on various pictures",
+long_description=open('README.md').read(),
+long_description_content_type='text/markdown',
+packages=find_packages(),
+install_requires=[
+    'pillow <= 10.2.0',
+],
+url="https://pypi.org/project/WaterMarkTool/",
+download_url="https://files.pythonhosted.org/packages/f8/78/62f49f1724ecd30cd4f998d5f9c2a4e041a56cb4dc7adaec21df502b7b95/WaterMarkTool-0.0.2.tar.gz",
+license="MIT",
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+python_requires=">=3.6",
+)
