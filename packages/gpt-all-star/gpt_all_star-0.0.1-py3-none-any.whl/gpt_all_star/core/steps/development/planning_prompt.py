ec819743567c94af735324f61d54b4565d98e32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 14:04:56 +0000

    Add package gpt-all-star/0.0.1/gpt_all_star-0.0.1-py3-none-any.whl

---
+++ packages/gpt-all-star/gpt_all_star-0.0.1-py3-none-any.whl/gpt_all_star/core/steps/development/planning_prompt.py
@@ -0,0 +1,26 @@
+from langchain_core.prompts import PromptTemplate
+
+planning_prompt_template = PromptTemplate.from_template(
+    """
+# Instructions
+---
+Create a detailed and specific development plan from project creation to source code implementation in order to build a fully working application.
+
+# Constraints
+---
+- The application specifications must be carefully understood and accurately reflected in the application.
+
+# Requirements
+---
+
+## Application Specifications to be met
+```specifications.md
+{specifications}
+```
+
+## Technology stack to be used
+```technologies.md
+{technologies}
+```
+"""
+)
