32c0da92b324e71af6cd697d33d6177eb2b89400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:03:52 +0000

    Add package stellarmortis/0.0.6/stellarmortis-0.0.6.tar.gz

---
+++ packages/stellarmortis/stellarmortis-0.0.6.tar.gz/stellarmortis-0.0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools 
+
+REQUIRED_PACKAGES = [
+    'numpy',
+    'pandas',
+    'matplotlib',
+    'seaborn'
+    'ebfpy',
+    'galpy',
+    'astropy',
+    'requests',
+    'astroquery',
+    'ray'
+]
+
+DESCRIPTION = "A package to synthesise populations of dead stars and calculate microlensing events caused by this population."
+
+setuptools.setup( 
+	name="stellarmortis",
+	version="0.0.6",
+	author="David Sweeney", 
+	author_email="david.sweeney@sydney.edu.au", 
+	packages=setuptools.find_packages(), 
+	description=DESCRIPTION, 
+	long_description=DESCRIPTION, 
+	url="https://github.com/David-Sweeney/StellarMortis",
+	license='MIT', 
+	python_requires='>=3.7', 
+	install_requires=REQUIRED_PACKAGES,
+)
