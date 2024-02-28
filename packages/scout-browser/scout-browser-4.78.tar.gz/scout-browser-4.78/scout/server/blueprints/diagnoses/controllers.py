5e4268fc1eb350c83d29b5d489d173aad46da9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:12 +0000

    Add package scout-browser/4.78/scout-browser-4.78.tar.gz

---
+++ packages/scout-browser/scout-browser-4.78.tar.gz/scout-browser-4.78/scout/server/blueprints/diagnoses/controllers.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from scout.adapter import MongoAdapter
+from scout.server.links import disease_link
+
+
+def disease_entry(store, disease_id) -> dict:
+    """Retrieve specific info for a disease term
+
+    Args:
+        store(obj): an adapter to the scout database
+        disease_id(str): a disease_id
+
+    Returns:
+        disease_obj(obj): a disease term containing description and genes
+    """
+
+    disease_obj = store.disease_term(disease_identifier=disease_id, filter_project={})
+    disease_obj["genes_complete"] = store.disease_to_genes(disease_obj)
+    disease_obj["hpo_complete"] = [
+        store.hpo_term(hpo_id) for hpo_id in disease_obj.get("hpo_terms", [])
+    ]
+    disease_obj["disease_link"] = disease_link(disease_id=disease_obj["disease_id"])
+    return disease_obj
+
+
+def disease_terms(store: MongoAdapter, query: str, source: str) -> dict:
+    """Retrieve disease terms, queried by source or a text-match for the disease description."""
+
+    data = {"terms": list(store.query_disease(query=query, source=source, filter_project=None))}
+    return data
