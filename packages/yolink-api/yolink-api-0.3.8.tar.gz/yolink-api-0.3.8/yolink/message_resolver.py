18127a362f68b5967143cf04cb0a121de58f6ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:13 +0000

    Add package yolink-api/0.3.8/yolink-api-0.3.8.tar.gz

---
+++ packages/yolink-api/yolink-api-0.3.8.tar.gz/yolink-api-0.3.8/yolink/message_resolver.py
@@ -0,0 +1,15 @@
+"""YoLink cloud message resolver."""
+
+from typing import Any
+from math import log2
+
+
+def smart_remoter_message_resolver(msg_data: dict[str, Any]) -> dict[str, Any]:
+    """SmartRemoter message resolver"""
+    btn_press_event = msg_data.get("event")
+    if btn_press_event is not None:
+        key_mask = btn_press_event["keyMask"]
+        button_sequence = 0 if key_mask == 0 else (int(log2(key_mask)) + 1)
+        # replace with button sequence
+        msg_data["event"]["keyMask"] = button_sequence
+    return msg_data
