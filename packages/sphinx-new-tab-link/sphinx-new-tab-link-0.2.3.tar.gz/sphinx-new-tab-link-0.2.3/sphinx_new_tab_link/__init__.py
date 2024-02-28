19a889194841bb57bdfc947dfdf41d0e295f8195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:03:35 +0000

    Add package sphinx-new-tab-link/0.2.3/sphinx-new-tab-link-0.2.3.tar.gz

---
+++ packages/sphinx-new-tab-link/sphinx-new-tab-link-0.2.3.tar.gz/sphinx-new-tab-link-0.2.3/sphinx_new_tab_link/__init__.py
@@ -0,0 +1,33 @@
+from sphinx.writers.html import HTMLTranslator
+
+
+class NewTabLinkHTMLTranslator(HTMLTranslator):
+    """Patched translator to open an external link in a new tab of the browser.
+
+    ref: https://stackoverflow.com/a/67153583
+    """
+
+    def starttag(self, node, tagname, *args, **atts):
+        if (
+            tagname == "a"
+            and "target" not in atts
+            and (
+                "external" in atts.get("class", "")
+                or "external" in atts.get("classes", [])
+            )
+        ):
+            atts["target"] = "_blank"
+            atts["rel"] = "noopener noreferrer"
+        # Support method assignment like the following (But hack)
+        # SomeTranslator.starttag = NewTabLinkHTMLTranslator.starttag
+        if not isinstance(self, NewTabLinkHTMLTranslator):
+            return super(self.__class__, self).starttag(
+                node, tagname, *args, **atts
+            )
+        return super().starttag(node, tagname, *args, **atts)
+
+
+def setup(app):
+    app.set_translator("html", NewTabLinkHTMLTranslator)
+    app.set_translator("dirhtml", NewTabLinkHTMLTranslator)
+    app.set_translator("singlehtml", NewTabLinkHTMLTranslator)
