ebe8475fcff369312f0adfd3d368a5d8ec973437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:03:55 +0000

    Add package survivors/1.6.2/survivors-1.6.2.tar.gz

---
+++ packages/survivors/survivors-1.6.2.tar.gz/survivors-1.6.2/README.rst
@@ -0,0 +1,139 @@
+|License| |PyPi|_ |DOI|_
+
+===============
+survivors
+===============
+
+`survivors <https://pypi.org/project/survivors/>`_ is a Python library for **survival analysis**.
+
+It allows for building survival models: Survival Tree, Bagging ensemble, Adaptive Boosting ensemble.
+
+=======================
+About Survival Analysis
+=======================
+
+The objective in `survival analysis`_ (also referred to as time-to-event or reliability analysis)
+is to establish a connection between covariates and the time of an event.
+
+Survival analysis is a set of statistical models and methods used for estimating time until the occurrence of an event (or the probability that an event has not occurred). These methods are widely used in demography, e.g. for estimating lifespan or age at the first childbirth, in healthcare, e.g. for estimating the duration of staying in a hospital or survival time after the diagnosis of a disease, in engineering (for reliability analysis), in insurance, economics, and social sciences.
+
+Statistical methods need data, but complete data may not be available, i.e. the exact time of the event may be unknown for certain reasons  (the event did not occur before the end of the study or it is unknown whether it occurred). In this case, events are called censored. The data are censored from below (left-censored) when below a given value the exact values of observations are unknown. Right censored data (censored from above) does not have exact observations above a given value. Further in this paper, right censoring is considered.
+
+============
+Requirements
+============
+
+- Python 3.9 or later
+- joblib
+- pickle-mixin
+- numpy >= 1.22
+- numba >= 0.58.0
+- matplotlib >= 3.5.0
+- seaborn
+- graphviz >= 2.50.0
+- pandas >=0.25
+- scipy
+- python-dateutil
+- scikit-learn >= 1.0.2
+- lifelines >= 0.27.8
+- scikit-survival >= 0.17.2
+- openpyxl
+
+============
+Installation
+============
+
+The easiest way to install survivors is to use by running::
+
+  pip install survivors
+
+========
+Examples
+========
+
+The user guides at *doc* and *demonstration* directories provide in-depth information on the key concepts of survivors, an overview of available survival models,
+and hands-on examples in the form of `Jupyter notebooks <https://jupyter.org/>`_.
+
+There are examples of using the library with Jupyter Notebook.
+The example is a variant of solving a multi-stage problem:
+
+1. Import the library
+2. Loading and preparing data
+3. Model training
+4. Getting a forecast
+5. Visualization of the constructed model
+6. Visualization of true and predicted survival functions
+
+==========
+Scenarios
+==========
+
+The library allows to carry out the following work scenarios:
+
+1. Collecting data from patient medical histories (hospital tests, medications, treatment) from various medical institutions. The medical history can be presented as a set of tables in csv or xlsx format, or as a hierarchical structure of xml files.
+
+2. Building survival analysis models. There are available the following models: a decision tree, a bagging ensemble, and a boosting ensemble. For each model, there is a wide range of hyperparameters, which provide the flexibility of the model.
+
+3. Predicting the probability and time of the event. Forecasts can be used by the user to solve the problem of classifying or ranking new patients according to the expected severity of the disease.
+
+4. Predicting the individual survival functions and cumulative hazard of patients. Forecasts can be used to support medical decisions and adjust treatment.
+
+==============
+Communication
+==============
+
+- Mail: iuliivasilev@gmail.com
+- LinkedIn: https://www.linkedin.com/in/iulii-vasilev
+
+==========
+References
+==========
+
+Please cite the following paper if you are using **survivors**.
+
+    Vasilev I., Petrovskiy M., Mashechkin I. Survival Analysis Algorithms based on Decision Trees with Weighted Log-rank Criteria. - 2022.
+
+    Vasilev, Iulii, Mikhail Petrovskiy, and Igor Mashechkin. "Sensitivity of Survival Analysis Metrics." Mathematics 11.20 (2023): 4246.
+
+    Vasilev, Iulii, Mikhail Petrovskiy, and Igor Mashechkin. "Adaptive Sampling for Weighted Log-Rank Survival Trees Boosting." International Conference on Pattern Recognition Applications and Methods. Cham: Springer International Publishing, 2021.
+
+.. code::
+
+    @inproceedings{vasilev2022survival,
+        title={Survival Analysis Algorithms based on Decision Trees with Weighted Log-rank Criteria.},
+        author={Vasilev, Iulii and Petrovskiy, Mikhail and Mashechkin, Igor V},
+        booktitle={ICPRAM},
+        pages={132--140},
+        year={2022}
+    }
+
+    @inproceedings{vasilev2023adaptive,
+        title={Adaptive Sampling for Weighted Log-Rank Survival Trees Boosting},
+        author={Vasilev, Iulii and Petrovskiy, Mikhail and Mashechkin, Igor},
+        booktitle={Pattern Recognition Applications and Methods: 10th International Conference, ICPRAM 2021, and 11th International Conference, ICPRAM 2022, Virtual Event, February 4--6, 2021 and February 3--5, 2022, Revised Selected Papers},
+        pages={98--115},
+        year={2023},
+        organization={Springer}
+    }
+
+    @article{vasilev2023sensitivity,
+        title={Sensitivity of Survival Analysis Metrics},
+        author={Vasilev, Iulii and Petrovskiy, Mikhail and Mashechkin, Igor},
+        journal={Mathematics},
+        volume={11},
+        number={20},
+        pages={4246},
+        year={2023},
+        publisher={MDPI}
+    }
+
+.. _survival analysis: https://en.wikipedia.org/wiki/Survival_analysis
+
+.. |PyPi| image:: https://img.shields.io/pypi/v/survivors
+.. _PyPi: https://pypi.org/project/survivors/
+
+.. |License| image:: https://img.shields.io/badge/license-BSD%203--Clause-blue.svg
+  :target: https://github.com/iuliivasilev/dev-survivors/blob/master/LICENSE
+
+.. |DOI| image:: https://zenodo.org/badge/DOI/10.5281/zenodo.10649986.svg
+.. _DOI: https://zenodo.org/doi/10.5281/zenodo.10649777
---
+++ packages/survivors/survivors-1.6.2.tar.gz/survivors-1.6.2/survivors/criteria.py
@@ -0,0 +1,252 @@
+import numpy as np
+
+from scipy import stats
+# from lifelines import CoxPHFitter
+# from lifelines.statistics import logrank_test
+# import fastlogranktest as flr
+from numba import njit, jit
+
+# Interface functions
+def logrank(durations_A, durations_B, event_observed_A=None, event_observed_B=None):
+    return weight_lr_fast(durations_A, durations_B, event_observed_A, event_observed_B)
+
+
+def wilcoxon(durations_A, durations_B, event_observed_A=None, event_observed_B=None):
+    return weight_lr_fast(durations_A, durations_B, event_observed_A, event_observed_B, weightings="wilcoxon")
+
+
+def peto(durations_A, durations_B, event_observed_A=None, event_observed_B=None):
+    return weight_lr_fast(durations_A, durations_B, event_observed_A, event_observed_B, weightings="peto")
+
+
+def tarone_ware(durations_A, durations_B, event_observed_A=None, event_observed_B=None):
+    return weight_lr_fast(durations_A, durations_B, event_observed_A, event_observed_B, weightings="tarone-ware")
+
+
+CRITERIA_DICT = {
+    "logrank": logrank,
+    "wilcoxon": wilcoxon,
+    "peto": peto,
+    "tarone-ware": tarone_ware
+}
+""" dict: Available criteria in library and its realization """
+
+
+def logrank_self(durations_A, durations_B, event_observed_A=None, event_observed_B=None) -> float:
+    # 27.1 ms ± 1.26 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+    try:
+        event_times_A, event_times_B = (np.array(durations_A), np.array(durations_B))
+        time = np.r_[event_times_A, event_times_B]
+        group_indicator = np.r_[np.zeros(event_times_A.shape[0], dtype=int), np.ones(event_times_B.shape[0], dtype=int)]
+        event = np.r_[event_observed_A, event_observed_B]
+        n_samples = time.shape[0]
+        groups, group_counts = np.unique(group_indicator, return_counts=True)
+        n_groups = groups.shape[0]
+        if n_groups == 1:
+            raise ValueError("At least two groups must be specified, "
+                             "but only one was provided.")
+
+        # sort descending
+        o = np.argsort(-time, kind="mergesort")
+        x = group_indicator[o]
+        event = event[o]
+        time = time[o]
+
+        at_risk = np.zeros(n_groups, dtype=np.int_)
+        observed = np.zeros(n_groups, dtype=np.int_)
+        expected = np.zeros(n_groups, dtype=np.float_)
+        covar = np.zeros((n_groups, n_groups), dtype=np.float_)
+        k = 0
+        while k < n_samples:
+            ti = time[k]
+            total_events = 0
+            while k < n_samples and ti == time[k]:
+                idx = np.searchsorted(groups, x[k])
+                if event[k]:
+                    observed[idx] += 1
+                    total_events += 1
+                at_risk[idx] += 1
+                k += 1
+
+            if total_events != 0:
+                total_at_risk = k
+                expected += at_risk * (total_events / total_at_risk)
+                if total_at_risk > 1:
+                    multiplier = total_events * (total_at_risk - total_events) / (total_at_risk * (total_at_risk - 1))
+                    for g1 in range(n_groups):
+                        temp = at_risk[g1] * multiplier
+                        covar[g1, g1] += temp
+                        for g2 in range(n_groups):
+                            covar[g1, g2] -= temp * at_risk[g2] / total_at_risk
+
+        df = n_groups - 1
+        zz = observed[:df] - expected[:df]
+        chisq = np.linalg.solve(covar[:df, :df], zz).dot(zz)
+        pval = stats.chi2.sf(chisq, df)
+    except:
+        pval = 1.0
+    return pval
+    
+
+# def cox(durations_A, durations_B, event_observed_A=None, event_observed_B=None) -> float:
+#     dfA = pd.DataFrame({'E': event_observed_A, 'T': durations_A, 'group': 0})
+#     dfB = pd.DataFrame({'E': event_observed_B, 'T': durations_B, 'group': 1})
+#     df_ = pd.concat([dfA, dfB])
+#
+#     cph = CoxPHFitter().fit(df_, 'T', 'E')
+#     return cph.log_likelihood_ratio_test().p_value
+
+
+# 06/02/2022 Numpy+Numba
+# accelerate 2.8 according to library criteria
+@njit
+def iterate_coeffs_t_j(dur_A, dur_B, cens_A, cens_B, t_j, weightings):
+    N_1_j = (dur_A >= t_j).sum()
+    N_2_j = (dur_B >= t_j).sum()
+    if N_1_j == 0 or N_2_j == 0:
+        return 0, 0, 0
+    O_1_j = ((dur_A == t_j) * cens_A).sum()  # np.where(dur_A == t_j, cens_A,0).sum()
+    O_2_j = ((dur_B == t_j) * cens_B).sum()  # np.where(dur_B == t_j, cens_B,0).sum()
+    
+    N_j = N_1_j + N_2_j
+    O_j = O_1_j + O_2_j
+    E_1_j = N_1_j*O_j/N_j
+    w_j = 1
+    if weightings == "wilcoxon":
+        w_j = N_j
+    elif weightings == "tarone-ware":
+        w_j = np.sqrt(N_j)
+    elif weightings == "peto":
+        w_j = (1.0 - float(O_j)/(N_j+1))
+    
+    num = O_1_j - E_1_j
+    denom = E_1_j*(N_j - O_j) * N_2_j/(N_j*(N_j - 1))
+    return w_j, num, denom
+
+
+@njit
+def iterate_lr_statistic(dur_A, dur_B, cens_A, cens_B, times, weightings) -> float:
+    res = np.zeros((times.shape[0], 3), dtype=np.float32)
+    for j, t_j in enumerate(times):
+        res[j] = iterate_coeffs_t_j(dur_A, dur_B, cens_A, cens_B, t_j, weightings)
+
+    if weightings == "peto":
+        res[:, 0] = np.cumprod(res[:, 0])
+    # logrank = np.dot(res[:, 0], res[:, 1])**2 / np.dot(res[:, 0]*res[:, 0], res[:, 2])
+    logrank = np.power((res[:, 0]*res[:, 1]).sum(), 2) / ((res[:, 0]*res[:, 0]*res[:, 2]).sum())
+    return logrank
+
+
+def iterate_weight_lr_fast(dur_A, dur_B, cens_A=None, cens_B=None, weightings="") -> float:
+    try:
+        if cens_A is None:
+            cens_A = np.ones(dur_A.shape[0])
+        if cens_B is None:
+            cens_B = np.ones(dur_B.shape[0])
+
+        #     a1 = np.unique(dur_A)
+        #     a2 = np.unique(dur_B)
+        #     times = np.unique(np.clip(np.union1d(a1,a2), 0, np.min([a1.max(), a2.max()])))
+        times = np.union1d(np.unique(dur_A), np.unique(dur_B))
+        logrank = lr_statistic(dur_A, dur_B, cens_A, cens_B, times, weightings)
+        pvalue = stats.chi2.sf(logrank, df=1)
+        return pvalue
+    except:
+        return 1.0
+
+
+# 11/08/2022 Numpy+Numba
+# dur_A = np.random.choice(10000, 10000)
+# cens_A = np.random.choice(2, 10000)
+# dur_B = np.random.choice(10000, 10000)
+# cens_B = np.random.choice(2, 10000)
+# new version (for 10000 elem): 2.16 ms ± 15.2 µs per loop
+# old version (for 10000 elem): 103 ms ± 171 µs per loop
+
+
+# @cuda.jit
+@njit('f8(f8[:], f8[:], i8[:], i8[:], i8)', cache=True)
+def lr_statistic(dur_1, dur_2, cens_1, cens_2, weightings):
+    times = np.unique(np.hstack((dur_1, dur_2)))
+    dur_1 = np.searchsorted(times, dur_1) + 1
+    dur_2 = np.searchsorted(times, dur_2) + 1
+    times_range = np.array([1, times.shape[0]], dtype=np.int32)
+
+    bins = times_range[1] - times_range[0] + 1
+    n_1_j = np.histogram(dur_1, bins=bins, range=times_range)[0]
+    n_2_j = np.histogram(dur_2, bins=bins, range=times_range)[0]
+    O_1_j = np.histogram(dur_1 * cens_1, bins=bins, range=times_range)[0]
+    O_2_j = np.histogram(dur_2 * cens_2, bins=bins, range=times_range)[0]
+
+    N_1_j = np.cumsum(n_1_j[::-1])[::-1]
+    N_2_j = np.cumsum(n_2_j[::-1])[::-1]
+    ind = np.where(N_1_j * N_2_j != 0)
+    N_1_j = N_1_j[ind]
+    N_2_j = N_2_j[ind]
+    O_1_j = O_1_j[ind]
+    O_2_j = O_2_j[ind]
+
+    N_j = N_1_j + N_2_j
+    O_j = O_1_j + O_2_j
+    E_1_j = N_1_j * O_j / N_j
+    res = np.zeros((N_j.shape[0], 3), dtype=np.float32)
+    res[:, 1] = O_1_j - E_1_j
+    res[:, 2] = E_1_j * (N_j - O_j) * N_2_j / (N_j * (N_j - 1))
+    res[:, 0] = 1.0
+    # if np.any(N_j <= 1):
+    #     return 0.0
+    if weightings == 2:
+        res[:, 0] = N_j
+    elif weightings == 3:
+        res[:, 0] = np.sqrt(N_j)
+    elif weightings == 4:
+        res[:, 0] = np.cumprod((1.0 - O_j / (N_j + 1)))
+    logrank = np.power((res[:, 0] * res[:, 1]).sum(), 2) / ((res[:, 0] * res[:, 0] * res[:, 2]).sum())
+    return logrank
+
+
+def weight_lr_fast(dur_A, dur_B, cens_A=None, cens_B=None, weightings=""):
+    """
+    Count weighted log-rank criteria
+
+    Parameters
+    ----------
+    dur_A : array-like
+        Time of occurred events from first sample.
+    dur_B : array-like
+        Time of occurred events from second sample.
+    cens_A : array-like, optional
+        Indicate of occurred events from first sample.
+        The default is None (all events occurred).
+    cens_B : array-like, optional
+        Indicate of occurred events from second sample.
+        The default is None (all events occurred).
+    weightings : str, optional
+        Weights of criteria. The default is "" (log-rank).
+        Log-rank :math:'w = 1'
+        Wilcoxon :math:'w = N_j'
+        Tarone-ware :math:'w = \\sqrt(N_j)'
+        Peto-peto :math:'w = \\fraq{1 - O_j}{N_j + 1}'
+
+    Returns
+    -------
+    logrank : float
+        Chi2 statistic value of weighted log-rank test
+    # p-value : float
+    #     Chi2 p-value of weighted log-rank test
+    """
+    try:
+        if cens_A is None:
+            cens_A = np.ones(dur_A.shape[0])
+        if cens_B is None:
+            cens_B = np.ones(dur_B.shape[0])
+        d = {"logrank": 1, "wilcoxon": 2, "tarone-ware": 3, "peto": 4}
+        weightings = d.get(weightings, 1)
+        logrank = lr_statistic(dur_A.astype("float64"),
+                               dur_B.astype("float64"),
+                               cens_A.astype("int64"),
+                               cens_B.astype("int64"),
+                               np.int64(weightings))
+        return logrank
+    except Exception as err:
+        return 0.0
