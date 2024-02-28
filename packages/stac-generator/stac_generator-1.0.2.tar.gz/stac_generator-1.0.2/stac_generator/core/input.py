87754381f9ea537d97292cc283ed7ec083947889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 16:04:16 +0000

    Add package stac-generator/1.0.2/stac_generator-1.0.2.tar.gz

---
+++ packages/stac-generator/stac_generator-1.0.2.tar.gz/stac_generator-1.0.2/stac_generator/core/input.py
@@ -0,0 +1,54 @@
+# encoding: utf-8
+"""
+
+"""
+__author__ = "Richard Smith"
+__date__ = "02 Jun 2021"
+__copyright__ = "Copyright 2018 United Kingdom Research and Innovation"
+__license__ = "BSD - see LICENSE file in top-level package directory"
+__contact__ = "richard.d.smith@stfc.ac.uk"
+
+from abc import ABC, abstractmethod
+
+from stac_generator.core.generator import BaseGenerator
+from stac_generator.core.utils import load_plugins
+
+
+class BaseInput(ABC):
+    """
+    Base class to define an input
+    """
+
+    def __init__(self, **kwargs):
+        self.filters = None
+        if kwargs.get("filters"):
+            self.filters = load_plugins(kwargs["filters"], "stac_generator.filters")
+
+    def should_process(self, uri) -> bool:
+        """
+        Should the path be sent for processing?
+
+        Will run through any filter plugins. All plugins must pass for a True
+        response. Any False will short circuit the logic and return False
+
+        :param uri: uri to test
+
+        :return: Bool, ``default: True``
+        """
+        if self.filters:
+            return any((filter.run(uri) for filter in self.filters))
+
+        return True
+
+    def start(self, generator: BaseGenerator):
+        """
+        Start the input plugin.
+        """
+        self.run(generator)
+        generator.finished()
+
+    @abstractmethod
+    def run(self, generator: BaseGenerator):
+        """
+        Run the input plugin.
+        """
---
+++ packages/stac-generator/stac_generator-1.0.2.tar.gz/stac_generator-1.0.2/stac_generator/plugins/inputs/text_file.py
@@ -0,0 +1,82 @@
+"""
+Text File
+---------
+
+Takes file or directory path, uses the dictionary
+in the file(s) to pass into the extractor.
+
+**Plugin name:** ``text_file``
+
+.. list-table::
+    :header-rows: 1
+
+    * - Option
+      - Value Type
+      - Description
+    * - ``filepath``
+      - ``string``
+      - ``REQUIRED`` the path to input file(s)
+
+Example Configuration:
+    .. code-block:: yaml
+
+        inputs:
+            - method: text_file
+              filepath: input_file(s)_location
+
+"""
+
+import json
+import traceback
+from datetime import datetime
+from os import listdir
+from os.path import isdir, isfile, join
+
+from stac_generator.core.generator import BaseGenerator
+from stac_generator.core.input import BaseInput
+
+
+class TextFileInput(BaseInput):
+    """
+    Use external file(s) as input to enter data to pass to
+    the processor.
+    """
+
+    def __init__(self, **kwargs):
+        self.filepath = kwargs["filepath"]
+
+        if isdir(self.filepath):
+            self.file_list = [
+                join(self.filepath, file)
+                for file in listdir(self.filepath)
+                if isfile(join(self.filepath, file))
+            ]
+        else:
+            self.file_list = [self.filepath]
+
+    def run(self, generator: BaseGenerator):
+        start = datetime.now()
+        total_generated = 0
+        unique_lines = set()
+
+        errors_file = "errors.txt"
+        failed_file = "failed.txt"
+
+        for file in self.file_list:
+            with open(file, "r", encoding="utf-8") as f, open(
+                errors_file, "w+", encoding="utf-8"
+            ) as errors, open(failed_file, "w+", encoding="utf-8") as failed:
+                for line in f:
+                    if line not in unique_lines:
+                        total_generated += 1
+                        unique_lines.add(line)
+                        data = json.loads(line)
+                        try:
+                            generator.process(**data)
+                        except Exception:
+                            errors.write(line)
+                            errors.write(traceback.format_exc())
+                            failed.write(line)
+
+        end = datetime.now()
+        print(f"Processed {total_generated} elasticsearch records in {end-start}")
