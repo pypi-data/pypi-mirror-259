509fa87e7ea8679217aedac958e4fca8405feaba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:04:17 +0000

    Add package lsdb/0.1.3/lsdb-0.1.3-py3-none-any.whl

---
+++ packages/lsdb/lsdb-0.1.3-py3-none-any.whl/lsdb/catalog/dataset/healpix_dataset.py
@@ -0,0 +1,108 @@
+from typing import List
+
+import dask.dataframe as dd
+import numpy as np
+from hipscat.catalog.healpix_dataset.healpix_dataset import HealpixDataset as HCHealpixDataset
+from hipscat.pixel_math import HealpixPixel
+from hipscat.pixel_math.healpix_pixel_function import get_pixel_argsort
+from typing_extensions import Self
+
+from lsdb.catalog.dataset.dataset import Dataset
+from lsdb.types import DaskDFPixelMap
+
+
+class HealpixDataset(Dataset):
+    """LSDB Catalog DataFrame to perform analysis of sky catalogs and efficient
+    spatial operations.
+
+    Attributes:
+        hc_structure: `hipscat.Dataset` object representing the structure
+                      and metadata of the HiPSCat catalog
+    """
+
+    hc_structure: HCHealpixDataset
+
+    def __init__(
+        self,
+        ddf: dd.DataFrame,
+        ddf_pixel_map: DaskDFPixelMap,
+        hc_structure: HCHealpixDataset,
+    ):
+        """Initialise a Catalog object.
+
+        Not to be used to load a catalog directly, use one of the `lsdb.from_...` or
+        `lsdb.load_...` methods
+
+        Args:
+            ddf: Dask DataFrame with the source data of the catalog
+            ddf_pixel_map: Dictionary mapping HEALPix order and pixel to partition index of ddf
+            hc_structure: `hipscat.Catalog` object with hipscat metadata of the catalog
+        """
+        super().__init__(ddf, hc_structure)
+        self._ddf_pixel_map = ddf_pixel_map
+
+    def get_healpix_pixels(self) -> List[HealpixPixel]:
+        """Get all HEALPix pixels that are contained in the catalog
+
+        Returns:
+            List of all Healpix pixels in the catalog
+        """
+        return self.hc_structure.get_healpix_pixels()
+
+    def get_ordered_healpix_pixels(self) -> List[HealpixPixel]:
+        """Get all HEALPix pixels that are contained in the catalog,
+        ordered by breadth-first nested ordering.
+
+        Returns:
+            List of all Healpix pixels in the catalog
+        """
+        pixels = self.get_healpix_pixels()
+        return np.array(pixels)[get_pixel_argsort(pixels)]
+
+    def get_partition(self, order: int, pixel: int) -> dd.DataFrame:
+        """Get the dask partition for a given HEALPix pixel
+
+        Args:
+            order: Order of HEALPix pixel
+            pixel: HEALPix pixel number in NESTED ordering scheme
+        Returns:
+            Dask Dataframe with a single partition with data at that pixel
+        Raises:
+            Value error if no data exists for the specified pixel
+        """
+        partition_index = self.get_partition_index(order, pixel)
+        return self._ddf.partitions[partition_index]
+
+    def get_partition_index(self, order: int, pixel: int) -> int:
+        """Get the dask partition for a given HEALPix pixel
+
+        Args:
+            order: Order of HEALPix pixel
+            pixel: HEALPix pixel number in NESTED ordering scheme
+        Returns:
+            Dask Dataframe with a single partition with data at that pixel
+        Raises:
+            Value error if no data exists for the specified pixel
+        """
+        hp_pixel = HealpixPixel(order, pixel)
+        if not hp_pixel in self._ddf_pixel_map:
+            raise ValueError(f"Pixel at order {order} pixel {pixel} not in Catalog")
+        partition_index = self._ddf_pixel_map[hp_pixel]
+        return partition_index
+
+    def query(self, expr: str) -> Self:
+        """Filters catalog using a complex query expression
+
+        Args:
+            expr (str): Query expression to evaluate. The column names that are not valid Python
+                variables names should be wrapped in backticks, and any variable values can be
+                injected using f-strings. The use of '@' to reference variables is not supported.
+                More information about pandas query strings is available
+                `here <https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.query.html>`__.
+
+        Returns:
+            A catalog that contains the data from the original catalog that complies
+            with the query expression
+        """
+        ddf = self._ddf.query(expr)
+        return self.__class__(ddf, self._ddf_pixel_map, self.hc_structure)
