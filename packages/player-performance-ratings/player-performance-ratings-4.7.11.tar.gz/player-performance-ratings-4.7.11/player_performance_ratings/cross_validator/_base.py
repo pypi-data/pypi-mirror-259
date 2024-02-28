307243135277d57c136057997821ad8c3786f9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 06:03:50 +0000

    Add package player-performance-ratings/4.7.11/player-performance-ratings-4.7.11.tar.gz

---
+++ packages/player-performance-ratings/player-performance-ratings-4.7.11.tar.gz/player-performance-ratings-4.7.11/player_performance_ratings/cross_validator/_base.py
@@ -0,0 +1,36 @@
+from abc import abstractmethod, ABC
+from typing import Optional
+
+import pandas as pd
+
+from player_performance_ratings.predictor._base import BasePredictor
+from player_performance_ratings.scorer.score import BaseScorer
+from player_performance_ratings.transformation.base_transformer import BaseTransformer
+
+
+
+class CrossValidator(ABC):
+
+    def __init__(self, scorer: Optional[BaseScorer]):
+        self.scorer = scorer
+
+
+    @abstractmethod
+    def generate_validation_df(self,
+                               df: pd.DataFrame,
+                               post_transformers: list[BaseTransformer],
+                               predictor: BasePredictor,
+                               estimator_features: list[str],
+                               keep_features: bool,
+                               add_train_prediction: bool = False
+                               ) -> pd.DataFrame:
+        pass
+
+
+    def cross_validation_score(self, validation_df: pd.DataFrame, scorer: Optional[BaseScorer] = None ) -> float:
+        if not scorer and not self.scorer:
+            raise ValueError("scorer is not defined. Either pass into constructor or as argument to method")
+
+        scorer = scorer or self.scorer
+        return scorer.score(df=validation_df)
+
