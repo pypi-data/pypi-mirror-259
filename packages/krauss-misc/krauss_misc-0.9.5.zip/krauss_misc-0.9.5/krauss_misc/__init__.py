5814a993e941aab119516b5787eaef6a5db57726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 05:03:26 +0000

    Add package krauss-misc/0.9.5/krauss_misc-0.9.5.zip

---
+++ packages/krauss-misc/krauss_misc-0.9.5.zip/krauss_misc-0.9.5/krauss_misc/__init__.py
@@ -0,0 +1,4 @@
+from krauss_misc import basic_file_ops, txt_mixin, rwkos, \
+    tkinter_utils, relpath, string_cleaner
+version = "0.9.5"
+
---
+++ packages/krauss-misc/krauss_misc-0.9.5.zip/krauss_misc-0.9.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
+    name='krauss_misc',    # This is the name of your PyPI-package.
+    version='0.9.5',
+    url='https://github.com/ryanGT/krauss_misc',
+    author='Ryan Krauss',
+    author_email='ryanwkrauss@gmail.com',
+    description="package of misc code that supports other projects of mine",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    #packages=['basic_file_ops','rwkos','txt_mixin'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
