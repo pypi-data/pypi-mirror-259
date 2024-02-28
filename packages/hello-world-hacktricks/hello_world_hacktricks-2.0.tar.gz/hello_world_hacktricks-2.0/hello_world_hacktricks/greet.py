76174ddc2846505c548bf8e7f8f6b7fcc2b3b947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 05:04:34 +0000

    Add package hello-world-hacktricks/2.0/hello_world_hacktricks-2.0.tar.gz

---
+++ packages/hello-world-hacktricks/hello_world_hacktricks-2.0.tar.gz/hello_world_hacktricks-2.0/hello_world_hacktricks/greet.py
@@ -0,0 +1,4 @@
+# hello_world_hacktricks/greet.py
+def say_hello():
+    print("Hello from PyPi!")
+    return "Hello, World!"
---
+++ packages/hello-world-hacktricks/hello_world_hacktricks-2.0.tar.gz/hello_world_hacktricks-2.0/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='hello_world_hacktricks',
+    version='2.0',
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies your library needs
+    ],
+)
