36a51866d8b57dd5a8c183beeea2086aa67ba830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 06:04:05 +0000

    Add package azcam-lbtguiders/24.1.1/azcam_lbtguiders-24.1.1-py3-none-any.whl

---
+++ packages/azcam-lbtguiders/azcam_lbtguiders-24.1.1-py3-none-any.whl/azcam_lbtguiders/console.py
@@ -0,0 +1,80 @@
+"""
+Setup method for lbtguiders azcamconsole.
+Usage example:
+  python -i -m azcam_lbtguiders.console
+"""
+
+import os
+import sys
+import threading
+
+import azcam
+import azcam_console
+import azcam_console.shortcuts
+from azcam_server.tools.ds9display import Ds9Display
+
+
+def setup():
+    # command line args
+    try:
+        i = sys.argv.index("-datafolder")
+        datafolder = sys.argv[i + 1]
+    except ValueError:
+        datafolder = None
+    try:
+        i = sys.argv.index("-lab")
+        lab = 1
+    except ValueError:
+        lab = 0
+
+    # files and folders
+    azcam.db.systemname = "lbtguiders"
+
+    azcam.db.systemfolder = f"{os.path.dirname(__file__)}"
+
+    if datafolder is None:
+        droot = os.environ.get("AZCAM_DATAROOT")
+        if droot is None:
+            droot = "/data/lbtguiders"
+        azcam.db.datafolder = droot
+    else:
+        azcam.db.datafolder = datafolder
+    azcam.db.datafolder = azcam.utils.fix_path(azcam.db.datafolder)
+
+    parfile = os.path.join(
+        azcam.db.datafolder,
+        "parameters",
+        f"parameters_console_{azcam.db.systemname}.ini",
+    )
+
+    # start logging
+    logfile = os.path.join(azcam.db.datafolder, "logs", "console.log")
+    azcam.db.logger.start_logging(logfile=logfile)
+    azcam.log(f"Configuring console for {azcam.db.systemname}")
+
+    # display
+    display = Ds9Display()
+    dthread = threading.Thread(target=display.initialize, args=[])
+    dthread.start()  # thread just for speed
+
+    # console tools
+    from azcam_console.tools import create_console_tools
+
+    create_console_tools()
+
+    # try to connect to azcamserver
+    server = azcam.db.tools["server"]
+    connected = server.connect()
+    if connected:
+        azcam.log("Connected to azcamserver")
+    else:
+        azcam.log("Not connected to azcamserver")
+
+    # par file
+    azcam.db.parameters.read_parfile(parfile)
+    azcam.db.parameters.update_pars("azcamconsole")
+
+
+# start
+setup()
+from azcam.cli import *
