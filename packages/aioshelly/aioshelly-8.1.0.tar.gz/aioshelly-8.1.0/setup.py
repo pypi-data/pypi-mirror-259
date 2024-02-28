1b47024784d07fc7cd9ed6e735e2090bbf92e184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 06:04:04 +0000

    Add package aioshelly/8.1.0/aioshelly-8.1.0.tar.gz

---
+++ packages/aioshelly/aioshelly-8.1.0.tar.gz/aioshelly-8.1.0/setup.py
@@ -0,0 +1,38 @@
+"""Setup module for aioshelly."""
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+PROJECT_DIR = Path(__file__).parent.resolve()
+README_FILE = PROJECT_DIR / "README.md"
+VERSION = "8.1.0"
+
+
+setup(
+    name="aioshelly",
+    version=VERSION,
+    license="Apache License 2.0",
+    url="https://github.com/home-assistant-libs/aioshelly",
+    author="Paulus Schoutsen",
+    author_email="paulus@home-assistant.io",
+    description="Asynchronous library to control Shelly devices.",
+    long_description=README_FILE.read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    package_data={"aioshelly": ["py.typed"]},
+    zip_safe=True,
+    platforms="any",
+    install_requires=list(val.strip() for val in open("requirements.txt")),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
