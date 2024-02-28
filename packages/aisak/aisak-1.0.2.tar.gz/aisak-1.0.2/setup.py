b80f96d8551de1dd61914cae0c71aaafb831a400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:04:38 +0000

    Add package aisak/1.0.2/aisak-1.0.2.tar.gz

---
+++ packages/aisak/aisak-1.0.2.tar.gz/aisak-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aisak",
+    version="1.0.2",
+    author="Mandela Logan",
+    author_email="mandelakorilogan@gmail.com",
+    description="AISAK, short for Artificially Intelligent Swiss Army Knife, is a state-of-the-art language model designed for text generation tasks. Developed by Mandela Logan, this Large Language Model (LLM) is fine-tuned on extensive datasets to excel in understanding and interpreting your various queries in natural language text.",
+    packages=find_packages(),
+    install_requires=[
+        "accelerate",
+        "torch",
+		  "transformers",	
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
