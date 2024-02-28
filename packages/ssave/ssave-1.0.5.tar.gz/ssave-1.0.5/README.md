3524cae551bd1d8ac000aad7535bc1eb8a9c8036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:03:12 +0000

    Add package ssave/1.0.5/ssave-1.0.5.tar.gz

---
+++ packages/ssave/ssave-1.0.5.tar.gz/ssave-1.0.5/README.md
@@ -0,0 +1,7 @@
+# SCOS Save Utility for Python 3
+
+## What is this?
+
+This utility is used for enabling access to `.ini` files to get a save like experience.
+
+Using this is easy and fast, that even somebody who doesn't know Python really well can use this.
---
+++ packages/ssave/ssave-1.0.5.tar.gz/ssave-1.0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+def read_file(file):
+   with open(file) as f:
+        return f.read()
+    
+long_description = read_file("README.md")
+version = 'v1.0.5'
+
+setup(
+    name = 'ssave',
+    version = version,
+    author = 'SCOS-Apps',
+    author_email = 'hsc100zz@gmail.com',
+    url = 'https://github.com/SCOS-Apps/Save-Utility',
+    description = 'SCOS Save Utility for Python3',
+    long_description_content_type = "text/markdown",  # If this causes a warning, upgrade your setuptools package
+    long_description = long_description,
+)
