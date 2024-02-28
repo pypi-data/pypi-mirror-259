2b073535bb499332c42fa2489f2630ae0f551384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:04:15 +0000

    Add package tlogg/0.6.0a7/tlogg-0.6.0a7.tar.gz

---
+++ packages/tlogg/tlogg-0.6.0a7.tar.gz/tlogg-0.6.0a7/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+version = "0.6.0a7"
+name = "tlogg"
+
+print(f"Version: {version}")
+print(f"Name: {name}")
+
+setup(
+    name=name,
+    version=version,
+    author='Eugenio Parodi',
+    author_email='ceccopierangiolieugenio@googlemail.com',
+    description='A fast, advanced log explorer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ceccopierangiolieugenio/tlogg",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Topic :: Terminals",
+        "Topic :: Software Development :: User Interfaces"],
+    include_package_data=False,
+    packages=['tlogg','tlogg.app'],
+    python_requires=">=3.9",
+    install_requires=[
+        'pyTermTk>=0.39.0a6',
+        'appdirs',
+        'pygments',
+        'pyyaml'],
+    extras_require = {
+        'Clipboard Support':  ['pyperclip']
+    },
+    entry_points={
+        'console_scripts': [
+            'tlogg = tlogg:main',
+        ],
+    },
+)
