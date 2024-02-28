888d7ce43f8172ce68baa46418c4b4e67a25001c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 05:04:29 +0000

    Add package pytest-testmon/2.1.1/pytest_testmon-2.1.1-py3-none-any.whl

---
+++ packages/pytest-testmon/pytest_testmon-2.1.1-py3-none-any.whl/testmon/common.py
@@ -0,0 +1,104 @@
+import logging
+import os
+import re
+
+try:
+    import importlib.metadata
+
+    def get_system_packages_raw():
+        return (
+            (pkg.metadata["Name"], pkg.version)
+            for pkg in importlib.metadata.distributions()
+        )
+
+except ImportError:
+    import pkg_resources
+
+    def get_system_packages_raw():
+        return ((pkg.project_name, pkg.version) for pkg in pkg_resources.working_set)
+
+
+from pathlib import Path
+
+
+def dummy():
+    pass
+
+
+def get_logger(name):
+    formatter = logging.Formatter("%(levelname)s: %(message)s")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    tm_logger = logging.getLogger(name)
+    tm_logger.setLevel(logging.INFO)
+    tm_logger.addHandler(handler)
+    return tm_logger
+
+
+logger = get_logger(__name__)
+
+
+def get_system_packages(ignore=None):
+    if not ignore:
+        ignore = set(("pytest-testmon", "pytest-testmon"))
+    return ", ".join(
+        sorted(
+            {
+                f"{package} {version}"
+                for (package, version) in get_system_packages_raw()
+                if not package in ignore
+            }
+        )
+    )
+
+
+def drop_patch_version(system_packages):
+    return re.sub(
+        r"\b([\w_-]+\s\d+\.\d+)\.\w+\b",
+        r"\1",
+        system_packages,
+    )
+
+
+def git_path(start_path=None):
+    start_path = Path(start_path or os.getcwd()).resolve()
+    current_path = start_path
+    while current_path != current_path.parent:
+        path = current_path / ".git"
+        if path.exists() and path.is_dir():
+            return str(path)
+        current_path = current_path.parent
+    return None
+
+
+def git_current_branch(path=None):
+    path = git_path(path)
+    if not path:
+        return None
+    git_head_file = os.path.join(path, "HEAD")
+    try:
+        with open(git_head_file, "r", encoding="utf8") as head_file:
+            head_content = head_file.read().strip()
+        if head_content.startswith("ref:"):
+            return head_content.split("/")[-1]
+    except FileNotFoundError:
+        pass
+    return None
+
+
+def git_current_head(path=None):
+    path = git_path(path)
+    if not path:
+        return None
+    current_branch = git_current_branch(path)
+    if not current_branch:
+        return None
+    git_branch_file = os.path.join(path, "refs", "heads", current_branch)
+    try:
+        with open(git_branch_file, "r", encoding="utf8") as branch_file:
+            head_sha = branch_file.read().strip()
+        return head_sha
+    except FileNotFoundError:
+        pass
+    return None
