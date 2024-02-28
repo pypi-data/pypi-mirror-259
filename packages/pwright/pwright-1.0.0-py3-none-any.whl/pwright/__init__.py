ae2dfdb476f9bad4ece6a2fe52b50968e3aa7815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:04:33 +0000

    Add package pwright/1.0.0/pwright-1.0.0-py3-none-any.whl

---
+++ packages/pwright/pwright-1.0.0-py3-none-any.whl/pwright/__init__.py
@@ -0,0 +1,20 @@
+from . import apw
+from . import pw
+from .apw import pw_browser as apw_browser
+from .apw import pw_context as apw_context
+from .apw import pw_page as apw_page
+from .pw import pw_browser
+from .pw import pw_context
+from .pw import pw_page
+
+
+__all__ = [
+    'apw',
+    'pw',
+    'apw_browser',
+    'apw_context',
+    'apw_page',
+    'pw_browser',
+    'pw_context',
+    'pw_page',
+]
