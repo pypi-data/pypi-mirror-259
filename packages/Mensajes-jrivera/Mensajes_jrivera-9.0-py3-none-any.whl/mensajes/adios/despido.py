d1c6445cee0eb57fb87adef5e1f13f877a1c4543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 15:04:02 +0000

    Add package Mensajes-jrivera/9.0/Mensajes_jrivera-9.0-py3-none-any.whl

---
+++ packages/Mensajes-jrivera/Mensajes_jrivera-9.0-py3-none-any.whl/mensajes/adios/despido.py
@@ -0,0 +1,6 @@
+def despedir():
+    print('Me despido desde la funcion mensajes.adios.despido.despedir()')
+
+class Despido():
+    def __init__(self):
+        print('Me despido  desde la clase mensajes.adios.despido.Despido()')
---
+++ packages/Mensajes-jrivera/Mensajes_jrivera-9.0-py3-none-any.whl/test/test_hola.py
@@ -0,0 +1,15 @@
+import unittest
+import numpy as np
+from mensajes.hola.saludos import *
+
+class PruebasHola(unittest.TestCase):
+
+    def test_generar_array(self):
+        np.testing.assert_array_equal(np.array([0, 1, 2, 3, 4, 5]),generar_array(6))
+        
+
+#from mensajes.adios.despido import *
+#saludar()
+#Saludo()
+#despedir()
+#Despido()
