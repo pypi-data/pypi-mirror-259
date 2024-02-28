03b87a73cb4cfee271c99deb34e9fab02c61bf5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:04:04 +0000

    Add package plone.app.iterate/5.0.5/plone.app.iterate-5.0.5-py3-none-any.whl

---
+++ packages/plone.app.iterate/plone.app.iterate-5.0.5-py3-none-any.whl/plone/app/iterate/setuphandlers.py
@@ -0,0 +1,29 @@
+from plone.base.interfaces.installable import INonInstallable
+from zope.interface import implementer
+
+import warnings
+
+
+@implementer(INonInstallable)
+class HiddenProfiles:
+    def getNonInstallableProfiles(self):
+        """Prevents uninstall profile from showing up in the profile list
+        when creating a Plone site.
+
+        """
+        return [
+            "plone.app.iterate:uninstall",
+            "plone.app.iterate:plone.app.iterate",
+            "plone.app.iterate:to1000",
+        ]
+
+
+def deprecate_profile(tool):
+    """Deprecation profile plone.app.iterate.
+
+    Deprecation warning for plone.app.iterate:plone.app.iterate profile.
+    """
+    warnings.warn(
+        'The profile with id "plone.app.iterate" was renamed to "default".',
+        DeprecationWarning,
+    )
