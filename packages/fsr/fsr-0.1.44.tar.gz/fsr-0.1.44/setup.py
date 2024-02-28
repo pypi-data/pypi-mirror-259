3809d41e22a107bf0dbe9fd21d44819a6c475257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 04:04:38 +0000

    Add package fsr/0.1.44/fsr-0.1.44.tar.gz

---
+++ packages/fsr/fsr-0.1.44.tar.gz/fsr-0.1.44/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fsr',
+    version='0.1.44',
+    author='Roman Pluzhnikov',
+    author_email='pluzhnikov137@gmail.com',
+    description='CLI tool for generating and serving test reports',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rpluzhnikov/fsr',
+    package_data={
+        'reporter': ['report_web/*', 'report_web/*/*'], 
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'fsr=reporter.main:main', 
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
