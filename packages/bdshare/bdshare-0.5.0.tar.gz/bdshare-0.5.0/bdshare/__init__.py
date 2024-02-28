cb5bd042ca73ab361f9e0ff83087cd8c6aaea1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:03:50 +0000

    Add package bdshare/0.5.0/bdshare-0.5.0.tar.gz

---
+++ packages/bdshare/bdshare-0.5.0.tar.gz/bdshare-0.5.0/bdshare/__init__.py
@@ -0,0 +1,20 @@
+from ._version import __version__
+
+"""
+for trading data
+"""
+from bdshare.stock.trading import (get_current_trade_data, get_dsex_data, get_current_trading_code,
+                                    get_hist_data, get_basic_hist_data,
+                                    get_close_price_data, get_last_trade_price_data, get_cse_current_trade_data)
+
+
+"""
+for trading news
+"""
+from bdshare.stock.news import (get_agm_news, get_all_news)
+
+
+"""
+for market data
+"""
+from bdshare.stock.market import (get_market_inf, get_latest_pe, get_market_inf_more_data, get_market_depth_data)
