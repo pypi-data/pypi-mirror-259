342b92c8924d3c455e2c6e15e828c137a182289f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 06:03:59 +0000

    Add package deksecrets/0.1.0/deksecrets-0.1.0-py3-none-any.whl

---
+++ packages/deksecrets/deksecrets-0.1.0-py3-none-any.whl/deksecrets/click/infisical.py
@@ -0,0 +1,19 @@
+import sys
+import typer
+from getpass import getpass
+from dektools.file import write_file
+from ..tools.infisical import fetch_secrets
+
+app = typer.Typer(add_completion=False)
+
+
+@app.command()
+def env(site_url, client_id, project, environment, path=None, out=None):
+    client_secret = getpass('Please input the ClientSecret:')
+    data = fetch_secrets(site_url, client_id, client_secret, project, environment, path)
+    s = "\n".join(f"{k}={v}" for k, v in data.items())
+    if out:
+        write_file(out, s=s)
+    else:
+        out = write_file('.env', s=s, t=True)
+        sys.stdout.write(out)
