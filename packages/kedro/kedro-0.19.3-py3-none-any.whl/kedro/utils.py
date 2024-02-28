6506f52778e458e512ef9014e56d163b770b1e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-28 03:03:55 +0000

    Add package kedro/0.19.3/kedro-0.19.3-py3-none-any.whl

---
+++ packages/kedro/kedro-0.19.3-py3-none-any.whl/kedro/utils.py
@@ -0,0 +1,31 @@
+"""This module provides a set of helper functions being used across different components
+of kedro package.
+"""
+import importlib
+import os
+from typing import Any
+
+
+def load_obj(obj_path: str, default_obj_path: str = "") -> Any:
+    """Extract an object from a given path.
+
+    Args:
+        obj_path: Path to an object to be extracted, including the object name.
+        default_obj_path: Default object path.
+
+    Returns:
+        Extracted object.
+
+    Raises:
+        AttributeError: When the object does not have the given named attribute.
+
+    """
+    obj_path_list = obj_path.rsplit(".", 1)
+    obj_path = obj_path_list.pop(0) if len(obj_path_list) > 1 else default_obj_path
+    obj_name = obj_path_list[0]
+    module_obj = importlib.import_module(obj_path)
+    return getattr(module_obj, obj_name)
+
+
+def _is_databricks() -> bool:
+    return "DATABRICKS_RUNTIME_VERSION" in os.environ
