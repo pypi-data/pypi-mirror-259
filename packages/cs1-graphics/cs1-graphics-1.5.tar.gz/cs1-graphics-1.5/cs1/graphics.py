773c8ccf68f2c889f048695fb3be0e97d03849cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 06:04:40 +0000

    Add package cs1-graphics/1.5/cs1-graphics-1.5.tar.gz

---
+++ packages/cs1-graphics/cs1-graphics-1.5.tar.gz/cs1-graphics-1.5/cs1/graphics.py
@@ -0,0 +1,112 @@
+import turtle
+
+h=0
+
+
+def open_canvas(width, height):
+    turtle.setup(width=width, height=height)
+    h = height
+
+
+def set_background_color_rgb(r, g, b):
+    turtle.bgcolor((int(r / 255), int(g / 255), int(b / 255)))
+
+
+def set_color_rgb(r, g, b):
+    turtle.colormode(255)
+    turtle.color((r, g, b))
+
+
+def set_color(color):
+    turtle.color(color)
+
+
+def set_background_color(color):
+    turtle.bgcolor(color)
+
+
+def set_line_thickness(thickness):
+    turtle.width(thickness)
+
+
+def draw_filled_circle(centerx, centery, radius):
+    turtle.penup()
+    turtle.setpos(centerx, h - (centery - radius))
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()
+
+
+def draw_filled_rect(x, y, width, height):
+    turtle.penup()
+    turtle.setpos(x, h - y)
+    turtle.pendown()
+    turtle.begin_fill()
+    for _ in range(2):
+        turtle.forward(width)
+        turtle.right(90)
+        turtle.forward(height)
+        turtle.right(90)
+    turtle.end_fill()
+
+
+def draw_filled_polygon(x1, y1, x2, y2, x3, y3, x4, y4):
+    turtle.penup()
+    turtle.setpos(x1, h - y1)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.goto(x2, h - y2)
+    turtle.goto(x3, h - y3)
+    turtle.goto(x4, h - y4)
+    turtle.goto(x1, h - y1)
+    turtle.end_fill()
+
+
+def draw_oval(x, y, width, height):
+    turtle.penup()
+    turtle.setpos(x, h - (y - height))
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.left(45)
+    for _ in range(2):
+        turtle.circle(width / 2, 90)
+        turtle.circle(height / 2, 90)
+    turtle.end_fill()
+
+
+def draw_rect(x, y, width, height):
+    turtle.penup()
+    turtle.setpos(x, h - y)
+    turtle.pendown()
+    for _ in range(2):
+        turtle.forward(width)
+        turtle.right(90)
+        turtle.forward(height)
+        turtle.right(90)
+
+
+def draw_filled_oval(x, y, width, height):
+    turtle.penup()
+    turtle.setpos(x, h - (y - height))
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.left(45)
+    for _ in range(2):
+        turtle.circle(width / 2, 90)
+        turtle.circle(height / 2, 90)
+    turtle.end_fill()
+
+
+def draw_line(x1, y1, x2, y2):
+    turtle.penup()
+    turtle.setpos(x1, y1)
+    turtle.pendown()
+    turtle.goto(x2, y2)
+
+
+def draw_circle(centerx, centery, radius):
+    turtle.penup()
+    turtle.setpos(centerx, centery - radius)
+    turtle.pendown()
+    turtle.circle(radius)
---
+++ packages/cs1-graphics/cs1-graphics-1.5.tar.gz/cs1-graphics-1.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='cs1-graphics',
+    version='1.5',
+    install_requires='None',
+    author='Jack Seigerman',
+    author_email='jacks3ds@gmail.com',
+    description='python 3 drawing library that takes calls similar to the cs1-graphics library and outputs with the '
+                'turtle library',
+    long_description_content_type='text/markdown',
+    long_description='Please keep in mind that this is still a work in progress. This project was made because the '
+                     'pre existing cs1 graphics library does not work with python3. This library takes calls that '
+                     'would work with cs1.graphics and renders the image in turtle instead.',
+    url='https://github.com/jackSeigerman/cs1-graphics',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
