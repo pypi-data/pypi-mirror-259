3f72a7acaf0fb3ae246fda383e5bb33859e9f75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:04:18 +0000

    Add package funcs-aux/0.0.6/funcs_aux-0.0.6.tar.gz

---
+++ packages/funcs-aux/funcs_aux-0.0.6.tar.gz/funcs_aux-0.0.6/README.md
@@ -0,0 +1,39 @@
+# funcs_aux (v0.0.6)
+
+## DESCRIPTION_SHORT
+useful funcs in one place
+
+## DESCRIPTION_LONG
+Now its just a beginning!
+Designed to collect useful funcs in one place!
+
+
+## Features
+
+
+********************************************************************************
+## License
+See the [LICENSE](LICENSE) file for license rights and limitations (MIT).
+
+
+## Release history
+See the [HISTORY.md](HISTORY.md) file for release history.
+
+
+## Installation
+```commandline
+pip install funcs-aux
+```
+
+
+## Import
+```python
+from funcs_aux import *
+```
+
+
+********************************************************************************
+## USAGE EXAMPLES
+See tests and sourcecode for other examples.
+
+********************************************************************************
---
+++ packages/funcs-aux/funcs_aux-0.0.6.tar.gz/funcs_aux-0.0.6/funcs_aux/collects.py
@@ -0,0 +1,70 @@
+from typing import *
+import time
+from object_info import ObjectInfo
+
+
+# =====================================================================================================================
+def collection__get_original_item__case_insensitive(item: Any, data: Collection) -> Optional[Any]:
+    """
+    NOTES:
+    1. DONT TRY use None in keys
+
+    USEFUL in case-insensitive systems (like terminals or serial devices):
+    1. get key in dict
+    2. find attribute name in objects
+
+    :param item:
+    :param data:
+    :return:
+    """
+    # FIXME: what if several items? - it is not useful!!! returning first is most expected!
+    for value in list(data):
+        if str(value).lower() == str(item).lower():
+            return value
+
+
+def collection__path_create_original_names(path: Union[str, int, List], data: Collection) -> Optional[List[Any]]:
+    """
+    NOTES:
+    1. path used as address as index for list and key for dicts
+    2. separator is simple SLASH!
+
+    :param item:
+    :param data:
+    :return:
+        None - if path is unreachable/incorrect
+        List[Any] - reachable path which could be used to get value from data by chain data[i1][i2][i3]
+    """
+
+    # TODO: add tests!!!
+    if not isinstance(path, list):
+        path = str(path)
+
+    if isinstance(path, str):
+        path = path.split("/")
+
+    # work ----------------------------
+    result_path = []
+    for path_part in path:
+        if isinstance(data, dict):
+            # DICT ----------------
+            address_original = collection__get_original_item__case_insensitive(path_part, data)
+            if address_original is None:
+                return
+            data = data[address_original]
+
+        elif isinstance(data, (list, tuple)):
+            # ITERABLE ----------------
+            try:
+                address_original = int(path_part)
+                data = data[address_original]
+            except:
+                return
+        else:
+            return
+        result_path.append(address_original)
+
+    return result_path
+
+
+# =====================================================================================================================
