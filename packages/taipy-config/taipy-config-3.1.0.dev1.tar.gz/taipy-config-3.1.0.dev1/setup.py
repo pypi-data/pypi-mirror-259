7023ee953e971c71469cdb15a817305675726882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:04:06 +0000

    Add package taipy-config/3.1.0.dev1/taipy-config-3.1.0.dev1.tar.gz

---
+++ packages/taipy-config/taipy-config-3.1.0.dev1.tar.gz/taipy-config-3.1.0.dev1/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+# Copyright 2021-2024 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+"""The setup script."""
+import json
+import os
+
+from setuptools import find_namespace_packages, find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+version_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "version.json")
+
+with open(version_path) as version_file:
+    version = json.load(version_file)
+    version_string = f'{version.get("major", 0)}.{version.get("minor", 0)}.{version.get("patch", 0)}'
+    if vext := version.get("ext"):
+        version_string = f"{version_string}.{vext}"
+
+requirements = ["toml>=0.10,<0.11", "deepdiff>=6.2,<6.3"]
+
+test_requirements = ["pytest>=3.8"]
+
+setup(
+    author="Avaiga",
+    author_email="dev@taipy.io",
+    python_requires=">=3.8",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    description="A Taipy package dedicated to easily configure a Taipy application.",
+    install_requires=requirements,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="Apache License 2.0",
+    keywords="taipy-config",
+    name="taipy-config",
+    packages=find_namespace_packages(where=".")
+    + find_packages(include=["taipy", "taipy.config", "taipy.config.*", "taipy.logger", "taipy.logger.*"]),
+    include_package_data=True,
+    data_files=[('version', ['version.json'])],
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/avaiga/taipy-config",
+    version=version_string,
+    zip_safe=False,
+)
