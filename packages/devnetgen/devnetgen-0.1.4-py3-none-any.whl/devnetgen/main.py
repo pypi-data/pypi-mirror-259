5a9a234a6aed762e78ab901eb979a1a2dc70eb4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:58 +0000

    Add package devnetgen/0.1.4/devnetgen-0.1.4-py3-none-any.whl

---
+++ packages/devnetgen/devnetgen-0.1.4-py3-none-any.whl/devnetgen/main.py
@@ -0,0 +1,16 @@
+import typer
+
+from devnetgen.app import FileClass, Executor
+
+
+app = typer.Typer()
+
+
+@app.command()
+def main(path: str, legacy_controller: bool = False):
+    entity = FileClass(path)
+    executor = Executor(entity)
+    executor.create_dto(legacy_controller=legacy_controller)
+
+#if __name__ == "__main__":
+    #main('/home/alex/Documents/RiderProjects/MinstroyGasDistributionNetworks/Domain/Entities/Reports/AppealContract.cs')
