44c9442dfcb33e280d7b711ca2577b54f08060df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:04:12 +0000

    Add package freeflux/0.3.6/freeflux-0.3.6-py3-none-any.whl

---
+++ packages/freeflux/freeflux-0.3.6-py3-none-any.whl/freeflux/utils/context.py
@@ -0,0 +1,30 @@
+'''Define the Context class.'''
+
+
+__author__ = 'Chao Wu'
+__date__ = '03/30/2022'
+
+
+class Context():
+    
+    def __init__(self):
+        
+        self.operations = []
+        
+    
+    def add_undo(self, op):
+        '''
+        Parameters
+        ----------
+        op: callable
+            Operation to reset the model.
+        '''
+        
+        self.operations.append(op)
+
+
+    def undo(self):
+        
+        while self.operations:
+            op = self.operations.pop()
+            op()
