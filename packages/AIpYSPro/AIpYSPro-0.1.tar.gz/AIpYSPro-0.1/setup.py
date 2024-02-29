9536e79eb80d1a91f135a75696db4fdd0fb03fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 05:03:57 +0000

    Add package AIpYSPro/0.1/AIpYSPro-0.1.tar.gz

---
+++ packages/AIpYSPro/AIpYSPro-0.1.tar.gz/AIpYSPro-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AIpYSPro",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Gil Kanfer",
+    author_email="gil.kanfer.il@gmail.com",
+    description="AI Powered Photoswitchable Screen",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="",
+)
