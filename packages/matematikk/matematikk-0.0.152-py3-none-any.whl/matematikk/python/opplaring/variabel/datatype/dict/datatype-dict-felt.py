5ea638359365668dc7dad313a5f40066c9e54898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 06:04:44 +0000

    Add package matematikk/0.0.152/matematikk-0.0.152-py3-none-any.whl

---
+++ packages/matematikk/matematikk-0.0.152-py3-none-any.whl/matematikk/python/opplaring/variabel/datatype/dict/datatype-dict-felt.py
@@ -0,0 +1,17 @@
+# 🚀 programmering.no | 🤓 matematikk.as
+# - Endre value med key
+
+# Variabler
+obj = {
+                "navn":  "Supermann",
+                "alder": 23
+            }
+
+# Print obj
+print(obj)
+
+# Legger til et nytt felt fordi key ikke eksisterer
+obj["emoji"] = "💫"
+
+# Print oppdatert obj
+print(obj)
