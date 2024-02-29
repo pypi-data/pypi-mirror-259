f6d9640a78c69a719a47ac45ad3d1a3228baa665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 17:04:44 +0000

    Add package okta-integration/0.0.1/okta-integration-0.0.1.tar.gz

---
+++ packages/okta-integration/okta-integration-0.0.1.tar.gz/okta-integration-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='okta-integration',
+version='0.0.1',
+author='Subscription-pypi Creatoriq',
+author_email='subscription-pypi@creatoriq.com',
+description='Packageholdername okta-integration  for circle-area-math function',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.9',
+)
