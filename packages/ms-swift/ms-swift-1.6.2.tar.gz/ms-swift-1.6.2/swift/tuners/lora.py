9a2146f3600523dd65cdc0ceba43cae22bdca259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:04:18 +0000

    Add package ms-swift/1.6.2/ms-swift-1.6.2.tar.gz

---
+++ packages/ms-swift/ms-swift-1.6.2.tar.gz/ms-swift-1.6.2/swift/tuners/lora.py
@@ -0,0 +1,125 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+from dataclasses import asdict, dataclass, field
+
+import torch
+from packaging import version
+from peft.tuners.lora import LoraLayer
+
+from swift import LoraConfig
+from .lora_layers import *  # noqa
+from .utils import SwiftAdapter, SwiftConfig, SwiftOutput, set_adapter
+
+logger = get_logger()
+
+
+@dataclass
+class LoRAConfig(LoraConfig, SwiftConfig):
+    """
+    The configuration class for the loRA module.
+
+    Args:
+        use_qa_lora(bool): Use
+            QA-LoRA:[Quantization-Aware Low-Rank Adaptation of Large Language Models](https://arxiv.org/abs/2309.14717)
+            instead of LoRA. QA-LoRA only supports AutoGPTQ quantized models.
+    """
+
+    use_qa_lora: bool = field(
+        default=False,
+        metadata={
+            'help':
+            'Use [qa-lora](https://github.com/yuhuixu1993/qa-lora) or not'
+        })
+
+    use_merged_linear: bool = field(
+        default=False, metadata={'help': 'Use merged Linear'})
+
+    enable_lora: List[bool] = field(
+        default=None,
+        metadata={
+            'help':
+            'The modules need to be turned on when using the merged linear layer'
+        })
+
+    lora_dtype: str = field(
+        default=None,
+        metadata={
+            'help':
+            'The lora dtype, default None means following the original layer\'s dtype'
+        })
+
+    def __post_init__(self):
+        from .mapping import SwiftTuners
+        self.swift_type = SwiftTuners.LORA
+
+    def can_be_saved_to_peft(self) -> bool:
+        return not self.use_qa_lora and not self.use_merged_linear \
+            and (not self.lora_dtype or self.lora_dtype == 'fp32')
+
+    def to_peft_config(self) -> LoraConfig:
+        _dict = asdict(self)
+        _dict.pop('use_qa_lora')
+        _dict.pop('enable_lora')
+        _dict.pop('lora_dtype')
+        _dict.pop('use_merged_linear')
+        _dict['peft_type'] = _dict['swift_type']
+        _dict.pop('swift_type')
+        return LoraConfig(**_dict)
+
+
+class LoRA(SwiftAdapter):
+
+    @staticmethod
+    def prepare_model(model: nn.Module, config: LoRAConfig, adapter_name: str):
+        LoraModel(model, config, adapter_name)
+
+        def state_dict_callback(state_dict, adapter_name):
+            return lora_state_dict(state_dict, adapter_name, config.bias)
+
+        def mark_trainable_callback(model):
+            mark_lora_as_trainable(model, adapter_name, config.bias)
+
+        return SwiftOutput(config, state_dict_callback,
+                           mark_trainable_callback)
+
+    @staticmethod
+    def activate_adapter(module: torch.nn.Module,
+                         adapter_name: str,
+                         activate: bool,
+                         offload: str = None):
+        set_adapter(module, adapter_name, activate, offload)
+        for sub_module in module.modules():
+            if isinstance(sub_module, (LoraLayer, LoRALayer)):
+                sub_module.set_activation(adapter_name, activate)
+                if hasattr(sub_module, 'save_memory'):
+                    sub_module.save_memory(adapter_name, activate, offload)
+
+    @staticmethod
+    def unpatch_lora(model, config: LoRAConfig, adapter_name: str):
+        """Unpatch lora modules and merge the weights to original modules.
+
+        LoRA constructs an additional layer with low-rank decomposition matrices of the weights in the network.
+        'LoRA: Low-Rank Adaptation of Large Language Models' by Hu et al.(2021)
+        See https://arxiv.org/abs/2106.09685
+
+        Args:
+            model(`torch.nn.Module`): The model called with `tune` function.
+            config(`LoRAConfig`): The `LoRAConfig` to use. Deprecated
+            adapter_name(`str`): The adapter name
+        """
+        if not config.use_merged_linear:
+            if version.parse(peft.__version__) < version.parse('0.6.3'):
+                logger.info('All adapters will be merged.')
+                LoraModel(model, None, '').merge_and_unload()
+            else:
+                LoraModel(model, None,
+                          '').merge_and_unload(adapter_names=[adapter_name])
+        else:
+            for name, sub_module in model.named_modules():
+                if isinstance(sub_module, MergedLinear):
+                    sub_module.merge()
+                    parent = model.get_submodule('.'.join(
+                        name.split('.')[:-1]))
+                    target_name = name.split('.')[-1]
+                    setattr(parent, target_name, sub_module.base_layer)
---
+++ packages/ms-swift/ms-swift-1.6.2.tar.gz/ms-swift-1.6.2/swift/ui/llm_infer/llm_infer.py
@@ -0,0 +1,239 @@
+import os
+import re
+from typing import Type
+
+import gradio as gr
+import json
+import torch
+from gradio import Accordion, Tab
+
+from swift import snapshot_download
+from swift.llm import (InferArguments, inference_stream, limit_history_length,
+                       prepare_model_template)
+from swift.ui.base import BaseUI
+from swift.ui.llm_infer.model import Model
+
+
+class LLMInfer(BaseUI):
+
+    group = 'llm_infer'
+
+    sub_ui = [Model]
+
+    locale_dict = {
+        'generate_alert': {
+            'value': {
+                'zh': '请先加载模型',
+                'en': 'Please load model first',
+            }
+        },
+        'llm_infer': {
+            'label': {
+                'zh': 'LLM推理',
+                'en': 'LLM Inference',
+            }
+        },
+        'load_alert': {
+            'value': {
+                'zh': '加载模型中，请等待',
+                'en': 'Start to load model, please wait'
+            }
+        },
+        'loaded_alert': {
+            'value': {
+                'zh': '模型加载完成',
+                'en': 'Model loaded'
+            }
+        },
+        'chatbot': {
+            'value': {
+                'zh': '对话框',
+                'en': 'Chat bot'
+            },
+        },
+        'prompt': {
+            'label': {
+                'zh': '请输入：',
+                'en': 'Input:'
+            },
+        },
+        'clear_history': {
+            'value': {
+                'zh': '清除对话信息',
+                'en': 'Clear history'
+            },
+        },
+        'submit': {
+            'value': {
+                'zh': '🚀 发送',
+                'en': '🚀 Send'
+            },
+        },
+        'gpu_id': {
+            'label': {
+                'zh': '选择可用GPU',
+                'en': 'Choose GPU'
+            },
+            'info': {
+                'zh': '选择训练使用的GPU号，如CUDA不可用只能选择CPU',
+                'en': 'Select GPU to train'
+            }
+        },
+    }
+
+    choice_dict = BaseUI.get_choices_from_dataclass(InferArguments)
+
+    @classmethod
+    def do_build_ui(cls, base_tab: Type['BaseUI']):
+        with gr.TabItem(elem_id='llm_infer', label=''):
+            gpu_count = 0
+            default_device = 'cpu'
+            if torch.cuda.is_available():
+                gpu_count = torch.cuda.device_count()
+                default_device = '0'
+            with gr.Blocks():
+                model_and_template = gr.State([])
+                Model.build_ui(base_tab)
+                gr.Dropdown(
+                    elem_id='gpu_id',
+                    multiselect=True,
+                    choices=[str(i) for i in range(gpu_count)] + ['cpu'],
+                    value=default_device,
+                    scale=8)
+                chatbot = gr.Chatbot(
+                    elem_id='chatbot', elem_classes='control-height')
+                prompt = gr.Textbox(
+                    elem_id='prompt', lines=1, interactive=False)
+
+                with gr.Row():
+                    clear_history = gr.Button(elem_id='clear_history')
+                    submit = gr.Button(elem_id='submit')
+
+                submit.click(
+                    cls.generate_chat,
+                    inputs=[
+                        model_and_template,
+                        cls.element('template_type'), prompt, chatbot,
+                        cls.element('max_new_tokens'),
+                        cls.element('system')
+                    ],
+                    outputs=[prompt, chatbot],
+                    queue=True)
+                clear_history.click(
+                    fn=cls.clear_session, inputs=[], outputs=[prompt, chatbot])
+                cls.element('load_checkpoint').click(
+                    cls.reset_memory, [], [model_and_template])\
+                    .then(cls.reset_loading_button, [], [cls.element('load_checkpoint')]).then(
+                        cls.prepare_checkpoint, [
+                            value for value in cls.elements().values()
+                            if not isinstance(value, (Tab, Accordion))
+                        ], [model_and_template]).then(cls.change_interactive, [],
+                                                 [prompt]).then( # noqa
+                    cls.clear_session,
+                    inputs=[],
+                    outputs=[prompt, chatbot],
+                    queue=True).then(cls.reset_load_button, [], [cls.element('load_checkpoint')])
+
+    @classmethod
+    def reset_load_button(cls):
+        return gr.update(
+            value=cls.locale('load_checkpoint', cls.lang)['value'])
+
+    @classmethod
+    def reset_loading_button(cls):
+        return gr.update(value=cls.locale('load_alert', cls.lang)['value'])
+
+    @classmethod
+    def reset_memory(cls):
+        return []
+
+    @classmethod
+    def prepare_checkpoint(cls, *args):
+        torch.cuda.empty_cache()
+        infer_args = cls.get_default_value_from_dataclass(InferArguments)
+        kwargs = {}
+        kwargs_is_list = {}
+        other_kwargs = {}
+        more_params = {}
+        keys = [
+            key for key, value in cls.elements().items()
+            if not isinstance(value, (Tab, Accordion))
+        ]
+        for key, value in zip(keys, args):
+            compare_value = infer_args.get(key)
+            compare_value_arg = str(compare_value) if not isinstance(
+                compare_value, (list, dict)) else compare_value
+            compare_value_ui = str(value) if not isinstance(
+                value, (list, dict)) else value
+            if key in infer_args and compare_value_ui != compare_value_arg and value:
+                if isinstance(value, str) and re.fullmatch(
+                        cls.int_regex, value):
+                    value = int(value)
+                elif isinstance(value, str) and re.fullmatch(
+                        cls.float_regex, value):
+                    value = float(value)
+                kwargs[key] = value if not isinstance(
+                    value, list) else ' '.join(value)
+                kwargs_is_list[key] = isinstance(value, list)
+            else:
+                other_kwargs[key] = value
+            if key == 'more_params' and value:
+                more_params = json.loads(value)
+
+        kwargs.update(more_params)
+        if kwargs['model_type'] == cls.locale('checkpoint', cls.lang)['value']:
+            model_dir = kwargs.pop('model_id_or_path')
+            if not os.path.exists(model_dir):
+                model_dir = snapshot_download(model_dir)
+            kwargs['ckpt_dir'] = model_dir
+        if 'ckpt_dir' in kwargs or (
+                'model_id_or_path' in kwargs
+                and not os.path.exists(kwargs['model_id_or_path'])):
+            kwargs.pop('model_type', None)
+
+        devices = other_kwargs['gpu_id']
+        devices = [d for d in devices if d]
+        assert (len(devices) == 1 or 'cpu' not in devices)
+        gpus = ','.join(devices)
+        if gpus != 'cpu':
+            os.environ['CUDA_VISIBLE_DEVICES'] = gpus
+        infer_args = InferArguments(**kwargs)
+        model, template = prepare_model_template(infer_args)
+        gr.Info(cls.locale('loaded_alert', cls.lang)['value'])
+        return [model, template]
+
+    @classmethod
+    def clear_session(cls):
+        return '', None
+
+    @classmethod
+    def change_interactive(cls):
+        return gr.update(interactive=True)
+
+    @classmethod
+    def generate_chat(cls, model_and_template, template_type, prompt: str,
+                      history, max_new_tokens, system):
+        if not model_and_template:
+            gr.Warning(cls.locale('generate_alert', cls.lang)['value'])
+            return '', None
+        model, template = model_and_template
+        if os.environ.get('MODELSCOPE_ENVIRONMENT') == 'studio':
+            model.cuda()
+        if not template_type.endswith('generation'):
+            old_history, history = limit_history_length(
+                template, prompt, history, int(max_new_tokens))
+        else:
+            old_history = []
+            history = []
+        gen = inference_stream(
+            model,
+            template,
+            prompt,
+            history,
+            system=system,
+            stop_words=['Observation:'])
+        for _, history in gen:
+            total_history = old_history + history
+            yield '', total_history
+        if os.environ.get('MODELSCOPE_ENVIRONMENT') == 'studio':
+            model.cpu()
