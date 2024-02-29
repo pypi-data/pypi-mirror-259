7f2c98997a967863d3e6a7c469d49f39b5685128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 05:04:01 +0000

    Add package datasette-explain/0.2/datasette-explain-0.2.tar.gz

---
+++ packages/datasette-explain/datasette-explain-0.2.tar.gz/datasette-explain-0.2/tests/test_explain.py
@@ -0,0 +1,98 @@
+from datasette.app import Datasette
+import pytest
+import pytest_asyncio
+
+
+@pytest_asyncio.fixture
+async def ds():
+    datasette = Datasette(memory=True)
+    db = datasette.add_memory_database("test")
+    await db.execute_write("create table if not exists t1 (id integer primary key)")
+    await db.execute_write("create table if not exists t2 (id integer primary key)")
+    return datasette
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "sql,expected",
+    (
+        (
+            "select 1",
+            {
+                "ok": True,
+                "explain_tree": [{"detail": "SCAN CONSTANT ROW", "children": []}],
+                "tables": [],
+            },
+        ),
+        (
+            "select * from t1",
+            {
+                "ok": True,
+                "explain_tree": [{"detail": "SCAN t1", "children": []}],
+                "tables": [{"name": "t1", "columns": ["id"]}],
+            },
+        ),
+        (
+            "select * from t1 where id > :id",
+            {
+                "ok": True,
+                "explain_tree": [
+                    {
+                        "detail": "SEARCH t1 USING INTEGER PRIMARY KEY (rowid>?)",
+                        "children": [],
+                    }
+                ],
+                "tables": [{"name": "t1", "columns": ["id"]}],
+            },
+        ),
+        (
+            "select id, (select id from t2 where t2.id = t1.id) from t1",
+            {
+                "ok": True,
+                "explain_tree": [
+                    {"detail": "SCAN t1", "children": []},
+                    {
+                        "detail": "CORRELATED SCALAR SUBQUERY 1",
+                        "children": [
+                            {
+                                "detail": "SEARCH t2 USING INTEGER PRIMARY KEY (rowid=?)",
+                                "children": [],
+                            }
+                        ],
+                    },
+                ],
+                "tables": [
+                    {"name": "t1", "columns": ["id"]},
+                    {"name": "t2", "columns": ["id"]},
+                ],
+            },
+        ),
+        (
+            "select count(*) from t1 where id > :id",
+            {
+                "ok": True,
+                "explain_tree": [
+                    {
+                        "detail": "SEARCH t1 USING INTEGER PRIMARY KEY (rowid>?)",
+                        "children": [],
+                    }
+                ],
+                "tables": [{"name": "t1", "columns": ["id"]}],
+            },
+        ),
+        (
+            "explain select count(*) from t1",
+            {
+                "ok": True,
+                "explain_tree": [],
+                "tables": [],
+            },
+        ),
+    ),
+)
+async def test_explain(ds, sql, expected):
+    params = {"sql": sql}
+    response = await ds.client.get("/test/-/explain", params=params)
+    assert response.status_code == 200
+    data = response.json()
+    assert data == expected
