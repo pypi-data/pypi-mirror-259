d698ecd505fe4a1c093e959dca5c3ee3701bcd11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:03:59 +0000

    Add package nonebot_plugin_userinfo/0.2.1/nonebot_plugin_userinfo-0.2.1.tar.gz

---
+++ packages/nonebot_plugin_userinfo/nonebot_plugin_userinfo-0.2.1.tar.gz/nonebot_plugin_userinfo-0.2.1/nonebot_plugin_userinfo/adapters/onebot_v11.py
@@ -0,0 +1,81 @@
+from typing import Optional
+
+from nonebot.exception import ActionFailed
+from nonebot.log import logger
+
+from ..getter import UserInfoGetter, register_user_info_getter
+from ..image_source import QQAvatar
+from ..user_info import UserGender, UserInfo
+
+try:
+    from nonebot.adapters.onebot.v11 import (
+        Bot,
+        Event,
+        GroupAdminNoticeEvent,
+        GroupBanNoticeEvent,
+        GroupDecreaseNoticeEvent,
+        GroupIncreaseNoticeEvent,
+        GroupMessageEvent,
+        GroupRecallNoticeEvent,
+        GroupRequestEvent,
+        GroupUploadNoticeEvent,
+    )
+
+    @register_user_info_getter(Bot, Event)
+    class Getter(UserInfoGetter[Bot, Event]):
+        async def _get_info(self, user_id: str) -> Optional[UserInfo]:
+            info = None
+
+            if isinstance(
+                self.event,
+                (
+                    GroupAdminNoticeEvent,
+                    GroupBanNoticeEvent,
+                    GroupDecreaseNoticeEvent,
+                    GroupIncreaseNoticeEvent,
+                    GroupMessageEvent,
+                    GroupRecallNoticeEvent,
+                    GroupRequestEvent,
+                    GroupUploadNoticeEvent,
+                ),
+            ):
+                try:
+                    info = await self.bot.get_group_member_info(
+                        group_id=self.event.group_id, user_id=int(user_id)
+                    )
+                except ActionFailed as e:
+                    logger.warning(f"Error calling get_group_member_info: {e}")
+
+            if not info:
+                try:
+                    info = await self.bot.get_stranger_info(user_id=int(user_id))
+                except ActionFailed as e:
+                    logger.warning(f"Error calling get_stranger_info failed: {e}")
+
+            if info:
+                qq = info["user_id"]
+                sex = info.get("sex")
+                user_gender = (
+                    UserGender.male
+                    if sex == "male"
+                    else UserGender.female
+                    if sex == "female"
+                    else UserGender.unknown
+                )
+                return UserInfo(
+                    user_id=str(qq),
+                    user_name=info.get("nickname", ""),
+                    user_displayname=info.get("card"),
+                    user_avatar=QQAvatar(qq=qq),
+                    user_gender=user_gender,
+                )
+
+            if user_id.isdigit() and 5 <= len(user_id) <= 11:
+                return UserInfo(
+                    user_id=user_id,
+                    user_name="",
+                    user_avatar=QQAvatar(qq=int(user_id)),
+                )
+
+except ImportError:
+    pass
