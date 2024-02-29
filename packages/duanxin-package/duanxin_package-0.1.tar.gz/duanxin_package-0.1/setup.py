d39d8af4924bec3af1b5ff3b3fb774da0681a634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 17:04:44 +0000

    Add package duanxin-package/0.1/duanxin_package-0.1.tar.gz

---
+++ packages/duanxin-package/duanxin_package-0.1.tar.gz/duanxin_package-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='duanxin_package',
+    version='0.1',
+    description='A package to increment numbers by 1',
+    # url='https://example.com',
+    author='Lin p',
+    author_email='L_hp777@163.com',
+    py_modules=['duanxin'],
+)
