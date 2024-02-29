ddf5c9dcd737dd109217e44b9cc06bf2e5ed36a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:04:09 +0000

    Add package vanta-yc/0.0.1/vanta_yc-0.0.1.tar.gz

---
+++ packages/vanta-yc/vanta_yc-0.0.1.tar.gz/vanta_yc-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Log to table converter package'
+
+# Setting up
+setup(
+    name="vanta_yc",
+    version=VERSION,
+    author="YC",
+    author_email="<yatipa.c@vanta-capital.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pandas', 'datetime', 'decimal', 're'],
+    keywords=['python', 'log', 'table', 'lob', 'mbp', 'onItch'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
