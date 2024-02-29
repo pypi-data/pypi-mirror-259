02d1597afe7f273341639d4257c929eeba683b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:04:17 +0000

    Add package KUtil-jakubaugustyn/0.0.3/KUtil-jakubaugustyn-0.0.3.tar.gz

---
+++ packages/KUtil-jakubaugustyn/KUtil-jakubaugustyn-0.0.3.tar.gz/KUtil-jakubaugustyn-0.0.3/src/kutil/buffer/ByteBuffer.py
@@ -0,0 +1,112 @@
+#  -*- coding: utf-8 -*-
+__author__ = "kubik.augustyn@post.cz"
+
+import copy
+from typing import Iterable, Self
+
+
+class ByteBuffer(Iterable[int]):
+    data: bytearray
+    pointer: int
+
+    def __init__(self, dataOrLength: Iterable[int] | int = 0):
+        self.data = bytearray(dataOrLength) if isinstance(dataOrLength, int) else bytearray(
+            dataOrLength)
+        self.pointer = 0
+
+    def readByte(self) -> int:
+        self.has(1)
+        self.pointer += 1
+        return self.data[self.pointer - 1]
+
+    def read(self, amount: int) -> bytearray:
+        if amount == 0:
+            return bytearray()
+        self.has(amount)
+        self.pointer += amount
+        return self.data[self.pointer - amount:self.pointer]
+
+    def readLine(self, newLine: bytes = b"\r\n") -> bytearray:
+        amount: int = self.index(newLine)
+        data: bytearray = self.read(amount) if amount > 0 else bytearray()
+        self.skip(len(newLine))
+        return data
+
+    def index(self, seq: bytes) -> int:
+        self.has(len(seq))
+        for i in range(len(self) - len(seq) + 1):
+            if self.data[self.pointer + i:self.pointer + i + len(seq)] == seq:
+                return i
+        raise IndexError
+
+    def skip(self, amount: int):
+        assert amount > 0
+        self.has(amount)
+        self.pointer += amount
+
+    def back(self, amount: int):
+        assert amount > 0
+        self.has(-amount)
+        self.pointer -= amount
+
+    def __len__(self) -> int:
+        return len(self.data) - self.pointer
+
+    def readAll(self) -> bytearray:
+        if len(self) == 0:
+            return bytearray(0)
+        self.has(1)
+        amount = len(self)
+        self.pointer += amount
+        return self.data[self.pointer - amount:self.pointer]
+
+    def writeByte(self, byte: int, i: int = -1) -> Self:
+        if i == -1:
+            self.data.append(byte)
+        else:
+            self.data.insert(i, byte)
+        return self
+
+    def write(self, data: Iterable[int], i: int = -1) -> Self:
+        if i == -1:
+            self.data.extend(data)
+        else:
+            for byteI, byte in enumerate(data):
+                self.writeByte(byte, i + byteI)
+        return self
+
+    def export(self) -> bytes:
+        return bytes(self.data)
+
+    def reset(self, data: Iterable[int] | None = None) -> Self:
+        self.resetPointer()
+        self.data.clear()
+        if data is not None:
+            self.data.extend(data)
+        return self
+
+    def resetPointer(self) -> Self:
+        self.pointer = 0
+        return self
+
+    def has(self, amount: int) -> bool:
+        if amount == 0:
+            raise ValueError("Invalid amount")
+        if amount < 0:
+            if -amount > self.pointer:
+                raise ValueError("Not enough bytes")
+        else:
+            if len(self.data) < self.pointer + amount:
+                raise IndexError("Not enough bytes")
+        return True
+
+    def copy(self) -> Self:
+        copyBuff = ByteBuffer()
+        copyBuff.data = copy.copy(self.data)
+        copyBuff.pointer = self.pointer
+        return copyBuff
+
+    def __iter__(self):
+        """Iterates over all bytes of the buffer, ignoring and not mutating the pointer."""
+        for byte in self.data:
+            yield byte
---
+++ packages/KUtil-jakubaugustyn/KUtil-jakubaugustyn-0.0.3.tar.gz/KUtil-jakubaugustyn-0.0.3/src/kutil/protocol/ProtocolConnection.py
@@ -0,0 +1,118 @@
+#  -*- coding: utf-8 -*-
+__author__ = "kubik.augustyn@post.cz"
+
+from threading import Thread
+from typing import Callable, Any
+from socket import socket, AF_INET, SOCK_STREAM
+from kutil.protocol.AbstractProtocol import AbstractProtocol, NeedMoreDataError
+from kutil.buffer.ByteBuffer import ByteBuffer
+
+type OnDataListener = Callable[[Any], None]
+
+
+class ProtocolConnection:
+    """An event-based connection, doesn't block any thread (creates one thread for its needs)"""
+    layers: list[AbstractProtocol]
+    onData: OnDataListener
+    receiverThread: Thread
+    closed: bool
+    sock: socket
+
+    def __init__(self, address: tuple[str, int], layers: list[AbstractProtocol], onData: OnDataListener,
+                 sock: socket | None = None):
+        self.layers = layers
+        self.onData = onData
+        self.receiverThread = Thread(target=self.receive)
+        self.closed = False
+        if sock is None:  # If we are a client connection
+            self.connect(address)
+            self.startRecv()
+        else:  # If we are a server's client connection handler
+            self.sock = sock
+            # Note that you need to manually call startRecv()
+        self.init()
+
+    def init(self):
+        pass  # Subclasses will overwrite this
+
+    def onDataInner(self, data: Any) -> bool | Any:
+        return True  # Subclasses will overwrite this, return whether you want to call the onData handler
+
+    def startRecv(self):
+        self.receiverThread.start()
+
+    def connect(self, address: tuple[str, int]):
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock.connect(address)
+
+    def addProtocol(self, protocol: AbstractProtocol):
+        assert protocol not in self.layers, "Protocol already added"
+        assert isinstance(protocol, AbstractProtocol)
+        self.layers.append(protocol)
+
+    def removeProtocol(self, protocol: AbstractProtocol):
+        """Removes a protocol and it's contained protocols"""
+        self.layers = self.layers[:self.layers.index(protocol)]
+
+    def close(self):
+        self.closed = True
+        # try:
+        self.sock.close()
+        # except OSError:
+        #     pass
+
+    def receive(self):
+        buff: ByteBuffer = ByteBuffer()
+        try:
+            while not self.closed:
+                data = self.sock.recv(1024 * 1024)
+                if not len(data):
+                    self.close()
+                    return
+                buff.write(data)
+                buff.resetPointer()
+                if self.tryReceivedData(buff):
+                    buff.reset()
+        except OSError:
+            self.close()
+        except (ConnectionAbortedError, ConnectionError, ConnectionResetError):
+            self.close()
+
+    def tryReceivedData(self, buff: ByteBuffer) -> bool:
+        try:
+            for layer in self.layers[:-1]:
+                buff = layer.unpackSubProtocol(buff)
+            data: Any = self.layers[-1].unpackData(buff)
+            dataInner: bool | Any = self.ownConnection.onDataInner(data)
+            isBool: bool = isinstance(dataInner, bool)
+            if not isBool or dataInner is True:
+                if not isBool:
+                    # print(data, "-->", dataInner)
+                    data = dataInner
+                # print("On data:", data, "with inner:", dataInner)
+                self.onData(data)
+            return True
+        except NeedMoreDataError:
+            print("Need more data!")
+            print(buff.data)
+            return False
+
+    def sendData(self, data: Any) -> bool:
+        buff: ByteBuffer = ByteBuffer()
+        for i in range(len(self.layers)):
+            if i == 0:
+                self.layers[-1].packData(data, buff)
+            else:
+                self.layers[len(self.layers) - 1 - i].packSubProtocol(buff)
+        try:
+            self.sock.sendall(buff.export())
+        except OSError:
+            self.close()
+            return False
+        return True
+
+    @property
+    def ownConnection(self):
+        """Returns self (usually). Used to split the work across different connection protocol
+         classes when the protocols change (e.g. HTTP --> WebSocket)"""
+        return self  # Maybe the connection changed (protocol switch)
