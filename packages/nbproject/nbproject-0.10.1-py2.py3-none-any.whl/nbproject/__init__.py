76a5fa44ea5ee4f787d71a3b69c6563d1b11f535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 05:04:23 +0000

    Add package nbproject/0.10.1/nbproject-0.10.1-py2.py3-none-any.whl

---
+++ packages/nbproject/nbproject-0.10.1-py2.py3-none-any.whl/nbproject/__init__.py
@@ -0,0 +1,41 @@
+"""nbproject: Manage Jupyter notebooks.
+
+.. currentmodule:: nbproject
+
+Most users will only need these two functions:
+
+.. autosummary::
+   :toctree:
+
+   header
+   publish
+
+Use them with default arguments after importing them like this::
+
+   from nbproject import header, publish
+
+For more fine-grained access, use:
+
+.. autosummary::
+   :toctree:
+
+   meta
+   dev
+
+"""
+__version__ = "0.10.1"
+
+# init jupyter lab frontend immediately on import
+# nothing happens if this is not jupyter lab
+from .dev._jupyter_lab_commands import _init_frontend
+
+# trying to init ipylab JupyterFrontEnd can lead to errors on jupyter notebook
+try:
+    _init_frontend()
+except:  # noqa: E722
+    pass
+
+from . import dev
+from ._header import header
+from ._meta import meta
+from ._publish import publish
