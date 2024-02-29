df26c9cb13a789493dd503770c97901263d93d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:58 +0000

    Add package scBiMapping/1.0.0/scBiMapping-1.0.0.tar.gz

---
+++ packages/scBiMapping/scBiMapping-1.0.0.tar.gz/scBiMapping-1.0.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 16:54:42 2024
+
+@author: qiuteng1
+"""
+
+from setuptools import setup,find_packages
+
+setup(
+    name='scBiMapping',
+    version='1.0.0',
+    description='non-linear dimension reduction',
+    packages = find_packages(),
+    py_modeles = 'scBiMapping.py',
+    author='Qiu Teng',
+    pakages=['scBiMapping'],
+)
