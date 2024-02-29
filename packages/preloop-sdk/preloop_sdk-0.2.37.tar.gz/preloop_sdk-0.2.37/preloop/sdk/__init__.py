e99435b03889c4023a8a1f6f82b96649c9a7730e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 15:04:21 +0000

    Add package preloop-sdk/0.2.37/preloop_sdk-0.2.37.tar.gz

---
+++ packages/preloop-sdk/preloop_sdk-0.2.37.tar.gz/preloop_sdk-0.2.37/preloop/sdk/__init__.py
@@ -0,0 +1,21 @@
+from preloop.sdk.public_api_stubs.exceptions import PreloopError
+from preloop.sdk.public_api_stubs.models import (
+    CreateMLModelRequest,
+    CreateMLModelResult,
+    DeleteMLModelRequest,
+    DeleteMLModelResult,
+    DeployMLModelRequest,
+    DeployMLModelResult,
+    ListHostedMLModelsRequest,
+    ListHostedMLModelsResult,
+    ListMLModelsRequest,
+    ListMLModelsResult,
+    ListTrainingJobsRequest,
+    ListTrainingJobsResult,
+    MLModelDetails,
+    RetrainMLModelRequest,
+    RetrainMLModelResult,
+    StopMLModelRequest,
+    StopMLModelResult,
+)
+from preloop.sdk.public_api_stubs.preloop_client import PreloopClient
