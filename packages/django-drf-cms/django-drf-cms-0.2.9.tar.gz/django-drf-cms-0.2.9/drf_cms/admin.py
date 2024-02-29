793174aacb6966e2ee0f742e55eeeeef9799d8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 05:04:04 +0000

    Add package django-drf-cms/0.2.9/django-drf-cms-0.2.9.tar.gz

---
+++ packages/django-drf-cms/django-drf-cms-0.2.9.tar.gz/django-drf-cms-0.2.9/drf_cms/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django.db import models
+from .models import *
+from .forms import PreviewSelect
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+	model = Page
+	list_filter = ('site',)
+
+@admin.register(Text)
+class TextAdmin(admin.ModelAdmin):
+	model = Text
+	list_display = ('key', 'page', )
+	list_filter = ('page', 'page__site')
+
+@admin.register(FileData)
+class FileDataAdmin(admin.ModelAdmin):
+	list_display = ('id', 'file', 'description', 'uploaded_at',)
+	readonly_fields=['uploaded_at']
+	search_fields = ['description']
+	list_filter = ('description',)
+	ordering = ('-uploaded_at', )
+
+@admin.register(ImageData)
+class ImageDataAdmin(admin.ModelAdmin):
+	list_display = ('preview', 'file', 'description', 'uploaded_at',)
+	readonly_fields=['uploaded_at', 'preview', 'height', 'width']
+	search_fields = ['description']
+	list_filter = ('description',)
+	ordering = ('-uploaded_at', )
+
+
+class PreviewModelAdmin(admin.ModelAdmin):
+	formfield_overrides = {models.ForeignKey:{'widget': PreviewSelect}}
+
+class PreviewTabularInline(admin.TabularInline):
+	formfield_overrides = {models.ForeignKey:{'widget': PreviewSelect}}
