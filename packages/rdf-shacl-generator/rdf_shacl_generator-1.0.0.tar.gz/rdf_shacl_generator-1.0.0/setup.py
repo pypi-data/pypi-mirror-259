9d59636273e279f26979f162571d45518798fd8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:05 +0000

    Add package rdf-shacl-generator/1.0.0/rdf_shacl_generator-1.0.0.tar.gz

---
+++ packages/rdf-shacl-generator/rdf_shacl_generator-1.0.0.tar.gz/rdf_shacl_generator-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='rdf_shacl_generator',
+    version='1.0.0',
+    packages=find_packages(),
+    package_data={'rdf_shacl_generator': ['datasets/*.csv']},
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'genrdf = rdf_shacl_generator.script:main',
+        ],
+    },
+)
