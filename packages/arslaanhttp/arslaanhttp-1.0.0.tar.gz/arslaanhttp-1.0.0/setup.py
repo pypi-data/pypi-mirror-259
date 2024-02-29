18b3fa6d7ef1a3c2ac6169be4e97c4d7a5c43e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 15:04:57 +0000

    Add package arslaanhttp/1.0.0/arslaanhttp-1.0.0.tar.gz

---
+++ packages/arslaanhttp/arslaanhttp-1.0.0.tar.gz/arslaanhttp-1.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arslaanhttp',
+    version='1.0.0',
+    description='A simple HTTP server.',
+    long_description="ArslaanHTTP is a simple HTTP server based on Python's http.server module." + '\n\n' + """
+Changelog
+=========
+
+0.0.1 (29/02/2024)
+------------------
+- First Release
+
+0.0.2 (29/02/2024)
+------------------
+- Fixed install error
+
+0.0.3 (29/02/2024)
+------------------
+- Updated changelog and description
+
+1.0.0 (29/02/2024)
+------------------
+- Updated changelog and changed version number to 1.0.0
+""",
+    url='',
+    author='Arslaan Pathan',
+    author_email='xminecrafterfun@gmail.com',
+    license='MIT',
+    keywords='http',
+    packages=find_packages(),
+    install_requires=['']
+)
