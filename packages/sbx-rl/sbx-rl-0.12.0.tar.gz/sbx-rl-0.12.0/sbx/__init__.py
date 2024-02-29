2935a0eb97373a77fa2cd6567e1818b6251cd744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 07:04:10 +0000

    Add package sbx-rl/0.12.0/sbx-rl-0.12.0.tar.gz

---
+++ packages/sbx-rl/sbx-rl-0.12.0.tar.gz/sbx-rl-0.12.0/sbx/__init__.py
@@ -0,0 +1,24 @@
+import os
+
+from sbx.ddpg import DDPG
+from sbx.dqn import DQN
+from sbx.droq import DroQ
+from sbx.ppo import PPO
+from sbx.sac import SAC
+from sbx.td3 import TD3
+from sbx.tqc import TQC
+
+# Read version from file
+version_file = os.path.join(os.path.dirname(__file__), "version.txt")
+with open(version_file) as file_handler:
+    __version__ = file_handler.read().strip()
+
+__all__ = [
+    "DDPG",
+    "DQN",
+    "DroQ",
+    "PPO",
+    "SAC",
+    "TD3",
+    "TQC",
+]
---
+++ packages/sbx-rl/sbx-rl-0.12.0.tar.gz/sbx-rl-0.12.0/tests/test_run.py
@@ -0,0 +1,103 @@
+from typing import Optional, Type
+
+import numpy as np
+import pytest
+from stable_baselines3 import HerReplayBuffer
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.envs import BitFlippingEnv
+from stable_baselines3.common.evaluation import evaluate_policy
+
+from sbx import DDPG, DQN, PPO, SAC, TD3, TQC, DroQ
+
+
+def test_droq(tmp_path):
+    model = DroQ(
+        "MlpPolicy",
+        "Pendulum-v1",
+        learning_starts=50,
+        learning_rate=1e-3,
+        tau=0.02,
+        gamma=0.98,
+        verbose=1,
+        buffer_size=5000,
+        gradient_steps=2,
+        ent_coef="auto_1.0",
+        seed=1,
+        dropout_rate=0.001,
+        layer_norm=True,
+        # action_noise=NormalActionNoise(np.zeros(1), np.zeros(1)),
+    )
+    model.learn(total_timesteps=1500)
+    # Check that something was learned
+    evaluate_policy(model, model.get_env(), reward_threshold=-800)
+    model.save(tmp_path / "test_save.zip")
+    env = model.get_env()
+    obs = env.observation_space.sample()
+    action_before = model.predict(obs, deterministic=True)[0]
+    # Check we have the same performance
+    model = DroQ.load(tmp_path / "test_save.zip")
+    evaluate_policy(model, env, reward_threshold=-800)
+    action_after = model.predict(obs, deterministic=True)[0]
+    assert np.allclose(action_before, action_after)
+    # Continue training
+    model.set_env(env, force_reset=False)
+    model.learn(100, reset_num_timesteps=False)
+
+
+def test_tqc() -> None:
+    # Multi env
+    train_env = make_vec_env("Pendulum-v1", n_envs=4)
+    model = TQC(
+        "MlpPolicy",
+        train_env,
+        top_quantiles_to_drop_per_net=1,
+        ent_coef=0.01,
+        verbose=1,
+        gradient_steps=1,
+        use_sde=True,
+        qf_learning_rate=1e-3,
+    )
+    model.learn(200)
+
+
+@pytest.mark.parametrize("model_class", [SAC, TD3, DDPG])
+def test_sac_td3(model_class) -> None:
+    model = model_class(
+        "MlpPolicy",
+        "Pendulum-v1",
+        verbose=1,
+        gradient_steps=1,
+        learning_rate=1e-3,
+    )
+    model.learn(110)
+
+
+@pytest.mark.parametrize("env_id", ["Pendulum-v1", "CartPole-v1"])
+def test_ppo(env_id: str) -> None:
+    model = PPO(
+        "MlpPolicy",
+        env_id,
+        verbose=1,
+        n_steps=64,
+        n_epochs=2,
+    )
+    model.learn(128, progress_bar=True)
+
+
+def test_dqn() -> None:
+    model = DQN(
+        "MlpPolicy",
+        "CartPole-v1",
+        verbose=1,
+        gradient_steps=-1,
+        target_update_interval=10,
+    )
+    model.learn(128)
+
+
+@pytest.mark.parametrize("replay_buffer_class", [None, HerReplayBuffer])
+def test_dict(replay_buffer_class: Optional[Type[HerReplayBuffer]]) -> None:
+    env = BitFlippingEnv(n_bits=2, continuous=True)
+    model = SAC("MultiInputPolicy", env, replay_buffer_class=replay_buffer_class)
+
+    model.learn(200, progress_bar=True)
