780535fc539d358a21785da770dd37845df82b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:06 +0000

    Add package external-url-scanner/0.0.1/external-url-scanner-0.0.1.tar.gz

---
+++ packages/external-url-scanner/external-url-scanner-0.0.1.tar.gz/external-url-scanner-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='external-url-scanner',
+version='0.0.1',
+author='Subscription-pypi Creatoriq',
+author_email='subscription-pypi@creatoriq.com',
+description='Packageholdername for add-math functions',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.9',
+)
