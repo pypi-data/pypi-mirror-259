f4739676c935eab461046f9f94ca0e12b3a8193b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:04:41 +0000

    Add package leads/0.7.6/leads-0.7.6.tar.gz

---
+++ packages/leads/leads-0.7.6.tar.gz/leads-0.7.6/leads/data_persistence.py
@@ -0,0 +1,105 @@
+from copy import copy as _copy
+from typing import TextIO as _TextIO, TypeVar as _TypeVar, Generic as _Generic, Sequence as _Sequence, \
+    override as _override
+
+from numpy import mean as _mean
+
+from leads.types import Compressor, Stringifier
+
+T = _TypeVar("T")
+
+
+def mean_compressor(sequence: list[T], target_size: int) -> list[T]:
+    """
+    A compression method that reduces data memory usage by averaging adjacent numbers and merging them.
+    :param sequence: the sequence to compress
+    :param target_size: expected size
+    :return: the compressed sequence
+    """
+    chunk_size = int(len(sequence) / target_size)
+    if chunk_size < 2:
+        return sequence
+    r = []
+    for i in range(target_size - 1):
+        r.append(_mean(sequence[i * chunk_size: (i + 1) * chunk_size]))
+    r.append(_mean(sequence[(target_size - 1) * chunk_size:]))
+    return r
+
+
+def csv_stringifier(element: T) -> str:
+    """
+    Dump an element as a CSV string.
+    :param element: the element to stringify
+    :return: CSV string
+    """
+    return str(element) + ","
+
+
+class DataPersistence(_Sequence, _Generic[T]):
+    def __init__(self,
+                 file: str | _TextIO | None = None,
+                 max_size: int = -1,
+                 chunk_scale: int = 1,
+                 persistence: bool = False,
+                 compressor: Compressor[T] = mean_compressor,
+                 stringifier: Stringifier[T] = csv_stringifier) -> None:
+        """
+        :param file: the file into which the data is written
+        :param max_size: maximum cached size
+        :param chunk_scale: chunk scaling factor (compression)
+        :param compressor: compressor interface
+        :param stringifier: stringifier interface
+        """
+        self._file: _TextIO | None = open(file, "a") if isinstance(file, str) else file
+        self._max_size: int = max_size
+        self._chunk_scale: int = chunk_scale
+        self._persistence: bool = persistence if file else False
+        self._compressor: Compressor[T] = compressor
+        self._stringifier: Stringifier[T] = stringifier
+        self._data: list[T] = []
+        self._chunk: list[T] = []
+        self._chunk_size: int = chunk_scale
+
+    @_override
+    def __len__(self) -> int:
+        return len(self._data)
+
+    @_override
+    def __getitem__(self, item: int | slice) -> T | list[T]:
+        return self._data[item]
+
+    @_override
+    def __str__(self) -> str:
+        return str(self._data)
+
+    def close(self) -> None:
+        if self._file:
+            self._file.close()
+
+    def get_chunk_size(self) -> int:
+        return self._chunk_size
+
+    def to_list(self) -> list[T]:
+        return _copy(self._data)
+
+    def get_chunk(self) -> list[T]:
+        return _copy(self._chunk)
+
+    def _push_to_data(self, element: T) -> None:
+        self._data.append(element)
+        if self._max_size < 2:
+            return
+        if len(self._data) >= self._max_size:
+            self._data = self._compressor(self._data, int(len(self._data) * .5))
+            self._chunk_size *= 2
+
+    def append(self, element: T) -> None:
+        if self._persistence:
+            self._file.write(self._stringifier(element))
+        if self._chunk_size == 1:
+            return self._push_to_data(element)
+        self._chunk.append(element)
+        if len(self._chunk) >= self._chunk_size:
+            for e in self._compressor(self._chunk, self._chunk_scale):
+                self._push_to_data(e)
+            self._chunk.clear()
---
+++ packages/leads/leads-0.7.6.tar.gz/leads-0.7.6/leads_vec_rc/cli.py
@@ -0,0 +1,72 @@
+from json import loads
+from os import mkdir
+from os.path import abspath, exists
+from typing import Any
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from leads import *
+from leads.comm import *
+from leads.data_persistence import *
+from leads_gui import *
+
+config = load_config(abspath(__file__)[:-6] + "config.json", Config)
+if not exists(config.data_dir):
+    mkdir(config.data_dir)
+    L.info(f"Data dir \"{config.data_dir}\" created")
+
+time_stamp_record = DataPersistence(config.data_dir + "/time_stamp.csv", persistence=config.enable_data_persistence,
+                                    max_size=2000)
+speed_record = DataPersistence(config.data_dir + "/speed.csv", persistence=config.enable_data_persistence,
+                               max_size=2000)
+
+
+class CommCallback(Callback):
+    def on_connect(self, service: Service, connection: Connection) -> None:
+        L.debug("Connected")
+
+    def on_fail(self, service: Service, error: Exception) -> None:
+        L.error("Comm client error: " + str(error))
+
+    def on_receive(self, service: Service, msg: bytes) -> None:
+        data = loads(msg.decode())
+        time_stamp_record.append(data["t"])
+        speed_record.append(data["front_wheel_speed"])
+
+    def on_disconnect(self, service: Service, connection: Connection) -> None:
+        time_stamp_record.close()
+        speed_record.close()
+
+
+client = start_client(config.comm_addr, create_client(config.comm_port, CommCallback()), True)
+
+app = FastAPI(title="LEADS VeC Remote Controller")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def index() -> str:
+    return "LEADS VeC Remote Controller"
+
+
+@app.get("/current")
+async def current() -> dict[str, Any]:
+    return {"t": time_stamp_record[-1], "speed": speed_record[-1]}
+
+
+@app.get("/time_stamp")
+async def time_stamp() -> list[int]:
+    return time_stamp_record.to_list()
+
+
+@app.get("/speed")
+async def speed() -> list[float]:
+    return speed_record.to_list()
