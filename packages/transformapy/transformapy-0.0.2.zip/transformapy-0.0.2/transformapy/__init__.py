9606e1f156eaad5a7eebfadcfd45748dd55621b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 14:04:57 +0000

    Add package transformapy/0.0.2/transformapy-0.0.2.zip

---
+++ packages/transformapy/transformapy-0.0.2.zip/transformapy-0.0.2/transformapy/__init__.py
@@ -0,0 +1,26 @@
+from . import transformapy
+
+__version__ = "0.0.2"
+
+__all__ = [
+    "generate_possible_TP_structures",
+    "modify_chemical_formula",
+    "generate_fragments",
+    "split_peak_picking",
+    "calculate_formula_differences",
+    "calculate_changed_num",
+    "parse_formula",
+    "recursive_reaction",
+    "reaction_type",
+    "combine_fragments_and_generate_smiles",
+    "replace_dummies_with_hydrogens",
+    "replace_hydrogen_with_substituent",
+    "split_molecule_at_bond",
+    "Remove_2H",
+    "is_atom_in_double_bond",
+    "add_or_modify_bond",
+    "has_hydrogen_count",
+    "Add_2H",
+    
+    
+]
