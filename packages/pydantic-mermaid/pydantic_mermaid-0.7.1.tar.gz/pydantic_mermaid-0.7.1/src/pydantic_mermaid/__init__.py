ee9682cf5997d58d1a500b7acf520d06634dc78e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:20 +0000

    Add package pydantic-mermaid/0.7.1/pydantic_mermaid-0.7.1.tar.gz

---
+++ packages/pydantic-mermaid/pydantic_mermaid-0.7.1.tar.gz/pydantic_mermaid-0.7.1/src/pydantic_mermaid/__init__.py
@@ -0,0 +1,5 @@
+from pydantic_mermaid.__main__ import main
+from pydantic_mermaid.mermaid_generator import MermaidGenerator
+from pydantic_mermaid.models import *  # noqa F401
+
+__all__ = ["main", "MermaidGenerator"]
