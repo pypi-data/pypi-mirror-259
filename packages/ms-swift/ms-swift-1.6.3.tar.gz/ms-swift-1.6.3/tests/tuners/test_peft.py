f26d1653fbc675388f4d7a44fce615ea6397d9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:04:25 +0000

    Add package ms-swift/1.6.3/ms-swift-1.6.3.tar.gz

---
+++ packages/ms-swift/ms-swift-1.6.3.tar.gz/ms-swift-1.6.3/tests/tuners/test_peft.py
@@ -0,0 +1,88 @@
+import copy
+import os
+import shutil
+import tempfile
+import unittest
+from time import time
+
+import torch
+from modelscope.models.nlp.structbert import (SbertConfig,
+                                              SbertForSequenceClassification)
+from peft import PeftModel
+from peft.utils import WEIGHTS_NAME
+
+from swift import AdaLoraConfig, LoraConfig, LoRAConfig, Swift
+
+
+class TestPeft(unittest.TestCase):
+
+    def setUp(self):
+        print(('Testing %s.%s' % (type(self).__name__, self._testMethodName)))
+        self.tmp_dir = tempfile.TemporaryDirectory().name
+        if not os.path.exists(self.tmp_dir):
+            os.makedirs(self.tmp_dir)
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_dir)
+        super().tearDown()
+
+    def test_peft_lora_injection(self):
+        model = SbertForSequenceClassification(SbertConfig())
+        model2 = copy.deepcopy(model)
+        lora_config = LoraConfig(target_modules=['query', 'key', 'value'])
+        model = Swift.prepare_model(model, lora_config)
+        model.save_pretrained(self.tmp_dir, safe_serialization=False)
+        with open(os.path.join(self.tmp_dir, 'configuration.json'), 'w') as f:
+            f.write('{}')
+        self.assertTrue(
+            os.path.exists(os.path.join(self.tmp_dir, WEIGHTS_NAME)))
+        model2 = Swift.from_pretrained(model2, self.tmp_dir)
+        state_dict = model.state_dict()
+        state_dict2 = model2.state_dict()
+        for key in state_dict:
+            self.assertTrue(key in state_dict2)
+            self.assertTrue(
+                all(
+                    torch.isclose(state_dict[key],
+                                  state_dict2[key]).flatten().detach().cpu()))
+
+    def test_lora_reload_by_peft(self):
+        lora_config = LoRAConfig(target_modules=['query', 'key', 'value'])
+        model = SbertForSequenceClassification(SbertConfig())
+        model2 = copy.deepcopy(model)
+        model = Swift.prepare_model(model, lora_config)
+        model.save_pretrained(self.tmp_dir, peft_format=True)
+        model2 = PeftModel.from_pretrained(model2, self.tmp_dir)
+        state_dict = model.state_dict()
+        state_dict2 = model2.state_dict()
+        state_dict2 = {
+            key[len('base_model.model.'):]: value
+            for key, value in state_dict2.items() if 'lora' in key
+        }
+        for key in state_dict:
+            self.assertTrue(key in state_dict2)
+            self.assertTrue(
+                all(
+                    torch.isclose(state_dict[key],
+                                  state_dict2[key]).flatten().detach().cpu()))
+
+    def test_peft_adalora_injection(self):
+        model = SbertForSequenceClassification(SbertConfig())
+        model2 = copy.deepcopy(model)
+        adalora_config = AdaLoraConfig(
+            target_modules=['query', 'key', 'value'])
+        model = Swift.prepare_model(model, adalora_config)
+        model.save_pretrained(self.tmp_dir, safe_serialization=False)
+        with open(os.path.join(self.tmp_dir, 'configuration.json'), 'w') as f:
+            f.write('{}')
+        self.assertTrue(
+            os.path.exists(os.path.join(self.tmp_dir, WEIGHTS_NAME)))
+        model2 = Swift.from_pretrained(model2, self.tmp_dir)
+        state_dict = model.state_dict()
+        state_dict2 = model2.state_dict()
+        for key in state_dict:
+            self.assertTrue(key in state_dict2)
+            self.assertTrue(
+                all(
+                    torch.isclose(state_dict[key],
+                                  state_dict2[key]).flatten().detach().cpu()))
