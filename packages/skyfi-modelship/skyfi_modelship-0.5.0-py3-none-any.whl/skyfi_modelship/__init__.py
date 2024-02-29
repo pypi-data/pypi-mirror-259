13b572cd45b86309e7890a0ebde909b866881cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 17:04:39 +0000

    Add package skyfi-modelship/0.5.0/skyfi_modelship-0.5.0-py3-none-any.whl

---
+++ packages/skyfi-modelship/skyfi_modelship-0.5.0-py3-none-any.whl/skyfi_modelship/__init__.py
@@ -0,0 +1,46 @@
+from builtins import int, float, str, list, dict
+
+from .skyfi_app import SkyfiApp
+from .skyfi_types import (
+    Polygon,
+    GeoJSON,
+    PNG,
+    GeoTIFF,
+    ENVI,
+    Package,
+    IntOutput,
+    FloatOutput,
+    StrOutput,
+    PolygonOutput,
+    GeoJSONOutput,
+    PNGOutput,
+    GeoTIFFOutput,
+    ENVIOutput,
+    PackageOutput,
+)
+
+__version__ = "0.5.0"
+
+__all__ = [
+    SkyfiApp,
+    list,
+    dict,
+    int,
+    float,
+    str,
+    Polygon,
+    GeoJSON,
+    PNG,
+    GeoTIFF,
+    ENVI,
+    Package,
+    IntOutput,
+    FloatOutput,
+    StrOutput,
+    PolygonOutput,
+    GeoJSONOutput,
+    PNGOutput,
+    GeoTIFFOutput,
+    ENVIOutput,
+    PackageOutput,
+]
