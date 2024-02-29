04e031f3ec6c6064d7024e07ada4ea6d04d8ac31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:59 +0000

    Add package odoo14-addon-l10n-it-split-payment/14.0.1.0.8.dev6/odoo14_addon_l10n_it_split_payment-14.0.1.0.8.dev6-py3-none-any.whl

---
+++ packages/odoo14-addon-l10n-it-split-payment/odoo14_addon_l10n_it_split_payment-14.0.1.0.8.dev6-py3-none-any.whl/odoo/addons/l10n_it_split_payment/migrations/14.0.1.0.0/pre-move_split_amount.py
@@ -0,0 +1,33 @@
+#  Copyright 2021 Simone Rubino - Agile Business Group
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    if not openupgrade.column_exists(env.cr, "account_move", "amount_sp"):
+        openupgrade.add_fields(
+            env,
+            [
+                (
+                    "amount_sp",
+                    "account.move",
+                    False,
+                    "float",
+                    False,
+                    "l10n_it_split_payment",
+                )
+            ],
+        )
+        openupgrade.logged_query(
+            env.cr,
+            """
+            update account_move
+            set
+                amount_sp = inv.amount_sp
+            from account_invoice inv
+            where
+                account_move.id = inv.move_id;
+        """,
+        )
