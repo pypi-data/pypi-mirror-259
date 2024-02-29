22d4574bbb11fe4a170f47db264f85c1c2ab9eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:03:43 +0000

    Add package hatchet/1.4.0rc4/hatchet-1.4.0rc4-cp312-cp312-win_amd64.whl

---
+++ packages/hatchet/hatchet-1.4.0rc4-cp312-cp312-win_amd64.whl/hatchet/external/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2017-2024 Lawrence Livermore National Security, LLC and other
+# Hatchet Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
