93168db11988356bd1cfe013d80842a7becb953b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 15:04:32 +0000

    Add package deksecrets/0.1.3/deksecrets-0.1.3.tar.gz

---
+++ packages/deksecrets/deksecrets-0.1.3.tar.gz/deksecrets-0.1.3/deksecrets/click/infisical.py
@@ -0,0 +1,27 @@
+import sys
+import json
+import typer
+from getpass import getpass
+from dektools.file import write_file
+from ..tools.infisical import fetch_secrets
+
+app = typer.Typer(add_completion=False)
+
+
+@app.command()
+def env(site_url, client_id, project, environment, path=None, out=None, client_secret=None, fmt=None):
+    if not client_secret:
+        client_secret = getpass('Please input the ClientSecret:')
+    data = fetch_secrets(site_url, client_id, client_secret, project, environment, path)
+    fmt = fmt or 'env'
+    if fmt == 'env':
+        s = "\n".join(f'{k}="{v}"' for k, v in data.items())
+    elif fmt == 'json':
+        s = json.dumps(data)
+    else:
+        raise TypeError(f'Please provide a correct format: {fmt}')
+    if out:
+        write_file(out, s=s)
+    else:
+        out = write_file(f'.{fmt}', s=s, t=True)
+        sys.stdout.write(out)
