5d9a9945f1bda91b17326d49e1fd3c4dd1b31d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 05:04:29 +0000

    Add package pytigon-batteries/0.240228/pytigon-batteries-0.240228.tar.gz

---
+++ packages/pytigon-batteries/pytigon-batteries-0.240228.tar.gz/pytigon-batteries-0.240228/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+
+def package_files(directory, ext=None):
+    paths = []
+    for path, directories, filenames in os.walk(directory):
+        for filename in filenames:
+            if not ext or (ext and filename.endswith(ext)):
+                paths.append(os.path.join("..", path, filename))
+    return paths
+
+
+extra_files = ["../requirements.txt"]
+
+
+with open("requirements.txt") as f:
+    tmp = f.read().strip().split("\n")
+    install_requires = [pos for pos in tmp]
+
+setup(
+    name="pytigon-batteries",
+    version="0.240228",
+    description="Pytigon library",
+    author="Sławomir Chołaj",
+    author_email="slawomir.cholaj@gmail.com",
+    license="LGPLv3",
+    packages=find_packages(),
+    package_data={"": extra_files},
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3",
+)
