5d27cfed9efc7bc8707f2e79890f3f29acee3133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 15:04:22 +0000

    Add package arslaanhttp/0.0.2/arslaanhttp-0.0.2.tar.gz

---
+++ packages/arslaanhttp/arslaanhttp-0.0.2.tar.gz/arslaanhttp-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arslaanhttp',
+    version='0.0.2',
+    description='A simple HTTP server.',
+    url='',
+    author='Arslaan Pathan',
+    author_email='xminecrafterfun@gmail.com',
+    license='MIT',
+    keywords='http',
+    packages=find_packages(),
+    install_requires=['']
+)
