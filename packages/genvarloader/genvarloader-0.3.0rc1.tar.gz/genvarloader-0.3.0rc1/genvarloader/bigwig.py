2173e0f41666a15409b24deef4c07b951b3088e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 14:04:14 +0000

    Add package genvarloader/0.3.0rc1/genvarloader-0.3.0rc1.tar.gz

---
+++ packages/genvarloader/genvarloader-0.3.0rc1.tar.gz/genvarloader-0.3.0rc1/genvarloader/bigwig.py
@@ -0,0 +1,84 @@
+from typing import Dict
+
+import numpy as np
+import pyBigWig
+from numpy.typing import ArrayLike, NDArray
+
+from .types import Reader
+from .util import get_rel_starts
+
+
+class BigWigs(Reader):
+    dtype: np.float32  # pyBigWig always returns f32
+    chunked = False
+
+    def __init__(self, name: str, paths: Dict[str, str]) -> None:
+        """Read data from bigWig files.
+
+        Parameters
+        ----------
+        name : str
+            Name of the reader, for example `'signal'`.
+        paths : Dict[str, str]
+            Dictionary of sample names and paths to bigWig files for those samples.
+        """
+        self.name = name
+        self.paths = paths
+        self.readers = None
+        self.samples = list(self.paths)
+        self.coords = {"sample": np.asarray(self.samples)}
+        self.sizes = {"sample": len(self.samples)}
+        f = pyBigWig.open(next(iter((self.paths.values()))), "r")
+        self.contigs: Dict[str, int] = {
+            contig: int(length) for contig, length in f.chroms().items()
+        }
+        f.close()
+
+    def read(
+        self,
+        contig: str,
+        starts: ArrayLike,
+        ends: ArrayLike,
+        **kwargs,
+    ) -> NDArray[np.float32]:
+        """Read data corresponding to given genomic coordinates. The output shape will
+        have length as the final dimension/axis i.e. (..., length).
+
+        Parameters
+        ----------
+        contig : str
+            Name of the contig/chromosome.
+        starts : ArrayLike
+            Start coordinates, 0-based.
+        ends : ArrayLike
+            End coordinates, 0-based, exclusive.
+        sample : ArrayLike
+            Name of the samples to read data from.
+
+        Returns
+        -------
+        NDArray
+            Shape: (samples length). Data corresponding to the given genomic coordinates and samples.
+        """
+        if self.readers is None:
+            self.readers = {s: pyBigWig.open(p, "r") for s, p in self.paths.items()}
+
+        samples = kwargs.get("sample", self.samples)
+        if isinstance(samples, str):
+            samples = [samples]
+        if not set(samples).issubset(self.samples):
+            raise ValueError(f"Sample {samples} not found in bigWig paths.")
+
+        starts = np.atleast_1d(np.asarray(starts, dtype=int))
+        ends = np.atleast_1d(np.asarray(ends, dtype=int))
+        rel_starts = get_rel_starts(starts, ends)
+        rel_ends = rel_starts + (ends - starts)
+
+        out = np.empty((len(samples), (ends - starts).sum()), dtype=np.float32)
+        for s, e, r_s, r_e in zip(starts, ends, rel_starts, rel_ends):
+            for i, sample in enumerate(samples):
+                out[i, r_s:r_e] = self.readers[sample].values(contig, s, e, numpy=True)
+
+        out[np.isnan(out)] = 0
+
+        return out
---
+++ packages/genvarloader/genvarloader-0.3.0rc1.tar.gz/genvarloader-0.3.0rc1/genvarloader/variants/__init__.py
@@ -0,0 +1,238 @@
+from pathlib import Path
+from typing import Dict, Optional, Tuple, Union
+
+import numpy as np
+from attrs import define
+from numpy.typing import ArrayLike, NDArray
+
+from ..util import normalize_contig_name
+from .genotypes import Genotypes, MemmapGenos, PgenGenos, VCFGenos, ZarrGenos
+from .records import Records, VLenAlleles
+
+__all__ = [
+    "PgenGenos",
+    "ZarrGenos",
+    "MemmapGenos",
+    "VCFGenos",
+    "Variants",
+    "Records",
+]
+
+
+@define
+class DenseGenotypes:
+    """Dense array(s) of genotypes.
+
+    Attributes
+    ----------
+    positions : NDArray[np.int32]
+        Shape: (variants)
+    size_diffs : NDArray[np.int32]
+        Shape : (variants). Difference in length between the REF and the ALT alleles.
+    ref : VLenAlleles
+        Shape: (variants). REF alleles.
+    alt : VLenAlleles
+        Shape: (variants). ALT alleles.
+    genotypes : NDArray[np.int8]
+        Shape: (samples, ploid, variants)
+    offsets : NDArray[np.uint32], optional
+        Shape: (regions + 1). Offsets for the index boundaries of each region such
+        that variants for region `i` are `positions[offsets[i] : offsets[i+1]]`,
+        `size_diffs[offsets[i] : offsets[i+1]]`, ..., etc.
+    """
+
+    positions: NDArray[np.int32]
+    size_diffs: NDArray[np.int32]
+    ref: VLenAlleles
+    alt: VLenAlleles
+    genotypes: NDArray[np.int8]
+    offsets: NDArray[np.uint32]
+
+
+@define
+class Variants:
+    records: Records
+    genotypes: Genotypes
+    _sample_idxs: Optional[NDArray[np.intp]] = None
+
+    @property
+    def chunked(self):
+        return self.genotypes.chunked
+
+    @property
+    def samples(self):
+        if self._sample_idxs is None:
+            return self.genotypes.samples
+        return self.genotypes.samples[self._sample_idxs]
+
+    @property
+    def n_samples(self):
+        if self._sample_idxs is None:
+            return self.genotypes.n_samples
+        return len(self._sample_idxs)
+
+    @property
+    def ploidy(self):
+        return self.genotypes.ploidy
+
+    @classmethod
+    def from_vcf(cls, vcf: Union[str, Path, Dict[str, Path]], use_cache: bool = True):
+        records = Records.from_vcf(vcf)
+
+        if use_cache:
+            try:
+                genotypes = ZarrGenos(vcf)
+            except FileNotFoundError:
+                genotypes = VCFGenos(vcf, records.contig_offsets)
+        else:
+            genotypes = VCFGenos(vcf, records.contig_offsets)
+
+        return cls(records, genotypes)
+
+    @classmethod
+    def from_gvl(cls, path: Union[str, Path, Dict[str, Path]]):
+        """Construct a Variants object from GVL files. The path(s) must end with `.gvl`.
+
+        Parameters
+        ----------
+        path : Union[str, Path, Dict[str, Path]]
+            Path to the GVL file(s).
+
+        Returns
+        -------
+        Variants
+        """
+        records = Records.from_gvl_arrow(path)
+        genotypes = ZarrGenos(path)
+        return cls(records, genotypes)
+
+    # TODO: read sample names from .psam file by using #IID or IID column. Implement a .psam reader.
+    @classmethod
+    def from_pgen(
+        cls, pgen: Union[str, Path, Dict[str, Path]], sample_names: ArrayLike
+    ):
+        if isinstance(pgen, str):
+            pgen = Path(pgen)
+
+        if isinstance(pgen, Path):
+            pgen = {"_all": pgen}
+        records = Records.from_pvar(
+            {c: p.with_suffix(".pvar") for c, p in pgen.items()}
+        )
+        sample_names = np.atleast_1d(np.asarray(sample_names))
+        genotypes = PgenGenos(pgen, sample_names)
+        return cls(records, genotypes)
+
+    def subset_samples(self, samples: ArrayLike):
+        samples = np.atleast_1d(np.asarray(samples, dtype=str))
+        geno_sample_idxs, sample_idxs = np.intersect1d(
+            self.genotypes.samples, samples, return_indices=True
+        )[1:]
+        if len(sample_idxs) != len(samples):
+            raise ValueError("Some samples were not found")
+        self._sample_idxs = geno_sample_idxs[sample_idxs]
+
+    def read(
+        self,
+        contig: str,
+        starts: ArrayLike,
+        ends: ArrayLike,
+        sample: Optional[ArrayLike] = None,
+        ploid: Optional[ArrayLike] = None,
+    ):
+        contig = normalize_contig_name(
+            contig, self.records.contigs
+        )  # pyright: ignore[reportAssignmentType]
+        if contig is None:
+            return None
+
+        starts = np.atleast_1d(np.asarray(starts, dtype=int))
+        ends = np.atleast_1d(np.asarray(ends, dtype=int))
+
+        recs = self.records.vars_in_range(contig, starts, ends)
+        if recs is None:
+            return None
+
+        if sample is not None:
+            sample = np.atleast_1d(np.asarray(sample, dtype=str))
+            geno_sample_idxs, sample_idxs = np.intersect1d(
+                self.genotypes.samples, sample, return_indices=True
+            )[1:]
+            sample_idxs = geno_sample_idxs[sample_idxs]
+            if len(sample_idxs) != len(sample):
+                raise ValueError("Some samples were not found")
+        else:
+            geno_sample_idxs = self._sample_idxs
+
+        if ploid is not None:
+            ploid = np.atleast_1d(np.asarray(ploid, dtype=int))
+
+        # (s p v)
+        genos = self.genotypes.read(
+            contig, recs.start_idxs, recs.end_idxs, sample_idxs, ploid
+        )
+
+        return DenseGenotypes(
+            recs.positions,
+            recs.size_diffs,
+            recs.refs,
+            recs.alts,
+            genos,
+            recs.offsets,
+        )
+
+    def read_for_haplotype_construction(
+        self,
+        contig: str,
+        starts: ArrayLike,
+        ends: ArrayLike,
+        sample: Optional[ArrayLike] = None,
+        ploid: Optional[ArrayLike] = None,
+    ) -> Tuple[Optional[DenseGenotypes], NDArray[np.int32]]:
+        starts = np.atleast_1d(np.asarray(starts, dtype=int))
+        ends = np.atleast_1d(np.asarray(ends, dtype=int))
+
+        contig = normalize_contig_name(
+            contig, self.records.contigs
+        )  # pyright: ignore[reportAssignmentType]
+        if contig is None:
+            return None, ends.astype(np.int32)
+
+        recs, max_ends = self.records.vars_in_range_for_haplotype_construction(
+            contig, starts, ends
+        )
+        if recs is None:
+            return None, ends
+
+        if sample is not None:
+            sample = np.atleast_1d(np.asarray(sample, dtype=str))
+            geno_sample_idxs, sample_idxs = np.intersect1d(
+                self.genotypes.samples, sample, return_indices=True
+            )[1:]
+            if len(sample_idxs) != len(sample):
+                raise ValueError("Some samples were not found")
+        else:
+            geno_sample_idxs = None
+
+        if ploid is not None:
+            ploid = np.atleast_1d(np.asarray(ploid, dtype=int))
+
+        # (s p v)
+        genos = self.genotypes.read(
+            contig, recs.start_idxs, recs.end_idxs, geno_sample_idxs, ploid
+        )
+
+        if sample is not None:
+            genos = genos[sample_idxs]
+
+        return (
+            DenseGenotypes(
+                recs.positions,
+                recs.size_diffs,
+                recs.refs,
+                recs.alts,
+                genos,
+                recs.offsets,
+            ),
+            max_ends,
+        )
