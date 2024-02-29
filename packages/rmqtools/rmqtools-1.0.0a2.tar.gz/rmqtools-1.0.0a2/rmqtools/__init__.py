dd03800f7aced6569f0d3b405e91b0852c64691b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 07:04:14 +0000

    Add package rmqtools/1.0.0a2/rmqtools-1.0.0a2.tar.gz

---
+++ packages/rmqtools/rmqtools-1.0.0a2.tar.gz/rmqtools-1.0.0a2/rmqtools/__init__.py
@@ -0,0 +1,27 @@
+"""
+Rmqtools
+========
+
+Provides wrappers on top of the Pika library for easy RabbitMQ development in
+Python.
+
+How to use the documentation
+----------------------------
+Documentation is available in two forms: docstrings provided with the code,
+and an API reference, available on Read the Docs at ...
+"""
+
+__version__ = '1.0.0-alpha'
+
+import logging
+
+# suppress logging warnings while importing rabbitmq-tools
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+from rmqtools.connection import ResponseObject
+from rmqtools.connection import Connection
+from rmqtools.publisher import Publisher
+from rmqtools.subscriber import Subscriber
+from rmqtools.rpc import RpcClient, RpcServer
+
+from rmqtools.rmq import RmqConnection
