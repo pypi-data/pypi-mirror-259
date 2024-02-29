7c262eb81819d66b923ef301c017aff371523939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:05:05 +0000

    Add package AppAuthN/0.0.7/AppAuthN-0.0.7.tar.gz

---
+++ packages/AppAuthN/AppAuthN-0.0.7.tar.gz/AppAuthN-0.0.7/setup.py
@@ -0,0 +1,10 @@
+#setup.py代码里面的name一定要跟程序包src文件下的mylibrary一致
+from setuptools import setup, find_packages
+setup(
+    name='AppAuthN',
+    version='0.0.7',
+    packages=find_packages('src'), #包含所有src中的包
+    package_dir={'': 'src'}, #告訴distutils包都在src下
+    install_requires=['requests'],
+    python_requires='>=3'
+)
