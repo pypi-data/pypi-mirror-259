58dcbe2aa49434f7d78cb6e8d615a608ba0e81a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:04:46 +0000

    Add package hdx-python-api/6.2.2/hdx_python_api-6.2.2.tar.gz

---
+++ packages/hdx-python-api/hdx_python_api-6.2.2.tar.gz/hdx_python_api-6.2.2/workingexample/my_code.py
@@ -0,0 +1,12 @@
+"""
+Generate a dataset
+
+"""
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def generate_dataset():
+    """Create a dataset"""
+    logger.debug("Generating dataset!")
