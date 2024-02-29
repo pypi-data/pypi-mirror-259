686a5f4d3c2424819ffc251ee1785bdcb39876bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 15:04:49 +0000

    Add package pytest-matcher/1.6.0/pytest_matcher-1.6.0.tar.gz

---
+++ packages/pytest-matcher/pytest_matcher-1.6.0.tar.gz/pytest_matcher-1.6.0/src/matcher/plugin.py
@@ -0,0 +1,395 @@
+#
+# SPDX-FileCopyrightText: 2017-now, See `CONTRIBUTORS.lst`
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+"""A `pytest` plugin to match test output against expectations."""
+
+from __future__ import annotations
+
+# Standard imports
+import os
+import pathlib
+import platform
+import re
+import shutil
+import sys
+import urllib.parse
+from typing import Final, cast
+
+# Third party packages
+import pytest
+import yaml
+
+if sys.version_info < (3, 11):
+    _RE_NOFLAG: Final[re.RegexFlag] = cast(re.RegexFlag, 0)
+else:
+    _RE_NOFLAG: Final[re.RegexFlag] = re.NOFLAG
+
+
+class _ContentMatchResult:
+    """TODO Is this job for Python `dataclass`?"""
+
+    def __init__(self, *, result: bool, text: list[str], regex: str, filename: pathlib.Path) -> None:
+        self._result = result
+        self._text = text
+        self._regex = regex
+        self._filename = filename
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, bool):
+            return self._result == other
+        return False
+
+    def __bool__(self) -> bool:
+        return self._result
+
+    def report_regex_mismatch(self) -> list[str]:
+        return [
+            ''
+          , "The test output doesn't match to the expected regex"
+          , f'(from `{self._filename}`):'
+          , '---[BEGIN actual output]---'
+          , *self._text
+          , '---[END actual output]---'
+          , '---[BEGIN expected regex]---'
+          , *self._regex.splitlines()
+          , '---[END expected regex]---'
+          ]
+
+
+class _ContentCheckOrStorePattern:
+
+    def __init__(self, filename: pathlib.Path, *, store: bool) -> None:
+        self._pattern_filename = filename
+        self._store = store
+        self._expected_file_content: str | None = None
+
+    def _maybe_store_pattern(self, text: str) -> None:
+        if not self._store:
+            return
+
+        # Make a directory to store a pattern file if it doesn't exist yet
+        if not self._pattern_filename.parent.exists():
+            self._pattern_filename.parent.mkdir(parents=True)
+
+        # Store!
+        self._pattern_filename.write_text(text)
+
+        # Also mark the test as skipped!
+        pytest.skip(f'Pattern file has been saved `{self._pattern_filename}`')
+
+    def _read_expected_file_content(self) -> None:
+        if not (self._pattern_filename.exists() and self._pattern_filename.is_file()):
+            pytest.skip(f'Pattern file not found `{self._pattern_filename}`')
+
+        self._expected_file_content = self._pattern_filename.read_text()
+
+    def __eq__(self, text: object) -> bool:
+        if not isinstance(text, str):
+            msg = 'An argument to `__eq__` must be `str` type'
+            raise TypeError(msg)
+
+        self._maybe_store_pattern(text)
+        self._read_expected_file_content()
+        return self._expected_file_content == text
+
+    def match(self, text: str, flags: re.RegexFlag = _RE_NOFLAG) -> _ContentMatchResult:
+        self._maybe_store_pattern(text)
+        self._read_expected_file_content()
+        content = (
+            '.*\n' if flags & re.MULTILINE else ' '
+          ).join(
+              self._expected_file_content.strip().splitlines()  # type: ignore[union-attr]
+            )
+        try:
+            if flags & re.MULTILINE:
+                what = re.compile('.*' + content + '.*', flags=flags)
+            else:
+                what = re.compile(content, flags=flags)
+
+        except re.error as ex:
+            pytest.skip(f'Compile a regular expression from the pattern has failed: {ex!s}')
+
+        text_lines = text.splitlines()
+
+        m = what.fullmatch(('\n' if flags & re.MULTILINE else ' ').join(text_lines))
+        return _ContentMatchResult(
+            result=m is not None and bool(m)
+          , text=text_lines
+          , regex=self._expected_file_content               # type: ignore[arg-type]
+          , filename=self._pattern_filename
+          )
+
+    def report_compare_mismatch(self, actual: str) -> list[str]:
+        assert self._expected_file_content is not None
+        return [
+            ''
+          , "The test output doesn't equal to the expected"
+          , f'(from `{self._pattern_filename}`):'
+          , '---[BEGIN actual output]---'
+          , *actual.splitlines()
+          , '---[END actual output]---'
+          , '---[BEGIN expected output]---'
+          , *self._expected_file_content.splitlines()
+          , '---[END expected output]---'
+          ]
+
+
+def _try_cli_option(request: pytest.FixtureRequest) -> tuple[pathlib.Path | None, bool]:
+    result = request.config.getoption('--pm-patterns-base-dir')
+    return (pathlib.Path(result) if result is not None else None, True)
+
+
+def _try_ini_option(request: pytest.FixtureRequest) -> tuple[pathlib.Path | None, bool]:
+    result = request.config.getini('pm-patterns-base-dir')
+    return (pathlib.Path(result) if result else None, False)
+
+
+def _try_module_path(request: pytest.FixtureRequest) -> tuple[pathlib.Path, bool]:
+    assert request.path.parent is not None
+    # NOTE Suppose the current test module's directory has `data/expected/` inside
+    return (pathlib.Path(request.path.parent) / 'data' / 'expected', False)
+
+
+def _make_expected_filename(
+    request: pytest.FixtureRequest
+  , ext: str
+  , *
+  , use_system_suffix: bool = True
+  ) -> pathlib.Path:
+    result: pathlib.Path | None = None
+    use_cwd_as_base = False
+    for alg in [_try_cli_option, _try_ini_option, _try_module_path]:
+        result, use_cwd_as_base = alg(request)
+        if result is not None:
+            break
+
+    assert result is not None
+
+    if use_system_suffix:
+        use_system_suffix = request.config.getini('pm-pattern-file-use-system-name')
+        use_system_suffix = use_system_suffix in ('true', '1')
+
+    # Make the found path absolute using pytest's rootdir as the base
+    if not result.is_absolute():
+        if use_cwd_as_base:
+            result = pathlib.Path.cwd()
+        elif request.config.inipath is not None:
+            result = pathlib.Path(request.config.inipath.parent) / result
+        else:
+            # TODO Better error description.
+            pytest.fail('Unable to determine the path to expectation files')
+
+    # Make sure base directory exists
+    if not result.exists():
+        pytest.skip(f'Base directory for pattern-matcher do not exists: `{result}`')
+
+    if request.cls is not None:
+        result /= request.cls.__name__
+
+    result /= (                                             # type: ignore[operator]
+        urllib.parse.quote(request.node.name, safe='[]')
+      + ('-' + platform.system() if use_system_suffix else '')
+      + ext
+      )
+
+    return result
+
+
+@pytest.fixture()
+def expected_out(request: pytest.FixtureRequest) -> _ContentCheckOrStorePattern:
+    """A pytest fixture to match `STDOUT` against a file."""
+    return _ContentCheckOrStorePattern(
+        _make_expected_filename(request, '.out')
+      , store=request.config.getoption('--pm-save-patterns')
+      )
+
+
+@pytest.fixture()
+def expected_err(request: pytest.FixtureRequest) -> _ContentCheckOrStorePattern:
+    """A pytest fixture to match `STDERR` against a file."""
+    return _ContentCheckOrStorePattern(
+        _make_expected_filename(request, '.err')
+      , store=request.config.getoption('--pm-save-patterns')
+      )
+
+
+class _YAMLCheckOrStorePattern:
+
+    def __init__(self, filename: pathlib.Path, *, store: bool) -> None:
+        self._expected_file = filename
+        self._store = store
+
+    def _store_pattern_file(self, result_file: pathlib.Path) -> None:
+        assert self._store, 'Code review required!'
+
+        if not self._expected_file.parent.exists():
+            self._expected_file.parent.mkdir(parents=True)
+
+        assert self._expected_file.parent.is_dir()
+
+        shutil.copy(str(result_file), str(self._expected_file))
+
+    def __eq__(self, result_file: object) -> bool:
+        assert isinstance(result_file, pathlib.Path)
+
+        if self._store:
+            self._store_pattern_file(result_file)
+            return True
+
+        if not result_file.exists():
+            pytest.skip(f'Result YAML file not found `{result_file}`')
+
+        if not self._expected_file.exists():
+            pytest.skip(f'Expected YAML file not found `{self._expected_file}`')
+
+        # Load data to compare
+        with result_file.open('r') as result_fd, self._expected_file.open('r') as expected_fd:
+            self._result = yaml.safe_load(result_fd)
+            self._expected = yaml.safe_load(expected_fd)
+
+            return bool(self._result == self._expected)
+
+    def report_compare_mismatch(self, actual: pathlib.Path) -> list[str]:
+        assert self._result is not None
+        assert self._expected is not None
+        return [
+            ''
+          , f'Comparing the test result (`{actual}`) and the expected (`{self._expected_file}`) YAML files:'
+          , '---[BEGIN actual result]---'
+          , repr(self._result)
+          , '---[END actual result]---'
+          , '---[BEGIN expected result]---'
+          ,  repr(self._expected)
+          , '---[END expected result]---'
+          ]
+
+
+@pytest.fixture()
+def expected_yaml(request: pytest.FixtureRequest) -> _YAMLCheckOrStorePattern:
+    """A pytest fixture to match YAML file content."""
+    return _YAMLCheckOrStorePattern(
+        _make_expected_filename(request, '.yaml', use_system_suffix=False)
+      , store=request.config.getoption('--pm-save-patterns')
+      )
+
+
+class _UnusedFilesReporter:
+    """A reporter to reveal unused pattern files."""
+    def __init__(self, *, return_codes: bool = False) -> None:
+        self._return_codes = return_codes
+
+    def pytest_collection_finish(self, session: pytest.Session) -> None:
+        """Once test items have been collected, check for and show unused files."""
+        if not session.items:
+            return
+
+        for alg in [_try_cli_option, _try_ini_option]:
+            patterns_base_dir, _ = alg(session.items[0]._request)  # type: ignore[attr-defined] # NOQA: SLF001
+            if patterns_base_dir:
+                break
+        else:
+            message = 'Failed to determine a patterns base directory'
+            raise pytest.UsageError(message)
+
+        known_extensions = '.out', '.err'
+
+        all_paths = {
+            p.resolve()
+            for p in patterns_base_dir.rglob('*')
+            if p.is_file() and p.suffix in known_extensions
+          }
+
+        collected_paths = {
+            _make_expected_filename(item._request, ext)  # type: ignore[attr-defined] # NOQA: SLF001
+            for item in session.items
+            for fixture, ext in zip((expected_out, expected_err), known_extensions)
+            if fixture.__name__ in item.fixturenames  # type: ignore[attr-defined]
+          }
+
+        unused_paths = all_paths - collected_paths
+        if unused_paths:
+            sys.stdout.write('\n'.join(map(str, sorted(unused_paths))) + '\n')
+            if self._return_codes:
+                pytest.exit('Found unused pattern files', 1)
+
+
+# BEGIN Pytest hooks
+
+def pytest_assertrepr_compare(op: str, left: object, right: object) -> list[str] | None:  # NOQA: PLR0911
+    """Hook into comparison failure."""
+    if op == '==':
+        match left, right:
+            case _ContentMatchResult() as left, bool(right):
+                return left.report_regex_mismatch()
+
+            case _ContentCheckOrStorePattern() as left, str(right):
+                return left.report_compare_mismatch(right)
+
+            case str(left), _ContentCheckOrStorePattern() as right:
+                return right.report_compare_mismatch(left)
+
+            # Enhance YAML checker failures
+            case _YAMLCheckOrStorePattern() as left, pathlib.Path() as right:
+                return left.report_compare_mismatch(right)
+
+            case pathlib.Path() as left, _YAMLCheckOrStorePattern() as right:
+                return right.report_compare_mismatch(left)
+
+    elif op == 'is':
+        match left, right:
+            case _ContentMatchResult() as left,  bool(right):
+                return left.report_regex_mismatch()
+
+    return None
+
+
+# Add CLI option
+def pytest_addoption(parser: pytest.Parser) -> None:
+    """Add plugin options to CLI and the configuration file."""
+    group = parser.getgroup('pattern matcher')
+    group.addoption(
+        '--pm-save-patterns'
+      , action='store_true'
+        # TODO Better description
+      , help='store matching patterns instead of checking them'
+      )
+    group.addoption(
+        '--pm-patterns-base-dir'
+      , metavar='PATH'
+      , help='base directory to read/write pattern files'
+      , type=pathlib.Path
+      )
+    group.addoption(
+        '--pm-reveal-unused-files'
+      , action='store_true'
+      , help='reveal and print unused pattern files'
+      )
+
+    # Also add INI file (TOML table) options
+    parser.addini(
+        'pm-patterns-base-dir'
+      , help='base directory to read/write pattern files'
+      , default=None
+      )
+    parser.addini(
+        'pm-pattern-file-use-system-name'
+      , help='expect a system name (`platform.system()`) to be a pattern filename suffix'
+      , type='bool'
+      )
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_configure(config: pytest.Config) -> None:
+    """Register additional configuration."""
+    if not config.getoption('--pm-reveal-unused-files'):
+        return
+
+    return_codes = os.getenv('PYTEST_MATCHER_RETURN_CODES', '').lower() in ('yes', 'true', '1')
+    config.option.collectonly = True
+    reporter = _UnusedFilesReporter(return_codes=return_codes)
+    config.pluginmanager.unregister(name='terminalreporter')
+    config.pluginmanager.register(reporter, 'terminalreporter')
+
+# END Pytest hooks
---
+++ packages/pytest-matcher/pytest_matcher-1.6.0.tar.gz/pytest_matcher-1.6.0/test/test_matcher.py
@@ -0,0 +1,284 @@
+#
+# SPDX-FileCopyrightText: 2017-now, See `CONTRIBUTORS.lst`
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+import pytest
+
+pytest_plugins = ['pytester']
+
+@pytest.fixture()
+def ourtestdir(testdir: pytest.Testdir) -> pytest.Testdir:
+    # Write a sample config file
+    testdir.tmpdir.join('pytest.ini').write(
+        '[pytest]\n'
+        'addopts = -vv -ra\n'
+        f'pm-patterns-base-dir = {testdir.tmpdir}'
+      )
+    return testdir
+
+
+def no_file_test(ourtestdir) -> None:
+    # Create a temporary pytest test file
+    ourtestdir.makepyfile("""
+        def test_no_expected_file(capfd, expected_out):
+            stdout, stderr = capfd.readouterr()
+            assert expected_out == stdout
+    """)
+
+    # Run all tests with pytest
+    result = ourtestdir.runpytest()
+    result.assert_outcomes(skipped=1)
+    result.stdout.re_match_lines([
+        '.*Pattern file not found .*test_no_expected_file.out`'
+      ])
+
+
+def simple_test(ourtestdir) -> None:
+    # Write a sample expectations file
+    ourtestdir.tmpdir.join('test_sample_out.out').write('Hello Africa!\n')
+    # Write a sample test (finally)
+    ourtestdir.makepyfile("""
+        def test_sample_out(capfd, expected_out):
+            print('Hello Africa!')
+            stdout, stderr = capfd.readouterr()
+            assert expected_out == stdout
+            assert stderr == ''
+    """)
+
+    # Run all tests with pytest
+    result = ourtestdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def failed_test(ourtestdir) -> None:
+    # Write a sample expectations file
+    ourtestdir.tmpdir.join('test_not_matched.out').write('Hello Africa!')
+    # Write a sample test (finally)
+    ourtestdir.makepyfile("""
+        def test_not_matched(capfd, expected_out):
+            print('Unexpected output')
+            stdout, stderr = capfd.readouterr()
+            assert expected_out == stdout
+    """)
+
+    # Run all tests with pytest
+    result = ourtestdir.runpytest()
+    result.assert_outcomes(failed=1)
+    result.stdout.fnmatch_lines([
+        'E         ---[BEGIN actual output]---'
+      , 'E         Unexpected output'
+      , 'E         ---[END actual output]---'
+      , 'E         ---[BEGIN expected output]---'
+      , 'E         Hello Africa!'
+      , 'E         ---[END expected output]---'
+      ])
+
+
+def regex_match_test(ourtestdir) -> None:
+    # Write a sample expectations file
+    ourtestdir.tmpdir.join('test_sample_out.out').write('.*Africa!\nHello\\s+.*!\n')
+    # Write a sample test (finally)
+    ourtestdir.makepyfile("""
+        def test_sample_out(capfd, expected_out):
+            print('Hello Africa!')
+            print('Hello Asia!')
+            stdout, stderr = capfd.readouterr()
+            assert expected_out.match(stdout) == True
+            assert stderr == ''
+    """)
+
+    # Run all tests with pytest
+    result = ourtestdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def regex_fail_match_test(ourtestdir) -> None:
+    # Write a sample expectations file
+    ourtestdir.tmpdir.join('test_sample_out.out').write('.*Africa!\nEhlo\\s+.*!\n')
+    # Write a sample test (finally)
+    ourtestdir.makepyfile("""
+        def test_sample_out(capfd, expected_out):
+            print('Hello Africa!')
+            print('Hello Asia!')
+            stdout, stderr = capfd.readouterr()
+            assert expected_out.match(stdout) == True
+            assert stderr == ''
+    """)
+
+    # Run all tests with pytest
+    result = ourtestdir.runpytest()
+    result.assert_outcomes(failed=1)
+    result.stdout.re_match_lines([
+        ".*The test output doesn't match to the expected regex"
+      ])
+
+
+def result_yaml_not_found_test(ourtestdir) -> None:
+    # Write a sample test (finally)
+    ourtestdir.makepyfile("""
+        import pathlib
+        def test_yaml(capfd, expected_yaml):
+            assert expected_yaml == pathlib.Path('result.yaml')
+    """)
+    # Run all tests with pytest
+    result = ourtestdir.runpytest()
+    result.assert_outcomes(skipped=1)
+    result.stdout.re_match_lines([
+        '.*Result YAML file not found `result.yaml`'
+      ])
+
+
+def expected_yaml_not_found_test(ourtestdir) -> None:
+    # Write a sample result file
+    ourtestdir.makefile(
+        '.yaml'
+      , result="""
+        some-key: some-value
+        another-key: another-value
+        simple-array:
+            - satu
+            - dua
+            - tiga
+    """)
+    # Write a sample test (finally)
+    ourtestdir.makepyfile("""
+        import pathlib
+        def test_yaml(capfd, expected_yaml):
+            assert expected_yaml == pathlib.Path('result.yaml')
+    """)
+    # Run all tests with pytest
+    result = ourtestdir.runpytest()
+    result.assert_outcomes(skipped=1)
+    result.stdout.re_match_lines([
+        '.*Expected YAML file not found `.*test_yaml.yaml`'
+      ])
+
+
+def yaml_match_test(ourtestdir) -> None:
+    # Write a sample result and expected file
+    ourtestdir.makefile(
+        '.yaml'
+      , expected_yaml="""
+        some-key: some-value
+        another-key: another-value
+        simple-array:
+            - satu
+            - dua
+            - tiga
+    """)
+    ourtestdir.makefile(
+        '.yaml'
+      , test_yaml="""
+        another-key: another-value
+        simple-array:
+            - satu
+            - dua
+            - tiga
+        some-key: some-value
+    """)
+    # Write a sample test (finally)
+    ourtestdir.makepyfile("""
+        import pathlib
+        def test_yaml(capfd, expected_yaml):
+            assert expected_yaml == pathlib.Path('test_yaml.yaml')
+    """)
+    # Run all tests with pytest
+    result = ourtestdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def yaml_match_failure_test(ourtestdir) -> None:
+    # Write a sample result and expected file
+    ourtestdir.makefile(
+        '.yaml'
+      , result="""
+        some-key: some-value
+        another-key: another-value
+        simple-array:
+            - satu
+            - dua
+            - tiga
+    """)
+    ourtestdir.makefile(
+        '.yaml'
+      , test_yaml="""
+        another-key: another-value
+        simple-array:
+            - dua
+            - tiga
+            - satu
+    """)
+    # Write a sample test (finally)
+    ourtestdir.makepyfile("""
+        import pathlib
+        def test_yaml(capfd, expected_yaml):
+            assert expected_yaml == pathlib.Path('result.yaml')
+    """)
+
+    # Run all tests with pytest
+    result = ourtestdir.runpytest()
+    result.assert_outcomes(failed=1)
+    result.stdout.fnmatch_lines([
+        'E         ---[BEGIN actual result]---'
+      , 'E         ---[END actual result]---'
+      , 'E         ---[BEGIN expected result]---'
+      , 'E         ---[END expected result]---'
+      ])
+
+
+def parametrized_case_test(ourtestdir) -> None:
+    # Given testing argvalues and expected decorations
+    testing_pairs = [
+        ((0, 'y'), '[0-y]'),
+        ((1, 'some words'), '[1-some%20words]'),
+        ((2, '~/some/path/'), '[2-~%2Fsome%2Fpath%2F]'),
+    ]
+    # Write sample expectation files
+    for values, decoration in testing_pairs:
+        ourtestdir.tmpdir.join(f'test_parametrized{decoration}.out').write(str(values) + '\n')
+
+    # Write a sample test (finally)
+    ourtestdir.makepyfile(f"""
+        import pytest
+        @pytest.mark.parametrize('x,y', {[p[0] for p in testing_pairs]})
+        def test_parametrized(x, y, capfd, expected_out):
+            print((x, y))
+            stdout, stderr = capfd.readouterr()
+            assert expected_out == stdout
+            assert stderr == ''
+    """)
+
+    # Run all tests with pytest
+    result = ourtestdir.runpytest()
+    result.assert_outcomes(passed=3)
+
+
+@pytest.mark.parametrize(('return_codes', 'expected_code'), [(False, 0), (True, 1)])
+def reveal_unused_files_test(return_codes, expected_code, ourtestdir, monkeypatch) -> None:
+    # Write sample expectation files
+    ourtestdir.tmpdir.join('test_a.out').write('')
+    ourtestdir.tmpdir.join('test_a.out.bak').write('')
+    ourtestdir.tmpdir.mkdir('TestClass')
+    ourtestdir.tmpdir.join('TestClass/test_a.out').write('')
+    # Write unused files
+    ourtestdir.tmpdir.join('test_a.err').write('')
+    ourtestdir.tmpdir.join('test_b.out').write('')
+
+    # Write a sample test (finally)
+    ourtestdir.makepyfile("""
+        def test_a(expected_out): pass
+        class TestClass:
+            def test_a(self, expected_out): pass
+    """)
+
+    # Run all tests with pytest
+    if return_codes:
+        monkeypatch.setenv('PYTEST_MATCHER_RETURN_CODES', 'yes')
+
+    result = ourtestdir.runpytest('--pm-reveal-unused-files')
+    assert result.ret == expected_code
+    result.stdout.fnmatch_lines([
+        ourtestdir.tmpdir.join('test_a.err')
+      , ourtestdir.tmpdir.join('test_b.out')
+      ])
