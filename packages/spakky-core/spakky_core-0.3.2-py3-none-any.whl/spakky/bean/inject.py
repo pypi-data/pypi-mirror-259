a64958a18f453e921d4985350b35559811a718cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:04:15 +0000

    Add package spakky-core/0.3.2/spakky_core-0.3.2-py3-none-any.whl

---
+++ packages/spakky-core/spakky_core-0.3.2-py3-none-any.whl/spakky/bean/inject.py
@@ -0,0 +1,26 @@
+from typing import overload
+
+from spakky.bean.application_context import ApplicationContext
+from spakky.core.types import AnyT
+
+
+@overload
+def inject(context: ApplicationContext, *, required_type: type[AnyT]) -> AnyT:
+    ...
+
+
+@overload
+def inject(context: ApplicationContext, *, name: str) -> object:
+    ...
+
+
+def inject(
+    context: ApplicationContext,
+    required_type: type[AnyT] | None = None,
+    name: str | None = None,
+) -> AnyT | object:
+    if name is not None:
+        return context.get(name=name)
+    if required_type is None:  # pragma: no cover
+        raise ValueError("'name' and 'required_type' both cannot be None")
+    return context.get(required_type=required_type)
