60e8fad8ed18210156ff5957c7321029f7c759df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 14:04:17 +0000

    Add package quantscripts/0.1.3/quantscripts-0.1.3.tar.gz

---
+++ packages/quantscripts/quantscripts-0.1.3.tar.gz/quantscripts-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quantscripts',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        # 在这里列出你的包的依赖项
+        'pandas',
+        'numpy',
+        'statsmodels',
+        'matplotlib',
+        'scipy',
+    ],
+    
+    author='tonyyang1223',
+    author_email='tonyyang1223@gmail.com',
+    description='A package for calculating statistical data in finance',
+    long_description=open('README.md').read(),
+    url='https://github.com/tonyyang1223/quantscripts.git',
+    license='MIT',
+    project_urls={
+        'Source': 'https://github.com/tonyyang1223/quantscripts.git',
+    },
+)
+
