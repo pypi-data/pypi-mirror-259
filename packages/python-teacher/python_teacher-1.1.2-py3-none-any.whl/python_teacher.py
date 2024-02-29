2b2c7e0f67fb2aeb7093d9dfd2b36e39f0dc9bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 14:04:30 +0000

    Add package python-teacher/1.1.2/python_teacher-1.1.2-py3-none-any.whl

---
+++ packages/python-teacher/python_teacher-1.1.2-py3-none-any.whl/python_teacher.py
@@ -0,0 +1,14 @@
+# import sys
+def start():
+    version = '1.1.2'
+    print('Starting Python Teacher v'+version)
+    # Main Loop
+    name = input('Enter your name: ')
+    q = input('Are you ready to begin learning '+name+'? [yes]: ')
+    if 'y' in q.lower():
+        print('Time to Learn Python!')
+        #while True
+    
+    # Closing
+    print('Closing Python Teacher')
+
