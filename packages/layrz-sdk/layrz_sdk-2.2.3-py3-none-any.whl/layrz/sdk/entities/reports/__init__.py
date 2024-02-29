46e190e5782bba90950f9e9b018bd6e35fcc4f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:07 +0000

    Add package layrz-sdk/2.2.3/layrz_sdk-2.2.3-py3-none-any.whl

---
+++ packages/layrz-sdk/layrz_sdk-2.2.3-py3-none-any.whl/layrz/sdk/entities/reports/__init__.py
@@ -0,0 +1,7 @@
+""" Init file """
+from .col import ReportCol, ReportDataType
+from .format import ReportFormat
+from .header import ReportHeader
+from .page import ReportPage, CustomReportPage
+from .report import Report
+from .row import ReportRow
