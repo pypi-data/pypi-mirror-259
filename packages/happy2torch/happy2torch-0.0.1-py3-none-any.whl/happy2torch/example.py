bc49a0780636578046b8953e56605272b1c92ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:04:13 +0000

    Add package happy2torch/0.0.1/happy2torch-0.0.1-py3-none-any.whl

---
+++ packages/happy2torch/happy2torch-0.0.1-py3-none-any.whl/happy2torch/example.py
@@ -0,0 +1,10 @@
+'''
+Author: 2-mo 1982800736@qq.com
+Date: 2024-02-27 18:46:41
+LastEditors: 2-mo 1982800736@qq.com
+LastEditTime: 2024-02-27 18:46:53
+FilePath: /a2pip/src/example_package_2-mo/example.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+def add_one(number):
+    return number + 1
