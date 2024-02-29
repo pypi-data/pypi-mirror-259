db5b184514e899399920071af45af5b06035029e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 17:04:21 +0000

    Add package wagtail-mailchimp-integration/0.0.9/wagtail-mailchimp-integration-0.0.9.tar.gz

---
+++ packages/wagtail-mailchimp-integration/wagtail-mailchimp-integration-0.0.9.tar.gz/wagtail-mailchimp-integration-0.0.9/wagtailmailchimp/widgets.py
@@ -0,0 +1,60 @@
+from django.db.utils import ProgrammingError
+from django.forms.widgets import Input, Select
+from wagtail.models import Site
+
+from .api import MailchimpApi
+
+
+class CustomSelect(Select):
+    def create_option(self, *args, **kwargs):
+        option = super().create_option(*args, **kwargs)
+        if not option.get('value'):
+            option['attrs']['disabled'] = True
+
+        return option
+
+
+class MailchimpSubscriberOptinWidget(Input):
+    input_type = 'checkbox'
+    template_name = 'wagtailmailchimp/widgets/subscriber_optin_widget.html'
+
+    def __init__(self, attrs=None, interest_categories=None, label=None, interests_field_name=None):
+        super(MailchimpSubscriberOptinWidget, self).__init__()
+        self.interest_categories = interest_categories
+        self.interests_field_name = interests_field_name
+        self.label = label
+
+    def get_context(self, name, value, attrs):
+        ctx = super(MailchimpSubscriberOptinWidget, self).get_context(name, value, attrs)
+        ctx['widget']['interest_categories'] = self.interest_categories
+        ctx['widget']['label'] = self.label
+        ctx['widget']['interests_field_name'] = self.interests_field_name
+        return ctx
+
+
+class MailchimpAudienceListWidget(Select):
+    def __init__(self, attrs=None, choices=()):
+        super().__init__(attrs, choices)
+
+        audience_lists = self.get_mailchimp_audience_lists()
+        audience_choices = [("", "-- None --")]
+
+        for audience in audience_lists:
+            audience_choices.append([audience.get("id"), audience.get("name")])
+
+        self.choices = audience_choices
+
+    def get_mailchimp_audience_lists(self):
+        from .models import MailchimpSettings
+
+        # catch error where 'wagtailcore_site' relation is not migrated to db yet
+        try:
+            current_site = Site.objects.get(is_default_site=True)
+        except ProgrammingError:
+            return []
+
+        mc_settings = MailchimpSettings.for_site(current_site)
+        mailchimp = MailchimpApi(api_key=mc_settings.api_key)
+        lists = mailchimp.get_lists()
+
+        return lists
