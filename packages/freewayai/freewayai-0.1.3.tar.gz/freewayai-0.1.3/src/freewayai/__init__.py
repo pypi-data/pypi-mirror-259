75fa304ce3b093ba04f1d50ac0217366a6f0a2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 15:04:57 +0000

    Add package freewayai/0.1.3/freewayai-0.1.3.tar.gz

---
+++ packages/freewayai/freewayai-0.1.3.tar.gz/freewayai-0.1.3/src/freewayai/__init__.py
@@ -0,0 +1,9 @@
+from .llm_system import LLMSystem
+from .prompt_template import PromptTemplate
+from .system_logger import SystemLogger
+
+__all__ = [
+  LLMSystem,
+  PromptTemplate,
+  SystemLogger
+]
