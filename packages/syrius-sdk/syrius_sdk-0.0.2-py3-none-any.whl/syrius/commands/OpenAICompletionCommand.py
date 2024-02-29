360449a031e4a7db690e6e9ee5c1324cf3059792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:10 +0000

    Add package syrius-sdk/0.0.2/syrius_sdk-0.0.2-py3-none-any.whl

---
+++ packages/syrius-sdk/syrius_sdk-0.0.2-py3-none-any.whl/syrius/commands/OpenAICompletionCommand.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+from syrius.commands.abstract import Command
+from syrius.types import InputType
+
+
+class OpenAICompletionCommand(Command):
+    id: int = 18
+    messages: InputType[list[dict[str, str]]]
+    api_key: InputType[str]
+    model: InputType[str]
+    temperature: InputType[float]
+    tools: dict[str, Any]
