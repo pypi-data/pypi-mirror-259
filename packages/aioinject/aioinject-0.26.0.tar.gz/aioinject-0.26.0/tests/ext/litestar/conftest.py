f00724c4f11ebc5564b78eba398e28b93173c32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 06:04:39 +0000

    Add package aioinject/0.26.0/aioinject-0.26.0.tar.gz

---
+++ packages/aioinject/aioinject-0.26.0.tar.gz/aioinject-0.26.0/tests/ext/litestar/conftest.py
@@ -0,0 +1,31 @@
+from collections.abc import AsyncIterator
+from typing import Annotated
+
+import httpx
+import pytest
+from litestar import Litestar, get
+
+import aioinject
+from aioinject import Inject
+from aioinject.ext.litestar import AioInjectPlugin, inject
+
+
+@pytest.fixture
+def app(container: aioinject.Container) -> Litestar:
+    @get("/function-route")
+    @inject
+    async def function_route(
+        provided: Annotated[int, Inject],
+    ) -> dict[str, str | int]:
+        return {"value": provided}
+
+    return Litestar(
+        [function_route],
+        plugins=[AioInjectPlugin(container=container)],
+    )
+
+
+@pytest.fixture
+async def http_client(app: Litestar) -> AsyncIterator[httpx.AsyncClient]:
+    async with httpx.AsyncClient(app=app, base_url="http://test") as client:
+        yield client
---
+++ packages/aioinject/aioinject-0.26.0.tar.gz/aioinject-0.26.0/tests/ext/strawberry/conftest.py
@@ -0,0 +1,33 @@
+from collections.abc import AsyncIterator
+from typing import Any
+
+import httpx
+import pytest
+from strawberry import Schema
+from strawberry.asgi import GraphQL
+
+import aioinject
+from aioinject.ext.strawberry import AioInjectExtension
+from tests.ext.strawberry.app import StrawberryApp, _Query
+
+
+@pytest.fixture
+def anyio_backend() -> str:
+    return "asyncio"
+
+
+@pytest.fixture
+async def app(container: aioinject.Container) -> GraphQL[Any, Any]:
+    schema = Schema(
+        query=_Query,
+        extensions=[AioInjectExtension(container=container)],
+    )
+    return StrawberryApp(schema=schema)
+
+
+@pytest.fixture
+async def http_client(
+    app: GraphQL[Any, Any],
+) -> AsyncIterator[httpx.AsyncClient]:
+    async with httpx.AsyncClient(app=app, base_url="http://test") as client:
+        yield client
