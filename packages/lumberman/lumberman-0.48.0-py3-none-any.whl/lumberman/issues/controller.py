c75028f6ada1e52bdfaea7ee25832248ffa4e596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 06:04:47 +0000

    Add package lumberman/0.48.0/lumberman-0.48.0-py3-none-any.whl

---
+++ packages/lumberman/lumberman-0.48.0-py3-none-any.whl/lumberman/issues/controller.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Optional
+
+from rich.progress import Progress, SpinnerColumn, TextColumn
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from .provider import Issue, IssueProvider
+    from .selecter import IssueSelecter
+
+
+@dataclass(frozen=True)
+class IssueController:
+    view: "IssueSelecter"
+    provider: "IssueProvider"
+    in_progress_label: str
+
+    def _get_latest_issues(self) -> "Sequence[Issue]":
+        with Progress(
+            SpinnerColumn(), TextColumn("[progress.description]{task.description}"), transient=True
+        ) as progress:
+            progress.add_task("Getting latest issues", start=True)
+            latest_issues = self.provider.get_latest_issues(
+                in_progress_label=self.in_progress_label
+            )
+
+        if not latest_issues:
+            return []
+
+        return latest_issues
+
+    def _get_my_issues(self) -> "Sequence[Issue]":
+        with Progress(
+            SpinnerColumn(), TextColumn("[progress.description]{task.description}"), transient=True
+        ) as progress:
+            progress.add_task("Getting issues assigned to you", start=True)
+            my_issues = self.provider.get_issues_assigned_to_me(
+                in_progress_label=self.in_progress_label
+            )
+
+        return my_issues
+
+    def label_issue_in_progress(self, issue: "Issue"):
+        self.provider.label_issue(issue, label=self.in_progress_label)
+
+    def select_issue(self, issues: Optional["Sequence[Issue]"] = None) -> "Issue":
+        if not issues:
+            issues = [*self._get_my_issues(), *self._get_latest_issues()]
+
+        selected_issue = self.view.select_issue_dialog(issues)
+
+        if isinstance(selected_issue, str):
+            raise NotImplementedError(f"Command {selected_issue} not implemented")
+
+        return selected_issue
