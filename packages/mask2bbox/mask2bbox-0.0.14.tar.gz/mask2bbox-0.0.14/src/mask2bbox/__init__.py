30629358b6b84cee9edce9c769fbf22c79bcbddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:09 +0000

    Add package mask2bbox/0.0.14/mask2bbox-0.0.14.tar.gz

---
+++ packages/mask2bbox/mask2bbox-0.0.14.tar.gz/mask2bbox-0.0.14/src/mask2bbox/__init__.py
@@ -0,0 +1,2 @@
+from ._bboxes import BBoxes
+from .version import __version__
---
+++ packages/mask2bbox/mask2bbox-0.0.14.tar.gz/mask2bbox-0.0.14/tests/test_mask2bbox/test_mask2bbox.py
@@ -0,0 +1,5 @@
+from mask2bbox import BBoxes
+
+def test_BBoxes():
+    # Create a BBoxes object and get the bounding boxes
+    assert 1==1
