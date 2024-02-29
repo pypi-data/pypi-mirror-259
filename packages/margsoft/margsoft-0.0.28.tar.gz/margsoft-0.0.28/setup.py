7a05a749b4db5c62c9bdafa5f44701ec850016a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:18 +0000

    Add package margsoft/0.0.28/margsoft-0.0.28.tar.gz

---
+++ packages/margsoft/margsoft-0.0.28.tar.gz/margsoft-0.0.28/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from setuptools import find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "datacollection"
+AUTHOR_USER_NAME = "Codebugged-Research"
+SRC_REPO = "src"
+
+
+setuptools.setup(
+    name="margsoft",
+    version="0.0.28",
+    author=AUTHOR_USER_NAME,
+    description="data collection package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="thecodebugged@gmail.com",
+    package_dir={'': "src"},
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(where="src"),
+    license="MIT",
+    install_requires=['opencv-python',
+                   'numpy','ultralytics'],
+        classifiers=[
+        'Intended Audience :: Developers', 'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python :: 3.6', 'Topic :: Utilities',
+        'Programming Language :: Python :: 3.7', 'Topic :: Utilities',
+        'Programming Language :: Python :: 3.8', 'Topic :: Utilities'
+    ], )
