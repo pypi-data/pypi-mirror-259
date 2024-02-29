88b791740283404af26cf6296aa445dca23f128d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 17:04:37 +0000

    Add package hashable-df/0.0.8/hashable_df-0.0.8-py3-none-any.whl

---
+++ packages/hashable-df/hashable_df-0.0.8-py3-none-any.whl/hashable_df/__init__.py
@@ -0,0 +1,28 @@
+"""Make DataFrame cells hashable."""
+import collections
+from autohash import AutoHash
+
+
+def f(value):
+    """Make value hashable."""
+    if isinstance(value, collections.abc.Hashable):
+        return value
+    else:
+        return AutoHash(value)
+
+
+def hashable_df(df):
+    """Return a DataFrame with hashable cell values."""
+    # map might change types. Try to restore the original.
+    orig_types = df.dtypes
+    # Use map on Pandas >= 2.1.0 and applymap below it
+    df = getattr(df, "map", df.applymap)(f).astype(orig_types, errors="ignore")
+    return df
+
+
+def hashable_series(ser):
+    """Return a Series with hashable values."""
+    return ser.map(f)
+
+
+__version__ = '0.0.8'
