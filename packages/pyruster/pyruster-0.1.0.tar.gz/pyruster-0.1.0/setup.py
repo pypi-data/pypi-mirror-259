ac207db2c7e156117a51ea25105c2396a3e700a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:04:25 +0000

    Add package pyruster/0.1.0/pyruster-0.1.0.tar.gz

---
+++ packages/pyruster/pyruster-0.1.0.tar.gz/pyruster-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyruster',
+    version='0.1.0',
+    author='Elling',
+    description='Implementing some syntax like rust.',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
