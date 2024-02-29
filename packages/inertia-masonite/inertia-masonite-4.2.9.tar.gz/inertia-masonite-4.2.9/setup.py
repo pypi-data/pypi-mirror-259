658f0fee3aa5ef2d1f36a9f5969ea57882549236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 06:05:24 +0000

    Add package inertia-masonite/4.2.9/inertia-masonite-4.2.9.tar.gz

---
+++ packages/inertia-masonite/inertia-masonite-4.2.9.tar.gz/inertia-masonite-4.2.9/setup.py
@@ -0,0 +1,70 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="inertia-masonite",
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version="4.2.9",
+    packages=[
+        "masonite.inertia",
+        "masonite.inertia.config",
+        "masonite.inertia.providers",
+        "masonite.inertia.middleware",
+        "masonite.inertia.core",
+        "masonite.inertia.testing",
+    ],
+    package_dir={"": "src"},
+    description="Server-side Masonite adapter for Inertia.js",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # The project's main homepage.
+    url="https://github.com/eaguad1337/masonite-inertia",
+    # Author details
+    author="Samuel Girardin",
+    author_email="samuelgirardin@pm.me",
+    # Choose your license
+    license="MIT license",
+    # If your package should include things you specify in your MANIFEST.in file
+    # Use this option if your package needs to include files that are not python files
+    # like html templates or css files
+    include_package_data=True,
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Masonite",
+    ],
+    # What does your project relate to?
+    keywords="Masonite, Python, Inertia.js",
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=["masonite>=4.0,<5.0"],
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # $ pip install your-package[dev,test]
+    extras_require={
+        "test": ["coverage", "pytest", "pytest-cov", "coveralls"],
+        "dev": [
+            "black",
+            "flake8",
+            "twine>=1.5.0",
+            "wheel",
+            "bumpversion",
+            "masonite-package-sync",
+        ],
+    },
+)
---
+++ packages/inertia-masonite/inertia-masonite-4.2.9.tar.gz/inertia-masonite-4.2.9/src/masonite/inertia/core/InertiaResponse.py
@@ -0,0 +1,40 @@
+import html
+import json
+
+from jinja2.exceptions import TemplateNotFound
+
+from masonite.views import View
+from masonite.facades import View as ViewFacade
+
+from ..helpers import inertia as inertia_helper
+
+
+class InertiaResponse(View):
+    def __init__(self, application, page_data, root_view):
+        super().__init__(application)
+        # TODO: weird we have to do this...
+        self.loaders = ViewFacade.loaders
+        self._shared = ViewFacade._shared
+        self.namespaces = ViewFacade.namespaces
+        # inertia specifics
+        self.component = page_data["component"]
+        self.props = page_data["props"]
+        self.root_view = root_view
+        self.version = page_data["version"]
+        self.page_data = page_data
+        # looks like it's this one which is working
+        self.share({"inertia": inertia_helper})
+
+    def render(self):
+        return super().render(self.root_view, {"page": self.page_data})
+
+    def _render(self):
+        # serialize and escape page data here to keep original dictionary as dict
+        page_payload = html.escape(json.dumps(self.page_data))
+        dictionary = {**self.dictionary, "page": page_payload}
+        try:
+            # Try rendering the template with '.html' appended
+            return self.env.get_template(self.filename).render(dictionary)
+        except TemplateNotFound:
+            # Try rendering the direct template the user has supplied
+            return self.env.get_template(self.template).render(dictionary)
