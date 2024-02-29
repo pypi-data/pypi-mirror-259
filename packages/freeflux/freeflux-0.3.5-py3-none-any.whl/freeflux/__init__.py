947ca8180456d72fe4be16bdf98255895c9f46e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:04:16 +0000

    Add package freeflux/0.3.5/freeflux-0.3.5-py3-none-any.whl

---
+++ packages/freeflux/freeflux-0.3.5-py3-none-any.whl/freeflux/__init__.py
@@ -0,0 +1,9 @@
+__author__ = 'Chao Wu'
+__version__ = '0.3.5'
+
+
+from .core.metabolite import Metabolite
+from .core.reaction import Reaction
+from .core.model import Model
+from .core.emu import EMU
+from .core.mdv import MDV, get_natural_MDV, get_substrate_MDV, conv
