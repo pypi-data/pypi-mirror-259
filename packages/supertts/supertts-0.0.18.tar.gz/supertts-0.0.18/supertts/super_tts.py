90e7b8b7f3cecbc7d982150c6713f9d03580d852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 05:04:05 +0000

    Add package supertts/0.0.18/supertts-0.0.18.tar.gz

---
+++ packages/supertts/supertts-0.0.18.tar.gz/supertts-0.0.18/supertts/super_tts.py
@@ -0,0 +1,55 @@
+from supertts.providers.azure_provider import AzureProvider
+from supertts.providers.openai_provider import OpenAIProvider
+from supertts.providers.google_provider import GoogleProvider
+from supertts.constants import PROVIDERS
+
+class SuperTTS:
+    def __init__(self, provider='openai'):
+        self.provider = provider
+        self.openai = OpenAIProvider()
+        self.azure = AzureProvider()
+        self.google = GoogleProvider()
+
+    def tts(
+        self, 
+        text, 
+        provider=None, 
+        voice=None, 
+        model=None
+    ):
+        """
+        Converts text to speech using the specified voice and model.
+
+        :param text: The text to convert to speech.
+        :param provider: The TTS provider to use. (openai, google, azure)
+        :param voice: The voice to use for the TTS.
+        :param model: The TTS model to use.
+        :return: An audio stream or file.
+        """
+        provider = provider or self.provider
+        if provider == PROVIDERS["openai"]:
+            return self.openai.synthesis(text, voice=voice, model=model)
+        elif provider == PROVIDERS["google"]:
+            return self.google.synthesis(text)
+        elif provider == PROVIDERS["azure"]:
+            return self.azure.synthesis(text)
+        else:
+            raise ValueError(f"Unsupported provider: {provider}")
+
+    def voices(self, provider=None):
+        """
+        Lists available voices for the current TTS provider.
+
+        :param provider: The TTS provider to use. (openai, google, azure)
+        :return: A list of available voices.
+        """
+        provider = provider or self.provider
+        if provider == PROVIDERS["openai"]:
+            tts = self.openai
+        elif provider == PROVIDERS["google"]:
+            tts = self.google
+        elif provider == PROVIDERS["azure"]:
+            tts = self.azure
+        else:
+            raise ValueError(f"Unsupported provider: {provider}")
+        return tts.available_voices()
