9cac5613a57f42218c6df625bfecf2f00b3137d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 15:04:45 +0000

    Add package pujbapy/0.0.2/pujbapy-0.0.2-py3-none-any.whl

---
+++ packages/pujbapy/pujbapy-0.0.2-py3-none-any.whl/pujbapy/utils.py
@@ -0,0 +1,15 @@
+from matplotlib.colors import LinearSegmentedColormap
+import numpy as np
+
+
+DIC_ALTERNATIVAS = {'diferente de': 'two-sided',
+                    'menor que': 'less',
+                    'mayor que': 'greater'}
+
+def crear_paleta_colores(*colors):
+  n_bins = 1000
+  cmap = LinearSegmentedColormap.from_list("cmap_propio", list(colors), N=n_bins)
+  gradient = np.linspace(0, 1, 256)
+  gradient = np.vstack((gradient, gradient))
+
+  return cmap
