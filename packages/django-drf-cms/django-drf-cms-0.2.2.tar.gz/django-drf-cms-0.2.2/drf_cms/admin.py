e11e1ceef652fa6063ff8221e9cb421a8c250ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:55 +0000

    Add package django-drf-cms/0.2.2/django-drf-cms-0.2.2.tar.gz

---
+++ packages/django-drf-cms/django-drf-cms-0.2.2.tar.gz/django-drf-cms-0.2.2/drf_cms/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import *
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    model = Page
+    list_filter = ('site',)
+
+@admin.register(Text)
+class TextAdmin(admin.ModelAdmin):
+    model = Text
+    list_display = ('key', 'page', )
+    list_filter = ('page', 'page__site')
+
+@admin.register(FileData)
+class FileDataAdmin(admin.ModelAdmin):
+	list_display = ('id', 'file', 'description', 'uploaded_at',)
+	readonly_fields=['uploaded_at']
+	search_fields = ['description']
+	list_filter = ('description',)
+	ordering = ('-uploaded_at', )
+
+@admin.register(ImageData)
+class ImageDataAdmin(admin.ModelAdmin):
+	list_display = ('id', 'file', 'description', 'uploaded_at',)
+	readonly_fields=['uploaded_at', 'image_tag',]
+	search_fields = ['description']
+	list_filter = ('description',)
+	ordering = ('-uploaded_at', )
---
+++ packages/django-drf-cms/django-drf-cms-0.2.2.tar.gz/django-drf-cms-0.2.2/drf_cms/serializers.py
@@ -0,0 +1,61 @@
+from rest_framework import serializers
+from .models import Page, Text, FileData, ImageData
+import pathlib
+from django.conf import settings
+from urllib.parse import urlparse
+
+
+class TextSerializer(serializers.ModelSerializer):
+    key = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Text
+        fields = ('content', 'key')
+
+
+class PageSerializer(serializers.ModelSerializer):
+    texts = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Page
+        fields = ('key', 'title', 'description', 'texts')
+
+    def get_texts(self, instance):
+        texts = instance.text_set.all()
+        result = {}
+        for text in texts:
+            result[text.key] = text.content
+        return result
+
+
+class FileDataSerializer(serializers.ModelSerializer):
+	file = serializers.FileField()
+	extra = serializers.SerializerMethodField()
+
+	class Meta:
+		model = FileData
+		fields = ('file', 'id', 'description', 'extra')
+
+	def get_extra(self, instance):
+		if not instance.file:
+			return None
+		if (pathlib.Path(instance.file.url).suffix.lower() == '.mp4'):
+			url = urlparse(instance.file.url)
+			filename = pathlib.Path(url.path).stem
+			base_url = 'https://{}.s3.amazonaws.com/{}'.format(
+				settings.AWS_STORAGE_VIDEO_BUCKET_NAME, filename)
+			file = {
+				'hls': '{}/HLS/{}.m3u8'.format(base_url, filename),
+				'thumbnail': '{}/Thumbnails/{}.0000004.jpg'.format(
+								base_url, filename),
+				'thumbnail_fallback': '{}/Thumbnails/{}.0000002.jpg'.format(
+								base_url, filename)
+			}
+			return file
+		return None
+
+
+class ImageSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = ImageData
+		fields = ('url', 'id', 'description', 'width', 'height')
