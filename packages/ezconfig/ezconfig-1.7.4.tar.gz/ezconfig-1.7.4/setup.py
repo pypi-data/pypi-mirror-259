bb9378acf2a74b967d9244a897e9448f474168d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 06:04:50 +0000

    Add package ezconfig/1.7.4/ezconfig-1.7.4.tar.gz

---
+++ packages/ezconfig/ezconfig-1.7.4.tar.gz/ezconfig-1.7.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="ezconfig",
+      version="1.7.4",
+      description="Utility class for reading configuration files.",
+      author="Nathan Addy",
+      author_email="nathan.addy@windfalltechnical.com",
+      url="https://github.com/maronnax/ezconfig",
+      license="MIT",
+      packages=["ezconfig"],
+      install_requires = ["dateutils"],
+      python_requires='>3.6.0',
+      zip_safe=False
+)
