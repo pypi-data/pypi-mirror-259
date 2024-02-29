531d9197d136bd64b29f4c1999631cf641c089ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:03:39 +0000

    Add package print-package/1.1.0/print_package-1.1.0.tar.gz

---
+++ packages/print-package/print_package-1.1.0.tar.gz/print_package-1.1.0/print_package/print_package.py
@@ -0,0 +1,10 @@
+# print_package.py
+
+def pm(message):
+    """
+    Print the given message to the console.
+
+    Args:
+    - message (str): The message to be printed.
+    """
+    print(message)
