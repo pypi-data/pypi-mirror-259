4fad13fbe9a5536775865d810888e553ed6db999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:55 +0000

    Add package conf-mat/1.0.3/conf-mat-1.0.3.tar.gz

---
+++ packages/conf-mat/conf-mat-1.0.3.tar.gz/conf-mat-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='conf-mat',
+    version='1.0.3',
+    author='khiat Mohammed Abderrezzak',
+    author_email='khiat.abderrezzak@gmail.com',
+    description='Sophisticate Open Confusion Matrix',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "tabulate>=0.9.0",
+        "matplotlib>=3.8.3",
+    ],
+    keywords=[
+        "confusion matrix",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
