ac948331ca2c1584d84561255ed563894bb81bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:03:56 +0000

    Add package perian/0.1.25/perian-0.1.25.tar.gz

---
+++ packages/perian/perian-0.1.25.tar.gz/perian-0.1.25/perian/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from perian.api.admin_api import AdminApi
+from perian.api.instance_type_api import InstanceTypeApi
+from perian.api.job_api import JobApi
+from perian.api.organization_api import OrganizationApi
+from perian.api.service_account_api import ServiceAccountApi
+from perian.api.user_api import UserApi
+
