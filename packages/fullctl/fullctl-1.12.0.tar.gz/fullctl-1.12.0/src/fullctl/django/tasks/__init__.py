2fcc7e8b302b9ed9dea62f25e298dec1c1d8de2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 14:05:13 +0000

    Add package fullctl/1.12.0/fullctl-1.12.0.tar.gz

---
+++ packages/fullctl/fullctl-1.12.0.tar.gz/fullctl-1.12.0/src/fullctl/django/tasks/__init__.py
@@ -0,0 +1,52 @@
+TASK_MODELS = {}
+
+
+def register(cls):
+    TASK_MODELS[cls.HandleRef.tag] = cls
+    return cls
+
+
+def specify_task(task):
+    return TASK_MODELS[task.op].objects.get(id=task.id)
+
+
+def create_tasks_from_json(config, parent=None, user=None, org=None, tasks=None):
+    """
+    Creates tasks from JSON config
+    """
+
+    if not tasks:
+        tasks = []
+
+    task_configs = config.get("tasks", [])
+
+    for task_config in task_configs:
+        op = task_config.get("op")
+
+        model = TASK_MODELS.get(op)
+        param = task_config.get("param", {})
+        args = param.get("args", [])
+        kwargs = param.get("kwargs", {})
+        timeout = task_config.get("timeout", 0)
+
+        task = model.create_task(timeout=timeout, user=user, org=org, *args, **kwargs)
+
+        tasks.append(task)
+
+        create_tasks_from_json(
+            task_config, parent=task, user=user, org=org, tasks=tasks
+        )
+
+    return tasks
+
+
+def requeue(generic_task):
+    """
+    Method to re-queue a task. This will create a new task with the same arguments.
+    """
+    task = specify_task(generic_task)
+    param = task.param
+    new_task = task.__class__.create_task(
+        *param["args"], **param["kwargs"], user=task.user, org=task.org
+    )
+    return new_task
