c85243b062ac076f3209c00973426387c85c3ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:04:09 +0000

    Add package funcaptcha-challenger/0.16.2/funcaptcha_challenger-0.16.2-py3-none-any.whl

---
+++ packages/funcaptcha-challenger/funcaptcha_challenger-0.16.2-py3-none-any.whl/funcaptcha_challenger/rockstack.py
@@ -0,0 +1,11 @@
+from funcaptcha_challenger.model import BaseModel
+from funcaptcha_challenger.predictor import ImagePairClassifierPredictor
+
+
+class RockstackPredictor(ImagePairClassifierPredictor):
+
+    def _get_model(self):
+        return BaseModel("rockstack_v2.onnx")
+
+    def is_support(self, variant, instruction):
+        return 'rockstack' == variant
