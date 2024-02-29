9bc10259cdc1263732c0943158ac45763a959e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 06:04:44 +0000

    Add package matematikk/0.0.152/matematikk-0.0.152.tar.gz

---
+++ packages/matematikk/matematikk-0.0.152.tar.gz/matematikk-0.0.152/src/matematikk/_exports/defs/ch_4_fil.py
@@ -0,0 +1,10 @@
+# 🚀 programmering.no | 🤓 matematikk.as
+# S1 - Eksamen - 2023 Høst (Udir)
+# Oppgave 5 Grensekostnaden K'(x) til bedrift 
+
+def ch_5_fn():
+    print("Dette er en vellykket chain 5-test!")
+
+# Calling-test
+# print("Kaller ch_5_fn() ...")
+# ch_5_fn()
