a15abac3ba999ffa541b051429077b4bb886e035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 17:04:23 +0000

    Add package sentry-integration/0.0.1/sentry-integration-0.0.1.tar.gz

---
+++ packages/sentry-integration/sentry-integration-0.0.1.tar.gz/sentry-integration-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='sentry-integration',
+version='0.0.1',
+author='Subscription-pypi Creatoriq',
+author_email='subscription-pypi@creatoriq.com',
+description='Packageholdername sentry-integration for circle-circumference-math functions',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.9',
+)
