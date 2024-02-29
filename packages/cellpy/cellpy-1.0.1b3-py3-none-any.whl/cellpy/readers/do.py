6181e517487552c2742e6679ba1b93489b98f31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:59 +0000

    Add package cellpy/1.0.1b3/cellpy-1.0.1b3-py3-none-any.whl

---
+++ packages/cellpy/cellpy-1.0.1b3-py3-none-any.whl/cellpy/readers/do.py
@@ -0,0 +1,14 @@
+"""Modifiers for cellpy.CellPyCell objects.
+
+This module is used for modifying cellpy.CellPyCell objects after they have been created.
+All modifiers should take a cellpy.CellPyCell object as input and return a new cellpy.CellPyCell object.
+This is to ensure that the original cellpy.CellPyCell object is not modified in place and that the
+raw data is not changed (unless explicitly requested).
+"""
+
+from copy import deepcopy
+
+
+def copy(c_old):
+    c = deepcopy(c_old)
+    return c
