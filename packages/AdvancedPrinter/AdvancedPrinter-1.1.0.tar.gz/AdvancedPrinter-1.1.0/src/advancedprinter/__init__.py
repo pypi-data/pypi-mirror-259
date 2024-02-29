508d3d9828ba944f6c01357268ec11c52e678c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:03:37 +0000

    Add package AdvancedPrinter/1.1.0/AdvancedPrinter-1.1.0.tar.gz

---
+++ packages/AdvancedPrinter/AdvancedPrinter-1.1.0.tar.gz/AdvancedPrinter-1.1.0/src/advancedprinter/__init__.py
@@ -0,0 +1,7 @@
+from .main import AdvancedPrinter
+
+# Importing print and line functions directly
+print = AdvancedPrinter.print
+line = AdvancedPrinter.line
+frame = AdvancedPrinter.frame
+help = AdvancedPrinter.help
