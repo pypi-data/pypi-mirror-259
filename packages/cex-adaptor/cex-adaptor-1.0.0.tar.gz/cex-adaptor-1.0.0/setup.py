1eaec4d793bf4e56d8abd82123146a143b719597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:03:51 +0000

    Add package cex-adaptor/1.0.0/cex-adaptor-1.0.0.tar.gz

---
+++ packages/cex-adaptor/cex-adaptor-1.0.0.tar.gz/cex-adaptor-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cex-adaptor",
+    version="1.0.0",
+    packages=find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
