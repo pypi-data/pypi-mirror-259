15c1abb3f9b7510b7dc44932ef8bd8524174abf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 17:04:01 +0000

    Add package geomanager/0.4.6/geomanager-0.4.6.tar.gz

---
+++ packages/geomanager/geomanager-0.4.6.tar.gz/geomanager-0.4.6/geomanager/utils/__init__.py
@@ -0,0 +1,22 @@
+import json
+from uuid import UUID
+
+from django.utils.translation import gettext_lazy as _
+
+
+class UUIDEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, UUID):
+            # if the obj is uuid, we simply return the value of uuid
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+DATE_FORMAT_CHOICES = (
+    ("yyyy-MM-dd HH:mm", _("Hour minute:second - (E.g 2023-01-01 00:00)")),
+    ("yyyy-MM-dd", _("Day - (E.g 2023-01-01)")),
+    ("pentadal", _("Pentadal - (E.g Jan 2023 - P1 1-5th)")),
+    ("yyyy-MM", _("Month number - (E.g 2023-01)")),
+    ("MMMM yyyy", _("Month name - (E.g January 2023)")),
+    ("yyyy", _("Year - (E.g 2023)")),
+)
