046017cf98b5776e7e63ba729bc04c5f6c5ac3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:04:10 +0000

    Add package kakao-chat-parser/0.0.1/kakao_chat_parser-0.0.1.tar.gz

---
+++ packages/kakao-chat-parser/kakao_chat_parser-0.0.1.tar.gz/kakao_chat_parser-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kakao_chat_parser',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Donghyeon Han',
+    author_email='dhn2416@gmail.com',
+    description='A python kakaotalk chat parser and utility for analyzing kakaotalk chats',
+    license='MIT',
+    url='https://github.com/gks26/kakao_chat_parser',
+)
