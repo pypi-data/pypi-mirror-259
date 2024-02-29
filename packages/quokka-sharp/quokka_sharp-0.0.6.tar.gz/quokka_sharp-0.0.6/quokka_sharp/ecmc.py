8f1d5b55d72deb8f20bf331f9cbc5e74870183d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 05:04:11 +0000

    Add package quokka-sharp/0.0.6/quokka_sharp-0.0.6.tar.gz

---
+++ packages/quokka-sharp/quokka_sharp-0.0.6.tar.gz/quokka_sharp-0.0.6/quokka_sharp/ecmc.py
@@ -0,0 +1,128 @@
+import argparse
+import copy
+import os
+import re
+import shutil
+import sys
+import tempfile
+import time
+from .encoding.qasm_parser import QASMparser
+from .encoding.qasm2cnf import QASM2CNF
+from subprocess import PIPE, Popen
+from queue import Queue
+from .encoding.memory import ReturnValueThread, memory_monitor
+from .settings import TOOL_PATH
+
+
+def GPMC(cnf_file):
+    # gpmc_path = shutil.which("gpmc")
+    # if gpmc_path == None:
+    #     sys.exit("Binary gpmc not found in path.")
+    gpmc_path = TOOL_PATH
+    proc = Popen([gpmc_path, "-mode=1", cnf_file], stdout= PIPE, stderr=PIPE)
+    # result, error = proc.communicate()
+    return proc
+
+def get_result(result):
+    result = str(result)
+    gpmc_ans_str = re.findall(r"exact.double.prec-sci.(.+?)\\nc s",result)[0]
+    gpmc_ans = float(gpmc_ans_str)
+    if abs(gpmc_ans - 1) > 1e-16:
+        return False
+    else: return True
+
+def checker(i, Z_or_X, cnf):
+    cnf_temp = copy.deepcopy(cnf)
+    cnf_temp.leftProjectZXi(Z_or_X, i)
+    cnf_temp.rightProjectZXi(Z_or_X, i)
+    cnf_file = tempfile.gettempdir() + "/ecmc_eq_check_"+ ("Z" if Z_or_X else "X") + str(i) + ".cnf"
+    cnf_temp.write_to_file(cnf_file)
+    proc = GPMC(cnf_file)
+    return proc
+
+def EQ_check(qasmfile1, qasmfile2):
+
+    encode_start = time.time()
+
+    circuit1 = QASMparser(qasmfile1, True)
+    circuit2 = QASMparser(qasmfile1, True)
+
+    if circuit1.n != circuit2.n:
+            f1 = qasmfile1.split("/")[0]
+            f2 = qasmfile2.split("/")[0]
+            raise Exception("Different number of qubits:,"+ f1 +" has "+ circuit1.n +" while "+ f2 +" has "+ circuit2.n)
+    # print("N: "+ str(circuit1.n) + " Clifford: " + str(len(circuit1.circ) - circuit1.tgate) + " T: " + str(circuit1.tgate))
+    circuit1.dagger()
+    circuit1.merge(circuit2)
+    cnf = QASM2CNF(circuit1)
+
+    encode_end = time.time()
+    encode_time = encode_end - encode_start
+
+    queue = Queue()
+    poll_interval = 0.1
+    monitor_thread = ReturnValueThread(target=memory_monitor, args=(queue, poll_interval))
+    monitor_thread.start()
+    # wait a bit for monitor thread to start measuring mem
+    time.sleep(.5)
+    result = True
+    argulist = []
+    for i in range(cnf.n):
+        argulist.append((i, True,  cnf))
+        argulist.append((i, False, cnf))
+
+    start = time.time()
+# parallel processes
+    N = 16
+    while True:
+        proclist = []
+        length = len(argulist)
+        for i in range(min(N, length)):        
+            argu = argulist[i]
+            p = checker(argu[0], argu[1], argu[2])
+            proclist.append(p)
+        if len(proclist) == 0:
+            break
+        procdict = {proc.pid: proc for proc in proclist}
+        watched_pids = set(proc.pid for proc in proclist)
+        while True:
+            pid, _ = os.wait()
+            if pid in watched_pids:
+                res = procdict[pid].communicate()
+                result = get_result(res[0])
+                if result == False:
+                    break
+                else:
+                    watched_pids.remove(pid)
+            if len(watched_pids) == 0:
+                break
+
+        if result == False:
+            break
+
+        if length > N:
+            argulist = argulist[N: length]
+        else:
+            break
+    
+    for pid in watched_pids:
+        procdict[pid].terminate()
+    
+    end = time.time()
+
+    queue.put('stop')
+    max_rss = monitor_thread.join()
+
+    G1 = len(circuit1.circ)
+    G2 = len(circuit2.circ)
+    print(  ' {"time":', (end - start) + encode_time,
+            ', "result":' + ' "' + str(result) + '"',
+            ', "MaxRSS":', max_rss / 1024 / 1024,
+            ', "N":', cnf.n, ', "G1":', G1, ', "G2":', G2, "}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='ECMC: The quantum circuit Equivalence Checker based on Model Counting from the Quokka-Sharp (Quokka#) package')
+    parser.add_argument('qasmfile1')
+    parser.add_argument('qasmfile2')
+    args = parser.parse_args()
+    EQ_check(args.qasmfile1, args.qasmfile2)
---
+++ packages/quokka-sharp/quokka_sharp-0.0.6.tar.gz/quokka_sharp-0.0.6/quokka_sharp/qcmc.py
@@ -0,0 +1,79 @@
+import argparse
+import math
+import re
+import shutil
+import sys
+import time 
+from .encoding.qasm_parser import QASMparser
+from .encoding.qasm2cnf import QASM2CNF
+from .encoding.memory import ReturnValueThread, memory_monitor
+from queue import Queue
+from time import sleep
+from subprocess import Popen, PIPE, TimeoutExpired
+from .settings import TOOL_PATH
+
+def QC2SAT(qasm_file, multi_or_single):
+    wmc_file = qasm_file + ".cnf"
+    prep_start = time.time()
+    circuit = QASMparser(qasm_file, True)
+    circuit.add_measurement(multi_or_single)
+    cnf = QASM2CNF(circuit)
+    cnf.leftProjectAllZero()
+    ## Alternative to circuit.add_measurement(multi_or_single):
+    # if multi_or_single:
+    #     cnf.rightProjectAllZero()
+    # else:
+    #     cnf.rightProjectZXi(True, 0)
+    cnf.write_to_file(wmc_file)
+    prep_end = time.time()
+    t_prep = round(prep_end - prep_start, 3)
+    return [t_prep, circuit.n]
+
+def GPMC(qasm_file, n, multi_or_single):
+    # gpmc_path = shutil.which("gpmc")
+    gpmc_path = TOOL_PATH
+    if gpmc_path == None:
+        sys.exit("Binary gpmc not found in path.")
+    wmc_file = qasm_file + ".cnf"
+    p = Popen([gpmc_path, "-mode=1", wmc_file], stdout= PIPE, stderr=PIPE)
+    start_time = time.time()
+    result,err = p.communicate()
+    result = str(result)
+    end_time = time.time()
+    gpmc_time = end_time - start_time
+    gpmc_ans_str = re.findall(r"exact.double.prec-sci.(.+?)\\nc s",result)[0]
+    gpmc_ans = float(gpmc_ans_str)
+    if multi_or_single == "multi":
+        prob = gpmc_ans / math.pow(2,int(n))
+    else:
+        prob = gpmc_ans/2+1/2        
+    return gpmc_time, prob
+
+
+def Measure(qasm_file, multi_or_single):
+    # start monitor thread for measuring mem
+    queue = Queue()
+    poll_interval = 0.1
+    monitor_thread = ReturnValueThread(target=memory_monitor, args=(queue, poll_interval))
+    monitor_thread.start()
+    # wait a bit for monitor thread to start measuring mem
+    sleep(.5)    
+    encode_time, n = QC2SAT(qasm_file, multi_or_single)
+    gpmc_time, prob = GPMC(qasm_file, n, multi_or_single)
+    
+    time = encode_time + gpmc_time
+    
+    queue.put('stop')
+    max_rss = monitor_thread.join()
+    
+    print(qasm_file.split("/")[-1], 
+            ' time:', time,
+            ' prob:', prob,
+            ' Max RSS:', max_rss / 1024 / 1024, "MB")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='QCMC: The Quantum Circuit simulator based on Model Counting from the Quokka-Sharp (Quokka#) package')
+    parser.add_argument('filename')
+    parser.add_argument('-m', '--measurement', choices=['firstzero', 'allzero'])
+    args = parser.parse_args()
+    Measure(args.filename, args.measurement == 'allzero')
