e4dfaa7719c7286398d419ff26403a173ba7271e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:04:07 +0000

    Add package kfactory/0.11.4/kfactory-0.11.4-py3-none-any.whl

---
+++ packages/kfactory/kfactory-0.11.4-py3-none-any.whl/kfactory/cells/bezier.py
@@ -0,0 +1,153 @@
+"""Bezier curve based bends and functions."""
+
+from collections.abc import Sequence
+from typing import Any
+
+import numpy as np
+import numpy.typing as nty
+from scipy.special import binom  # type:ignore[import-untyped,unused-ignore]
+
+from .. import KCell, KCLayout, LayerEnum, kcl, kdb
+from ..enclosure import LayerEnclosure
+
+__all__ = ["bend_s"]
+
+
+def bezier_curve(
+    t: nty.NDArray[np.float64],
+    control_points: Sequence[tuple[np.float64 | float, np.float64 | float]],
+) -> list[kdb.DPoint]:
+    """Calculates the backbone of a bezier bend."""
+    xs = np.zeros(t.shape, dtype=np.float64)
+    ys = np.zeros(t.shape, dtype=np.float64)
+    n = len(control_points) - 1
+    for k in range(n + 1):
+        ank = binom(n, k) * (1 - t) ** (n - k) * t**k
+        xs += ank * control_points[k][0]
+        ys += ank * control_points[k][1]
+
+    return [kdb.DPoint(float(x), float(y)) for x, y in zip(xs, ys)]
+
+
+class BendS:
+    """Creat a bezier bend.
+
+    Args:
+        width: Width of the core. [um]
+        height: height difference of left/right. [um]
+        length: Length of the bend. [um]
+        layer: Layer index of the core.
+        nb_points: Number of points of the backbone.
+        t_start: start
+        t_stop: end
+        enclosure: Slab/Exclude definition. [dbu]
+    """
+
+    kcl: KCLayout
+
+    def __init__(
+        self, kcl: KCLayout, basename: str | None = None, **cell_kwargs: Any
+    ) -> None:
+        """Bezier bend function on custom KCLayout."""
+        self.kcl = kcl
+        self._cell = self.kcl.cell(
+            basename=basename or self.__class__.__name__, **cell_kwargs
+        )(self._kcell)
+
+    def __call__(
+        self,
+        width: float,
+        height: float,
+        length: float,
+        layer: int | LayerEnum,
+        nb_points: int = 99,
+        t_start: float = 0,
+        t_stop: float = 1,
+        enclosure: LayerEnclosure | None = None,
+    ) -> KCell:
+        """Creat a bezier bend.
+
+        Args:
+            width: Width of the core. [um]
+            height: height difference of left/right. [um]
+            length: Length of the bend. [um]
+            layer: Layer index of the core.
+            nb_points: Number of points of the backbone.
+            t_start: start
+            t_stop: end
+            enclosure: Slab/Exclude definition. [dbu]
+        """
+        return self._cell(
+            width=width,
+            height=height,
+            length=length,
+            layer=layer,
+            nb_points=nb_points,
+            t_start=t_start,
+            t_stop=t_stop,
+            enclosure=enclosure,
+        )
+
+    def _kcell(
+        self,
+        width: float,
+        height: float,
+        length: float,
+        layer: int | LayerEnum,
+        nb_points: int = 99,
+        t_start: float = 0,
+        t_stop: float = 1,
+        enclosure: LayerEnclosure | None = None,
+    ) -> KCell:
+        """Creat a bezier bend.
+
+        Args:
+            width: Width of the core. [um]
+            height: height difference of left/right. [um]
+            length: Length of the bend. [um]
+            layer: Layer index of the core.
+            nb_points: Number of points of the backbone.
+            t_start: start
+            t_stop: end
+            enclosure: Slab/Exclude definition. [dbu]
+        """
+        c = self.kcl.kcell()
+        _length, _height = length, height
+        pts = bezier_curve(
+            control_points=[
+                (0.0, 0.0),
+                (_length / 2, 0.0),
+                (_length / 2, _height),
+                (_length, _height),
+            ],
+            t=np.linspace(t_start, t_stop, nb_points),
+        )
+
+        if enclosure is None:
+            enclosure = LayerEnclosure()
+
+        enclosure.extrude_path(
+            c, path=pts, main_layer=layer, width=width, start_angle=0, end_angle=0
+        )
+
+        c.create_port(
+            width=int(width / c.kcl.dbu),
+            trans=kdb.Trans(2, False, 0, 0),
+            layer=layer,
+            port_type="optical",
+        )
+        c.create_port(
+            width=int(width / c.kcl.dbu),
+            trans=kdb.Trans(
+                0, False, c.bbox().right, c.bbox().top - int(width / c.kcl.dbu) // 2
+            ),
+            layer=layer,
+            port_type="optical",
+        )
+
+        c.auto_rename_ports()
+
+        return c
+
+
+bend_s = BendS(kcl)
---
+++ packages/kfactory/kfactory-0.11.4-py3-none-any.whl/kfactory/cells/dbu/straight.py
@@ -0,0 +1,147 @@
+"""Straight waveguide in dbu.
+
+A waveguide is a rectangle of material with excludes and/or slab around it::
+
+    ┌──────────────────────────────┐
+    │         Slab/Exclude         │
+    ├──────────────────────────────┤
+    │                              │
+    │             Core             │
+    │                              │
+    ├──────────────────────────────┤
+    │         Slab/Exclude         │
+    └──────────────────────────────┘
+
+The slabs and excludes can be given in the form of an
+[Enclosure][kfactory.enclosure.LayerEnclosure].
+"""
+from typing import Any
+
+from ... import KCell, KCLayout, LayerEnum, kcl, kdb
+from ...conf import config
+from ...enclosure import LayerEnclosure
+from ...kcell import Info
+
+__all__ = ["straight"]
+
+
+class Straight:
+    """Waveguide defined in dbu.
+
+        ┌──────────────────────────────┐
+        │         Slab/Exclude         │
+        ├──────────────────────────────┤
+        │                              │
+        │             Core             │
+        │                              │
+        ├──────────────────────────────┤
+        │         Slab/Exclude         │
+        └──────────────────────────────┘
+    Args:
+        width: Waveguide width. [dbu]
+        length: Waveguide length. [dbu]
+        layer: Main layer of the waveguide.
+        enclosure: Definition of slab/excludes. [dbu]
+    """
+
+    kcl: KCLayout
+
+    def __init__(self, kcl: KCLayout, basename: str | None = None, **cell_kwargs: Any):
+        """Initialize A straight class on a defined KCLayout."""
+        self.kcl = kcl
+        self._cell = self.kcl.cell(
+            basename=basename or self.__class__.__name__, **cell_kwargs
+        )(self._kcell)
+
+    def __call__(
+        self,
+        width: int,
+        length: int,
+        layer: int | LayerEnum,
+        enclosure: LayerEnclosure | None = None,
+    ) -> KCell:
+        """Waveguide defined in dbu.
+
+            ┌──────────────────────────────┐
+            │         Slab/Exclude         │
+            ├──────────────────────────────┤
+            │                              │
+            │             Core             │
+            │                              │
+            ├──────────────────────────────┤
+            │         Slab/Exclude         │
+            └──────────────────────────────┘
+        Args:
+            width: Waveguide width. [dbu]
+            length: Waveguide length. [dbu]
+            layer: Main layer of the waveguide.
+            enclosure: Definition of slab/excludes. [dbu]
+        """
+        return self._cell(width=width, length=length, layer=layer, enclosure=enclosure)
+
+    def _kcell(
+        self,
+        width: int,
+        length: int,
+        layer: int | LayerEnum,
+        enclosure: LayerEnclosure | None = None,
+    ) -> KCell:
+        """Waveguide defined in dbu.
+
+            ┌──────────────────────────────┐
+            │         Slab/Exclude         │
+            ├──────────────────────────────┤
+            │                              │
+            │             Core             │
+            │                              │
+            ├──────────────────────────────┤
+            │         Slab/Exclude         │
+            └──────────────────────────────┘
+        Args:
+            width: Waveguide width. [dbu]
+            length: Waveguide length. [dbu]
+            layer: Main layer of the waveguide.
+            enclosure: Definition of slab/excludes. [dbu]
+        """
+        c = self.kcl.kcell()
+
+        if length < 0:
+            config.logger.critical(
+                f"Negative lengths are not allowed {length} as ports"
+                " will be inverted. Please use a positive number. Forcing positive"
+                " lengths."
+            )
+            length = -length
+        if width < 0:
+            config.logger.critical(
+                f"Negative widths are not allowed {width} as ports"
+                " will be inverted. Please use a positive number. Forcing positive"
+                " lengths."
+            )
+            width = -width
+
+        if width // 2 * 2 != width:
+            raise ValueError("The width (w) must be a multiple of 2 database units")
+
+        c.shapes(layer).insert(kdb.Box(0, -width // 2, length, width // 2))
+        c.create_port(trans=kdb.Trans(2, False, 0, 0), layer=layer, width=width)
+        c.create_port(trans=kdb.Trans(0, False, length, 0), layer=layer, width=width)
+
+        if enclosure is not None:
+            enclosure.apply_minkowski_y(c, layer)
+        c.info = Info(
+            **{
+                "width_um": width * c.kcl.dbu,
+                "length_um": length * c.kcl.dbu,
+                "width_dbu": width,
+                "length_dbu": length,
+            }
+        )
+
+        c.boundary = c.dbbox()
+        c.auto_rename_ports()
+        return c
+
+
+straight = Straight(kcl)
+"""Default straight on the "default" kcl."""
