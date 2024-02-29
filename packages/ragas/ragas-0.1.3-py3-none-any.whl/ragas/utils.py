9cbf8ac4c7e39b90e7715017391ac6e06d1a92f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 06:04:34 +0000

    Add package ragas/0.1.3/ragas-0.1.3-py3-none-any.whl

---
+++ packages/ragas/ragas-0.1.3-py3-none-any.whl/ragas/utils.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import logging
+import os
+import typing as t
+from functools import lru_cache
+
+import numpy as np
+
+if t.TYPE_CHECKING:
+    from ragas.metrics.base import Metric
+    from ragas.testset.evolutions import Evolution
+
+DEBUG_ENV_VAR = "RAGAS_DEBUG"
+
+
+@lru_cache(maxsize=1)
+def get_cache_dir() -> str:
+    "get cache location"
+    DEFAULT_XDG_CACHE_HOME = "~/.cache"
+    xdg_cache = os.getenv("XDG_CACHE_HOME", DEFAULT_XDG_CACHE_HOME)
+    default_ragas_cache = os.path.join(xdg_cache, "ragas")
+    return os.path.expanduser(os.getenv("RAGAS_CACHE_HOME", default_ragas_cache))
+
+
+@lru_cache(maxsize=1)
+def get_debug_mode() -> bool:
+    if os.environ.get(DEBUG_ENV_VAR, str(False)).lower() == "true":
+        return True
+    else:
+        return False
+
+
+def check_if_sum_is_close(
+    values: t.List[float], close_to: float, num_places: int
+) -> bool:
+    multiplier = 10**num_places
+    total = sum(int(round(v * multiplier)) for v in values)
+    return total == int(round(close_to * multiplier))
+
+
+def patch_logger(module: str, level: int):
+    # enable debug logging
+    patched_logger = logging.getLogger(module)
+    patched_logger.setLevel(level=level)
+    # Create a handler for the asyncio logger
+    handler = logging.StreamHandler()  # or another type of Handler
+    handler.setLevel(logging.DEBUG)
+    # Optional: Set a formatter if you want a specific format for the logs
+    formatter = logging.Formatter("[%(name)s.%(levelname)s] %(message)s")
+    handler.setFormatter(formatter)
+    # Add the handler to the asyncio logger
+    patched_logger.addHandler(handler)
+    # Set propagate to False if you don't want it to log to the root logger's handlers as well
+    patched_logger.propagate = False
+
+
+# Function to check if an element is NaN
+def is_nan(x):
+    try:
+        return np.isnan(x)
+    except TypeError:
+        return False
+
+
+def get_feature_language(feature: t.Union[Metric, Evolution]) -> t.Optional[str]:
+    from ragas.llms.prompt import Prompt
+
+    languags = [
+        value.language
+        for _, value in vars(feature).items()
+        if isinstance(value, Prompt)
+    ]
+    return languags[0] if len(languags) > 0 else None
