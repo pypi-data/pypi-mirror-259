08de4d101bff4c953d64822feb58c69a437dc2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 14:04:22 +0000

    Add package dateroll/0.0.13/dateroll-0.0.13.tar.gz

---
+++ packages/dateroll/dateroll-0.0.13.tar.gz/dateroll-0.0.13/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+# https://www.digitalocean.com/community/tutorials/how-to-package-and-distribute-python-applications
+
+setup(
+    name="dateroll",
+    version="0.0.13",
+    description="""dateroll makes working with dates less painful.""",
+    author="Anthony Malizzio",
+    author_email="anthony.malizzio@disent.com",
+    url="https://github.com/disentcorp/dateroll",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Utilities"
+    ),
+    packages=find_packages(),
+    package_data={
+        "dateroll": ["README.md", "LICENSE", "DOCS.md"],
+        "dateroll": ["calendars/sampledata.pkl"],
+    },
+    include_package_data=True,
+    install_requires=[
+    ],
+)
