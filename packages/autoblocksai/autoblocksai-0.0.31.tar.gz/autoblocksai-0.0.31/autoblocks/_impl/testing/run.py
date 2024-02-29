e3b6e1665f2a91a325a7e79ae8c87ea5cef669f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 05:05:09 +0000

    Add package autoblocksai/0.0.31/autoblocksai-0.0.31.tar.gz

---
+++ packages/autoblocksai/autoblocksai-0.0.31.tar.gz/autoblocksai-0.0.31/autoblocks/_impl/testing/run.py
@@ -0,0 +1,276 @@
+import asyncio
+import contextvars
+import dataclasses
+import inspect
+import traceback
+from typing import Any
+from typing import Callable
+from typing import List
+from typing import Optional
+
+import httpx
+import orjson
+
+from autoblocks._impl import global_state
+from autoblocks._impl.testing.models import BaseTestCase
+from autoblocks._impl.testing.models import BaseTestEvaluator
+from autoblocks._impl.testing.models import Evaluation
+from autoblocks._impl.util import AutoblocksEnvVar
+from autoblocks._impl.util import gather_with_max_concurrency
+
+# Global httpx client
+client: httpx.AsyncClient = None
+
+# Context
+current_test_id_var = contextvars.ContextVar("current_test_id")
+current_test_case_var = contextvars.ContextVar("current_test_case")
+
+
+def init_client():
+    global client
+    if client is None:
+        cli_server_address = AutoblocksEnvVar.CLI_SERVER_ADDRESS.get()
+        if not cli_server_address:
+            raise RuntimeError(
+                "Autoblocks tests must be run within the context of the testing CLI.\n"
+                "Make sure you are running your test command with:\n"
+                "$ npx autoblocks testing exec -- <your test command>"
+            )
+        client = httpx.AsyncClient(base_url=cli_server_address)
+
+
+def orjson_default(o: Any) -> str:
+    if hasattr(o, "model_dump_json") and callable(o.model_dump_json):
+        # pydantic v2
+        return orjson.loads(o.model_dump_json())
+    elif hasattr(o, "json") and callable(o.json):
+        # pydantic v1
+        return orjson.loads(o.json())
+    raise TypeError
+
+
+def serialize(x: Any) -> Any:
+    return orjson.loads(orjson.dumps(x, default=orjson_default))
+
+
+async def send_error(
+    test_id: str,
+    test_case_hash: Optional[str],
+    evaluator_id: Optional[str],
+    error: Exception,
+) -> None:
+    await client.post(
+        "/errors",
+        json=dict(
+            testExternalId=test_id,
+            testCaseHash=test_case_hash,
+            evaluatorExternalId=evaluator_id,
+            error=dict(
+                name=type(error).__name__,
+                message=str(error),
+                stacktrace=traceback.format_exc(),
+            ),
+        ),
+    )
+
+
+async def evaluate_output(
+    test_id: str,
+    test_case: BaseTestCase,
+    output: Any,
+    evaluator: BaseTestEvaluator,
+):
+    evaluation: Optional[Evaluation] = None
+
+    if inspect.iscoroutinefunction(evaluator.evaluate_test_case):
+        try:
+            evaluation = await evaluator.evaluate_test_case(test_case, output)
+        except Exception as err:
+            await send_error(
+                test_id=test_id,
+                test_case_hash=test_case._cached_hash,
+                evaluator_id=evaluator.id,
+                error=err,
+            )
+    else:
+        try:
+            ctx = contextvars.copy_context()
+            evaluation = await global_state.event_loop().run_in_executor(
+                None,
+                ctx.run,
+                evaluator.evaluate_test_case,
+                test_case,
+                output,
+            )
+        except Exception as err:
+            await send_error(
+                test_id=test_id,
+                test_case_hash=test_case._cached_hash,
+                evaluator_id=evaluator.id,
+                error=err,
+            )
+
+    if evaluation is None:
+        return
+
+    await client.post(
+        "/evals",
+        json=dict(
+            testExternalId=test_id,
+            testCaseHash=test_case._cached_hash,
+            evaluatorExternalId=evaluator.id,
+            score=evaluation.score,
+            threshold=dataclasses.asdict(evaluation.threshold) if evaluation.threshold else None,
+        ),
+    )
+
+
+async def run_test_case(
+    test_id: str,
+    test_case: BaseTestCase,
+    evaluators: List[BaseTestEvaluator],
+    fn: Callable,
+    max_evaluator_concurrency: int,
+):
+    current_test_id_var.set(test_id)
+    current_test_case_var.set(test_case)
+
+    output = None
+
+    if inspect.iscoroutinefunction(fn):
+        try:
+            output = await fn(test_case)
+        except Exception as err:
+            await send_error(
+                test_id=test_id,
+                test_case_hash=test_case._cached_hash,
+                evaluator_id=None,
+                error=err,
+            )
+    else:
+        try:
+            ctx = contextvars.copy_context()
+            output = await global_state.event_loop().run_in_executor(None, ctx.run, fn, test_case)
+        except Exception as err:
+            await send_error(
+                test_id=test_id,
+                test_case_hash=test_case._cached_hash,
+                evaluator_id=None,
+                error=err,
+            )
+
+    if output is None:
+        return
+
+    await client.post(
+        "/results",
+        json=dict(
+            testExternalId=test_id,
+            testCaseHash=test_case._cached_hash,
+            testCaseBody=serialize(test_case),
+            testCaseOutput=serialize(output),
+        ),
+    )
+
+    try:
+        await gather_with_max_concurrency(
+            max_evaluator_concurrency,
+            [
+                evaluate_output(
+                    test_id=test_id,
+                    test_case=test_case,
+                    output=output,
+                    evaluator=evaluator,
+                )
+                for evaluator in evaluators
+            ],
+        )
+    except Exception as err:
+        await send_error(
+            test_id=test_id,
+            test_case_hash=test_case._cached_hash,
+            evaluator_id=None,
+            error=err,
+        )
+
+
+async def async_run_test_suite(
+    test_id: str,
+    test_cases: List[BaseTestCase],
+    evaluators: List[BaseTestEvaluator],
+    fn: Callable,
+    max_test_case_concurrency: int,
+    max_evaluator_concurrency: int,
+):
+    try:
+        assert test_cases, f"[{test_id}] No test cases provided."
+        for test_case in test_cases:
+            assert isinstance(
+                test_case,
+                BaseTestCase,
+            ), f"[{test_id}] Test case {test_case} does not implement {BaseTestCase.__name__}."
+        for evaluator in evaluators:
+            assert isinstance(
+                evaluator,
+                BaseTestEvaluator,
+            ), f"[{test_id}] Evaluator {evaluator} does not implement {BaseTestEvaluator.__name__}."
+    except Exception as err:
+        await send_error(
+            test_id=test_id,
+            test_case_hash=None,
+            evaluator_id=None,
+            error=err,
+        )
+        return
+
+    await client.post("/start", json=dict(testExternalId=test_id))
+
+    try:
+        await gather_with_max_concurrency(
+            max_test_case_concurrency,
+            [
+                run_test_case(
+                    test_id=test_id,
+                    test_case=test_case,
+                    evaluators=evaluators,
+                    fn=fn,
+                    max_evaluator_concurrency=max_evaluator_concurrency,
+                )
+                for test_case in test_cases
+            ],
+        )
+    except Exception as err:
+        await send_error(
+            test_id=test_id,
+            test_case_hash=None,
+            evaluator_id=None,
+            error=err,
+        )
+
+    await client.post("/end", json=dict(testExternalId=test_id))
+
+
+def run_test_suite(
+    id: str,
+    test_cases: List[BaseTestCase],
+    evaluators: List[BaseTestEvaluator],
+    fn: Callable,
+    # How many test cases to run concurrently
+    max_test_case_concurrency: int = 10,
+    # How many evaluators to run concurrently on the result of a test case
+    max_evaluator_concurrency: int = 5,
+):
+    init_client()
+    global_state.init()
+    future = asyncio.run_coroutine_threadsafe(
+        async_run_test_suite(
+            test_id=id,
+            test_cases=test_cases,
+            evaluators=evaluators,
+            fn=fn,
+            max_test_case_concurrency=max_test_case_concurrency,
+            max_evaluator_concurrency=max_evaluator_concurrency,
+        ),
+        global_state.event_loop(),
+    )
+    future.result()
---
+++ packages/autoblocksai/autoblocksai-0.0.31.tar.gz/autoblocksai-0.0.31/autoblocks/_impl/tracer.py
@@ -0,0 +1,179 @@
+import asyncio
+import logging
+import uuid
+from contextlib import contextmanager
+from dataclasses import dataclass
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+from typing import Dict
+from typing import Optional
+
+from autoblocks._impl import global_state
+from autoblocks._impl.config.constants import INGESTION_ENDPOINT
+from autoblocks._impl.util import AutoblocksEnvVar
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class SendEventResponse:
+    trace_id: Optional[str]
+
+
+class AutoblocksTracer:
+    def __init__(
+        self,
+        ingestion_key: Optional[str] = None,
+        # Initialize the tracer with a trace_id. All events sent with this tracer will
+        # send this trace ID unless overwritten by:
+        # - calling set_trace_id
+        # - specifying a trace_id when calling send_event
+        trace_id: Optional[str] = None,
+        # Initialize the tracer with properties. All events sent with this tracer will
+        # send these properties unless overwritten by:
+        # - calling set_properties
+        # Additionally, these properties can be updated by:
+        # - calling update_properties
+        # - specifying properties when calling send_event
+        properties: Optional[Dict] = None,
+        # Timeout for sending events to Autoblocks
+        timeout: timedelta = timedelta(seconds=5),
+    ):
+        global_state.init()  # Start up event loop if not already started
+        self._trace_id: Optional[str] = trace_id
+        self._properties: Dict = properties or {}
+
+        ingestion_key = ingestion_key or AutoblocksEnvVar.INGESTION_KEY.get()
+        if not ingestion_key:
+            raise ValueError(
+                f"You must provide an ingestion_key or set the {AutoblocksEnvVar.INGESTION_KEY} environment variable."
+            )
+
+        self._client_headers = {"Authorization": f"Bearer {ingestion_key}"}
+        self._timeout_seconds = timeout.total_seconds()
+
+    def set_trace_id(self, trace_id: str) -> None:
+        """
+        Set the trace ID for all events sent by this tracer.
+        """
+        self._trace_id = trace_id
+
+    @property
+    def trace_id(self) -> Optional[str]:
+        """
+        Get the trace ID currently set on the tracer.
+        """
+        return self._trace_id
+
+    def set_properties(self, properties: Dict) -> None:
+        """
+        Set the properties for all events sent by this tracer.
+
+        NOTE: This will overwrite any existing properties.
+        """
+        self._properties = properties
+
+    def update_properties(self, properties: Dict) -> None:
+        """
+        Update the properties for all events sent by this tracer.
+        """
+        self._properties.update(properties)
+
+    @contextmanager
+    def start_span(self):
+        props = dict(span_id=str(uuid.uuid4()))
+        prev_span_id = self._properties.get("span_id")
+        prev_parent_span_id = self._properties.get("parent_span_id")
+        if prev_span_id:
+            props["parent_span_id"] = prev_span_id
+        self.update_properties(props)
+
+        try:
+            yield
+        finally:
+            props = dict(self._properties)
+            props.pop("span_id", None)
+            props.pop("parent_span_id", None)
+            if prev_parent_span_id:
+                props["parent_span_id"] = prev_parent_span_id
+            if prev_span_id:
+                props["span_id"] = prev_span_id
+            self.set_properties(props)
+
+    async def _send_event_unsafe(
+        self,
+        # Require all arguments to be specified via key=value
+        *,
+        message: str,
+        trace_id: Optional[str] = None,
+        span_id: Optional[str] = None,
+        parent_span_id: Optional[str] = None,
+        timestamp: Optional[str] = None,
+        properties: Optional[Dict] = None,
+        prompt_tracking: Optional[Dict] = None,
+    ) -> SendEventResponse:
+        merged_properties = dict(self._properties)
+        merged_properties.update(properties or {})
+        if span_id:
+            merged_properties["span_id"] = span_id
+        if parent_span_id:
+            merged_properties["parent_span_id"] = parent_span_id
+        if prompt_tracking:
+            merged_properties["promptTracking"] = prompt_tracking
+
+        trace_id = trace_id or self._trace_id
+        timestamp = timestamp or datetime.now(timezone.utc).isoformat()
+
+        req = await global_state.http_client().post(
+            url=INGESTION_ENDPOINT,
+            json={
+                "message": message,
+                "traceId": trace_id,
+                "timestamp": timestamp,
+                "properties": merged_properties,
+            },
+            headers=self._client_headers,
+            timeout=self._timeout_seconds,
+        )
+        req.raise_for_status()
+        resp = req.json()
+        return SendEventResponse(trace_id=resp.get("traceId"))
+
+    def send_event(
+        self,
+        message: str,
+        # Require all arguments after message to be specified via key=value
+        *,
+        trace_id: Optional[str] = None,
+        span_id: Optional[str] = None,
+        parent_span_id: Optional[str] = None,
+        timestamp: Optional[str] = None,
+        properties: Optional[Dict] = None,
+        prompt_tracking: Optional[Dict] = None,
+    ) -> SendEventResponse:
+        """
+        Sends an event to the Autoblocks ingestion API.
+
+        Always returns a SendEventResponse dataclass as the response. If sending
+        the event failed, the trace_id will be None.
+        """
+        try:
+            future = asyncio.run_coroutine_threadsafe(
+                self._send_event_unsafe(
+                    message=message,
+                    trace_id=trace_id,
+                    span_id=span_id,
+                    parent_span_id=parent_span_id,
+                    timestamp=timestamp,
+                    properties=properties,
+                    prompt_tracking=prompt_tracking,
+                ),
+                global_state.event_loop(),
+            )
+            return future.result()
+        except Exception as err:
+            log.error(f"Failed to send event to Autoblocks: {err}", exc_info=True)
+            if AutoblocksEnvVar.TRACER_THROW_ON_ERROR.get() == "1":
+                raise err
+            return SendEventResponse(trace_id=None)
