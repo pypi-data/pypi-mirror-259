e957f9f903e1759589d7f61b9fde0fb411f8f4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 04:04:17 +0000

    Add package ATH/0.0.4.5/ATH-0.0.4.5.tar.gz

---
+++ packages/ATH/ATH-0.0.4.5.tar.gz/ATH-0.0.4.5/ATH/__init__.py
@@ -0,0 +1,27 @@
+import pyttsx3
+from googlesearch import search
+
+voice = pyttsx3.init()
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    return num1 / num2
+
+def search(query):
+    results = list(search(query, num=3, stop=3, pause=2))
+    return '\n'.join(results)
+
+def voice_search(query):
+    results = list(search(query, num=3, stop=3, pause=2))
+    for result in results:
+        voice.say(result)
+        voice.runAndWait()
+    return '\n'.join(results)
