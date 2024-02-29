46429de4941c89d82dfa2e631fdc8662887af12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 14:05:02 +0000

    Add package openbb-nightly/4.1.4.dev202402290009/openbb_nightly-4.1.4.dev202402290009.tar.gz

---
+++ packages/openbb-nightly/openbb_nightly-4.1.4.dev202402290009.tar.gz/openbb_nightly-4.1.4.dev202402290009/core/openbb_core/app/static/utils/decorators.py
@@ -0,0 +1,79 @@
+"""Decorators for the OpenBB Platform static assets."""
+
+from functools import wraps
+from typing import Any, Callable, Optional, TypeVar, overload
+
+from openbb_core.app.model.abstract.error import OpenBBError
+from openbb_core.env import Env
+from pydantic import ValidationError, validate_call
+from typing_extensions import ParamSpec
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+@overload
+def validate(func: Callable[P, R]) -> Callable[P, R]:
+    pass
+
+
+@overload
+def validate(**dec_kwargs) -> Callable[P, R]:
+    pass
+
+
+def validate(
+    func: Optional[Callable[P, R]] = None,
+    **dec_kwargs,
+) -> Any:
+    """Validate function calls."""
+
+    def decorated(f: Callable[P, R]):
+        """Decorated function."""
+
+        @wraps(f)
+        def wrapper(*f_args, **f_kwargs):
+            return validate_call(f, **dec_kwargs)(*f_args, **f_kwargs)
+
+        return wrapper
+
+    return decorated if func is None else decorated(func)
+
+
+def exception_handler(func: Callable[P, R]) -> Callable[P, R]:
+    """Handle exceptions, attempting to focus on the last call from the traceback."""
+
+    @wraps(func)
+    def wrapper(*f_args, **f_kwargs):
+        try:
+            return func(*f_args, **f_kwargs)
+        except (ValidationError, Exception) as e:
+            if Env().DEBUG_MODE:
+                raise
+            if isinstance(e, ValidationError):
+                error_list = []
+
+                validation_error = f"{e.error_count()} validations errors in {e.title}"
+                for error in e.errors():
+                    arg_error = f"Arg {error['loc'][0]} ->\n"
+                    error_details = (
+                        f"  {error['msg']} "
+                        f"[validation_error_type={error['type']}, "
+                        f"input_type={type(error['input']).__name__}, "
+                        f"input_value={error['input']}]\n"
+                    )
+                    error_info = f"    For further information visit {error['url']}\n"
+                    error_list.append(arg_error + error_details + error_info)
+
+                error_list.insert(0, validation_error)
+                error_str = "\n".join(error_list)
+                raise OpenBBError(
+                    f"\nType -> ValidationError \n\nDetails -> {error_str}"
+                ) from None
+
+            # If the error is not a ValidationError, then it is a generic exception
+            raise OpenBBError(
+                f"\nType -> {e.original.original.__class__.__name__}\n\nDetail -> {str(e)}"
+            ) from None
+
+    return wrapper
