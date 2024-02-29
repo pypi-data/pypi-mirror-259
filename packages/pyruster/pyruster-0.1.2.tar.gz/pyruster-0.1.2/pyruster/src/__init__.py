4790bf09729f587752257896237b22f2c4c50546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:03:49 +0000

    Add package pyruster/0.1.2/pyruster-0.1.2.tar.gz

---
+++ packages/pyruster/pyruster-0.1.2.tar.gz/pyruster-0.1.2/pyruster/src/__init__.py
@@ -0,0 +1,2 @@
+from pyruster.src.rust_result import *  # noqa
+from pyruster.src.rust_option import *  # noqa
---
+++ packages/pyruster/pyruster-0.1.2.tar.gz/pyruster-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyruster',
+    version='0.1.2',
+    author='Elling',
+    description='Implementing some syntax like rust.',
+    long_description=open('README.rst').read(),
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
