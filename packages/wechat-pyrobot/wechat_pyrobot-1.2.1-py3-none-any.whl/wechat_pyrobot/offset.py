e7bd7b48a5e3aa2678caf0a7fb6c88d9782d2582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 16:04:28 +0000

    Add package wechat-pyrobot/1.2.1/wechat_pyrobot-1.2.1-py3-none-any.whl

---
+++ packages/wechat-pyrobot/wechat_pyrobot-1.2.1-py3-none-any.whl/wechat_pyrobot/offset.py
@@ -0,0 +1,24 @@
+
+CALL_OFFSET = {
+    "3.9.8.12": {
+        "SendMsgFreeCallOffset": 0x823370,
+        "SendTextCallOffset": 0xDE22D0,
+        "SendImageCall0Offset": 0x821E40,
+        "SendImageCall1Offset": 0xDE1880,
+        "SendImageCall2Offset": 0xF755C0,
+        "LogEnterCallOffset": 0x102C250,
+        "LogLeaveCallOffset": 0x102C584,
+        "HookMsgCallOffset": 0xE0F743,
+        "RevokeMsgCallOffset": 0xE14880,
+    },
+    "3.9.8.15": {
+        "SendMsgFreeCallOffset": 0x94E590,
+        "SendTextCallOffset": 0x1091CE0,
+        "SendImageCall0Offset": 0x94CD10,
+        "SendImageCall1Offset": 0x10911F0,
+        "LogEnterCallOffset": 0x13D6380,
+        "LogLeaveCallOffset": 0x13D6380,
+        "HookMsgCallOffset": 0x10E8E30,
+        "RevokeMsgCallOffset": 0x10E2A50,
+    }
+}
