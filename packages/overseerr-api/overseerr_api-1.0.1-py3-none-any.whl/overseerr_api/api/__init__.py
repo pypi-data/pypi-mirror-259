f7daad921ca5090c849416d6236107ed5c4a02e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 03:04:25 +0000

    Add package overseerr-api/1.0.1/overseerr_api-1.0.1-py3-none-any.whl

---
+++ packages/overseerr-api/overseerr_api-1.0.1-py3-none-any.whl/overseerr_api/api/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import apis into api package
+from overseerr_api.api.auth_api import AuthApi
+from overseerr_api.api.collection_api import CollectionApi
+from overseerr_api.api.issue_api import IssueApi
+from overseerr_api.api.media_api import MediaApi
+from overseerr_api.api.movies_api import MoviesApi
+from overseerr_api.api.other_api import OtherApi
+from overseerr_api.api.person_api import PersonApi
+from overseerr_api.api.public_api import PublicApi
+from overseerr_api.api.request_api import RequestApi
+from overseerr_api.api.search_api import SearchApi
+from overseerr_api.api.service_api import ServiceApi
+from overseerr_api.api.settings_api import SettingsApi
+from overseerr_api.api.tmdb_api import TmdbApi
+from overseerr_api.api.tv_api import TvApi
+from overseerr_api.api.users_api import UsersApi
+
