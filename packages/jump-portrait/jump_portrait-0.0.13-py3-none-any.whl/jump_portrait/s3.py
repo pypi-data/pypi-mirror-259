7b19a67a2f893a4af49f01212d863a997aa17d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-29 05:05:09 +0000

    Add package jump-portrait/0.0.13/jump_portrait-0.0.13-py3-none-any.whl

---
+++ packages/jump-portrait/jump_portrait-0.0.13-py3-none-any.whl/jump_portrait/s3.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env jupyter
+
+import io
+from io import BytesIO
+
+import boto3
+import matplotlib.image as mpimg
+import numpy as np
+import polars as pl
+import pyarrow as pa
+from botocore import UNSIGNED
+from botocore.config import Config
+from s3path import PureS3Path, S3Path
+
+"""
+Tools to Search for files in AWS's CellPainting Gallery
+S3_BUCKET_NAME = "cellpainting-gallery"
+prefix = "/cpg0020-varchamp/broad/images/"
+
+"""
+
+
+def s3client():
+    return boto3.client("s3", config=Config(signature_version=UNSIGNED))
+
+
+def get_image_from_s3uri(s3_image_uri) -> np.ndarray:
+    # Assumes we are accessing cellpainting-gallery
+
+    s3_image_uri = str(s3_image_uri)  # if instance is S3Path
+
+    # Remove all possible prefixes
+    bucket_name = "cellpainting-gallery"
+    s3_image_uri = s3_image_uri.removeprefix(f"s3://{bucket_name}/")
+    s3_image_uri = s3_image_uri.removeprefix(f"/{bucket_name}/")
+    s3_image_uri = s3_image_uri.removeprefix(f"{bucket_name}/")
+
+    response = s3client().get_object(Bucket="cellpainting-gallery", Key=s3_image_uri)
+    response_body = BytesIO(response["Body"].read())
+
+    if s3_image_uri.endswith(".tif") or s3_image_uri.endswith(".tiff"):
+        result = mpimg.imread(response_body, format="tiff")
+
+    elif s3_image_uri.endswith(".npy"):
+        result = np.load(response_body)
+    else:
+        raise Exception(f"Format not supported for {s3_image_uri}")
+
+    return result
+
+
+def get_corrected_image(
+    images_location: dict,
+    channel: str,
+    correction: str or None,
+    apply_correction: bool = True,
+) -> np.ndarray:
+    """Correct the image from a given location when appropriate by dividing it by another image in the same location dictionary.
+
+    Parameters
+    ----------
+    images_location : dict
+    channel : str
+    correction : str or None
+
+    Returns
+    -------
+    np.ndarray Corrected or raw image
+
+    Examples
+    --------
+    FIXME: Add docs.
+
+
+    """
+    s3_image_path = build_s3_image_path(
+        row=images_location, channel=channel, correction=correction
+    )
+    result = get_image_from_s3uri(s3_image_path)
+
+    if apply_correction and not correction in ("Orig", None):
+        original_image_path = build_s3_image_path(
+            row=images_location, channel=channel, correction="Orig"
+        )
+        result = get_image_from_s3uri(original_image_path) / result
+
+    return result
+
+
+def keys(Bucket, Prefix="", StartAfter="", Delimiter="/"):
+    Prefix = Prefix[1:] if Prefix.startswith(Delimiter) else Prefix
+    if not StartAfter:
+        del StartAfter
+        if Prefix.endswith(Delimiter):
+            StartAfter = Prefix
+    del Delimiter
+    for page in (
+        boto3.client("s3", config=Config(signature_version=UNSIGNED))
+        .get_paginator("list_objects_v2")
+        .paginate(**locals())
+    ):
+        for content in page.get("Contents", ()):
+            yield content["Key"]
+
+
+def build_s3_image_path(
+    row: dict[str, str], channel: str, correction: None or str = None
+) -> PureS3Path:
+    """ """
+    if correction is None:
+        correction = "Orig"
+    index_suffix = correction + channel
+    final_path = (
+        S3Path.from_uri(row["_".join(("PathName", index_suffix))])
+        / row["_".join(("FileName", index_suffix))]
+    )
+    return final_path
+
+
+def read_parquet_s3(path: str):
+    return pl.read_parquet(
+        path,
+        use_pyarrow=True,
+        # Temporarily removed due to them not enabling anonymous fetching
+        # from s3fs import S3FileSystem
+        # pyarrow_options={"filesystem": S3FileSystem(anonymous=True)},
+    )
