6955ff55ddf61de8224ecfdde924fc0d134424f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 06:05:53 +0000

    Add package vectorgebra/3.0.2/vectorgebra-3.0.2-py3-none-any.whl

---
+++ packages/vectorgebra/vectorgebra-3.0.2-py3-none-any.whl/vectorgebra/vectorgebra.py
@@ -0,0 +1,26 @@
+from .graph import *
+
+
+if __name__ == "__main__":
+    v = Vector(1, 2, 3)
+    m = Matrix(Vector(1, 2, 3), Vector(4, 5, 6))
+    print(m * v)
+    """t = Tensor(Matrix.randMint(2, 2, -2, 2),
+               Matrix.randMint(2, 2, -2, 2),
+               Matrix.randMint(2, 2, -2, 2))
+    print(t.dimension)
+    print(t)
+
+    t -= Vector(1, 1)
+    print(t)
+
+    print()
+    print(t)
+    print(t.flatten())
+    print(t.avg())
+
+    m = Matrix(Vector(1, 2, 3), Vector(1, 2, 3))
+    n = Matrix(Vector(1, 2, 3, 4), Vector(1, 2, 3, 4), Vector(1, 2, 3, 4))
+    print(m.dimension)
+    print(m * n)"""
+    
