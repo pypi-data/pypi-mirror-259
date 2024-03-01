b16572c87b8c1ee9f47c827e54c2190396b13ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:57 +0000

    Add package gulp-zhongwen-wendang/2024.3.1.0/gulp-zhongwen-wendang-2024.3.1.0.tar.gz

---
+++ packages/gulp-zhongwen-wendang/gulp-zhongwen-wendang-2024.3.1.0.tar.gz/gulp-zhongwen-wendang-2024.3.1.0/GulpZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gulp-zhongwen-wendang
+https://github.com/apachecn/gulp-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
