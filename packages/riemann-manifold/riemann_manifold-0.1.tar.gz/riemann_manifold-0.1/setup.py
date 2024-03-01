9330859f2293829a86e1790575b0a5c456c22619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 06:05:01 +0000

    Add package riemann-manifold/0.1/riemann_manifold-0.1.tar.gz

---
+++ packages/riemann-manifold/riemann_manifold-0.1.tar.gz/riemann_manifold-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='riemann_manifold',
+    version='0.1',
+    packages=find_packages(),
+    author='Simon Wittum',
+    author_email='simonwittum@gmx.de',
+    description='A package for working with Riemannian manifolds in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/swittum/riemannian_manifold',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
