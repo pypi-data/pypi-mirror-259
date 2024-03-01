8a54f744137118f3bf5b496f73ad29f33a8c5752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 04:06:00 +0000

    Add package track2p/0.4.4/track2p-0.4.4.tar.gz

---
+++ packages/track2p/track2p-0.4.4.tar.gz/track2p-0.4.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='track2p',
+    version='0.4.4',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.23.5',
+        'matplotlib==3.5.3',
+        'scikit-image==0.20.0',
+        'itk-elastix==0.19.1',
+        'PyQt5==5.15.10',
+        'qtpy==2.4.1',
+        ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    )
