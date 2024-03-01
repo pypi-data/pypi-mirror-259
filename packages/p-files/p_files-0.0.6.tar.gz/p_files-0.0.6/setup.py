04e06364e07976722e061380de83c76eb5fd42f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 07:06:06 +0000

    Add package p-files/0.0.6/p_files-0.0.6.tar.gz

---
+++ packages/p-files/p_files-0.0.6.tar.gz/p_files-0.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+files = {
+    "long_description": "README.md",
+    "requirements": "requirements.txt",
+    "version": "VERSION",
+}
+
+param = {}
+
+for key in files:
+    try:
+        with open(files[key], "r", encoding="utf-8") as file:
+            param[key] = file.read()
+    except(OSError, IOError) as exc:
+        param[key] = ""
+
+setup(
+    name="p_files",
+    version=param["version"],
+    description="A Python package for file operations.",
+    author="Gael",
+    author_email="",
+    packages=["p_files"],
+    install_requires=param["requirements"].split("\n"),
+    long_description=param["long_description"],
+    long_description_content_type="text/markdown",
+    url="https://github.com/Glawnn/p_files.git",
+    project_urls={
+        'Source': 'https://github.com/Glawnn/p_files.git',
+    },
+)
