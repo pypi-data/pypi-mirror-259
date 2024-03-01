caf69e8882b111b287b79981683c55f466b18efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:06:08 +0000

    Add package HA-cfg-cleaner-DiosWolf/0.0.13/HA_cfg_cleaner_DiosWolf-0.0.13-py3-none-any.whl

---
+++ packages/HA-cfg-cleaner-DiosWolf/HA_cfg_cleaner_DiosWolf-0.0.13-py3-none-any.whl/HA_cfg_cleaner_DiosWolf/data_classes_json/file_config.py
@@ -0,0 +1,23 @@
+# TODO: for file configurations
+from dataclasses import dataclass
+
+
+@dataclass
+class Configuration:
+    config_id: str
+    filename: str
+    first_key: str | None
+    second_key: str | None
+    id_key: str
+    identifiers_key: str
+    type_cfg: str
+    path: str
+    on_off_field: str
+    on_integration: None | str
+    off_integration: str
+
+
+@dataclass
+class ConfigurationFile:
+    configurations: list[Configuration]
+    automations_cfg: list[Configuration]
