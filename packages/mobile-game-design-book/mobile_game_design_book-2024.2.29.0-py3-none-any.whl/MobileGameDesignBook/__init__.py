ef6624188d58e4e26a8123df733367dfea678aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:04:38 +0000

    Add package mobile-game-design-book/2024.2.29.0/mobile_game_design_book-2024.2.29.0-py3-none-any.whl

---
+++ packages/mobile-game-design-book/mobile_game_design_book-2024.2.29.0-py3-none-any.whl/MobileGameDesignBook/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mobile-game-design-book
+https://github.com/apachecn/mobile-game-design-book"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.2.29.0"
