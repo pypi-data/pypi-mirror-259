ae4dc2f9c55e51f6530665c8494dfa7396042138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:49 +0000

    Add package go-yuyan-boke-shijian/2024.3.1.0/go-yuyan-boke-shijian-2024.3.1.0.tar.gz

---
+++ packages/go-yuyan-boke-shijian/go-yuyan-boke-shijian-2024.3.1.0.tar.gz/go-yuyan-boke-shijian-2024.3.1.0/GoYuyanBokeShijian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""go-yuyan-boke-shijian
+https://github.com/apachecn/go-yuyan-boke-shijian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
