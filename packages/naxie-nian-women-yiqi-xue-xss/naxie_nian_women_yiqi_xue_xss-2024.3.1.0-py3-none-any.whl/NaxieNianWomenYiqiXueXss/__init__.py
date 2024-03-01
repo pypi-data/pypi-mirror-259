8265688c007ccc44377bf2f5a281977dedbfb90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:48 +0000

    Add package naxie-nian-women-yiqi-xue-xss/2024.3.1.0/naxie_nian_women_yiqi_xue_xss-2024.3.1.0-py3-none-any.whl

---
+++ packages/naxie-nian-women-yiqi-xue-xss/naxie_nian_women_yiqi_xue_xss-2024.3.1.0-py3-none-any.whl/NaxieNianWomenYiqiXueXss/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""naxie-nian-women-yiqi-xue-xss
+https://github.com/apachecn/naxie-nian-women-yiqi-xue-xss"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
