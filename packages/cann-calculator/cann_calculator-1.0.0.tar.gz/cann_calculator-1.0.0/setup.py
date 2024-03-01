967b60b413f02bfada256e5dada6a00482d7d283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 14:05:26 +0000

    Add package cann-calculator/1.0.0/cann_calculator-1.0.0.tar.gz

---
+++ packages/cann-calculator/cann_calculator-1.0.0.tar.gz/cann_calculator-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cann_calculator',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.19.5',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cann-calculator=cann_calculator.main:run',
+        ],
+    },
+    author='cann',
+    author_email='savemefromthedark777@gmail.com',
+    description='A calculator application by Cann',
+    bugtrack_url='https://github.com/cann072000/cann_calculator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+    project_urls={
+        'Source': 'https://github.com/cann072000/cann_calculator',
+    },
+)
