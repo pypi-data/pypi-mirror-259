7aad0db46e1e657aacae9aed8b04f672224ebe18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:17 +0000

    Add package knob/0.0.37/knob-0.0.37.tar.gz

---
+++ packages/knob/knob-0.0.37.tar.gz/knob-0.0.37/knob/dt.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+__all__ = ['local_now', 'local_today', 'local_time', 'naive_time']
+
+import six
+from django.utils import timezone
+from dt_utils import T
+
+
+def local_now():
+    return timezone.now().astimezone(timezone.get_current_timezone())
+
+
+def local_today():
+    return local_now().date()
+
+
+def local_time(raw_time):
+    time = T(raw_time)
+    if timezone.is_aware(time):
+        return timezone.localtime(time)
+    else:
+        return timezone.make_aware(time)
+
+
+def naive_time(raw_time):
+    time = T(raw_time)
+    if timezone.is_aware(time):
+        # make local first, lest the result is based on a wrong timezone
+        return timezone.make_naive(timezone.localtime(time))
+    else:
+        return time
