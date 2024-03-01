05a863ff80f5ce63d9e1c5f5310848af6774aa52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 07:05:38 +0000

    Add package meze/0.1a11/meze-0.1a11.tar.gz

---
+++ packages/meze/meze-0.1a11.tar.gz/meze-0.1a11/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from os import path
+
+from setuptools import find_packages, setup
+
+from meze import __version__
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "../README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="meze",
+    version=__version__,
+    description="A framework for building React frontends for Django projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Karl Hobley",
+    author_email="karl@kaed.uk",
+    url="https://djream.io",
+    project_urls={
+        "Documentation": "https://dream.io/docs/",
+        "Source": "https://github.com/kaedroho/meze/",
+        "Tracker": "https://github.com/kaedroho/meze/issues",
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    license="BSD-3-Clause",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Framework :: Django",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 5.0",
+    ],
+    install_requires=["Django>=4.0,<6.0", "telepath>=0.3,<0.4"],
+    zip_safe=False,
+)
