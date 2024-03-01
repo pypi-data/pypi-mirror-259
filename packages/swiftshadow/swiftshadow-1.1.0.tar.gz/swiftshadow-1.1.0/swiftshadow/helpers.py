9d7bee3b6eda9e4175860f092f356cba6b440ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:46 +0000

    Add package swiftshadow/1.1.0/swiftshadow-1.1.0.tar.gz

---
+++ packages/swiftshadow/swiftshadow-1.1.0.tar.gz/swiftshadow-1.1.0/swiftshadow/helpers.py
@@ -0,0 +1,25 @@
+from swiftshadow.constants import CountryCodes
+from requests import get
+
+
+def getCountryCode(countryName):
+    try:
+        return CountryCodes[countryName]
+    except KeyError:
+        for name in list(CountryCodes.keys()):
+            if countryName in name:
+                return CountryCodes[name]
+
+
+def checkProxy(proxy, countries):
+    if countries != []:
+        if proxy[-1].upper() not in countries:
+            return False
+    proxyDict = {proxy[1]: proxy[0]}
+    try:
+        resp = get(f"{proxy[1]}://ipinfo.io/ip", proxies=proxyDict, timeout=2).text
+        if resp.count(".") == 3:
+            return True
+        return False
+    except Exception as e:
+        return False
