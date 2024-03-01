837eb253c751efc33e4ecfad80fc2049ba51e050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:04:35 +0000

    Add package linux-jiu-gai-zheme-xue-v1-02/2024.2.29.0/linux_jiu_gai_zheme_xue_v1_02-2024.2.29.0-py3-none-any.whl

---
+++ packages/linux-jiu-gai-zheme-xue-v1-02/linux_jiu_gai_zheme_xue_v1_02-2024.2.29.0-py3-none-any.whl/LinuxJiuGaiZhemeXueV102/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""linux-jiu-gai-zheme-xue-v1-02
+https://github.com/apachecn/linux-jiu-gai-zheme-xue-v1-02"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.2.29.0"
