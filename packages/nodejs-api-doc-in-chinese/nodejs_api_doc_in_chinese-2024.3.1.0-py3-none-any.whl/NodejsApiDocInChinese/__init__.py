1c3988a9a6646dd4f8ae3b88c468a2d4f34d54bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:37 +0000

    Add package nodejs-api-doc-in-chinese/2024.3.1.0/nodejs_api_doc_in_chinese-2024.3.1.0-py3-none-any.whl

---
+++ packages/nodejs-api-doc-in-chinese/nodejs_api_doc_in_chinese-2024.3.1.0-py3-none-any.whl/NodejsApiDocInChinese/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nodejs-api-doc-in-chinese
+https://github.com/apachecn/nodejs-api-doc-in-chinese"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
