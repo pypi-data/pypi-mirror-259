002464f61217eca0c1de6e02eecfc5439d48ff7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 04:06:24 +0000

    Add package openobd-protocol/0.0.25/openobd_protocol-0.0.25-py3-none-any.whl

---
+++ packages/openobd-protocol/openobd_protocol-0.0.25-py3-none-any.whl/openobd_protocol/RemoteDiagnosticServices_pb2.pyi
@@ -0,0 +1,10 @@
+import Authentication_pb2 as _Authentication_pb2
+import BasicResponse_pb2 as _BasicResponse_pb2
+import ModuleConfiguration_pb2 as _ModuleConfiguration_pb2
+import BusConfiguration_pb2 as _BusConfiguration_pb2
+import ObdMessage_pb2 as _ObdMessage_pb2
+import Status_pb2 as _Status_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
