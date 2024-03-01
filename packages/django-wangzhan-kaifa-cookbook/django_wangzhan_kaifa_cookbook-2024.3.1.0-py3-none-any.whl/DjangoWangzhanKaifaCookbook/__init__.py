b36dfaad8efed1696abb8703728ab275dbe5656f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:18 +0000

    Add package django-wangzhan-kaifa-cookbook/2024.3.1.0/django_wangzhan_kaifa_cookbook-2024.3.1.0-py3-none-any.whl

---
+++ packages/django-wangzhan-kaifa-cookbook/django_wangzhan_kaifa_cookbook-2024.3.1.0-py3-none-any.whl/DjangoWangzhanKaifaCookbook/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""django-wangzhan-kaifa-cookbook
+https://github.com/apachecn/django-wangzhan-kaifa-cookbook"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
