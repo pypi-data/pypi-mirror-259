5cf97bb189649951bac288f749ec6d43df014b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:16 +0000

    Add package nonebot_plugin_chatrecorder/0.6.0/nonebot_plugin_chatrecorder-0.6.0-py3-none-any.whl

---
+++ packages/nonebot_plugin_chatrecorder/nonebot_plugin_chatrecorder-0.6.0-py3-none-any.whl/nonebot_plugin_chatrecorder/message.py
@@ -0,0 +1,82 @@
+import abc
+from typing import Any, Dict, Generic, List, Type, TypeVar, Union
+
+from nonebot.adapters import Bot, Message
+from nonebot.compat import type_validate_python
+
+from .consts import SupportedAdapter
+from .exception import AdapterNotInstalled, AdapterNotSupported
+
+JsonMsg = List[Dict[str, Any]]
+TM = TypeVar("TM", bound="Message")
+
+
+class MessageSerializer(abc.ABC, Generic[TM]):
+    @classmethod
+    def serialize(cls, msg: TM) -> JsonMsg:
+        return [seg.__dict__ for seg in msg]
+
+
+class MessageDeserializer(abc.ABC, Generic[TM]):
+    @classmethod
+    @abc.abstractmethod
+    def get_message_class(cls) -> Type[TM]:
+        raise NotImplementedError
+
+    @classmethod
+    def deserialize(cls, msg: JsonMsg) -> TM:
+        return type_validate_python(cls.get_message_class(), msg)
+
+
+_serializers: Dict[SupportedAdapter, Type[MessageSerializer]] = {}
+_deserializers: Dict[SupportedAdapter, Type[MessageDeserializer]] = {}
+
+
+def get_adapter_type(bot_type: str) -> SupportedAdapter:
+    for adapter in SupportedAdapter:
+        if bot_type == adapter.value:
+            return adapter
+
+    raise AdapterNotSupported(bot_type)
+
+
+def get_serializer(adapter: SupportedAdapter) -> Type[MessageSerializer]:
+    if adapter not in _serializers:
+        raise AdapterNotInstalled(adapter.value)
+    return _serializers[adapter]
+
+
+def get_deserializer(adapter: SupportedAdapter) -> Type[MessageDeserializer]:
+    if adapter not in _deserializers:
+        raise AdapterNotInstalled(adapter.value)
+    return _deserializers[adapter]
+
+
+def register_serializer(adapter: SupportedAdapter, serializer: Type[MessageSerializer]):
+    _serializers[adapter] = serializer
+
+
+def register_deserializer(
+    adapter: SupportedAdapter, deserializer: Type[MessageDeserializer]
+):
+    _deserializers[adapter] = deserializer
+
+
+def serialize_message(
+    bot_type: Union[Bot, SupportedAdapter, str], msg: Message
+) -> JsonMsg:
+    if isinstance(bot_type, Bot):
+        bot_type = bot_type.type
+    if isinstance(bot_type, str):
+        bot_type = get_adapter_type(bot_type)
+    return get_serializer(bot_type).serialize(msg)
+
+
+def deserialize_message(
+    bot_type: Union[Bot, SupportedAdapter, str], msg: JsonMsg
+) -> Message:
+    if isinstance(bot_type, Bot):
+        bot_type = bot_type.type
+    if isinstance(bot_type, str):
+        bot_type = get_adapter_type(bot_type)
+    return get_deserializer(bot_type).deserialize(msg)
