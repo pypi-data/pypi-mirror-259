76207cbbeb15e30c8f8e1182139ea964cfffa0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:06:24 +0000

    Add package recpulse/0.0.11/recpulse-0.0.11-py3-none-any.whl

---
+++ packages/recpulse/recpulse-0.0.11-py3-none-any.whl/recpulse/activations.py
@@ -0,0 +1,75 @@
+"""Activations.
+================================================================
+Contains:
+  Functions:
+    linear         : Linear activation function (pass-through).
+    sigmoid        : Sigmoid activation function.
+    softmax        : Softmax activation function.
+    relu           : Rectified Linear Unit (ReLU) activation function.
+    leaky_relu     : Leaky ReLU activation function.
+    parametric_relu: Parametric ReLU activation function.
+    tanh           : Hyperbolic tangent activation function.
+    arctan         : Arctangent activation function.
+    elu            : Exponential Linear Unit (ELU) activation function.
+    swish          : Swish activation function.
+================================================================
+Authors: maksym-petrenko
+Contact: maksym.petrenko.a@gmail.com
+License: MIT
+================================================================
+"""
+import numpy as np
+
+from recpulse.np_dtypes import TENSOR_TYPE
+
+
+def linear(x: TENSOR_TYPE) -> TENSOR_TYPE:
+    """Linear activation function."""
+    return x
+
+
+def sigmoid(x: TENSOR_TYPE) -> TENSOR_TYPE:
+    """Sigmoid activation function."""
+    return 1 / (1 + np.exp(-x))
+
+
+def softmax(x: TENSOR_TYPE) -> TENSOR_TYPE:
+    """Softmax activation function."""
+    x = x - np.max(x)
+    exponentiated = np.exp(x)
+    return exponentiated / np.sum(exponentiated)
+
+
+def relu(x: TENSOR_TYPE) -> TENSOR_TYPE:
+    """Relu activation function."""
+    return np.where(x >= 0, x, 0)
+
+
+def leaky_relu(x: TENSOR_TYPE) -> TENSOR_TYPE:
+    """Leaky Relu activation function."""
+    return np.where(x >= 0, x, 0.1 * x)
+
+
+def parametric_relu(x: TENSOR_TYPE, alpha: float = 0) -> TENSOR_TYPE:
+    """Parametric Relu activation function."""
+    return np.where(x >= 0, x, alpha * x)
+
+
+def tanh(x: TENSOR_TYPE) -> TENSOR_TYPE:
+    """Tanh activation function."""
+    return np.tanh(x)
+
+
+def arctan(x: TENSOR_TYPE) -> TENSOR_TYPE:
+    """Arctan activation function."""
+    return np.arctan(x)
+
+
+def elu(x: TENSOR_TYPE, alpha: float = 1) -> TENSOR_TYPE:
+    """Elu activation function."""
+    return np.where(x >= 0, x, alpha * (np.exp(-x) - 1))
+
+
+def swish(x: TENSOR_TYPE) -> TENSOR_TYPE:
+    """Swish activation function."""
+    return x / (1 + np.exp(-x))
