5c242b6d4f910fbb4e1f5d56d204041f4d4546a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:06:08 +0000

    Add package xpart/0.17.0/xpart-0.17.0.tar.gz

---
+++ packages/xpart/xpart-0.17.0.tar.gz/xpart-0.17.0/xpart/longitudinal/__init__.py
@@ -0,0 +1,7 @@
+# copyright ############################### #
+# This file is part of the Xpart Package.   #
+# Copyright (c) CERN, 2021.                 #
+# ######################################### #
+
+from .generate_longitudinal import generate_longitudinal_coordinates
+from .single_rf_harmonic_matcher import SingleRFHarmonicMatcher
---
+++ packages/xpart/xpart-0.17.0.tar.gz/xpart-0.17.0/xpart/matched_gaussian.py
@@ -0,0 +1,126 @@
+# copyright ############################### #
+# This file is part of the Xpart Package.   #
+# Copyright (c) CERN, 2021.                 #
+# ######################################### #
+
+import numpy as np
+
+from .general import _print
+
+from .longitudinal import generate_longitudinal_coordinates
+from .build_particles import build_particles
+
+import xpart as xp # To get the right Particles class depending on pyheatail interface state
+
+def generate_matched_gaussian_bunch(num_particles,
+                                    nemitt_x, nemitt_y, sigma_z,
+                                    total_intensity_particles=None,
+                                    particle_on_co=None,
+                                    R_matrix=None,
+                                    circumference=None,
+                                    momentum_compaction_factor=None,
+                                    rf_harmonic=None,
+                                    rf_voltage=None,
+                                    rf_phase=None,
+                                    p_increment=0.,
+                                    tracker=None,
+                                    line=None,
+                                    particle_ref=None,
+                                    engine=None,
+                                    _context=None, _buffer=None, _offset=None,
+                                    **kwargs, # They are passed to build_particles
+                                    ):
+
+    '''
+    Generate a matched Gaussian bunch.
+
+    Parameters
+    ----------
+    line : xpart.Line
+        Line for which the bunch is generated.
+    num_particles : int
+        Number of particles to be generated.
+    nemitt_x : float
+        Normalized emittance in the horizontal plane (in m rad).
+    nemitt_y : float
+        Normalized emittance in the vertical plane (in m rad).
+    sigma_z : float
+        RMS bunch length in meters.
+    total_intensity_particles : float
+        Total intensity of the bunch in particles.
+
+    Returns
+    -------
+    part : xpart.Particles
+        Particles object containing the generated particles.
+
+    '''
+
+    if line is not None and tracker is not None:
+        raise ValueError(
+            'line and tracker cannot be provided at the same time.')
+
+    if tracker is not None:
+        _print(
+            "The argument tracker is deprecated. Please use line instead.",
+            DeprecationWarning)
+        line = tracker.line
+
+    if line is not None:
+        assert line.tracker is not None, ("The line has no tracker. Please use "
+                                          "`Line.build_tracker()`")
+
+    if (particle_ref is not None and particle_on_co is not None):
+        raise ValueError("`particle_ref` and `particle_on_co`"
+                " cannot be provided at the same time")
+
+    if particle_ref is None:
+        if particle_on_co is not None:
+            particle_ref = particle_on_co
+        elif line is not None and line.particle_ref is not None:
+            particle_ref = line.particle_ref
+        else:
+            raise ValueError(
+                "`line`, `particle_ref` or `particle_on_co` must be provided!")
+
+    zeta, delta = generate_longitudinal_coordinates(
+            distribution='gaussian',
+            num_particles=num_particles,
+            particle_ref=(particle_ref if particle_ref is not None
+                          else particle_on_co),
+            line=line,
+            circumference=circumference,
+            momentum_compaction_factor=momentum_compaction_factor,
+            rf_harmonic=rf_harmonic,
+            rf_voltage=rf_voltage,
+            rf_phase=rf_phase,
+            p_increment=p_increment,
+            sigma_z=sigma_z,
+            engine=engine,
+            **kwargs)
+
+    assert len(zeta) == len(delta) == num_particles
+
+    x_norm = np.random.normal(size=num_particles)
+    px_norm = np.random.normal(size=num_particles)
+    y_norm = np.random.normal(size=num_particles)
+    py_norm = np.random.normal(size=num_particles)
+
+    if total_intensity_particles is None:
+        # go to particles.weight = 1
+        total_intensity_particles = num_particles
+
+
+    part = build_particles(_context=_context, _buffer=_buffer, _offset=_offset,
+                      R_matrix=R_matrix,
+                      particle_on_co=particle_on_co,
+                      particle_ref=(
+                          particle_ref if particle_on_co is  None else None),
+                      line=line,
+                      zeta=zeta, delta=delta,
+                      x_norm=x_norm, px_norm=px_norm,
+                      y_norm=y_norm, py_norm=py_norm,
+                      nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                      weight=total_intensity_particles/num_particles,
+                      **kwargs)
+    return part
