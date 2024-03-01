c5df92f36bc78bd59ab4e7ceca1707ba7af2ceb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 04:05:20 +0000

    Add package bloock/2.8.1/bloock-2.8.1-cp39-cp39-win_amd64.whl

---
+++ packages/bloock/bloock-2.8.1-cp39-cp39-win_amd64.whl/bloock/entity/integrity/network.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from enum import Enum
+
+from bloock._bridge.proto.config_pb2 import Network as NetworkProto
+
+
+class Network(Enum):
+    """
+    Represents a network.
+    """
+    ETHEREUM_MAINNET = 0
+    ETHEREUM_GOERLI = 1
+    GNOSIS_CHAIN = 2
+    BLOOCK_CHAIN = 3
+    POLYGON_CHAIN = 4
+
+    def __int__(self):
+        return self.value
+
+    @staticmethod
+    def to_proto(network: Network) -> NetworkProto.ValueType:
+        if network == Network.ETHEREUM_MAINNET:
+            return NetworkProto.ETHEREUM_MAINNET
+        elif network == Network.ETHEREUM_GOERLI:
+            return NetworkProto.ETHEREUM_GOERLI
+        elif network == Network.GNOSIS_CHAIN:
+            return NetworkProto.GNOSIS_CHAIN
+        elif network == Network.BLOOCK_CHAIN:
+            return NetworkProto.BLOOCK_CHAIN
+        elif network == Network.POLYGON_CHAIN:
+            return NetworkProto.POLYGON_CHAIN
