add5b98b570e45489ca9226e90e5c2714158eb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:40 +0000

    Add package c-faq-lite/2024.3.1.0/c_faq_lite-2024.3.1.0-py3-none-any.whl

---
+++ packages/c-faq-lite/c_faq_lite-2024.3.1.0-py3-none-any.whl/CFaqLite/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""c-faq-lite
+https://github.com/apachecn/c-faq-lite"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
