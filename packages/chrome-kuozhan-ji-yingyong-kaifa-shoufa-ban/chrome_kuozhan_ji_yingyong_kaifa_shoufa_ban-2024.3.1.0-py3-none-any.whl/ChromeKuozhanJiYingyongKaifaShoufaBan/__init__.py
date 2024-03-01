b040a447a40d5d39a6dc36839c7a58b6702c3069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 17:04:27 +0000

    Add package chrome-kuozhan-ji-yingyong-kaifa-shoufa-ban/2024.3.1.0/chrome_kuozhan_ji_yingyong_kaifa_shoufa_ban-2024.3.1.0-py3-none-any.whl

---
+++ packages/chrome-kuozhan-ji-yingyong-kaifa-shoufa-ban/chrome_kuozhan_ji_yingyong_kaifa_shoufa_ban-2024.3.1.0-py3-none-any.whl/ChromeKuozhanJiYingyongKaifaShoufaBan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chrome-kuozhan-ji-yingyong-kaifa-shoufa-ban
+https://github.com/apachecn/chrome-kuozhan-ji-yingyong-kaifa-shoufa-ban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
