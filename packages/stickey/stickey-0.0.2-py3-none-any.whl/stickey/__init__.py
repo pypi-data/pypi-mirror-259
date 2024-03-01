96dc07912d7f60098d0f1079dc3ec91fdfdd6c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:29 +0000

    Add package stickey/0.0.2/stickey-0.0.2-py3-none-any.whl

---
+++ packages/stickey/stickey-0.0.2-py3-none-any.whl/stickey/__init__.py
@@ -0,0 +1,6 @@
+from stickey.stickey import app
+from stickey.stickey import indexhtml
+from stickey.stickey import quizhtml
+from stickey.stickey import quizchtml
+from stickey.stickey import stylecss
+from stickey.stickey import quizcss
