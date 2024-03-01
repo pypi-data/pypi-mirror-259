58fe15c69e61200c86b7148be7584c6e21440a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:20 +0000

    Add package pysnmp-lextudio/6.0.5/pysnmp_lextudio-6.0.5.tar.gz

---
+++ packages/pysnmp-lextudio/pysnmp_lextudio-6.0.5.tar.gz/pysnmp_lextudio-6.0.5/tests/hlapi/asyncio/manager/cmdgen/test_v1_next.py
@@ -0,0 +1,41 @@
+"""
+SNMPv1
+++++++
+
+Send SNMP GET request using the following options:
+
+  * with SNMPv1, community 'public'
+  * over IPv4/UDP
+  * to an Agent at demo.pysnmp.com:161
+  * for an instance of SNMPv2-MIB::sysDescr.0 MIB object
+  * Based on asyncio I/O framework
+
+Functionally similar to:
+
+| $ snmpgetnext -v1 -c public demo.pysnmp.com SNMPv2-MIB::sysDescr.0
+
+"""  #
+import asyncio
+import pytest
+from pysnmp.hlapi.asyncio.slim import Slim
+from pysnmp.smi.rfc1902 import ObjectIdentity, ObjectType
+from tests.agent_context import AGENT_PORT, AgentContextManager
+
+
+@pytest.mark.asyncio
+async def test_v1_next():
+    async with AgentContextManager():
+        slim = Slim(1)
+        errorIndication, errorStatus, errorIndex, varBinds = await slim.next(
+            "public",
+            "localhost",
+            AGENT_PORT,
+            ObjectType(ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)),
+        )
+
+        assert errorIndication is None
+        assert errorStatus == 0
+        assert errorIndex == 0
+        assert len(varBinds) == 1
+
+        slim.close()
