90f39b683f28a022cf1b3bba0bf261374b80ca4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:24 +0000

    Add package biancheng-xiaobai-de-diyiben-python-rumen-shu/2024.3.1.0/biancheng_xiaobai_de_diyiben_python_rumen_shu-2024.3.1.0-py3-none-any.whl

---
+++ packages/biancheng-xiaobai-de-diyiben-python-rumen-shu/biancheng_xiaobai_de_diyiben_python_rumen_shu-2024.3.1.0-py3-none-any.whl/BianchengXiaobaiDeDiyibenPythonRumenShu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""biancheng-xiaobai-de-diyiben-python-rumen-shu
+https://github.com/apachecn/biancheng-xiaobai-de-diyiben-python-rumen-shu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
