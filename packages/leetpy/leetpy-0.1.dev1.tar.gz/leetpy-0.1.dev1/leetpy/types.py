547b04d7fa7ed54ef5965bee83407d93d15c8b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 04:05:56 +0000

    Add package leetpy/0.1.dev1/leetpy-0.1.dev1.tar.gz

---
+++ packages/leetpy/leetpy-0.1.dev1.tar.gz/leetpy-0.1.dev1/leetpy/types.py
@@ -0,0 +1,59 @@
+from typing import List, Optional
+
+
+# References:
+# https://leetcode.com/problems/clone-graph/
+class Node_Graph:
+    """
+    Used in graphs.
+    NOTE: Leetcode refers to this as "Node"
+    """
+
+    def __init__(self, val=0, neighbors: List["Node_Graph"] = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+# References:
+# https://leetcode.com/problems/add-two-numbers/
+class ListNode:
+    """
+    Used in singly-linked lists.
+    """
+
+    def __init__(self, val: any = 0, next: Optional["ListNode"] = None):
+        self.val = val
+        self.next = next
+
+
+# References:
+# https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+class Node_DLL:
+    """
+    Used in doubly-linked lists.
+    NOTE: Leetcode refers to this as "Node"
+    """
+
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+
+
+# References:
+# https://leetcode.com/problems/binary-tree-inorder-traversal/
+class TreeNode:
+    """
+    Used in binary trees.
+    """
+
+    def __init__(
+        self,
+        val: any = 0,
+        left: Optional["TreeNode"] = None,
+        right: Optional["TreeNode"] = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
