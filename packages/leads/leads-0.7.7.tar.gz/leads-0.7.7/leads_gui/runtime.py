cf8fb06a335ad002ded3053b3dbf963f3dd59d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 05:05:49 +0000

    Add package leads/0.7.7/leads-0.7.7.tar.gz

---
+++ packages/leads/leads-0.7.7.tar.gz/leads-0.7.7/leads_gui/runtime.py
@@ -0,0 +1,18 @@
+from time import time as _time
+
+from leads import DataContainer as _DataContainer
+from leads.comm import Server as _Server
+
+
+class RuntimeData(object):
+    start_time: int = int(_time())
+    lap_time: list[int] = []
+    comm: _Server | None = None
+
+    def comm_notify(self, d: _DataContainer) -> None:
+        if self.comm:
+            self.comm.broadcast(d.encode())
+
+    def comm_kill(self) -> None:
+        if self.comm:
+            self.comm.kill()
