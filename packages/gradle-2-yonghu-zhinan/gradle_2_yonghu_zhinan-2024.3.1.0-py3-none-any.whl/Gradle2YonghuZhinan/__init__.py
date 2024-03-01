74016d0fcccadb1c410fc522552a36c379342986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:54 +0000

    Add package gradle-2-yonghu-zhinan/2024.3.1.0/gradle_2_yonghu_zhinan-2024.3.1.0-py3-none-any.whl

---
+++ packages/gradle-2-yonghu-zhinan/gradle_2_yonghu_zhinan-2024.3.1.0-py3-none-any.whl/Gradle2YonghuZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gradle-2-yonghu-zhinan
+https://github.com/apachecn/gradle-2-yonghu-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
