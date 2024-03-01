9a151ceddb4f040b83f55dbe0b08ad21ddeaa410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:28 +0000

    Add package scalifiai-client/1.0.3a1/scalifiai_client-1.0.3a1-py3-none-any.whl

---
+++ packages/scalifiai-client/scalifiai_client-1.0.3a1-py3-none-any.whl/scalifiai/mcs/main.py
@@ -0,0 +1,8 @@
+from scalifiai.base import BaseModule
+from .constants import URL_SLUG
+
+
+class MCS(BaseModule):
+    # TODO[VIMPORTANT] CHECK AND MAKE FUNCTIONS PRIVATE IF THEY SHOULD NOT BE ACCESSIBLE DIRECTLY
+
+    module_url_slug = URL_SLUG
