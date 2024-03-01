9397df49a2b0803223ab9e422d48d9a0cc293f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:18 +0000

    Add package django-zhige-jianyi-boke-dajian-jiaocheng/2024.3.1.0/django-zhige-jianyi-boke-dajian-jiaocheng-2024.3.1.0.tar.gz

---
+++ packages/django-zhige-jianyi-boke-dajian-jiaocheng/django-zhige-jianyi-boke-dajian-jiaocheng-2024.3.1.0.tar.gz/django-zhige-jianyi-boke-dajian-jiaocheng-2024.3.1.0/DjangoZhigeJianyiBokeDajianJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""django-zhige-jianyi-boke-dajian-jiaocheng
+https://github.com/apachecn/django-zhige-jianyi-boke-dajian-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
