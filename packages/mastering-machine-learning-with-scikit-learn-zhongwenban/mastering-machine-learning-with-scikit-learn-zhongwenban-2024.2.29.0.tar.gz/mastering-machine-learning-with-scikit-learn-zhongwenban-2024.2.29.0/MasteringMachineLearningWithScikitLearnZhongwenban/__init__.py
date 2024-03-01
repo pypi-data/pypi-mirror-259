5109add0f0aa32d9686424f9e0afac5e9340b1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:04:49 +0000

    Add package mastering-machine-learning-with-scikit-learn-zhongwenban/2024.2.29.0/mastering-machine-learning-with-scikit-learn-zhongwenban-2024.2.29.0.tar.gz

---
+++ packages/mastering-machine-learning-with-scikit-learn-zhongwenban/mastering-machine-learning-with-scikit-learn-zhongwenban-2024.2.29.0.tar.gz/mastering-machine-learning-with-scikit-learn-zhongwenban-2024.2.29.0/MasteringMachineLearningWithScikitLearnZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mastering-machine-learning-with-scikit-learn-zhongwenban
+https://github.com/apachecn/mastering-machine-learning-with-scikit-learn-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.2.29.0"
