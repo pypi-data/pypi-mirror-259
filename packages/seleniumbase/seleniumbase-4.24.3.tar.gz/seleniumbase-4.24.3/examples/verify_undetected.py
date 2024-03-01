22cf367f0dec203c95fcc4de9fbc51f731d326c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:34 +0000

    Add package seleniumbase/4.24.3/seleniumbase-4.24.3.tar.gz

---
+++ packages/seleniumbase/seleniumbase-4.24.3.tar.gz/seleniumbase-4.24.3/examples/verify_undetected.py
@@ -0,0 +1,19 @@
+"""Determine if your browser is detectable by anti-bot services.
+Some sites use scripts to detect Selenium, and then block you.
+To evade detection, add --uc as a pytest command-line option."""
+from seleniumbase import BaseCase
+BaseCase.main(__name__, __file__, "--uc", "-s")
+
+
+class UndetectedTest(BaseCase):
+    def test_browser_is_undetected(self):
+        if not self.undetectable:
+            self.get_new_driver(undetectable=True)
+        self.driver.uc_open_with_reconnect("https://top.gg/", 5)
+        if not self.is_text_visible("Discord Bots", "h1"):
+            self.get_new_driver(undetectable=True)
+            self.driver.uc_open_with_reconnect("https://top.gg/", 5)
+        self.assert_text("Discord Bots", "h1", timeout=3)
+        self.set_messenger_theme(location="top_center")
+        self.post_message("Selenium wasn't detected!", duration=2.8)
+        self._print("\n Success! Website did not detect Selenium! ")
