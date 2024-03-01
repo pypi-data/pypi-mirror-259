c963f17e3c14824f89b0aa23de2040c303afa6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:27 +0000

    Add package asp-net-documentation/2024.2.29.0/asp_net_documentation-2024.2.29.0-py3-none-any.whl

---
+++ packages/asp-net-documentation/asp_net_documentation-2024.2.29.0-py3-none-any.whl/AspNetDocumentation/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""asp-net-documentation
+https://github.com/apachecn/asp-net-documentation"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.2.29.0"
