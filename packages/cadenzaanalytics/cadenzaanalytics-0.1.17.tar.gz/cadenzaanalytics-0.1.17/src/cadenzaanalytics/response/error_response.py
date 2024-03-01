39f561faa90eb16368d914e831b9cace5ad624d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:04:38 +0000

    Add package cadenzaanalytics/0.1.17/cadenzaanalytics-0.1.17.tar.gz

---
+++ packages/cadenzaanalytics/cadenzaanalytics-0.1.17.tar.gz/cadenzaanalytics-0.1.17/src/cadenzaanalytics/response/error_response.py
@@ -0,0 +1,24 @@
+import json
+
+from flask import Response
+
+from cadenzaanalytics.response.extension_response import ExtensionResponse
+
+
+class ErrorResponse(ExtensionResponse):
+    def __init__(self, message: str, status: int = 400):
+        self._message = message
+        self._status = status
+
+    def get_response(self):
+        return self._create_response(self._message)
+
+    def _get_response_json(self, message: str):
+        error_message = {
+            "message": message
+        }
+
+        return json.dumps(error_message, default=str)
+
+    def _create_response(self, message: str):
+        return Response(response=self._get_response_json(message), status=self._status, mimetype="application/json")
