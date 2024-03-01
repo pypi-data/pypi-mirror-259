d0dca1ffbe63946790b73acd2e0a1c7d8da0cb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 07:06:26 +0000

    Add package ajenti.plugin.plugins/0.52/ajenti.plugin.plugins-0.52.tar.gz

---
+++ packages/ajenti.plugin.plugins/ajenti.plugin.plugins-0.52.tar.gz/ajenti.plugin.plugins-0.52/ajenti_plugin_plugins/tasks.py
@@ -0,0 +1,42 @@
+import sys
+import subprocess
+
+from aj.plugins.core.api.tasks import Task
+
+
+class InstallPlugin (Task):
+    name = 'Installing plugin'
+
+    def __init__(self, context, name=None, version=None):
+        Task.__init__(self, context)
+        self.spec = f'ajenti.plugin.{name}=={version}'
+
+    def run(self):
+        subprocess.check_output([sys.executable, '-m', 'pip', 'install', self.spec])
+
+
+class UpgradeAll (Task):
+    name = 'Upgrading Ajenti'
+
+    def run(self):
+        try:
+            if sys.executable.startswith('/opt/ajenti'):
+                # Virtualenv install
+                subprocess.check_output(['/opt/ajenti/bin/ajenti-upgrade'])
+            else:
+                # Trying with default paths
+                subprocess.check_output(['ajenti-upgrade'])
+        except FileNotFoundError as e:
+            subprocess.check_output(['/usr/local/bin/ajenti-upgrade'])
+
+
+class UnInstallPlugin (Task):
+    name = 'Uninstalling plugin'
+
+    def __init__(self, context, name=None):
+        Task.__init__(self, context)
+        self.spec = f'ajenti.plugin.{name}'
+
+    def run(self):
+        subprocess.check_output([sys.executable, '-m', 'pip', 'uninstall', '-y', self.spec])
+
