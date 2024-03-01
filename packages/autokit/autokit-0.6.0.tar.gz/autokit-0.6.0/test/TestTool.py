87c45064c4dee0112e82d33432aaa300cbcaa928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:15 +0000

    Add package autokit/0.6.0/autokit-0.6.0.tar.gz

---
+++ packages/autokit/autokit-0.6.0.tar.gz/autokit-0.6.0/test/TestTool.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import autokit.ExternalTool as ExternalTool
+from autokit.Config import ToolConfig, PlatformData
+
+
+class TestTool(ExternalTool):
+    def __init__(self, base_dir: str = "./third-party", progress_bar: bool = True, lazy_setup: bool = False):
+        super().__init__(base_dir, progress_bar, lazy_setup)
+
+    @property
+    def config(self) -> ToolConfig:
+        return ToolConfig(
+            tool_name="test-tool",
+            platform_data={
+                "windows": PlatformData(
+                    url="https://github.com/IRSS-UBC/MediaTools/releases/download/latest/win-x64.zip",
+                    subdir=Path(""),
+                    executable=Path("IRSSMediaTools.exe")
+                ),
+            },
+            python=False,
+        )
+
+    def help(self):
+        self.run_command("help")
+
+
+if __name__ == "__main__":
+    test = TestTool()
+    test.help()
