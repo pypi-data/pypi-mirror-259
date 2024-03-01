4d1e26c7a68c1378b7825de462c70194e075f584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:32 +0000

    Add package xiang-ide-yiyang-shiyong-vim/2024.2.29.0/xiang_ide_yiyang_shiyong_vim-2024.2.29.0-py3-none-any.whl

---
+++ packages/xiang-ide-yiyang-shiyong-vim/xiang_ide_yiyang_shiyong_vim-2024.2.29.0-py3-none-any.whl/XiangIdeYiyangShiyongVim/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiang-ide-yiyang-shiyong-vim
+https://github.com/apachecn/xiang-ide-yiyang-shiyong-vim"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.2.29.0"
