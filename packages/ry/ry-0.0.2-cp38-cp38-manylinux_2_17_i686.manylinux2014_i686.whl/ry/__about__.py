5185a5d74e9020ed1cc4d8cfc2be7d4a2ed55052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:40 +0000

    Add package ry/0.0.2/ry-0.0.2-cp38-cp38-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/ry/ry-0.0.2-cp38-cp38-manylinux_2_17_i686.manylinux2014_i686.whl/ry/__about__.py
@@ -0,0 +1,14 @@
+"""Package metadata/info"""
+
+from ry._ry import __build_profile__, __version__
+
+__all__ = (
+    "__title__",
+    "__description__",
+    "__pkgroot__",
+    "__version__",
+    "__build_profile__",
+)
+__title__ = "ry"
+__description__ = "ry = rust + python - most of the letters"
+__pkgroot__ = __file__.replace("__about__.py", "").rstrip("/\\")
