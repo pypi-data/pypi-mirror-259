6b4946d425755e8bd4f843ba96ad9974ea934e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:21 +0000

    Add package nwjs-doc/2024.3.1.0/nwjs_doc-2024.3.1.0-py3-none-any.whl

---
+++ packages/nwjs-doc/nwjs_doc-2024.3.1.0-py3-none-any.whl/NwjsDoc/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nwjs-doc
+https://github.com/apachecn/nwjs-doc"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
