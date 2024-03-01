ef4965fbd39b4367530f192f0f6cee6ec9f563fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 17:04:32 +0000

    Add package Kiper/0.0.19/Kiper-0.0.19.tar.gz

---
+++ packages/Kiper/Kiper-0.0.19.tar.gz/Kiper-0.0.19/kiper/templates/example_node.py
@@ -0,0 +1,7 @@
+# You can change filename/path
+# These are two example nodes (nodes are functions)
+def node_function_1(data, multiplier) -> int:
+    return data * multiplier
+
+def node_function_2(data, offset) -> int:
+    return data + offset
