bf58d10193497863e5829cca6cabc56d205a9dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:24 +0000

    Add package ke-tidai-de-tuandui-lingxiu-peiyang-jihua/2024.3.1.0/ke-tidai-de-tuandui-lingxiu-peiyang-jihua-2024.3.1.0.tar.gz

---
+++ packages/ke-tidai-de-tuandui-lingxiu-peiyang-jihua/ke-tidai-de-tuandui-lingxiu-peiyang-jihua-2024.3.1.0.tar.gz/ke-tidai-de-tuandui-lingxiu-peiyang-jihua-2024.3.1.0/KeTidaiDeTuanduiLingxiuPeiyangJihua/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ke-tidai-de-tuandui-lingxiu-peiyang-jihua
+https://github.com/apachecn/ke-tidai-de-tuandui-lingxiu-peiyang-jihua"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
