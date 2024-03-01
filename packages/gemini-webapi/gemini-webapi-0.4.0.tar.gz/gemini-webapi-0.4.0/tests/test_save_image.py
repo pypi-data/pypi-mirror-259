2f4f5c3f3bf1386795e9476db152311cd1fae408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 14:04:59 +0000

    Add package gemini-webapi/0.4.0/gemini-webapi-0.4.0.tar.gz

---
+++ packages/gemini-webapi/gemini-webapi-0.4.0.tar.gz/gemini-webapi-0.4.0/tests/test_save_image.py
@@ -0,0 +1,38 @@
+import os
+import unittest
+
+from gemini import GeminiClient, AuthError
+
+
+class TestGeminiClient(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.geminiclient = GeminiClient(
+            os.getenv("SECURE_1PSID"), os.getenv("SECURE_1PSIDTS")
+        )
+
+        try:
+            await self.geminiclient.init()
+        except AuthError:
+            self.skipTest("Test was skipped due to invalid cookies")
+
+    async def test_save_web_image(self):
+        response = await self.geminiclient.generate_content(
+            "Send me 10 pictures of random subjects"
+        )
+        self.assertTrue(response.images)
+        for i, image in enumerate(response.images):
+            self.assertTrue(image.url)
+            await image.save(verbose=True, skip_invalid_filename=True)
+
+    async def test_save_generated_image(self):
+        response = await self.geminiclient.generate_content(
+            "Generate some pictures of random subjects"
+        )
+        self.assertTrue(response.images)
+        for i, image in enumerate(response.images):
+            self.assertTrue(image.url)
+            await image.save(verbose=True)
+
+
+if __name__ == "__main__":
+    unittest.main()
