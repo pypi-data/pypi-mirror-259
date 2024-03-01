f514506173c0871e659221c6e961e66b441eae1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:58 +0000

    Add package roco/0.4.0/roco-0.4.0.tar.gz

---
+++ packages/roco/roco-0.4.0.tar.gz/roco-0.4.0/roco/config.py
@@ -0,0 +1,46 @@
+import os
+from typing import Literal
+from pydantic import model_validator, field_validator, ConfigDict
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    model_config = ConfigDict(env_prefix='PAPERMERGE__AUTH__')
+
+    oidc_client_id: str | None = None
+    oidc_authorize_url: str | None = None
+    oidc_redirect_url: str | None = None
+    oidc_scope: str = 'openid email'
+
+    login_provider: Literal['db', 'ldap'] = 'db'
+    remote: bool = False
+    remote_logout_endpoint: str | None = None
+
+    @field_validator('login_provider')
+    @classmethod
+    def db_or_ldap(cls, v: str) -> str:
+        if os.environ.get('PAPERMERGE__AUTH__LDAP_URL'):
+            return 'ldap'
+
+        return 'db'
+
+    @model_validator(mode='after')
+    def check_oidc_params(self):
+        three_values = [
+            self.oidc_client_id,
+            self.oidc_authorize_url,
+            self.oidc_redirect_url
+        ]
+        count = len([v for v in three_values if v])
+
+        if count not in (0, 3):
+            raise ValueError(
+                'oidc_client_id, oidc_authorize_url and oidc_redirect_uri'
+                ' should be either all absent or all present'
+            )
+
+        return self
+
+
+def get_settings():
+    return Settings()
