b351fc53f04547176c4eae9745c193df18d37e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 17:04:36 +0000

    Add package python-anquan-biancheng-jiaocheng/2024.3.1.0/python_anquan_biancheng_jiaocheng-2024.3.1.0-py3-none-any.whl

---
+++ packages/python-anquan-biancheng-jiaocheng/python_anquan_biancheng_jiaocheng-2024.3.1.0-py3-none-any.whl/PythonAnquanBianchengJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""python-anquan-biancheng-jiaocheng
+https://github.com/apachecn/python-anquan-biancheng-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
