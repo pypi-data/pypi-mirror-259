1656e2738f86e802804c87575ea3b1dd42b27f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:22 +0000

    Add package aukland-compsci-105-problemsolvingwithalgorithmsanddatastructures/2024.3.1.0/aukland_compsci_105_problemsolvingwithalgorithmsanddatastructures-2024.3.1.0-py3-none-any.whl

---
+++ packages/aukland-compsci-105-problemsolvingwithalgorithmsanddatastructures/aukland_compsci_105_problemsolvingwithalgorithmsanddatastructures-2024.3.1.0-py3-none-any.whl/AuklandCompsci105Problemsolvingwithalgorithmsanddatastructures/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aukland-compsci-105-problemsolvingwithalgorithmsanddatastructures
+https://github.com/apachecn/aukland-compsci-105-problemsolvingwithalgorithmsanddatastructures"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
