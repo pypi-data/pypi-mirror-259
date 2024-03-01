96b0e0977f9b6afb8644eafccda53b58a6549f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:52 +0000

    Add package yong-python-he-pygame-xie-youxi-cong-rumen-dao-jingtong/2024.2.29.0/yong_python_he_pygame_xie_youxi_cong_rumen_dao_jingtong-2024.2.29.0-py3-none-any.whl

---
+++ packages/yong-python-he-pygame-xie-youxi-cong-rumen-dao-jingtong/yong_python_he_pygame_xie_youxi_cong_rumen_dao_jingtong-2024.2.29.0-py3-none-any.whl/YongPythonHePygameXieYouxiCongRumenDaoJingtong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yong-python-he-pygame-xie-youxi-cong-rumen-dao-jingtong
+https://github.com/apachecn/yong-python-he-pygame-xie-youxi-cong-rumen-dao-jingtong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.2.29.0"
