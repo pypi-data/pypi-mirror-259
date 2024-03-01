fc536d82d8322fcdd5a92532a4c6cd779641c1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 05:07:24 +0000

    Add package torchrun-jsc/0.0.1/torchrun_jsc-0.0.1-py3-none-any.whl

---
+++ packages/torchrun-jsc/torchrun_jsc-0.0.1-py3-none-any.whl/torchrun_jsc/run_old.py
@@ -0,0 +1,75 @@
+"""
+Fixed version of `torchrun` on Jülich Supercomputing Centre for PyTorch
+versions <2. Requires Slurm usage.
+
+To use, modify your execution like the following:
+
+Old
+```shell
+torchrun [...]
+# or
+python -m torch.distributed.run [...]
+```
+
+New
+```shell
+python /path/to/torchrun_jsc/run_old.py [...]
+# or if `torchrun_jsc` is on `PYTHONPATH`
+python -m torchrun_jsc.run_old [...]
+```
+"""
+
+from argparse import ArgumentParser
+import ipaddress
+import runpy
+import socket
+
+from torch.distributed.elastic.agent.server import api as sapi
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--rdzv_endpoint', '--rdzv-endpoint')
+    parser.add_argument('--rdzv_conf', '--rdzv-conf')
+    args = parser.parse_known_args()[0]
+
+    endpoint = args.rdzv_endpoint
+    host = parsing.parse_host(endpoint)
+
+    conf = args.rdzv_conf
+    is_host = parsing.parse_is_host(conf)
+
+    return host, conf, is_host
+
+
+def fix_torch_run(host):
+    _orig_get_fq_hostname = sapi._get_fq_hostname
+
+    if host:
+        try:
+            ipaddress.ip_address(host)
+            is_ip = True
+        except ValueError:
+            is_ip = False
+
+        if is_ip:
+            def new_get_fq_hostname():
+                return socket.gethostbyaddr(host)[0]
+        else:
+            def new_get_fq_hostname():
+                return socket.getfqdn(host)
+    else:
+        new_get_fq_hostname = _orig_get_fq_hostname
+
+    sapi._get_fq_hostname = new_get_fq_hostname
+
+
+def main():
+    host, conf, is_host = parse_args()
+    arg_patching.fix_is_host(is_host, conf, 'SLURM_NODEID')
+    fix_torch_run(host)
+    runpy.run_module('torch.distributed.run', run_name='__main__')
+
+
+if __name__ == '__main__':
+    main()
