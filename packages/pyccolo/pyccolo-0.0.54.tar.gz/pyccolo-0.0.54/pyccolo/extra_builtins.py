bdb664a7db74cdda89dece3c0eff363c148bfed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 07:05:32 +0000

    Add package pyccolo/0.0.54/pyccolo-0.0.54.tar.gz

---
+++ packages/pyccolo/pyccolo-0.0.54.tar.gz/pyccolo-0.0.54/pyccolo/extra_builtins.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import ast
+from typing import Union
+
+EMIT_EVENT = "_X5ix_PYCCOLO_EVT_EMIT"
+TRACE_LAMBDA = "X5ix_PYCCOLO_TRACE_LAM"
+EXEC_SAVED_THUNK = "_X5ix_PYCCOLO_EXEC_SAVED_THUNK"
+TRACING_ENABLED = "_X5ix_PYCCOLO_TRACING_ENABLED"
+
+
+def make_guard_name(node: Union[int, ast.AST]):
+    node_id = node if isinstance(node, int) else id(node)
+    return "_X5ix_PYCCOLO_GUARD_{}".format(node_id)
