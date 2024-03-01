4a1727da7fb5309c191e96f72058c38c5c0fa735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 07:05:29 +0000

    Add package django-admin-index/3.1.1/django-admin-index-3.1.1.tar.gz

---
+++ packages/django-admin-index/django-admin-index-3.1.1.tar.gz/django-admin-index-3.1.1/django_admin_index/__init__.py
@@ -0,0 +1,5 @@
+# :copyright: (c) 2017, Maykin Media BV.
+#             All rights reserved.
+# :license:   BSD (3 Clause), see LICENSE for more details.
+__version__ = "3.1.1"
+__author__ = "Joeri Bekker"
