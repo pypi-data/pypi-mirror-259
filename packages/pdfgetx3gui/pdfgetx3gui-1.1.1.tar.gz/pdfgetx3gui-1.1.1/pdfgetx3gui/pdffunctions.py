23a3b6badbedb54670e47dccb6a3cc45cc7e5dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:28 +0000

    Add package pdfgetx3gui/1.1.1/pdfgetx3gui-1.1.1.tar.gz

---
+++ packages/pdfgetx3gui/pdfgetx3gui-1.1.1.tar.gz/pdfgetx3gui-1.1.1/pdfgetx3gui/pdffunctions.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+from diffpy.pdfgetx import PDFConfig
+from diffpy.pdfgetx import PDFGetter
+
+
+
+def run_pdfgetx3(file: str,bkgfile: str,bkgscale: float,composition: str, qmin: float,qmax: float,qmaxinst: float,rpoly: float,
+dataformat: str,rmin: float, rmax: float, rstep: float,wavelength = 0.2, x= None, y= None):
+	
+	config = PDFConfig(bgscale = bkgscale, qmin = qmin, qmax = qmax, qmaxinst = qmaxinst, dataformat = dataformat, rpoly = rpoly, composition = composition, 
+	backgroundfile = bkgfile, rmin = rmin, rmax = rmax, rstep = rstep)
+	if dataformat == 'twotheta':
+		config.wavelength = wavelength
+	pdfcalc = PDFGetter(config = config)
+	if type(x) == np.ndarray:
+		pdfcalc(x,y)
+	else:
+		pdfcalc(filename = file)
+
+
+	r = pdfcalc.gr[0]
+	gr = pdfcalc.gr[1]
+	fq = pdfcalc.fq[1]
+	sq = pdfcalc.sq[1]
+	q = pdfcalc.sq[0]
+	iq = pdfcalc.iq[1]
+	qi = pdfcalc.iq[0]
+	iqorig = pdfcalc.results[2][1]
+	bkg = iqorig-iq
+	return qi,iqorig,bkg,q, sq, fq, r, gr
+	
+def writeOutput(file: str,bkgfile: str,bkgscale: float,composition: str,qmin: float,qmax: float,qmaxinst: float,rpoly: float,
+dataformat: str,rmin: float, rmax: float, rstep: float,wavelength = 0.2, iqcheck = True, sqcheck = True, fqcheck = True, grcheck = True):
+	config = PDFConfig(bgscale = bkgscale, qmin = qmin, qmax = qmax, qmaxinst = qmaxinst, dataformat = dataformat, rpoly = rpoly, composition = composition, 
+	backgroundfile = bkgfile, rmin = rmin, rmax = rmax, rstep = rstep)
+	if dataformat == 'twotheta':
+		config.wavelength = wavelength
+	pdfcalc = PDFGetter(config = config)
+	pdfcalc(filename = file)
+	outfile = os.path.splitext(file)[0]
+	plotlist = np.array([iqcheck,sqcheck,fqcheck,grcheck])
+
+	for n in range(len(plotlist)):
+		if n:
+			#if n == 0:
+			#	pdfcalc.writeOutput(filename = outfile+'.iq', outputtype = 'iq')
+
+			if n == 1:
+				sqfile = f'{outfile}.sq'
+				pdfcalc.writeOutput(filename = sqfile, outputtype = 'sq')
+				print(f'writing {sqfile}')
+			elif n == 2:
+				fqfile = f'{outfile}.fq'
+				pdfcalc.writeOutput(filename = fqfile, outputtype = 'fq')
+				print(f'writing {fqfile}')
+			elif n == 3:
+				grfile = f'{outfile}.gr'
+				pdfcalc.writeOutput(filename = outfile+'.gr', outputtype = 'gr')
+				print(f'writing {grfile}')
+
+			
+	
+	
---
+++ packages/pdfgetx3gui/pdfgetx3gui-1.1.1.tar.gz/pdfgetx3gui-1.1.1/setup.py
@@ -0,0 +1,41 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name="pdfgetx3gui",  # Required
+
+    version="1.1",  # Required
+
+    description="a GUI for running PDFgetX3",  # Optional
+    #scripts=['pdfgetx3_gui.py'],
+    package_dir={ 'icon':'icon','exampleFiles':'exampleFiles'},
+    entry_points = {'console_scripts': ['pdfgetx3gui = pdfgetx3gui.pdfgetx3_gui:main',]},
+    include_package_data=True,
+    #url="https://github.com/pypa/sampleproject",  # Optional
+    author="Kenneth P. Marshall",  # Optional
+    # This should be a valid email address corresponding to the author listed
+    # above.
+    author_email="kenneth.marshall@esrf.fr",  # Optional
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+    #py_modules= ['pdffunctions','pdfgetx3_gui'],
+    packages=   ['pdfgetx3gui'],  # Required
+    install_requires = ['PyQt5','numpy','scipy','matplotlib'],
+    python_requires=">=3.7, <=3.10", #I believe these are the requirements for diffpy.pdfgetx
+)
