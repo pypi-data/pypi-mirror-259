7a68a5b13c372239175553a60ad25d15c9ba84ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 17:04:30 +0000

    Add package leadguru-jobs/0.471.0/leadguru_jobs-0.471.0.tar.gz

---
+++ packages/leadguru-jobs/leadguru_jobs-0.471.0.tar.gz/leadguru_jobs-0.471.0/lgt_jobs/__init__.py
@@ -0,0 +1,59 @@
+name = "lgt_jobs"
+
+from .jobs.user_balance_update import UpdateUserBalanceJob, UpdateUserBalanceJobData
+from .jobs.send_slack_message import SendSlackMessageJob, SendSlackMessageJobData
+from .jobs.analytics import (TrackAnalyticsJob, TrackAnalyticsJobData)
+from .jobs.archive_leads import (ArchiveLeadsJob, ArchiveLeadsJobData)
+from .jobs.bot_stats_update import (BotStatsUpdateJob, BotStatsUpdateJobData)
+from .jobs.chat_history import (LoadChatHistoryJob, LoadChatHistoryJobData)
+from .jobs.update_slack_profile import (UpdateExternalUserProfileJob, UpdateExternalUserProfileJobData)
+from .jobs.mass_message import SendMassMessageSlackChannelJob, SendMassMessageSlackChannelJobData
+from .basejobs import (BaseBackgroundJobData, BaseBackgroundJob, InvalidJobTypeException)
+from .smtp import (SendMailJob, SendMailJobData)
+from .runner import (BackgroundJobRunner)
+from .simple_job import (SimpleTestJob, SimpleTestJobData)
+
+jobs_map = {
+    "SimpleTestJob": SimpleTestJob,
+    "BotStatsUpdateJob": BotStatsUpdateJob,
+    "ArchiveLeadsJob": ArchiveLeadsJob,
+    "SendMailJob": SendMailJob,
+    "TrackAnalyticsJob": TrackAnalyticsJob,
+    "LoadChatHistoryJob": LoadChatHistoryJob,
+    "UpdateExternalUserProfileJob": UpdateExternalUserProfileJob,
+    "SendSlackMessageJob": SendSlackMessageJob,
+    "UpdateUserBalanceJob": UpdateUserBalanceJob,
+    "SendMassMessageSlackChannelJob": SendMassMessageSlackChannelJob,
+}
+__all__ = [
+    # Jobs
+    SimpleTestJob,
+    BotStatsUpdateJob,
+    ArchiveLeadsJob,
+    SendMailJob,
+    SimpleTestJob,
+    LoadChatHistoryJob,
+    UpdateExternalUserProfileJob,
+    TrackAnalyticsJob,
+    SendSlackMessageJob,
+    UpdateUserBalanceJob,
+    SendMassMessageSlackChannelJob,
+
+    # module classes
+    BackgroundJobRunner,
+    BaseBackgroundJobData,
+    BaseBackgroundJob,
+    InvalidJobTypeException,
+    BotStatsUpdateJobData,
+    ArchiveLeadsJobData,
+    SendMailJobData,
+    SimpleTestJobData,
+    LoadChatHistoryJobData,
+    UpdateExternalUserProfileJobData,
+    TrackAnalyticsJobData,
+    SendSlackMessageJobData,
+    UpdateUserBalanceJobData,
+    SendMassMessageSlackChannelJobData,
+    # mapping
+    jobs_map
+]
