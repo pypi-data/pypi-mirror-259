b7d2b56905711b1e022aa32649a86fa2991f532c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 05:06:01 +0000

    Add package pipelinenode/0.0.7/pipelinenode-0.0.7.tar.gz

---
+++ packages/pipelinenode/pipelinenode-0.0.7.tar.gz/pipelinenode-0.0.7/pipelinenode/pipeline/pipeline.py
@@ -0,0 +1,67 @@
+import os
+import importlib.util
+from pathlib import Path
+import toml
+from .node import Node
+import inspect
+
+class Pipeline:
+    def __init__(self):
+        self.nodes = {}
+      
+
+    def run(self):
+        outputs = {}
+        for node_name, node in self.nodes.items():
+            outputs[node_name] = node.execute()
+        return outputs
+
+    def add_node(self, name, func, inputs):
+        self.nodes[name] = Node(name, func, inputs)
+
+    def register(self, pipeline_instance):
+        for pipline in pipeline_instance:
+            if pipline not in self.nodes:
+                raise ValueError(f"Pipeline '{pipline}' ist erforderlich, wurde aber nicht gefunden.")
+        self.nodes.update(pipeline_instance.nodes)
+
+
+class PipelineManager:
+    def __init__(self):
+        self.pipelines = {}
+
+    def load_pipelines(self, base_path=None):
+        if base_path is None: base_path = Path(os.path.dirname(os.path.abspath(inspect.stack()[1].filename)))
+
+        # Lade project.toml-Datei und extrahiere den Pfad und die Namen der Pipelines
+        with open("project.toml", "r") as f:
+            project_config = toml.load(f)
+            pipeline_folder_name = project_config["project"]["place"]
+            pipeline_folder = base_path / pipeline_folder_name
+        
+            for pipeline_name in pipeline_folder.iterdir():
+                if pipeline_name.is_dir():
+                    pipeline_name = pipeline_name.name
+                    pipeline_module_path = pipeline_folder / pipeline_name / "pipeline.py"
+                    if pipeline_module_path.exists():
+                        spec = importlib.util.spec_from_file_location("pipeline_module", pipeline_module_path)
+                        pipeline_module = importlib.util.module_from_spec(spec)
+                        spec.loader.exec_module(pipeline_module)
+                        pipeline_register_function = getattr(pipeline_module, "register", None)
+                        if callable(pipeline_register_function):
+                            pipeline_definition = pipeline_register_function()
+                            if isinstance(pipeline_definition, Pipeline):
+                                self.pipelines[pipeline_name] = pipeline_definition
+                            else:
+                                raise ValueError(f"The register function in {pipeline_module_path} must return a Pipeline instance.")
+
+    def run(self, pipeline_order):
+        results = {}
+        for pipeline_name in pipeline_order:
+            if pipeline_name in self.pipelines:
+                pipeline = self.pipelines[pipeline_name]
+                results[pipeline_name] = pipeline.run()
+            else:
+                raise ValueError(f"Pipeline '{pipeline_name}' not found.")
+
+        return results
---
+++ packages/pipelinenode/pipelinenode-0.0.7.tar.gz/pipelinenode-0.0.7/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+install_requires = [
+    'click==8.1.7',
+    'colorama==0.4.6',
+    'toml==0.10.2'
+]
+
+setup(
+  name='pipelinenode',
+  version='0.0.7',
+  description='Pipeline Builder',
+  #long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Johannes Kanthak',
+  author_email='johannes.kanthak@kdc-solutions.de',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Pipeline', 
+  packages=find_packages(),
+  include_package_data=True,
+  install_requires=install_requires 
+)
