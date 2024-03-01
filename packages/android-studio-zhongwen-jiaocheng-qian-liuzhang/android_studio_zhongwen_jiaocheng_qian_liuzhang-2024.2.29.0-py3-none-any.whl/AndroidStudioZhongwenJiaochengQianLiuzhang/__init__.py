995926cf5cb4d7c7420a946edb5ef76f05307fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:32 +0000

    Add package android-studio-zhongwen-jiaocheng-qian-liuzhang/2024.2.29.0/android_studio_zhongwen_jiaocheng_qian_liuzhang-2024.2.29.0-py3-none-any.whl

---
+++ packages/android-studio-zhongwen-jiaocheng-qian-liuzhang/android_studio_zhongwen_jiaocheng_qian_liuzhang-2024.2.29.0-py3-none-any.whl/AndroidStudioZhongwenJiaochengQianLiuzhang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""android-studio-zhongwen-jiaocheng-qian-liuzhang
+https://github.com/apachecn/android-studio-zhongwen-jiaocheng-qian-liuzhang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.2.29.0"
