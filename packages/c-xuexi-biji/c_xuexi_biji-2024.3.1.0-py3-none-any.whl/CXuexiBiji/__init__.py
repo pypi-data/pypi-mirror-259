b0591b0ae75782c2e8c5e2fcc9c6333c4d42ca86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:40 +0000

    Add package c-xuexi-biji/2024.3.1.0/c_xuexi_biji-2024.3.1.0-py3-none-any.whl

---
+++ packages/c-xuexi-biji/c_xuexi_biji-2024.3.1.0-py3-none-any.whl/CXuexiBiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""c-xuexi-biji
+https://github.com/apachecn/c-xuexi-biji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
