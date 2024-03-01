09e9e0f6aef9a223afd725648f2083cc5c0bc958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 06:06:33 +0000

    Add package pipelinenode/0.0.11/pipelinenode-0.0.11.tar.gz

---
+++ packages/pipelinenode/pipelinenode-0.0.11.tar.gz/pipelinenode-0.0.11/pipelinenode/templates/data.py
@@ -0,0 +1,9 @@
+from pipelinenode.data import Data
+
+class YourDataClass(Data):
+    def __init__(self):
+        super().__init__(
+            name="Name",
+            data="Data",
+            description="Description"
+        )
