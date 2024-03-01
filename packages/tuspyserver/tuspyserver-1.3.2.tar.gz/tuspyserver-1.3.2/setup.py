44bd32c886754942d37bc1273d81a3f8c0341f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 14:05:04 +0000

    Add package tuspyserver/1.3.2/tuspyserver-1.3.2.tar.gz

---
+++ packages/tuspyserver/tuspyserver-1.3.2.tar.gz/tuspyserver-1.3.2/setup.py
@@ -0,0 +1,39 @@
+"""
+FastAPI Tus implementation
+-------------
+Implements the tus.io server-side file-upload protocol
+visit https://tus.io for more information
+"""
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='tuspyserver',
+    version='1.3.2',
+    description='TUS py protocol implementation in FastAPI',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Edi Hasaj',
+    license='MIT',
+    author_email='edihasaj@outlook.com',
+    url='https://github.com/edihasaj/tuspy-fast-api',
+    packages=find_packages(),
+    platforms="any",
+    include_package_data=True,
+    install_requires=[
+        'fastapi>=0.88.0',
+        'starlette>=0.22.0',
+        'pydantic>=1.10.4',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
