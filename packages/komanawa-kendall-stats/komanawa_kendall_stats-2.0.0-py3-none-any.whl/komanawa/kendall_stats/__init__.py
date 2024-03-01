e4808ea7dc84a690a712cbb36cfbd95a4332f9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 14:05:06 +0000

    Add package komanawa-kendall-stats/2.0.0/komanawa_kendall_stats-2.0.0-py3-none-any.whl

---
+++ packages/komanawa-kendall-stats/komanawa_kendall_stats-2.0.0-py3-none-any.whl/komanawa/kendall_stats/__init__.py
@@ -0,0 +1,8 @@
+"""
+created matt_dumont 
+on: 21/09/23
+"""
+from komanawa.kendall_stats.multi_part_kendall import MultiPartKendall, SeasonalMultiPartKendall
+from komanawa.kendall_stats.mann_kendall import MannKendall, SeasonalKendall
+import komanawa.kendall_stats.example_data
+from komanawa.kendall_stats.utils import estimate_runtime
