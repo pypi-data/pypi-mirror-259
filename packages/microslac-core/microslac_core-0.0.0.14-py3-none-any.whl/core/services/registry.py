e2ad8647d02f047cb8599192f797dea9884ca1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 05:06:57 +0000

    Add package microslac-core/0.0.0.14/microslac_core-0.0.0.14-py3-none-any.whl

---
+++ packages/microslac-core/microslac_core-0.0.0.14-py3-none-any.whl/core/services/registry.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+
+from core.services import ProxyService
+
+
+class AuthService(ProxyService):
+    _host = settings.MICROSERVICE_AUTH_HOST
+    _port = settings.MICROSERVICE_AUTH_PORT
+
+
+class TeamService(ProxyService):
+    _host = settings.MICROSERVICE_TEAMS_HOST
+    _port = settings.MICROSERVICE_TEAMS_PORT
+
+
+class UserService(ProxyService):
+    _host = settings.MICROSERVICE_USERS_HOST
+    _port = settings.MICROSERVICE_USERS_PORT
+
+
+class ConversationService(ProxyService):
+    _host = settings.MICROSERVICE_CONVERSATIONS_HOST
+    _port = settings.MICROSERVICE_CONVERSATIONS_PORT
