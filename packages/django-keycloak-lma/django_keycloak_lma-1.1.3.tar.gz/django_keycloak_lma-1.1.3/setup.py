7ad6e3f6f3e04a79ee5ffdc7352f759f76f28b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:47 +0000

    Add package django-keycloak-lma/1.1.3/django_keycloak_lma-1.1.3.tar.gz

---
+++ packages/django-keycloak-lma/django_keycloak_lma-1.1.3.tar.gz/django_keycloak_lma-1.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='django_keycloak_lma',
+      version='1.1.3',
+      description='Modified version of the package django_keycloak (0.1.1)',
+      long_description="""
+Available features:
+- basic auth with token request (KeycloakPasswordAuthentication)
+- bearer token auth (KeycloakTokenAuthentication)
+- refresh token method (services/oidc_profile)
+Examples of using features: tests/lma
+""",
+      long_description_content_type='text/markdown',
+      author='Vyacheslav Konovalov',
+      packages=find_packages(),
+      install_requires=[
+            'djangorestframework',
+            'python-keycloak-client-pkg',
+      ],
+      include_package_data=True,
+      zip_safe=False)
