3e1c824520a6067a9610065a7e5a779ea234edb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 17:04:34 +0000

    Add package programming-abstractions-in-c/2024.3.1.0/programming_abstractions_in_c-2024.3.1.0-py3-none-any.whl

---
+++ packages/programming-abstractions-in-c/programming_abstractions_in_c-2024.3.1.0-py3-none-any.whl/ProgrammingAbstractionsInC/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""programming-abstractions-in-c
+https://github.com/apachecn/programming-abstractions-in-c"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
