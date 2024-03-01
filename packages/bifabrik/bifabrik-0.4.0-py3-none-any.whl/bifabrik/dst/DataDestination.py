ae58baec49946a9c705b4ee76a8831f64f416dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 14:04:43 +0000

    Add package bifabrik/0.4.0/bifabrik-0.4.0-py3-none-any.whl

---
+++ packages/bifabrik/bifabrik-0.4.0-py3-none-any.whl/bifabrik/dst/DataDestination.py
@@ -0,0 +1,23 @@
+# from pyspark.sql.session import SparkSession
+# from pyspark.sql.dataframe import DataFrame
+from bifabrik.base.Task import Task
+
+class DataDestination(Task):
+    def __init__(self, parentPipeline):
+        super().__init__(parentPipeline)
+
+    # def __init__(self, dataLoader, sourceDf: DataFrame):
+    #     self._sourceDf = sourceDf
+    #     self._loader = dataLoader
+    #     dataLoader.destination = self
+    #     self._spark = self._loader.spark
+
+    def save(self) -> None:
+        """Save the data to the destination. This is the "run" method at the end of the chain.
+        """
+        self._pipeline.execute()
+
+    def run(self) -> None:
+        """Save the data to the destination. This is the "run" method at the end of the chain.
+        """
+        self._pipeline.execute()
