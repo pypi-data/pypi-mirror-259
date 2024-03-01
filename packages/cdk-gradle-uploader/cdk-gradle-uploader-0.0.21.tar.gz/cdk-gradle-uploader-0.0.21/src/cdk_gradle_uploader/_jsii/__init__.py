65addfbeeaea64d9705e385201e3acb8d0ad96a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:50 +0000

    Add package cdk-gradle-uploader/0.0.21/cdk-gradle-uploader-0.0.21.tar.gz

---
+++ packages/cdk-gradle-uploader/cdk-gradle-uploader-0.0.21.tar.gz/cdk-gradle-uploader-0.0.21/src/cdk_gradle_uploader/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "gradle_s3_uploader",
+    "0.0.21",
+    __name__[0:-6],
+    "gradle_s3_uploader@0.0.21.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
