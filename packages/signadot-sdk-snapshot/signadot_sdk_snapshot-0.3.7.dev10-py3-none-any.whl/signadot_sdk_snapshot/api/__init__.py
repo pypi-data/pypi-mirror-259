a4b8c575c3f261b18a3fa28853e13973a52520d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 07:06:07 +0000

    Add package signadot-sdk-snapshot/0.3.7.dev10/signadot_sdk_snapshot-0.3.7.dev10-py3-none-any.whl

---
+++ packages/signadot-sdk-snapshot/signadot_sdk_snapshot-0.3.7.dev10-py3-none-any.whl/signadot_sdk_snapshot/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from signadot_sdk_snapshot.api.cluster_api import ClusterApi
+from signadot_sdk_snapshot.api.resource_plugins_api import ResourcePluginsApi
+from signadot_sdk_snapshot.api.route_groups_api import RouteGroupsApi
+from signadot_sdk_snapshot.api.sandboxes_api import SandboxesApi
---
+++ packages/signadot-sdk-snapshot/signadot_sdk_snapshot-0.3.7.dev10-py3-none-any.whl/swagger_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.cluster_api import ClusterApi
+from swagger_client.api.sandboxes_api import SandboxesApi
