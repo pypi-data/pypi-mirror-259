d4c8f6d4a921873362830108b369141137c5d3ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 17:04:36 +0000

    Add package aparat-crawler/0.8.5/aparat_crawler-0.8.5.tar.gz

---
+++ packages/aparat-crawler/aparat_crawler-0.8.5.tar.gz/aparat_crawler-0.8.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aparat_crawler',
+    version='0.8.5',
+    packages=find_packages(),
+    author='Mohammad Amin Orojloo',
+    author_email='ma.orojloo@gmail.com',
+    description='this is an aparat crawler library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/maorojloo/aparat_crawler',
+    install_requires=[
+        'beautifulsoup4==4.12.3',
+        'persian==0.5.0',
+        'requests==2.31.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+    ],
+)
