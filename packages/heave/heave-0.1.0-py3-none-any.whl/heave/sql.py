75dff0d7e53e8b0becf1e451b235bcf835e8c95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:05:02 +0000

    Add package heave/0.1.0/heave-0.1.0-py3-none-any.whl

---
+++ packages/heave/heave-0.1.0-py3-none-any.whl/heave/sql.py
@@ -0,0 +1,23 @@
+"""Batch SQL operations."""
+from sqlalchemy import Connection, MetaData
+from sqlalchemy import Table as SqlTable
+
+from heave import Table
+
+
+def reflect_table(connection: Connection, table_name: str) -> SqlTable:
+    """Reflect a table from the database."""
+    metadata = MetaData()
+    return SqlTable(table_name, metadata, autoload_with=connection)
+
+
+def insert(connection: Connection, sql_table: SqlTable, data: Table) -> None:
+    """Insert data into a table."""
+    for row in data.rows:
+        connection.execute(sql_table.insert().values(dict(zip(data.header, row))))
+
+
+def read(connection: Connection, sql_table: SqlTable) -> Table:
+    """Read data from a table."""
+    result = connection.execute(sql_table.select())
+    return Table([tuple(sql_table.columns.keys())] + [row for row in result])
