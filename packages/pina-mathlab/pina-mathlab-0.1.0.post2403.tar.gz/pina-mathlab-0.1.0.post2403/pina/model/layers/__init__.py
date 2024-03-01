6e3babd0aadefbeb2092d7e7968e6e47529a712d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:17 +0000

    Add package pina-mathlab/0.1.0.post2403/pina-mathlab-0.1.0.post2403.tar.gz

---
+++ packages/pina-mathlab/pina-mathlab-0.1.0.post2403.tar.gz/pina-mathlab-0.1.0.post2403/pina/model/layers/__init__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "ContinuousConvBlock",
+    "ResidualBlock",
+    "EnhancedLinear",
+    "SpectralConvBlock1D",
+    "SpectralConvBlock2D",
+    "SpectralConvBlock3D",
+    "FourierBlock1D",
+    "FourierBlock2D",
+    "FourierBlock3D",
+    "PODBlock",
+]
+
+from .convolution_2d import ContinuousConvBlock
+from .residual import ResidualBlock, EnhancedLinear
+from .spectral import (
+    SpectralConvBlock1D,
+    SpectralConvBlock2D,
+    SpectralConvBlock3D,
+)
+from .fourier import FourierBlock1D, FourierBlock2D, FourierBlock3D
+from .pod import PODBlock
