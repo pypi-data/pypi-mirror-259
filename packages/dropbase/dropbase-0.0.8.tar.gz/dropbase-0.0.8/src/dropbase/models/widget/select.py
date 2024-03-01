a8382e973084bdf00e3e92acd974ecb7066e372e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 14:05:14 +0000

    Add package dropbase/0.0.8/dropbase-0.0.8.tar.gz

---
+++ packages/dropbase/dropbase-0.0.8.tar.gz/dropbase-0.0.8/src/dropbase/models/widget/select.py
@@ -0,0 +1,34 @@
+from typing import Annotated, Any, Dict, List, Literal, Optional
+
+from pydantic import BaseModel
+
+from dropbase.models.category import PropertyCategory
+from dropbase.models.common import ComponentDisplayProperties
+
+
+class SelectContextProperty(ComponentDisplayProperties):
+    pass
+
+
+# TODO: join this with on select
+class OnChange(BaseModel):
+    type: Literal["widget", "function"] = "function"
+    value: str
+
+
+class SelectDefinedProperty(BaseModel):
+    label: Annotated[str, PropertyCategory.default]
+    name: Annotated[str, PropertyCategory.default]
+
+    options: Annotated[Optional[List[Dict]], PropertyCategory.default]
+    default: Annotated[Optional[Any], PropertyCategory.other]
+    multiple: Annotated[Optional[bool], PropertyCategory.other] = False
+
+    # events
+    on_change: Annotated[Optional[OnChange], PropertyCategory.events]
+
+    # display_rules
+    display_rules: Annotated[Optional[List[dict]], PropertyCategory.display_rules]
+
+    # internal
+    component_type: Literal["select"]
