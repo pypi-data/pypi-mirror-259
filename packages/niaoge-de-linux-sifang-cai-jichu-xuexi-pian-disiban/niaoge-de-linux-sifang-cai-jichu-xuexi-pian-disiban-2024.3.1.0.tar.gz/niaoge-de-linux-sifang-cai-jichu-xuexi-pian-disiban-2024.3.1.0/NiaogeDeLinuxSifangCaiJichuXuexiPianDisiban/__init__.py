fd7488a98b916173b5795a4eed65b48d96230ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:31 +0000

    Add package niaoge-de-linux-sifang-cai-jichu-xuexi-pian-disiban/2024.3.1.0/niaoge-de-linux-sifang-cai-jichu-xuexi-pian-disiban-2024.3.1.0.tar.gz

---
+++ packages/niaoge-de-linux-sifang-cai-jichu-xuexi-pian-disiban/niaoge-de-linux-sifang-cai-jichu-xuexi-pian-disiban-2024.3.1.0.tar.gz/niaoge-de-linux-sifang-cai-jichu-xuexi-pian-disiban-2024.3.1.0/NiaogeDeLinuxSifangCaiJichuXuexiPianDisiban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""niaoge-de-linux-sifang-cai-jichu-xuexi-pian-disiban
+https://github.com/apachecn/niaoge-de-linux-sifang-cai-jichu-xuexi-pian-disiban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
