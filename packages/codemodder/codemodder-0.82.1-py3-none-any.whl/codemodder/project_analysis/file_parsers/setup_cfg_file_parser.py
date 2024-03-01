c9248af62ad0de3e093e72f1ebd58c6a5f52a3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 04:06:18 +0000

    Add package codemodder/0.82.1/codemodder-0.82.1-py3-none-any.whl

---
+++ packages/codemodder/codemodder-0.82.1-py3-none-any.whl/codemodder/project_analysis/file_parsers/setup_cfg_file_parser.py
@@ -0,0 +1,36 @@
+from codemodder.project_analysis.file_parsers.package_store import (
+    PackageStore,
+    FileType,
+)
+from pathlib import Path
+import configparser
+
+from .base_parser import BaseParser
+from codemodder.logging import logger
+
+
+class SetupCfgParser(BaseParser):
+    @property
+    def file_type(self):
+        return FileType.SETUP_CFG
+
+    def _parse_file(self, file: Path) -> PackageStore | None:
+        config = configparser.ConfigParser()
+        try:
+            config.read(file)
+        except configparser.ParsingError:
+            logger.debug("Unable to parse setup.cfg file.")
+            return None  # pragma: no cover
+
+        if "options" not in config:
+            return None
+
+        dependency_lines = config["options"].get("install_requires", "").split("\n")
+        python_requires = config["options"].get("python_requires", "")
+
+        return PackageStore(
+            type=self.file_type,
+            file=file,
+            dependencies=set(line for line in dependency_lines if line),
+            py_versions=[python_requires] if python_requires else [],
+        )
