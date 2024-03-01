930bab996002949497682d0995e29562692e26e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:21 +0000

    Add package autokit/0.7.3/autokit-0.7.3.tar.gz

---
+++ packages/autokit/autokit-0.7.3.tar.gz/autokit-0.7.3/test/TestTool.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from autokit.ExternalTool import ExternalTool
+from autokit.PlatformData import PlatformData
+from autokit.ToolConfig import ToolConfig
+
+
+class TestTool(ExternalTool):
+    def __init__(self, base_dir: str = "./third-party", progress_bar: bool = True, lazy_setup: bool = False):
+        super().__init__(Path(base_dir), progress_bar, lazy_setup)
+
+    @property
+    def config(self) -> ToolConfig:
+        return ToolConfig(
+            tool_name="test-tool",
+            platform_data={
+                "windows": PlatformData(
+                    url="https://github.com/IRSS-UBC/MediaTools/releases/download/latest/win-x64.zip",
+                    subdir=Path(""),
+                    executable=Path("IRSSMediaTools.exe")
+                ),
+            },
+            python=False,
+        )
+
+    def help(self):
+        self.run_command("help")
+
+
+if __name__ == "__main__":
+    test = TestTool()
+    test.help()
