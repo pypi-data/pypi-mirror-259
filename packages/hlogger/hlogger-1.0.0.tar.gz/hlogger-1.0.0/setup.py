605e5d78e1dbc9cd5bcb930de2da522a0fb169d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 17:04:32 +0000

    Add package hlogger/1.0.0/hlogger-1.0.0.tar.gz

---
+++ packages/hlogger/hlogger-1.0.0.tar.gz/hlogger-1.0.0/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="hlogger",
+    version="1.0.0",
+    description="A logger utility for python application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/llChameleoNll/hlogger",
+    author="llChameleoNll",
+    author_email="chameleon2318@gmail.com",
+    keywords="logger, logging",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.7, <4",
+    project_urls={
+        "Bug Reports": "https://github.com/sChameleoNz/hlogger/issues",
+        "Source": "https://github.com/sChameleoNz/hlogger",
+        "Buy me a coffee": "https://www.buymeacoffee.com/llhoyall",
+    },
+)
