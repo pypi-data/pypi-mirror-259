236596ba45b0c076c1a6423d9495982cc5366811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 05:07:00 +0000

    Add package CADET-RDM/0.0.23/CADET_RDM-0.0.23-py3-none-any.whl

---
+++ packages/CADET-RDM/CADET_RDM-0.0.23-py3-none-any.whl/cadetrdm/__init__.py
@@ -0,0 +1,5 @@
+from .conda_env_utils import prepare_conda_env
+from .initialize_repo import initialize_repo, clone
+from .repositories import ProjectRepo, JupyterInterfaceRepo
+
+__version__ = "0.0.23"
