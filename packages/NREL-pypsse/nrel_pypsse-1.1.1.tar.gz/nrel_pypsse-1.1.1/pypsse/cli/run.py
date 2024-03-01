ef6c362da8750c6d3928667f1ceca0c04f392c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 06:04:59 +0000

    Add package NREL-pypsse/1.1.1/nrel_pypsse-1.1.1.tar.gz

---
+++ packages/NREL-pypsse/nrel_pypsse-1.1.1.tar.gz/nrel_pypsse-1.1.1/pypsse/cli/run.py
@@ -0,0 +1,32 @@
+"""
+CLI to run a PyDSS project
+"""
+
+from pathlib import Path
+
+import click
+
+from pypsse.common import SIMULATION_SETTINGS_FILENAME
+from pypsse.simulator import Simulator
+
+
+@click.argument(
+    "project-path",
+)
+@click.option(
+    "-s",
+    "--simulations-file",
+    required=False,
+    default=SIMULATION_SETTINGS_FILENAME,
+    show_default=True,
+    help="scenario toml file to run (over rides default)",
+)
+@click.command()
+def run(project_path, simulations_file=None):
+    """Runs a valid PyPSSE simulation."""
+    file_path = Path(project_path) / simulations_file
+    msg = "Simulation file not found. Use -s to choose a valid settings file"
+    "if its name differs from the default file name."
+    assert file_path.exists(), msg
+    x = Simulator.from_setting_files(file_path)
+    x.run()
