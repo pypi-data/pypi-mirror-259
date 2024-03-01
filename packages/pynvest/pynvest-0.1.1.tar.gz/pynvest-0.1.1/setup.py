9f0fde24f21e33a69b5bf55bd9ea85641cb7ca2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:10 +0000

    Add package pynvest/0.1.1/pynvest-0.1.1.tar.gz

---
+++ packages/pynvest/pynvest-0.1.1.tar.gz/pynvest-0.1.1/setup.py
@@ -0,0 +1,47 @@
+"""Package setup script.
+
+This script handles everything about package publishing on PyPI.
+"""
+
+# Importing libraries
+from setuptools import setup, find_packages
+
+# Reading README.md for project description
+with open("README.md", "r", encoding='utf-8') as f:
+    __long_description__ = f.read()
+
+# Setting up package information
+setup(
+    name='pynvest',
+    version='0.1.1',
+    author='Thiago Panini',
+    author_email='panini.development@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "lxml",
+        "pandas"
+    ],
+    license='MIT',
+    description="Uma forma fácil de extrair indicadores de ativos da bolsa B3",
+    long_description=__long_description__,
+    long_description_content_type="text/markdown",
+    url='https://pynvest.readthedocs.io/pt/latest/',
+    keywords='B3, Ativos, Fundos Imobiliários, Mercado Financeiro',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: Portuguese (Brazilian)",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    python_requires=">=3.0.0"
+)
