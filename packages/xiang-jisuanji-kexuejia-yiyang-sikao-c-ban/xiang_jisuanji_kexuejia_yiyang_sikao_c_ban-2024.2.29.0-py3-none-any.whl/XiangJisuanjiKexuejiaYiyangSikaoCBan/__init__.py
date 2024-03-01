d4f6e9737832b31b152629f93a29bb300d79a94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:34 +0000

    Add package xiang-jisuanji-kexuejia-yiyang-sikao-c-ban/2024.2.29.0/xiang_jisuanji_kexuejia_yiyang_sikao_c_ban-2024.2.29.0-py3-none-any.whl

---
+++ packages/xiang-jisuanji-kexuejia-yiyang-sikao-c-ban/xiang_jisuanji_kexuejia_yiyang_sikao_c_ban-2024.2.29.0-py3-none-any.whl/XiangJisuanjiKexuejiaYiyangSikaoCBan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiang-jisuanji-kexuejia-yiyang-sikao-c-ban
+https://github.com/apachecn/xiang-jisuanji-kexuejia-yiyang-sikao-c-ban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.2.29.0"
