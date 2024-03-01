b5c60cce4b739893dd9618130430f7b5823c6ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 06:05:12 +0000

    Add package create-sequel-app/0.3.9/create-sequel-app-0.3.9.tar.gz

---
+++ packages/create-sequel-app/create-sequel-app-0.3.9.tar.gz/create-sequel-app-0.3.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="create-sequel-app",
+    version="0.3.9",
+    py_modules=["create_sequel_app"],
+    description="Create a new Sequel project",
+    author="Kyle Lee",
+    author_email="kyle@kylelee.dev",
+    include_package_data=True,
+    packages=find_namespace_packages(include=["create_sequel_app.*"]),
+    package_data={
+        "create_sequel_app": ["template_project/*", "template_project/**/*"],
+    },
+    setup_requires=["click"],
+    install_requires=[
+        "Click",
+    ],
+    entry_points="""
+        [console_scripts]
+        create-sequel-app=create_sequel_app:create_project
+    """,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.7",
+)
