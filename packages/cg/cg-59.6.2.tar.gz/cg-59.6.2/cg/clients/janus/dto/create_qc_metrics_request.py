423f1363ed76834dd829df50895e9c371a4a1a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:04:35 +0000

    Add package cg/59.6.2/cg-59.6.2.tar.gz

---
+++ packages/cg/cg-59.6.2.tar.gz/cg-59.6.2/cg/clients/janus/dto/create_qc_metrics_request.py
@@ -0,0 +1,27 @@
+"""Post request model for JanusAPI."""
+
+from pydantic import BaseModel
+
+from cg.constants import PrepCategory, Workflow
+
+
+class WorkflowInfo(BaseModel):
+    workflow: Workflow
+    version: str
+
+
+class FilePathAndTag(BaseModel):
+    """Model for the file path and its associated tag."""
+
+    file_path: str
+    tag: str
+
+
+class CreateQCMetricsRequest(BaseModel):
+    """Model for the qc collection request."""
+
+    case_id: str
+    sample_ids: list[str]
+    files: list[FilePathAndTag]
+    workflow_info: WorkflowInfo
+    prep_category: PrepCategory
