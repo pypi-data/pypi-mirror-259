54af56a68138bb19d467e3b02c186b2ff47c3118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 04:05:56 +0000

    Add package pipelinenode/0.0.2/pipelinenode-0.0.2.tar.gz

---
+++ packages/pipelinenode/pipelinenode-0.0.2.tar.gz/pipelinenode-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='pipelinenode',
+  version='0.0.2',
+  description='A very basic calculator',
+  #long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Johannes Kanthak',
+  author_email='johannes.kanthak@kdc-solutions.de',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Pipeline', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
