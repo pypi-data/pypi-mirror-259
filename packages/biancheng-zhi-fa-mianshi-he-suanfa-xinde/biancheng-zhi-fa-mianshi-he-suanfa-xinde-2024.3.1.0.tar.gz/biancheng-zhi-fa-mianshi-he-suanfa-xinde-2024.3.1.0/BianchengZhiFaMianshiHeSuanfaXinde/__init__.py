d9becb40ee84abea4b4c5dddf46a8f1c5c450b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:25 +0000

    Add package biancheng-zhi-fa-mianshi-he-suanfa-xinde/2024.3.1.0/biancheng-zhi-fa-mianshi-he-suanfa-xinde-2024.3.1.0.tar.gz

---
+++ packages/biancheng-zhi-fa-mianshi-he-suanfa-xinde/biancheng-zhi-fa-mianshi-he-suanfa-xinde-2024.3.1.0.tar.gz/biancheng-zhi-fa-mianshi-he-suanfa-xinde-2024.3.1.0/BianchengZhiFaMianshiHeSuanfaXinde/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""biancheng-zhi-fa-mianshi-he-suanfa-xinde
+https://github.com/apachecn/biancheng-zhi-fa-mianshi-he-suanfa-xinde"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
