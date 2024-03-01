eafdd9e07e908731c094cdbf6d978c6e3a7c2be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 14:04:32 +0000

    Add package openai-cost-tracker/0.5/openai_cost_tracker-0.5.tar.gz

---
+++ packages/openai-cost-tracker/openai_cost_tracker-0.5.tar.gz/openai_cost_tracker-0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='openai_cost_tracker',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'tiktoken==0.5.2',
+        'lorem-text==2.1',
+        'openai==0.28.0'
+    ],
+    author='Roy Xie',
+    author_email='royx252@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ruoyuxie/openai-cost-tracker'
+)
