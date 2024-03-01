e10b897b491fadcf148ad0db0660df206201c301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 05:07:38 +0000

    Add package nettime-py/0.0.4/nettime_py-0.0.4.tar.gz

---
+++ packages/nettime-py/nettime_py-0.0.4.tar.gz/nettime_py-0.0.4/nettime_py/containers/employee.py
@@ -0,0 +1,23 @@
+from pydantic import validate_call
+from .base import ContainerBase
+from ..schemas.list.employees import Employee as ListSchema
+from ..schemas.detail.employees import Employee as DetailSchema
+from ..schemas.detail import Planning
+
+
+class Employee(ContainerBase[ListSchema, DetailSchema]):
+    path_attribute = ""
+    order = "id"
+    container_name = "Persona"
+    list_schema = ListSchema
+    detail_schema = DetailSchema
+
+    @validate_call
+    def add_planning(
+        self, planning: Planning, all_: bool = False, **kwargs
+    ) -> Planning:
+        if isinstance(planning.employee, list):
+            return self._client.plannings.save_massive(
+                data=planning, elements=planning.employee, all_=all_, **kwargs
+            )
+        return self._client.plannings.save(data=planning, **kwargs)
---
+++ packages/nettime-py/nettime_py-0.0.4.tar.gz/nettime_py-0.0.4/nettime_py/containers/reader.py
@@ -0,0 +1,12 @@
+from .base import ContainerBase
+from ..schemas.list.readers import Reader as ListSchema
+from ..schemas.detail.readers import Reader as DetailSchema
+
+
+# TODO: create and replace view model
+class Reader(ContainerBase[ListSchema, DetailSchema]):
+    path_attribute = ""
+    order = "id"
+    container_name = "Lector"
+    list_schema = ListSchema
+    detail_schema = DetailSchema
