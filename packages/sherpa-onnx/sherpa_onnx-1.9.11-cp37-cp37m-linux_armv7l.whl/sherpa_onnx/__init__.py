48ca0bd971c36693fa62b82a6f34f3fc8787c3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 14:05:21 +0000

    Add package sherpa-onnx/1.9.11/sherpa_onnx-1.9.11-cp37-cp37m-linux_armv7l.whl

---
+++ packages/sherpa-onnx/sherpa_onnx-1.9.11-cp37-cp37m-linux_armv7l.whl/sherpa_onnx/__init__.py
@@ -0,0 +1,23 @@
+from _sherpa_onnx import (
+    CircularBuffer,
+    Display,
+    OfflineStream,
+    OfflineTts,
+    OfflineTtsConfig,
+    OfflineTtsModelConfig,
+    OfflineTtsVitsModelConfig,
+    OnlineStream,
+    SileroVadModelConfig,
+    SpeakerEmbeddingExtractor,
+    SpeakerEmbeddingExtractorConfig,
+    SpeakerEmbeddingManager,
+    SpeechSegment,
+    VadModel,
+    VadModelConfig,
+    VoiceActivityDetector,
+)
+
+from .offline_recognizer import OfflineRecognizer
+from .online_recognizer import OnlineRecognizer
+from .utils import text2token
+__version__ = '1.9.11'
