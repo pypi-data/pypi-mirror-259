5daafa8f27a23aa87b5a94f33693007eab8cd145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 06:04:59 +0000

    Add package ob-metaflow-stubs/2.11.4.2/ob-metaflow-stubs-2.11.4.2.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-2.11.4.2.tar.gz/ob-metaflow-stubs-2.11.4.2/metaflow-stubs/mflog/mflog.pyi
@@ -0,0 +1,22 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.4.2                                                           #
+# Generated on 2024-02-29T18:52:32.464844                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import datetime
+
+VERSION: bytes
+
+RE: bytes
+
+ISOFORMAT: str
+
+MISSING_TIMESTAMP: datetime.datetime
+
+MISSING_TIMESTAMP_STR: str
+
