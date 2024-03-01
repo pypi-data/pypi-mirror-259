160b3c1cca0f19ff93c6713fda1f692f773ab2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:26 +0000

    Add package x2015-hulianwang-qiye-xiaozhao-bishi-ti/2024.2.29.0/x2015_hulianwang_qiye_xiaozhao_bishi_ti-2024.2.29.0-py3-none-any.whl

---
+++ packages/x2015-hulianwang-qiye-xiaozhao-bishi-ti/x2015_hulianwang_qiye_xiaozhao_bishi_ti-2024.2.29.0-py3-none-any.whl/X2015HulianwangQiyeXiaozhaoBishiTi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""x2015-hulianwang-qiye-xiaozhao-bishi-ti
+https://github.com/apachecn/x2015-hulianwang-qiye-xiaozhao-bishi-ti"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.2.29.0"
