2c4b0030a9d08bc6c840c7e6161276d7b94b5404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:20 +0000

    Add package ecmascript-5-1-guifan-zhongwenban/2024.3.1.0/ecmascript_5_1_guifan_zhongwenban-2024.3.1.0-py3-none-any.whl

---
+++ packages/ecmascript-5-1-guifan-zhongwenban/ecmascript_5_1_guifan_zhongwenban-2024.3.1.0-py3-none-any.whl/Ecmascript51GuifanZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ecmascript-5-1-guifan-zhongwenban
+https://github.com/apachecn/ecmascript-5-1-guifan-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
