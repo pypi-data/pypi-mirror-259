1dcd6d635284993b02533bbf4275b7cd5fab9869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:15 +0000

    Add package django-girls-taipei-tutorial/2024.3.1.0/django_girls_taipei_tutorial-2024.3.1.0-py3-none-any.whl

---
+++ packages/django-girls-taipei-tutorial/django_girls_taipei_tutorial-2024.3.1.0-py3-none-any.whl/DjangoGirlsTaipeiTutorial/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""django-girls-taipei-tutorial
+https://github.com/apachecn/django-girls-taipei-tutorial"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
