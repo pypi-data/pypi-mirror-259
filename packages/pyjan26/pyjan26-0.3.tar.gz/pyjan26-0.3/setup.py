169aa90a63fd3fa2df278620581e20334cd68a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 04:06:25 +0000

    Add package pyjan26/0.3/pyjan26-0.3.tar.gz

---
+++ packages/pyjan26/pyjan26-0.3.tar.gz/pyjan26-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyjan26',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'Jinja2>=3.1.3',
+        'jinja2_markdown>=0.0.3'
+    ],
+    author='Josnin',
+    license='MIT',
+    description='PyJan26 is a static site generator written in Python. It allows you to generate static websites from templates and content files, with support for pagination, custom pages, custom filters, and custom collections.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    project_urls={
+        'Source': 'https://github.com/josnin/pyjan26',
+    }
+)
