4acebc2f81b3f9b20bf0e4127ad0b6abce9343f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 15:04:25 +0000

    Add package biancheng-xiaobai-de-diyiben-python-rumen-shu/2024.3.1.0/biancheng-xiaobai-de-diyiben-python-rumen-shu-2024.3.1.0.tar.gz

---
+++ packages/biancheng-xiaobai-de-diyiben-python-rumen-shu/biancheng-xiaobai-de-diyiben-python-rumen-shu-2024.3.1.0.tar.gz/biancheng-xiaobai-de-diyiben-python-rumen-shu-2024.3.1.0/BianchengXiaobaiDeDiyibenPythonRumenShu/README.md
@@ -0,0 +1,27 @@
+# 编程小白的第一本 Python 入门书
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/biancheng-xiaobai-de-diyiben-python-rumen-shu
+docker run -tid -p <port>:80 apachecn0/biancheng-xiaobai-de-diyiben-python-rumen-shu
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install biancheng-xiaobai-de-diyiben-python-rumen-shu
+biancheng-xiaobai-de-diyiben-python-rumen-shu <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g biancheng-xiaobai-de-diyiben-python-rumen-shu
+biancheng-xiaobai-de-diyiben-python-rumen-shu <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/biancheng-xiaobai-de-diyiben-python-rumen-shu/biancheng-xiaobai-de-diyiben-python-rumen-shu-2024.3.1.0.tar.gz/biancheng-xiaobai-de-diyiben-python-rumen-shu-2024.3.1.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import BianchengXiaobaiDeDiyibenPythonRumenShu
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('BianchengXiaobaiDeDiyibenPythonRumenShu'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="biancheng-xiaobai-de-diyiben-python-rumen-shu",
+    version=BianchengXiaobaiDeDiyibenPythonRumenShu.__version__,
+    url="https://github.com/apachecn/biancheng-xiaobai-de-diyiben-python-rumen-shu",
+    author=BianchengXiaobaiDeDiyibenPythonRumenShu.__author__,
+    author_email=BianchengXiaobaiDeDiyibenPythonRumenShu.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="编程小白的第一本 Python 入门书",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "biancheng-xiaobai-de-diyiben-python-rumen-shu=BianchengXiaobaiDeDiyibenPythonRumenShu.__main__:main",
+            "BianchengXiaobaiDeDiyibenPythonRumenShu=BianchengXiaobaiDeDiyibenPythonRumenShu.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
