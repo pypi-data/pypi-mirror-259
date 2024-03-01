091eed4ea5bfdf2c751f5df4f90c52e1258dd43e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:04:42 +0000

    Add package connectors-test/1.0.0/connectors_test-1.0.0-py3-none-any.whl

---
+++ packages/connectors-test/connectors_test-1.0.0-py3-none-any.whl/apiconnectors/mattermost/__init__.py
@@ -0,0 +1,4 @@
+"""Mattermost API connector."""
+
+# pylint: disable=W0406
+from apiconnectors.mattermost import MattermostClient
---
+++ packages/connectors-test/connectors_test-1.0.0-py3-none-any.whl/apiconnectors/mattermost/client.py
@@ -0,0 +1,32 @@
+"""Mattermost client."""
+
+from mattermostdriver import (
+    Client,
+    Driver,
+)
+
+
+class MattermostClient(Driver):
+    """
+    A class which wraps the `mattermostdriver.Driver` class.
+
+    Attributes:
+        options (dict): Configuration options for the Mattermost driver.
+        client_tls (Client): Transport Layer Security (TLS) settings for the Mattermost client.
+
+    Inherits:
+        Driver: Base class for MattermostClient, providing common functionality.
+    """
+
+    def __init__(
+        self,
+        options: dict,
+        client_tls: Client,
+    ):
+        """Initialize MattermostClient.
+
+        Args:
+            options (dict): Configuration options for the Mattermost driver.
+            client_tls (Client): Transport Layer Security (TLS) settings for the Mattermost client.
+        """
+        super().__init__(options, client_tls)
