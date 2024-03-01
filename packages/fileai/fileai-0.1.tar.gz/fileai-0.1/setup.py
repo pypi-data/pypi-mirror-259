f13e20386af9bafe40db22c45b6c6102fffb04a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 16:04:37 +0000

    Add package fileai/0.1/fileai-0.1.tar.gz

---
+++ packages/fileai/fileai-0.1.tar.gz/fileai-0.1/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='fileai',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-learn',
+        'numpy',
+        'speechrecognition'
+    ],
+    entry_points={
+        'console_scripts': [
+            'filely=filely:main',
+        ],
+    },
+)
