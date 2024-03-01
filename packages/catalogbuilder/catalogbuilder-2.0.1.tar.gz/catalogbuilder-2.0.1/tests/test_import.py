497b87676886876b4345b82eda75ecb45b718fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 06:05:16 +0000

    Add package catalogbuilder/2.0.1/catalogbuilder-2.0.1.tar.gz

---
+++ packages/catalogbuilder/catalogbuilder-2.0.1.tar.gz/catalogbuilder-2.0.1/tests/test_import.py
@@ -0,0 +1,10 @@
+def check_import():
+ try:
+  from intakebuilder import getinfo, localcrawler, CSVwriter
+  print("Imported intakebuilder and getinfo, localcrawler, CSVwriter ")
+ except ImportError:
+  raise ImportError('Error importing intakebuilder and other packages')
+  return -97
+ return True
+def test_import():
+    assert check_import() == True
