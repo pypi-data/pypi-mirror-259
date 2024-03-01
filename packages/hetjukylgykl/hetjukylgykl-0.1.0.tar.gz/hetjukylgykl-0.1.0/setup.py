4533bef0e58f9d81db1226c7d4553d212341f858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:10 +0000

    Add package hetjukylgykl/0.1.0/hetjukylgykl-0.1.0.tar.gz

---
+++ packages/hetjukylgykl/hetjukylgykl-0.1.0.tar.gz/hetjukylgykl-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='hetjukylgykl',
+version='0.1.0',
+author='querzy',
+author_email='fhuart250@gmail.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
