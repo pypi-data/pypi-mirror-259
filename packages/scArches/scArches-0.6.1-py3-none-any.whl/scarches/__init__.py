bec19ad0b391e5e265781937370330ef9e721961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 14:05:07 +0000

    Add package scArches/0.6.1/scArches-0.6.1-py3-none-any.whl

---
+++ packages/scArches/scArches-0.6.1-py3-none-any.whl/scarches/__init__.py
@@ -0,0 +1,15 @@
+from . import dataset, metrics, trainers, models, zenodo, plotting, utils, classifiers
+
+__author__ = ', '.join([
+    'Mohammad Lotfollahi',
+    'Sergei Rybakov',
+    'Marco Wagenstetter',
+    'Mohsen Naghipourfar',
+])
+
+__email__ = ', '.join([
+    'mohammad.lotfollahi@helmholtz-muenchen.de',
+    'sergei.rybakov@helmholtz-muenchen.de'
+])
+
+__version__ = '0.6.1'
