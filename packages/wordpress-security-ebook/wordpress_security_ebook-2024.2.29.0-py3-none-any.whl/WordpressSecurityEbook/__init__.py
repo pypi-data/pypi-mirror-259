1c5020624f841cd818a07778f289d54783a1bd47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:03 +0000

    Add package wordpress-security-ebook/2024.2.29.0/wordpress_security_ebook-2024.2.29.0-py3-none-any.whl

---
+++ packages/wordpress-security-ebook/wordpress_security_ebook-2024.2.29.0-py3-none-any.whl/WordpressSecurityEbook/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wordpress-security-ebook
+https://github.com/apachecn/wordpress-security-ebook"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.2.29.0"
