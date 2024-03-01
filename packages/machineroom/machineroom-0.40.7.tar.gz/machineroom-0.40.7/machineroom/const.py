b6009f1701bd83002c8a780ab7ef84e2a8580148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:34 +0000

    Add package machineroom/0.40.7/machineroom-0.40.7.tar.gz

---
+++ packages/machineroom/machineroom-0.40.7.tar.gz/machineroom-0.40.7/machineroom/const.py
@@ -0,0 +1,11 @@
+DATAPATH_BASE = "...._file....locator"
+TEMP_FILE = "tmp.txt"
+TEMP_JS = "tmp.js"
+REMOTE_WS = "...remote_locator"
+RAM_GB_REQUIREMENT = 4
+PUB_KEY = "/Users/xxxx/.ssh/id_rsa.pub"
+LOCAL_KEY_HOLDER = "/Users/xxxx/.ssh"
+MY_KEY_FEATURE = "xxx@xxxx"
+HOME = "/root"
+DOCKER_COMPOSE_VERSION = "2.24.6"
+
