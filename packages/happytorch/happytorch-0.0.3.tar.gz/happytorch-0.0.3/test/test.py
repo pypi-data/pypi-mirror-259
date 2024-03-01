579715419d392279c4047ee70fa0c5395fb506e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-01 03:05:11 +0000

    Add package happytorch/0.0.3/happytorch-0.0.3.tar.gz

---
+++ packages/happytorch/happytorch-0.0.3.tar.gz/happytorch-0.0.3/test/test.py
@@ -0,0 +1,10 @@
+
+
+import happytorch as ht
+
+args = ht.get_args('/home/nemo/Mourn/a2pip/test/configs/4117b_stln_shanhai.yaml')
+
+print(args.memory_size, type(args.memory_size)) 
+
+# ht.init_logger(args)
+
