c21b6f62aaac7ef039868a5995c0844d6532b480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:20 +0000

    Add package asyncj/1.1.2/asyncj-1.1.2-py3-none-any.whl

---
+++ packages/asyncj/asyncj-1.1.2-py3-none-any.whl/asyncj/__init__.py
@@ -0,0 +1,12 @@
+"""
+:authors: VengDevs
+:licence: Apache License, Version 2.0
+:copyright: (c) 2024 VengDevs
+"""
+
+from .asyncj import AsyncJson
+from .syncj import SyncJson
+
+
+__version__ = '1.0.7'
+__author__ = 'VengDevs'
