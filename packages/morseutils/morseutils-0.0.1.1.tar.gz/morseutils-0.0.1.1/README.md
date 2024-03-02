ac38542c697750a6dcd1d853cb63d93c5c125fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:25 +0000

    Add package morseutils/0.0.1.1/morseutils-0.0.1.1.tar.gz

---
+++ packages/morseutils/morseutils-0.0.1.1.tar.gz/morseutils-0.0.1.1/README.md
@@ -0,0 +1,1 @@
+# Python Mourse Code Utils
---
+++ packages/morseutils/morseutils-0.0.1.1.tar.gz/morseutils-0.0.1.1/tests/test_translator.py
@@ -0,0 +1,10 @@
+import unittest
+from translator import MorseCodeTranslator
+
+class TestTranslator(unittest.TestCase):
+    def test_encode(self):
+        self.assertEqual(MorseCodeTranslator.encode('hello'), '.... . .-.. .-.. ---')
+        self.assertEqual(MorseCodeTranslator.encode('world'), '.-- --- .-. .-.. -..')
+        self.assertEqual(MorseCodeTranslator.encode('hello world'), '.... . .-.. .-.. ---   .-- --- .-. .-.. -..')
+    
+unittest.main()
