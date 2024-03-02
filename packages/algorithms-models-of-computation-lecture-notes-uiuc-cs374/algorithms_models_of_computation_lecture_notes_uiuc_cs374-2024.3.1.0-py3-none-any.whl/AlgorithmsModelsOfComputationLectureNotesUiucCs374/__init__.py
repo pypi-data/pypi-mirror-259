aac2e8c1000e9684df680189c523af9cb2541f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:56 +0000

    Add package algorithms-models-of-computation-lecture-notes-uiuc-cs374/2024.3.1.0/algorithms_models_of_computation_lecture_notes_uiuc_cs374-2024.3.1.0-py3-none-any.whl

---
+++ packages/algorithms-models-of-computation-lecture-notes-uiuc-cs374/algorithms_models_of_computation_lecture_notes_uiuc_cs374-2024.3.1.0-py3-none-any.whl/AlgorithmsModelsOfComputationLectureNotesUiucCs374/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""algorithms-models-of-computation-lecture-notes-uiuc-cs374
+https://github.com/apachecn/algorithms-models-of-computation-lecture-notes-uiuc-cs374"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
