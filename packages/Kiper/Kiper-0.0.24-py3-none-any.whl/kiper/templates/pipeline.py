a7ff1c6bba7f4da76525826b5b7ee86a5ae6d432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:05:12 +0000

    Add package Kiper/0.0.24/Kiper-0.0.24-py3-none-any.whl

---
+++ packages/Kiper/Kiper-0.0.24-py3-none-any.whl/kiper/templates/pipeline.py
@@ -0,0 +1,22 @@
+from kiper.pipeline.pipeline import Pipeline
+from kiper.pipeline.node import Node
+from pipelines.example_pipeline.node import node_function_1, node_function_2
+
+# Dont change name and directory of __file__ (pipline.py)
+
+# Beispiel für die Registrierung einer Pipeline
+
+
+def register(data_manager) -> Pipeline:
+    # Definieren Sie die Nodes für die Pipeline
+    nodes = [
+        Node(name='node1', function=node_function_1, inputs=[1, 2,data_manager]),
+        Node(name='node2', function=node_function_2, inputs=[1, 2], )
+    ]
+
+    # Erstellen und konfigurieren Sie die Pipeline mit den Nodes
+    pipeline = Pipeline() 
+    for node in nodes:
+        pipeline.add_node(node.name, node.function, node.inputs)
+
+    return pipeline
