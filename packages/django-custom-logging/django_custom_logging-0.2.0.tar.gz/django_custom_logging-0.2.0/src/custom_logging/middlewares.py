34eefe2dade1c9029de9f259ffe8030edcbce4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:06:00 +0000

    Add package django-custom-logging/0.2.0/django_custom_logging-0.2.0.tar.gz

---
+++ packages/django-custom-logging/django_custom_logging-0.2.0.tar.gz/django_custom_logging-0.2.0/src/custom_logging/middlewares.py
@@ -0,0 +1,9 @@
+from . import local_thread
+
+
+def capture_request(get_response):
+    def middleware(request):
+        local_thread.request = request or None
+        return get_response(request)
+
+    return middleware
