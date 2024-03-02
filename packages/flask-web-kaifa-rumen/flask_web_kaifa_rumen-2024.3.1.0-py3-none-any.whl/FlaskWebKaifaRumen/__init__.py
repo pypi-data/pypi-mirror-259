f8c5ca7cd8842f013ec054961c25d55fe180dadd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:04:49 +0000

    Add package flask-web-kaifa-rumen/2024.3.1.0/flask_web_kaifa_rumen-2024.3.1.0-py3-none-any.whl

---
+++ packages/flask-web-kaifa-rumen/flask_web_kaifa_rumen-2024.3.1.0-py3-none-any.whl/FlaskWebKaifaRumen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""flask-web-kaifa-rumen
+https://github.com/apachecn/flask-web-kaifa-rumen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
