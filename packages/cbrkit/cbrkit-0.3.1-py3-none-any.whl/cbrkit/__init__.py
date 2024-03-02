63ba6790144dbbe6919bfb4ab7101160eb2c92d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:29 +0000

    Add package cbrkit/0.3.1/cbrkit-0.3.1-py3-none-any.whl

---
+++ packages/cbrkit/cbrkit-0.3.1-py3-none-any.whl/cbrkit/__init__.py
@@ -0,0 +1,16 @@
+"""
+**[🌟 GitHub Project 🌟](https://github.com/wi2trier/cbrkit)**
+
+.. include:: ../README.md
+
+"""
+
+from . import global_sim, loaders, retrieval, sim, typing
+
+__all__ = (
+    "loaders",
+    "sim",
+    "global_sim",
+    "typing",
+    "retrieval",
+)
---
+++ packages/cbrkit/cbrkit-0.3.1-py3-none-any.whl/cbrkit/sim/__init__.py
@@ -0,0 +1,16 @@
+from . import collections, generic, numeric, strings, taxonomy
+from ._helpers import AbstractFloat, dist2sim, sim2map, sim2seq, unpack_sim, unpack_sims
+
+__all__ = [
+    "collections",
+    "generic",
+    "numeric",
+    "strings",
+    "taxonomy",
+    "dist2sim",
+    "sim2map",
+    "sim2seq",
+    "unpack_sim",
+    "unpack_sims",
+    "AbstractFloat",
+]
