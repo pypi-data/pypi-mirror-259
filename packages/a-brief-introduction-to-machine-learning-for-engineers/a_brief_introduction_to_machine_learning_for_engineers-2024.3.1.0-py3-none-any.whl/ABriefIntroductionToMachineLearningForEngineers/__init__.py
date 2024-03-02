2ee2899239f81e6d5aad9353288d0384234f59b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:10 +0000

    Add package a-brief-introduction-to-machine-learning-for-engineers/2024.3.1.0/a_brief_introduction_to_machine_learning_for_engineers-2024.3.1.0-py3-none-any.whl

---
+++ packages/a-brief-introduction-to-machine-learning-for-engineers/a_brief_introduction_to_machine_learning_for_engineers-2024.3.1.0-py3-none-any.whl/ABriefIntroductionToMachineLearningForEngineers/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""a-brief-introduction-to-machine-learning-for-engineers
+https://github.com/apachecn/a-brief-introduction-to-machine-learning-for-engineers"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
