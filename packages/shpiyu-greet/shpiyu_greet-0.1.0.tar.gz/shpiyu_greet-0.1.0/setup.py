3da4e69a0bc7faedbc1190fc3a9a984b7817fe3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:22 +0000

    Add package shpiyu-greet/0.1.0/shpiyu_greet-0.1.0.tar.gz

---
+++ packages/shpiyu-greet/shpiyu_greet-0.1.0.tar.gz/shpiyu_greet-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shpiyu_greet',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A simple package to greet the user.',
+    author='Piyush Ranjan',
+    author_email='piyushranjan95@gmail.com',
+    url='https://github.com/shpiyu/greet',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+)
