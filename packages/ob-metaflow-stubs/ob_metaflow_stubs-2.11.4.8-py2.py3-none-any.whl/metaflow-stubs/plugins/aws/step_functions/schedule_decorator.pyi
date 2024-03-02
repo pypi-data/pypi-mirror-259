4f0cf09c4dbd784e4cd3ed5335a6bc0b6651952a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:42 +0000

    Add package ob-metaflow-stubs/2.11.4.8/ob_metaflow_stubs-2.11.4.8-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-2.11.4.8-py2.py3-none-any.whl/metaflow-stubs/plugins/aws/step_functions/schedule_decorator.pyi
@@ -0,0 +1,17 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.4.8                                                           #
+# Generated on 2024-03-02T01:35:34.942940                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.decorators
+
+class ScheduleDecorator(metaflow.decorators.FlowDecorator, metaclass=type):
+    def flow_init(self, flow, graph, environment, flow_datastore, metadata, logger, echo, options):
+        ...
+    ...
+
