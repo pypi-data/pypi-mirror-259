b58bc4e86bff048615c8f44c43b36b9220987500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:48 +0000

    Add package binance-spot-sdk/0.0.2/binance-spot-sdk-0.0.2.tar.gz

---
+++ packages/binance-spot-sdk/binance-spot-sdk-0.0.2.tar.gz/binance-spot-sdk-0.0.2/binance_spot/api/__init__.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from binance_spot.api.auto_invest_api import AutoInvestApi
+from binance_spot.api.blvt_api import BLVTApi
+from binance_spot.api.b_swap_api import BSwapApi
+from binance_spot.api.c2_c_api import C2CApi
+from binance_spot.api.convert_api import ConvertApi
+from binance_spot.api.crypto_loans_api import CryptoLoansApi
+from binance_spot.api.fiat_api import FiatApi
+from binance_spot.api.futures_api import FuturesApi
+from binance_spot.api.futures_algo_api import FuturesAlgoApi
+from binance_spot.api.gift_card_api import GiftCardApi
+from binance_spot.api.isolated_margin_stream_api import IsolatedMarginStreamApi
+from binance_spot.api.margin_api import MarginApi
+from binance_spot.api.margin_stream_api import MarginStreamApi
+from binance_spot.api.market_api import MarketApi
+from binance_spot.api.mining_api import MiningApi
+from binance_spot.api.nft_api import NFTApi
+from binance_spot.api.pay_api import PayApi
+from binance_spot.api.portfolio_margin_api import PortfolioMarginApi
+from binance_spot.api.rebate_api import RebateApi
+from binance_spot.api.savings_api import SavingsApi
+from binance_spot.api.simple_earn_api import SimpleEarnApi
+from binance_spot.api.spot_algo_api import SpotAlgoApi
+from binance_spot.api.staking_api import StakingApi
+from binance_spot.api.stream_api import StreamApi
+from binance_spot.api.sub_account_api import SubAccountApi
+from binance_spot.api.trade_api import TradeApi
+from binance_spot.api.vip_loans_api import VIPLoansApi
+from binance_spot.api.wallet_api import WalletApi
