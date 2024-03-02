b7f8fe0fb0ed853eef37a596f38f965f27982222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:15 +0000

    Add package chu-kuangren-windows-qudong-biancheng-jichujiaocheng/2024.3.2.0/chu_kuangren_windows_qudong_biancheng_jichujiaocheng-2024.3.2.0-py3-none-any.whl

---
+++ packages/chu-kuangren-windows-qudong-biancheng-jichujiaocheng/chu_kuangren_windows_qudong_biancheng_jichujiaocheng-2024.3.2.0-py3-none-any.whl/ChuKuangrenWindowsQudongBianchengJichujiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chu-kuangren-windows-qudong-biancheng-jichujiaocheng
+https://github.com/apachecn/chu-kuangren-windows-qudong-biancheng-jichujiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
