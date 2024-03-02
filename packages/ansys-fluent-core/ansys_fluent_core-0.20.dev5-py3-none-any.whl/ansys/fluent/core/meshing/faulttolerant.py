24a530053064ba8de9da42eea3bb66dc9416ecb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:05:31 +0000

    Add package ansys-fluent-core/0.20.dev5/ansys_fluent_core-0.20.dev5-py3-none-any.whl

---
+++ packages/ansys-fluent-core/ansys_fluent_core-0.20.dev5-py3-none-any.whl/ansys/fluent/core/meshing/faulttolerant.py
@@ -0,0 +1,34 @@
+"""Fault-tolerant workflow module."""
+
+from ansys.fluent.core.launcher.launcher import launch_fluent
+from ansys.fluent.core.launcher.launcher_utils import FluentMode
+
+from .meshing_workflow import EnhancedMeshingWorkflow
+
+
+def fault_tolerant_workflow(**launch_args) -> EnhancedMeshingWorkflow:
+    """Meshing workflow wrapper, initialized as fault-tolerant.
+
+    Parameters
+    ----------
+    launch_args
+        Additional arguments forwarded to the launch_fluent function.
+
+    Returns
+    -------
+    EnhancedMeshingWorkflow
+        Meshing workflow wrapper.
+    """
+    # TODO share launch code with watertight
+    dynamic_interface = True
+    if "dynamic_interface" in launch_args:
+        dynamic_interface = launch_args["dynamic_interface"]
+        del launch_args["dynamic_interface"]
+    if "session" in launch_args:
+        session = launch_args["session"]
+    else:
+        args = dict(mode=FluentMode.PURE_MESHING_MODE)
+        args.update(launch_args)
+        session = launch_fluent(**args)
+    meshing_workflow = session.fault_tolerant(dynamic_interface=dynamic_interface)
+    return meshing_workflow
