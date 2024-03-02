b12079a2627443fe6c828d8f4f97942b90292665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:48 +0000

    Add package dfpyre/0.4.6/dfpyre-0.4.6-py3-none-any.whl

---
+++ packages/dfpyre/dfpyre-0.4.6-py3-none-any.whl/dfpyre/items.py
@@ -0,0 +1,361 @@
+"""
+Contains class definitions for code items.
+"""
+
+from enum import Enum
+from typing import Literal, Dict
+from dfpyre.style import isAmpersandCoded, ampersandToMinimessage
+
+
+def _add_slot(d: Dict, slot: int|None):
+    if slot is not None:
+        d['slot'] = slot
+
+
+class item:
+    """
+    Represents a Minecraft item.
+    """
+    type = 'item'
+
+    def __init__(self, itemID: str, count: int=1):
+        self.id = itemID
+        self.count = count
+    
+    def format(self, slot: int|None):
+        formattedDict = {
+            "item": {
+              "id": self.type,
+              "data": {
+                "item": f"{{DF_NBT:2586,id:\"{self.id}\",Count:{self.count}b}}"
+              }
+            }
+          }
+        _add_slot(formattedDict, slot)
+        return formattedDict
+
+
+class string:
+    """
+    Represents a DiamondFire string object. (`txt`)
+    """
+    type = 'txt'
+
+    def __init__(self, value: str):
+        self.value = value
+    
+    def format(self, slot: int|None):
+        formattedDict = {
+              "item": {
+                "id": self.type,
+                "data": {
+                  "name": self.value
+                }
+              }
+            }
+        _add_slot(formattedDict, slot)
+        return formattedDict
+
+
+class text:
+    """
+    Represents a DiamondFire styled text object (`comp`)
+    """
+    type = 'comp'
+
+    def __init__(self, value: str):
+        if isAmpersandCoded(value):
+            value = ampersandToMinimessage(value)
+        self.value = value
+      
+    def format(self, slot: int|None):
+      formattedDict = {
+              "item": {
+                "id": self.type,
+                "data": {
+                  "name": self.value
+                }
+              }
+            }
+      _add_slot(formattedDict, slot)
+      return formattedDict
+
+
+class num:
+    """
+    Represents a DiamondFire number object.
+    """
+    type = 'num'
+
+    def __init__(self, num: int|float):
+        self.value = num
+    
+    def format(self, slot: int|None):
+        formattedDict = {
+            "item": {
+              "id": self.type,
+              "data": {
+                "name": str(self.value)
+              }
+            }
+          }
+        _add_slot(formattedDict, slot)
+        return formattedDict
+
+
+class loc:
+    """
+    Represents a DiamondFire location object.
+    """
+    type = 'loc'
+
+    def __init__(self, x: float=0, y: float=0, z: float=0, pitch: float=0, yaw: float=0):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+        self.pitch = float(pitch)
+        self.yaw = float(yaw)
+    
+    def format(self, slot: int|None):
+        formattedDict =  {
+            "item": {
+              "id": self.type,
+              "data": {
+                "isBlock": False,
+                "loc": {
+                  "x": self.x,
+                  "y": self.y,
+                  "z": self.z,
+                  "pitch": self.pitch,
+                  "yaw": self.yaw
+                }
+              }
+            }
+          }
+        _add_slot(formattedDict, slot)
+        return formattedDict
+
+
+class var:
+    """
+    Represents a DiamondFire variable object.
+    """
+    type = 'var'
+
+    def __init__(self, name: str, scope: Literal['unsaved', 'saved', 'local', 'line']='unsaved'):
+        self.name = name
+        self.scope = scope
+
+    def format(self, slot: int|None):
+        formattedDict = {
+            "item": {
+              "id": self.type,
+              "data": {
+                "name": self.name,
+                "scope": self.scope
+              }
+            }
+          }
+        _add_slot(formattedDict, slot)
+        return formattedDict
+
+
+class sound:
+    """
+    Represents a DiamondFire sound object.
+    """
+    type = 'snd'
+
+    def __init__(self, name: str, pitch: float=1.0, vol: float=2.0):
+        self.name = name
+        self.pitch = pitch
+        self.vol = vol
+
+    def format(self, slot: int|None):
+        formattedDict = {
+            "item": {
+              "id": self.type,
+              "data": {
+                "sound": self.name,
+                "pitch": self.pitch,
+                "vol": self.vol
+              }
+            }
+          }
+        _add_slot(formattedDict, slot)
+        return formattedDict
+
+
+class particle:
+    """
+    Represents a DiamondFire particle object.
+    """
+    type = 'part'
+    def __init__(self, name: str='Cloud', amount: int=1, horizontal: float=0.0, vertical: float=0.0, 
+                 x: float=1.0, y: float=0.0, z: float=0.0, motionVariation: float=100):
+        self.name = name
+        self.amount = amount
+        self.horizontal = horizontal
+        self.vertical = vertical
+        self.x = x
+        self.y = y
+        self.z = z
+        self.motionVariation = motionVariation
+    
+    def format(self, slot: int|None):
+        formattedDict = {
+            "item": {
+              "id": self.type,
+              "data": {
+                "particle": self.name,
+                "cluster": {
+                  "amount": self.amount,
+                  "horizontal": self.horizontal,
+                  "vertical": self.vertical
+                },
+                "data": {
+                  "x": self.x,
+                  "y": self.y,
+                  "z": self.z,
+                  "motionVariation": self.motionVariation
+                }
+              }
+            }
+          }
+        _add_slot(formattedDict, slot)
+        return formattedDict
+
+
+class potion:
+    """
+    Represents a DiamondFire potion object.
+    """
+    type = 'pot'
+
+    def __init__(self, name: str, dur: int=1000000, amp: int=0):
+        self.name = name
+        self.dur = dur
+        self.amp = amp
+    
+    def format(self, slot: int|None):
+        formattedDict = {
+            "item": {
+              "id": self.type,
+              "data": {
+                "pot": self.name,
+                "dur": self.dur,
+                "amp": self.amp
+              }
+            }
+          }
+        _add_slot(formattedDict, slot)
+        return formattedDict
+
+
+class gamevalue:
+    """
+    Represents a DiamondFire game value object.
+    """
+    type = 'g_val'
+
+    def __init__(self, name: str, target: str='Default'):
+        self.name = name
+        self.target = target
+    
+    def format(self, slot: int|None):
+        formattedDict = {
+            "item": {
+              "id": self.type,
+              "data": {
+                "type": self.name,
+                "target": self.target
+              }
+            }
+          }
+        _add_slot(formattedDict, slot)
+        return formattedDict
+
+
+class vector:
+    """
+    Represents a DiamondFire vector object.
+    """
+    type = 'vec'
+
+    def __init__(self, x: float=0.0, y: float=0.0, z: float=0.0):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+    
+    def format(self, slot: int|None):
+        formattedDict = {
+            "item": {
+              "id": self.type,
+              "data": {
+                "x": self.x,
+                "y": self.y,
+                "z": self.z
+              }
+            }
+          }
+        _add_slot(formattedDict, slot)
+        return formattedDict
+
+
+PARAMETER_TYPE_LOOKUP = ['txt', 'comp', 'num', 'loc', 'vec', 'snd', 'part', 'pot', 'item', 'any', 'var', 'list', 'dict']
+
+class ParameterType(Enum):
+    STRING = 0
+    TEXT = 1
+    NUMBER = 2
+    LOCATION = 3
+    VECTOR = 4
+    SOUND = 5
+    PARTICLE = 6
+    POTION_EFFECT = 7
+    ITEM = 8
+    ANY = 9
+    VAR = 10
+    LIST = 11
+    DICT = 12
+
+    def get_string_value(self) -> str:
+        return PARAMETER_TYPE_LOOKUP[self.value]
+
+class parameter:
+    """
+    Represents a DiamondFire parameter object.
+    """
+    type = 'pn_el'
+
+    def __init__(self, name: str, paramType: ParameterType, plural: bool=False, optional: bool=False, description: str="", note: str="", defaultValue=None):
+        self.name = name
+        self.paramType = paramType
+        self.plural = plural
+        self.optional = optional
+        self.description = description
+        self.note = note
+        self.defaultValue = defaultValue
+      
+    
+    def format(self, slot: int):
+        formattedDict = {
+            "item": {
+              "id": self.type,
+              "data": {
+                "name": self.name,
+                "type": self.paramType.get_string_value(),
+                "plural": self.plural,
+                "optional": self.optional,
+              }
+            },
+            "slot": slot
+          }
+        if self.description:
+            formattedDict['item']['data']['description'] = self.description
+        if self.note:
+            formattedDict['item']['data']['note'] = self.note
+        if self.defaultValue is not None and not self.plural and self.optional:
+            formattedDict['item']['data']['default_value'] = self.defaultValue.format(None)['item']
+        
+        return formattedDict
---
+++ packages/dfpyre/dfpyre-0.4.6-py3-none-any.whl/dfpyre/pyre.py
@@ -0,0 +1,479 @@
+"""
+A package for externally creating code templates for the DiamondFire Minecraft server.
+
+By Amp
+2/24/2024
+"""
+
+import base64
+import gzip
+import socket
+import websocket
+import time
+import json
+import os
+from difflib import get_close_matches
+import datetime
+from typing import Tuple, List, Dict
+from enum import Enum
+from mcitemlib.itemlib import Item as NbtItem
+from dfpyre.items import *
+
+COL_WARN = '\x1b[33m'
+COL_RESET = '\x1b[0m'
+COL_SUCCESS = '\x1b[32m'
+COL_ERROR = '\x1b[31m'
+
+CODEBLOCK_DATA_PATH = os.path.join(os.path.dirname(__file__), 'data/data.json')
+
+VARIABLE_TYPES = {'txt', 'comp', 'num', 'item', 'loc', 'var', 'snd', 'part', 'pot', 'g_val', 'vec', 'pn_el'}
+TEMPLATE_STARTERS = {'event', 'entity_event', 'func', 'process'}
+
+TARGETS = ['Selection', 'Default', 'Killer', 'Damager', 'Shooter', 'Victim', 'AllPlayers', 'Projectile', 'AllEntities', 'AllMobs', 'LastEntity']
+TARGET_CODEBLOCKS = {'player_action', 'entity_action', 'if_player', 'if_entity'}
+
+VAR_SHORTHAND_CHAR = '$'
+VAR_SCOPES = {'g': 'unsaved', 's': 'saved', 'l': 'local', 'i': 'line'}
+
+CODECLIENT_URL = 'ws://localhost:31375'
+
+
+class Target(Enum):
+    SELECTION = 0
+    DEFAULT = 1
+    KILLER = 2
+    DAMAGER = 3
+    SHOOTER = 4
+    VICTIM = 5
+    ALL_PLAYERS = 6
+    PROJECTILE = 7
+    ALL_ENTITIES = 8
+    ALL_MOBS = 9
+    LAST_ENTITY = 10
+
+    def getStringValue(self):
+        return TARGETS[self.value]
+
+DEFAULT_TARGET = Target.SELECTION
+
+
+class CodeBlock:
+    def __init__(self, name: str, args: Tuple=(), target: Target=DEFAULT_TARGET, data: Dict={}):
+        self.name = name
+        self.args = args
+        self.target = target
+        self.data = data
+
+
+def _warn(message):
+    print(f'{COL_WARN}! WARNING ! {message}{COL_RESET}')
+
+
+def _warnUnrecognizedName(codeblockType: str, codeblockName: str):
+    close = get_close_matches(codeblockName, TAGDATA[codeblockType].keys())
+    if close:
+        _warn(f'Code block name "{codeblockName}" not recognized. Did you mean "{close[0]}"?')
+    else:
+        _warn(f'Code block name "{codeblockName}" not recognized. Try spell checking or retyping without spaces.')
+
+
+def _loadCodeblockData() -> Tuple:
+    tagData = {}
+    if os.path.exists(CODEBLOCK_DATA_PATH):
+        with open(CODEBLOCK_DATA_PATH, 'r') as f:
+            tagData = json.load(f)
+    else:
+        _warn('data.json not found -- Item tags and error checking will not work.')
+        return ({}, set(), set())
+    
+    del tagData['meta']
+
+    allNames = [x for l in [d.keys() for d in tagData.values()] for x in l]  # flatten list
+    return (
+        tagData,
+        set(tagData['extras'].keys()),
+        set(allNames)
+    )
+
+TAGDATA, TAGDATA_EXTRAS_KEYS, ALL_CODEBLOCK_NAMES = _loadCodeblockData()
+
+def _addInverted(data, inverted):
+    """
+    If inverted is true, add 'inverted': 'NOT' to data.
+    """
+    if inverted:
+        data['inverted'] = 'NOT'
+
+
+def _convertDataTypes(args):
+    convertedArgs = []
+    for value in args:
+        if type(value) in {int, float}:
+            convertedArgs.append(num(value))
+        elif type(value) is str:
+            if value[0] == VAR_SHORTHAND_CHAR and value[1] in VAR_SCOPES:
+                varObject = var(value[2:], VAR_SCOPES[value[1]])
+                convertedArgs.append(varObject)
+            else:
+                convertedArgs.append(text(value))
+        else:
+            convertedArgs.append(value)
+    return tuple(convertedArgs)
+
+
+def _reformatCodeblockTags(tags, codeblockType: str, codeblockName: str):
+    """
+    Turns data.json tag items into DiamondFire formatted tag items
+    """
+    reformattedTags = []
+    for tagItem in tags:
+        actionValue = codeblockName if 'action' not in tagItem else tagItem['action']
+        newTagItem = {
+            'item': {
+                'id': 'bl_tag',
+                'data': {
+                    'option': tagItem['option'],
+                    'tag': tagItem['tag'],
+                    'action': actionValue,
+                    'block': codeblockType
+                }
+            },
+            'slot': tagItem['slot']
+        }
+        reformattedTags.append(newTagItem)
+    return reformattedTags
+
+
+def _getCodeblockTags(codeblockType: str, codeblockName: str):
+    """
+    Get tags for the specified codeblock type and name
+    """
+    tags = None
+    if codeblockType in TAGDATA_EXTRAS_KEYS:
+        tags = TAGDATA['extras'][codeblockType]
+    else:
+        tags = TAGDATA[codeblockType].get(codeblockName)
+    return _reformatCodeblockTags(tags, codeblockType, codeblockName)
+
+
+def _buildBlock(codeblock: CodeBlock, includeTags: bool):
+    """
+    Builds a properly formatted block from a CodeBlock object.
+    """
+    finalBlock = codeblock.data.copy()
+    codeblockType = codeblock.data.get('block')
+    
+    # add target if necessary ('Selection' is the default when 'target' is blank)
+    if codeblockType in TARGET_CODEBLOCKS and codeblock.target != DEFAULT_TARGET:
+        finalBlock['target'] = codeblock.target.getStringValue()
+    
+    # add items into args
+    finalArgs = [arg.format(slot) for slot, arg in enumerate(codeblock.args) if arg.type in VARIABLE_TYPES]
+    
+    # check for unrecognized name, add tags
+    if codeblockType is not None:  # for brackets
+        if codeblockType not in TAGDATA_EXTRAS_KEYS and codeblock.name not in ALL_CODEBLOCK_NAMES:
+            _warnUnrecognizedName(codeblockType, codeblock.name)
+        elif includeTags:
+            tags = _getCodeblockTags(codeblockType, codeblock.name)
+            if len(finalArgs) + len(tags) > 27:
+                finalArgs = finalArgs[:(27-len(tags))]  # trim list if over 27 elements
+            finalArgs.extend(tags)  # add tags to end
+    
+    finalBlock['args'] = {'items': finalArgs}
+    return finalBlock
+
+
+def _dfEncode(jsonString: str) -> str:
+    """
+    Encodes a stringified json.
+    """
+    encodedString = gzip.compress(jsonString.encode('utf-8'))
+    return base64.b64encode(encodedString).decode('utf-8')
+
+
+def getTemplateItem(templateCode: str, name: str, author: str) -> NbtItem:
+    now = datetime.datetime.now()
+
+    templateItem = NbtItem('yellow_shulker_box')
+    templateItem.set_name(f'&x&f&f&5&c&0&0>> &x&f&f&c&7&0&0{name}')
+    templateItem.set_lore([
+        f'&8Author: {author}',
+        f'&8Date: {now.strftime("%Y-%m-%d")}',
+        '',
+        '&7This template was generated by &6pyre&7.',
+        '&7https://github.com/Amp63/pyre'
+    ])
+    
+    pbvTag = {
+        'hypercube:codetemplatedata': f'{{"author":"{author}","name":"{name}","version": 1,"code":"{templateCode}"}}',
+        'hypercube:pyre_creation_timestamp': now.timestamp()
+    }
+    templateItem.set_tag('PublicBukkitValues', pbvTag, raw=True)
+
+    return templateItem
+
+
+def sendRecode(templateCode: str, name: str='Unnamed Template', author: str='pyre') -> int:
+    """
+    Sends a template to DiamondFire via recode item api.
+
+    :param str templateCode: The code for the template as a base64 string.
+    :param str name: The name of the template.
+    :param str author: The author of the template.
+
+    :return: status code
+        - `0` = Success
+        - `1` = Connection refused
+        - `2` = Other socket error
+    """
+    
+    templateItem = getTemplateItem(templateCode, name, author)
+    data = {'type': 'nbt', 'source': f'pyre Template - {name}', 'data': templateItem.get_nbt()}
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect(('127.0.0.1', 31372))
+    except ConnectionRefusedError:
+        print(f"""{COL_ERROR}Could not connect to recode item API. Possible problems:
+    - Minecraft is not open
+    - Recode is not installed (get it here: https://modrinth.com/mod/recode or join the discord here: https://discord.gg/GWxWtcwA2C){COL_RESET}""")
+        s.close()
+        return 1
+    
+    s.send((str(data) + '\n').encode('utf-8'))
+    received = json.loads(s.recv(1024).decode())
+    status = received['status']
+    s.close()
+    time.sleep(0.5)
+
+    if status == 'success':
+        print(f'{COL_SUCCESS}Template sent to client successfully.{COL_RESET}')
+        return 0
+    error = received['error']
+    print(f'{COL_ERROR}Error sending template: {error}{COL_RESET}')
+    return 2
+
+
+def sendCodeClient(templateCode: str, name: str='Unnamed Template', author: str='pyre') -> int:
+    try:
+        ws = websocket.WebSocket()
+        ws.connect(CODECLIENT_URL)
+        print(f'{COL_SUCCESS}Connected. {COL_WARN}Please run /auth in game.{COL_RESET}')
+        
+        ws.recv()  # auth response
+
+        templateItem = getTemplateItem(templateCode, name, author)
+        command = f'give {templateItem.get_nbt()}'
+        ws.send(command)
+        ws.close()
+
+        print(f'{COL_SUCCESS}Template sent to client successfully.{COL_RESET}')
+        return 0
+        
+    except Exception as e:
+        if isinstance(e, ConnectionRefusedError):
+            print(f'{COL_ERROR}Could not connect to CodeClient API. Possible problems:')
+            print(f'    - Minecraft is not open')
+            print(f'    - CodeClient is not installed (get it here: https://modrinth.com/mod/codeclient)')
+            print(f'    - CodeClient API is not enabled (enable it in CodeClient general settings)')
+            return 1
+        
+        print(f'Connection failed: {e}')
+        return 2
+
+
+class DFTemplate:
+    """
+    Represents a DiamondFire code template.
+    """
+    def __init__(self, name: str=None, author: str='pyre'):
+        self.codeBlocks: List[CodeBlock] = []
+        self.closebracket = None
+        self.name = name
+        self.author = author
+
+
+    def _setTemplateName(self, firstBlock):
+        if self.name is not None:
+            return
+        if 'data' in firstBlock:
+            self.name = firstBlock['data']
+            if not self.name:
+                self.name = 'Unnamed Template'
+        else:
+            self.name = firstBlock['block'] + '_' + firstBlock['action']
+
+
+    def build(self, includeTags: bool=True) -> str:
+        """
+        Build this template.
+
+        :param bool includeTags: If True, include item tags in code blocks. Otherwise omit them.
+        :return: String containing encoded template data.
+        """
+        templateDictBlocks = [_buildBlock(codeblock, includeTags) for codeblock in self.codeBlocks]
+        templateDict = {'blocks': templateDictBlocks}
+        firstBlock = templateDictBlocks[0]
+        if firstBlock['block'] not in TEMPLATE_STARTERS:
+            _warn('Template does not start with an event, function, or process.')
+
+        self._setTemplateName(firstBlock)
+
+        print(f'{COL_SUCCESS}Template built successfully.{COL_RESET}')
+
+        jsonString = json.dumps(templateDict, separators=(',', ':'))
+        return _dfEncode(jsonString)
+    
+
+    def buildAndSend(self, sendMethod: Literal['recode', 'codeclient']='recode', includeTags: bool=True) -> int:
+        """
+        Builds this template and sends it to DiamondFire automatically.
+        
+        :param bool includeTags: If True, include item tags in code blocks. Otherwise omit them.
+        """
+        templateCode = self.build(includeTags)
+        if sendMethod == 'recode':
+            return sendRecode(templateCode, name=self.name, author=self.author)
+        if sendMethod == 'codeclient':
+            return sendCodeClient(templateCode, name=self.name, author=self.author)
+        return -1
+    
+
+    def clear(self):
+        """
+        Clears this template's data.
+        """
+        self.__init__()
+    
+
+    def _openbracket(self, btype: Literal['norm', 'repeat']='norm'):
+        bracket = CodeBlock('Bracket', data={'id': 'bracket', 'direct': 'open', 'type': btype})
+        self.codeBlocks.append(bracket)
+        self.closebracket = btype
+    
+
+    # command methods
+    def playerEvent(self, name: str):
+        cmd = CodeBlock(name, data={'id': 'block', 'block': 'event', 'action': name})
+        self.codeBlocks.append(cmd)
+    
+
+    def entityEvent(self, name: str):
+        cmd = CodeBlock(name, data={'id': 'block', 'block': 'entity_event', 'action': name})
+        self.codeBlocks.append(cmd)
+    
+
+    def function(self, name: str, *args):
+        args = _convertDataTypes(args)
+        cmd = CodeBlock('function', args, data={'id': 'block', 'block': 'func', 'data': name})
+        self.codeBlocks.append(cmd)
+    
+
+    def process(self, name: str):
+        cmd = CodeBlock('process', data={'id': 'block', 'block': 'process', 'data': name})
+        self.codeBlocks.append(cmd)
+    
+
+    def callFunction(self, name: str, *args):
+        args = _convertDataTypes(args)
+        cmd = CodeBlock('call_func', args, data={'id': 'block', 'block': 'call_func', 'data': name})
+        self.codeBlocks.append(cmd)
+    
+
+    def startProcess(self, name: str):
+        cmd = CodeBlock('start_process', data={'id': 'block', 'block': 'start_process', 'data': name})
+        self.codeBlocks.append(cmd)
+
+
+    def playerAction(self, name: str, *args, target: Target=DEFAULT_TARGET):
+        args = _convertDataTypes(args)
+        cmd = CodeBlock(name, args, target=target, data={'id': 'block', 'block': 'player_action', 'action': name})
+        self.codeBlocks.append(cmd)
+    
+
+    def gameAction(self, name: str, *args):
+        args = _convertDataTypes(args)
+        cmd = CodeBlock(name, args, data={'id': 'block', 'block': 'game_action', 'action': name})
+        self.codeBlocks.append(cmd)
+    
+
+    def entityAction(self, name: str, *args, target: Target=DEFAULT_TARGET):
+        args = _convertDataTypes(args)
+        cmd = CodeBlock(name, args, target=target, data={'id': 'block', 'block': 'entity_action', 'action': name})
+        self.codeBlocks.append(cmd)
+    
+
+    def ifPlayer(self, name: str, *args, target: Target=DEFAULT_TARGET, inverted: bool=False):
+        args = _convertDataTypes(args)
+        data = {'id': 'block', 'block': 'if_player', 'action': name}
+        _addInverted(data, inverted)
+        cmd = CodeBlock(name, args, target=target, data=data)
+        self.codeBlocks.append(cmd)
+        self._openbracket()
+    
+
+    def ifVariable(self, name: str, *args, inverted: bool=False):
+        args = _convertDataTypes(args)
+        data = {'id': 'block', 'block': 'if_var', 'action': name}
+        _addInverted(data, inverted)
+        cmd = CodeBlock(name, args, data=data)
+        self.codeBlocks.append(cmd)
+        self._openbracket()
+    
+
+    def ifGame(self, name: str, *args, inverted: bool=False):
+        args = _convertDataTypes(args)
+        data = {'id': 'block', 'block': 'if_game', 'action': name}
+        _addInverted(data, inverted)
+        cmd = CodeBlock(name, args, data=data)
+        self.codeBlocks.append(cmd)
+        self._openbracket()
+    
+
+    def ifEntity(self, name: str, *args, target: Target=DEFAULT_TARGET, inverted: bool=False):
+        args = _convertDataTypes(args)
+        data = {'id': 'block', 'block': 'if_entity', 'action': name}
+        _addInverted(data, inverted)
+        cmd = CodeBlock(name, args, target=target, data=data)
+        self.codeBlocks.append(cmd)
+        self._openbracket()
+
+
+    def else_(self):
+        cmd = CodeBlock('else', data={'id': 'block', 'block': 'else'})
+        self.codeBlocks.append(cmd)
+        self._openbracket()
+    
+
+    def repeat(self, name: str, *args, subAction: str=None):
+        args = _convertDataTypes(args)
+        data = {'id': 'block', 'block': 'repeat', 'action': name}
+        if subAction is not None:
+            data['subAction'] = subAction
+        cmd = CodeBlock(name, args, data=data)
+        self.codeBlocks.append(cmd)
+        self._openbracket('repeat')
+
+
+    def bracket(self, *args):
+        args = _convertDataTypes(args)
+        cmd = CodeBlock('Bracket', data={'id': 'bracket', 'direct': 'close', 'type': self.closebracket})
+        self.codeBlocks.append(cmd)
+    
+
+    def control(self, name: str, *args):
+        args = _convertDataTypes(args)
+        cmd = CodeBlock(name, args, data={'id': 'block', 'block': 'control', 'action': name})
+        self.codeBlocks.append(cmd)
+    
+
+    def selectObject(self, name: str, *args):
+        args = _convertDataTypes(args)
+        cmd = CodeBlock(name, args, data={'id': 'block', 'block': 'select_obj', 'action': name})
+        self.codeBlocks.append(cmd)
+    
+
+    def setVariable(self, name: str, *args):
+        args = _convertDataTypes(args)
+        cmd = CodeBlock(name, args, data={'id': 'block', 'block': 'set_var', 'action': name})
+        self.codeBlocks.append(cmd)
