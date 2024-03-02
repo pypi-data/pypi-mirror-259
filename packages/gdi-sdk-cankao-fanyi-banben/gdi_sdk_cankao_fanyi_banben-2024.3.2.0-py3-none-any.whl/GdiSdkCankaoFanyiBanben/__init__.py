d64b676d13394141e8229c63c25adeac644ff1d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:11 +0000

    Add package gdi-sdk-cankao-fanyi-banben/2024.3.2.0/gdi_sdk_cankao_fanyi_banben-2024.3.2.0-py3-none-any.whl

---
+++ packages/gdi-sdk-cankao-fanyi-banben/gdi_sdk_cankao_fanyi_banben-2024.3.2.0-py3-none-any.whl/GdiSdkCankaoFanyiBanben/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gdi-sdk-cankao-fanyi-banben
+https://github.com/apachecn/gdi-sdk-cankao-fanyi-banben"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
