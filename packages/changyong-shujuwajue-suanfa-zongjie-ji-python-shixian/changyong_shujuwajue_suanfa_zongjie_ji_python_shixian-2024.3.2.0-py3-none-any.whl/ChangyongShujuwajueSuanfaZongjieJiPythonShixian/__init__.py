f5ecf0d601213a2730a7c2129cdec8fc73ab1736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:14 +0000

    Add package changyong-shujuwajue-suanfa-zongjie-ji-python-shixian/2024.3.2.0/changyong_shujuwajue_suanfa_zongjie_ji_python_shixian-2024.3.2.0-py3-none-any.whl

---
+++ packages/changyong-shujuwajue-suanfa-zongjie-ji-python-shixian/changyong_shujuwajue_suanfa_zongjie_ji_python_shixian-2024.3.2.0-py3-none-any.whl/ChangyongShujuwajueSuanfaZongjieJiPythonShixian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""changyong-shujuwajue-suanfa-zongjie-ji-python-shixian
+https://github.com/apachecn/changyong-shujuwajue-suanfa-zongjie-ji-python-shixian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
