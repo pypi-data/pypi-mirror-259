72da6b23e32d3c5c1470b8a7f89ce1a6c5ea0d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:05:50 +0000

    Add package linux-gongju-kuaisu-jiaocheng/2024.3.1.0/linux_gongju_kuaisu_jiaocheng-2024.3.1.0-py3-none-any.whl

---
+++ packages/linux-gongju-kuaisu-jiaocheng/linux_gongju_kuaisu_jiaocheng-2024.3.1.0-py3-none-any.whl/LinuxGongjuKuaisuJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""linux-gongju-kuaisu-jiaocheng
+https://github.com/apachecn/linux-gongju-kuaisu-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
