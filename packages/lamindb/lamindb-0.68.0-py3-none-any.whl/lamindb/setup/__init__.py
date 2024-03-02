46f8f65a239ca88019a74be043542e81af7869a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:43 +0000

    Add package lamindb/0.68.0/lamindb-0.68.0-py3-none-any.whl

---
+++ packages/lamindb/lamindb-0.68.0-py3-none-any.whl/lamindb/setup/__init__.py
@@ -0,0 +1,8 @@
+import lamindb_setup as _lamindb_setup
+from lamindb_setup import *  # noqa: F403
+from lamindb_setup import settings
+
+from . import dev
+
+__doc__ = _lamindb_setup.__doc__.replace("lamindb_setup", "lamindb.setup")
+settings.__doc__ = settings.__doc__.replace("lamindb_setup", "lamindb.setup")
---
+++ packages/lamindb/lamindb-0.68.0-py3-none-any.whl/lamindb/setup/dev/__init__.py
@@ -0,0 +1,17 @@
+"""Setup developer API.
+
+Settings:
+
+.. autosummary::
+   :toctree:
+
+   UserSettings
+   InstanceSettings
+   StorageSettings
+
+"""
+from lamindb_setup.dev import (  # pragma: no cover
+    InstanceSettings,
+    StorageSettings,
+    UserSettings,
+)
