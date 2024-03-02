3333560345a7a81ee6bb90bf1cf0c903016601b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:37 +0000

    Add package computer-and-network-security-by-avinash-kak/2024.3.2.0/computer_and_network_security_by_avinash_kak-2024.3.2.0-py3-none-any.whl

---
+++ packages/computer-and-network-security-by-avinash-kak/computer_and_network_security_by_avinash_kak-2024.3.2.0-py3-none-any.whl/ComputerAndNetworkSecurityByAvinashKak/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""computer-and-network-security-by-avinash-kak
+https://github.com/apachecn/computer-and-network-security-by-avinash-kak"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
