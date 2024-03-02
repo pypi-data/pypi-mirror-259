d665c83be3128390d50846cc25c0cdee7130fe51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:50 +0000

    Add package grscheller.boring-math/0.3.0/grscheller_boring_math-0.3.0.tar.gz

---
+++ packages/grscheller.boring-math/grscheller_boring_math-0.3.0.tar.gz/grscheller_boring_math-0.3.0/src/grscheller/boring_math/cli/__init__.py
@@ -0,0 +1,21 @@
+# Copyright 2016-2024 Geoffrey R. Scheller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""CLI script submodule
+
+CGI scripts are automatically generated in an OS independent way
+by the package build backend based on the `[project.scripts]` section
+of the `pyproject.toml` file. This module contains the entry points
+for these scripts.
+"""
