bbcc5e95555180bef214843243e11e73c35c67c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:56 +0000

    Add package gitoptim/0.1.1/gitoptim-0.1.1-py3-none-any.whl

---
+++ packages/gitoptim/gitoptim-0.1.1-py3-none-any.whl/gitoptim/main.py
@@ -0,0 +1,33 @@
+import rich
+import typer
+
+from gitoptim import commands
+from gitoptim.utils import (EndTag, EnvironmentVariables, StartTag,
+                            is_help_option_present)
+
+app = typer.Typer(rich_markup_mode="rich")
+app.add_typer(commands.analyse.app, name="analyse")
+
+
+# pylint: disable=unused-argument
+def teardown(*args, **kwargs):
+    rich.print(EndTag())
+
+
+@app.callback(result_callback=teardown)
+def main():
+    """
+    Gitoptim SDK
+    """
+
+    if is_help_option_present():
+        return
+
+    rich.print(StartTag())
+    rich.print(EnvironmentVariables().job_id)
+    rich.print(EnvironmentVariables().job_token)
+    rich.print(EnvironmentVariables().gitlab_api_url)
+
+
+app.command(name="memlab")(commands.memlab)
+app.command(name="tag")(commands.tag)
