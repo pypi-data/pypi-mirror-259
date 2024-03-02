09ba54ede62c07eb7f22c6bcc403a7cdc371c5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:14 +0000

    Add package leetcode-tijie-c/2024.3.2.0/leetcode-tijie-c-2024.3.2.0.tar.gz

---
+++ packages/leetcode-tijie-c/leetcode-tijie-c-2024.3.2.0.tar.gz/leetcode-tijie-c-2024.3.2.0/LeetcodeTijieC/README.md
@@ -0,0 +1,27 @@
+# LeetCode题解 (C++)
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/leetcode-tijie-c
+docker run -tid -p <port>:80 apachecn0/leetcode-tijie-c
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install leetcode-tijie-c
+leetcode-tijie-c <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g leetcode-tijie-c
+leetcode-tijie-c <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/leetcode-tijie-c/leetcode-tijie-c-2024.3.2.0.tar.gz/leetcode-tijie-c-2024.3.2.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import LeetcodeTijieC
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('LeetcodeTijieC'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="leetcode-tijie-c",
+    version=LeetcodeTijieC.__version__,
+    url="https://github.com/apachecn/leetcode-tijie-c",
+    author=LeetcodeTijieC.__author__,
+    author_email=LeetcodeTijieC.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="LeetCode题解 (C++)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "leetcode-tijie-c=LeetcodeTijieC.__main__:main",
+            "LeetcodeTijieC=LeetcodeTijieC.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
