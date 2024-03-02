e6dfe0bec31c7509dc59ed4164ed1ec7638e3f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:05:56 +0000

    Add package modern-java-a-guide-to-java-8/2024.3.1.0/modern-java-a-guide-to-java-8-2024.3.1.0.tar.gz

---
+++ packages/modern-java-a-guide-to-java-8/modern-java-a-guide-to-java-8-2024.3.1.0.tar.gz/modern-java-a-guide-to-java-8-2024.3.1.0/ModernJavaAGuideToJava8/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""modern-java-a-guide-to-java-8
+https://github.com/apachecn/modern-java-a-guide-to-java-8"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
