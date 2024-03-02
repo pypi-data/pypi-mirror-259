27204ff7810be057588bf924d9559279e97ca651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:19 +0000

    Add package qipian-de-yishu/2024.3.1.0/qipian-de-yishu-2024.3.1.0.tar.gz

---
+++ packages/qipian-de-yishu/qipian-de-yishu-2024.3.1.0.tar.gz/qipian-de-yishu-2024.3.1.0/QipianDeYishu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qipian-de-yishu
+https://github.com/apachecn/qipian-de-yishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
