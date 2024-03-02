5628637f0888ee65004f8172d8b0c7bacd4ff621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:38 +0000

    Add package theforger-s-win32-api-jiaocheng-dier-ban-jiantizhongwen/2024.3.2.0/theforger-s-win32-api-jiaocheng-dier-ban-jiantizhongwen-2024.3.2.0.tar.gz

---
+++ packages/theforger-s-win32-api-jiaocheng-dier-ban-jiantizhongwen/theforger-s-win32-api-jiaocheng-dier-ban-jiantizhongwen-2024.3.2.0.tar.gz/theforger-s-win32-api-jiaocheng-dier-ban-jiantizhongwen-2024.3.2.0/TheforgerSWin32ApiJiaochengDierBanJiantizhongwen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""theforger-s-win32-api-jiaocheng-dier-ban-jiantizhongwen
+https://github.com/apachecn/theforger-s-win32-api-jiaocheng-dier-ban-jiantizhongwen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
