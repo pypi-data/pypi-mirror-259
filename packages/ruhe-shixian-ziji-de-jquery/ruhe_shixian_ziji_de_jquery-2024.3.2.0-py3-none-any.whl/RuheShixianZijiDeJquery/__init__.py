197e3935e7ea6116f20a17022213e4ab7c199a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:42 +0000

    Add package ruhe-shixian-ziji-de-jquery/2024.3.2.0/ruhe_shixian_ziji_de_jquery-2024.3.2.0-py3-none-any.whl

---
+++ packages/ruhe-shixian-ziji-de-jquery/ruhe_shixian_ziji_de_jquery-2024.3.2.0-py3-none-any.whl/RuheShixianZijiDeJquery/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ruhe-shixian-ziji-de-jquery
+https://github.com/apachecn/ruhe-shixian-ziji-de-jquery"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
