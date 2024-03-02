4d8a26ec2b0c6223837dc2ae3df51e98677413f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:20 +0000

    Add package aps-toolkit/0.0.3/aps-toolkit-0.0.3.tar.gz

---
+++ packages/aps-toolkit/aps-toolkit-0.0.3.tar.gz/aps-toolkit-0.0.3/src/aps_toolkit_python/BIM360.py
@@ -0,0 +1,10 @@
+﻿import requests
+class BIM360:
+    def __init__(self,token):
+        self.token = token
+        self.host = "https://developer.api.autodesk.com"
+    def get_hubs(self):
+        headers = {'Authorization': 'Bearer ' + self.token.access_token}
+        url = "https://developer.api.autodesk.com/project/v1/hubs"
+        response = requests.get(url, headers=headers)
+        return response.json()
---
+++ packages/aps-toolkit/aps-toolkit-0.0.3.tar.gz/aps-toolkit-0.0.3/src/test/context.py
@@ -0,0 +1,14 @@
+﻿import os
+import sys
+
+# Assuming the src folder is at the same level as the test folder
+src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+sys.path.append(src_dir)
+# Now you can import modules from the project folder
+from aps_toolkit_python import PropReader
+from aps_toolkit_python import PropDbReaderRevit
+from aps_toolkit_python import Auth
+from aps_toolkit_python import Token
+APS_CLIENT_ID = os.environ["APS_CLIENT_ID"]
+APS_CLIENT_SECRET = os.environ["APS_CLIENT_SECRET"]
