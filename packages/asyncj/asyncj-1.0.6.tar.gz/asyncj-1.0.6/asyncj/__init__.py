9f418dd3e04181563cf34b091d2b7ceaff338156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:16 +0000

    Add package asyncj/1.0.6/asyncj-1.0.6.tar.gz

---
+++ packages/asyncj/asyncj-1.0.6.tar.gz/asyncj-1.0.6/asyncj/__init__.py
@@ -0,0 +1,11 @@
+"""
+:authors: VengDevs
+:licence: Apache License, Version 2.0
+:copyright: (c) 2024 VengDevs
+"""
+
+from .asyncj import AsyncJson
+
+
+__version__ = '1.0.6'
+__author__ = 'VengDevs'
