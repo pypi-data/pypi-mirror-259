71f969a62be95492878bfbce2c8f7f71e34007bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:36 +0000

    Add package normalize-japanese-addresses/0.0.9/normalize-japanese-addresses-0.0.9.tar.gz

---
+++ packages/normalize-japanese-addresses/normalize-japanese-addresses-0.0.9.tar.gz/normalize-japanese-addresses-0.0.9/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from distutils.core import setup
+from os import path
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="normalize-japanese-addresses",
+    version="0.0.9",
+    author="Takahiro Hama",
+    author_email="taka710.py@gmail.com",
+    maintainer="Takahiro Hama",
+    maintainer_email="taka710.py@gmail.com",
+    description="Ported version of @geolonia/normalize-japanese-addresses",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT License',
+    url="https://github.com/Taka710/normalize-japanese-addresses-py",
+    project_urls={
+        "Bug Tracker": "https://github.com/Taka710/normalize-japanese-addresses-py/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        "requests",
+        "kanjize",
+        "cachetools"
+    ],
+    python_requires=">=3.8",
+)
