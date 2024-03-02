2db6587e5e91819496dddb6fde1d3ace77c09ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:27 +0000

    Add package is-it-christmas/0.0.2/is_it_christmas-0.0.2.tar.gz

---
+++ packages/is-it-christmas/is_it_christmas-0.0.2.tar.gz/is_it_christmas-0.0.2/src/__init__.py
@@ -0,0 +1,22 @@
+import datetime
+
+def is_it_christmas():
+
+    # Get the current date and time in the user's timezone
+    current_datetime = datetime.datetime.now(datetime.timezone.utc).astimezone()
+
+    # Get the current year
+    current_year = current_datetime.year
+
+    # Define Christmas date
+    christmas_date = datetime.datetime(current_year, 12, 25)
+
+    # Check if today is Christmas
+    if current_datetime.date() == christmas_date.date():
+        print("YES")
+    else:
+        print("NO")
+
+# option to run it
+if __name__ == "__main__":
+    is_it_christmas()
