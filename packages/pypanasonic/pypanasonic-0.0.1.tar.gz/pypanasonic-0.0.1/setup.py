0e832df22f0a571fef42e2ea26402cd42f202559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:17 +0000

    Add package pypanasonic/0.0.1/pypanasonic-0.0.1.tar.gz

---
+++ packages/pypanasonic/pypanasonic-0.0.1.tar.gz/pypanasonic-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup,find_packages
+
+setup(
+    name='pypanasonic',
+    version='0.0.1',
+    author='luweizhao',
+    author_email='luweizhao09@gmail.com',
+    description='A panasonic serial communication tool',
+    license='MIT License',
+    packages=find_packages(),
+)
