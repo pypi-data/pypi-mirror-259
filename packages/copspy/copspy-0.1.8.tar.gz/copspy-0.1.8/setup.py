b3587c2afeac73a2568f98ad7bda2e8740936da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:10 +0000

    Add package copspy/0.1.8/copspy-0.1.8.tar.gz

---
+++ packages/copspy/copspy-0.1.8.tar.gz/copspy-0.1.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='copspy',
+    version='0.1.8',
+    long_description="a fan made python API wrapper for the c-ops public api",
+    description='a fan made python API wrapper for the c-ops public api',
+    license='MIT',
+    packages=['copspy/'],
+    author='Kitsune',
+    author_email='kitsune@yokaigroup.gg',
+    keywords=['cops', 'api', 'wrapper','apiwrapper', 'python', 'c-ops'],
+    url='https://github.com/Kitsune-San/cops.py',
+    install_requires = ['requests', 'colorama'],
+)
