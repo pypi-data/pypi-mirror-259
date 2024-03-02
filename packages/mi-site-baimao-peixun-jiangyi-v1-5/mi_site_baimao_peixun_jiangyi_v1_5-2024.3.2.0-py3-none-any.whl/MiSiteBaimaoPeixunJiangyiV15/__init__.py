4722f2bfa3ff900ece3c36cc28666f708228301f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:10 +0000

    Add package mi-site-baimao-peixun-jiangyi-v1-5/2024.3.2.0/mi_site_baimao_peixun_jiangyi_v1_5-2024.3.2.0-py3-none-any.whl

---
+++ packages/mi-site-baimao-peixun-jiangyi-v1-5/mi_site_baimao_peixun_jiangyi_v1_5-2024.3.2.0-py3-none-any.whl/MiSiteBaimaoPeixunJiangyiV15/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mi-site-baimao-peixun-jiangyi-v1-5
+https://github.com/apachecn/mi-site-baimao-peixun-jiangyi-v1-5"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
