ef39033d47c1f960ed7b377a165b17d7a63e6058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:34 +0000

    Add package replay-wizard/0.2.0/replay_wizard-0.2.0-py3-none-any.whl

---
+++ packages/replay-wizard/replay_wizard-0.2.0-py3-none-any.whl/replay_wizard/models/sequence.py
@@ -0,0 +1,35 @@
+"""
+Sequence module
+"""
+from typing import List
+from pydantic import BaseModel
+from .action import Action
+
+
+class Sequence(BaseModel):
+    """
+    Action sequence
+    """
+    name: str
+    actions: List[Action] = []
+
+    def __len__(self):
+        return len(self.actions)
+
+    def __iter__(self):
+        return iter(self.actions)
+
+    def add(self, new_action: Action):
+        """
+        Add action to sequence
+
+        :param new_action: Action to add
+        :return: None
+        """
+        self.actions.append(new_action)
+
+    def __contains__(self, item):
+        """
+        in method
+        """
+        return item in self.actions
