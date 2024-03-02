1d693f9a3a76b560291a86ead182d27b81070beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:54 +0000

    Add package benchmarks-runner/0.1.3/benchmarks_runner-0.1.3-py3-none-any.whl

---
+++ packages/benchmarks-runner/benchmarks_runner-0.1.3-py3-none-any.whl/benchmarks_runner/main.py
@@ -0,0 +1,38 @@
+"""Main Benchmarks Test Runner entry."""
+import asyncio
+import os
+import tempfile
+
+from benchmarks_runner.request import fetch_results
+from benchmarks_runner.cli.eval import evaluate_ara_results
+
+
+async def run_benchmarks(
+    benchmark: str,
+    target: str,
+):
+    """Run benchmark tests."""
+    with tempfile.TemporaryDirectory() as tmpdir:
+        output_dir = await fetch_results(benchmark, target, tmpdir)
+        output_results = []
+        output_imgs = {}
+        if target == 'ars':
+            # evaluate results for each ARA
+            for ara in [ara for ara in os.listdir(output_dir) if os.path.isdir(output_dir)]:
+                results_dir = os.path.join(output_dir, ara)
+                ara_output_result, ara_imgs = evaluate_ara_results(benchmark, results_dir, ara, save_plots=True)
+                output_imgs[ara] = ara_imgs
+                output_results.extend(ara_output_result)
+        else:
+            output_result, imgs = evaluate_ara_results(benchmark, output_dir, target, save_plots=True)
+            output_imgs[target] = imgs
+            output_results.extend(output_result)
+
+    return output_results, output_imgs
+
+
+if __name__ == "__main__":
+    asyncio.run(run_benchmarks(
+        "DrugCentral_subset",
+        "ars",
+    ))
