fd026da1503b51a7785ec2a88432dd3c6a49a316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:05:28 +0000

    Add package kmis-bi/0.2.1/KMIS_BI-0.2.1.tar.gz

---
+++ packages/kmis-bi/KMIS_BI-0.2.1.tar.gz/KMIS_BI-0.2.1/KMIS_BI/__init__.py
@@ -0,0 +1,24 @@
+#######################
+# BI library for KMIS #
+#######################
+
+import datetime
+from pyspark.sql.window import Window
+from pyspark.sql.functions import *
+from delta.tables import DeltaTable
+from notebookutils import mssparkutils
+
+from shareplum import Site
+from shareplum import Office365
+from shareplum.site import Version
+
+import traceback
+import sys
+import pandas as pd
+import time
+import pandera as pa
+import pyspark.sql.types as T
+
+def calc(num1, num2):
+    return num1 + num2
+    
