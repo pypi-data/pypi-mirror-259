130210d3fb644a15a4fac8f1f2be4e300c390243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:44 +0000

    Add package nm-magic-wand/0.2.0/nm_magic_wand-0.2.0-cp311-cp311-manylinux_2_17_x86_64.whl

---
+++ packages/nm-magic-wand/nm_magic_wand-0.2.0-cp311-cp311-manylinux_2_17_x86_64.whl/magic_wand/ops.py
@@ -0,0 +1,17 @@
+import torch
+
+from magic_wand.compressed_storage_format import SparseBEGemmStorageFormat
+
+
+def be_ds_gemm(A: torch.Tensor, B: SparseBEGemmStorageFormat) -> torch.Tensor:
+    return torch.ops.nm_ops.be_ds_gemm(
+        A,
+        B.layout_id,
+        B.values,
+        B.offsets,
+        B.counts,
+        B.bitmasks,
+        B.max_nnz_in_tile,
+        B.out_feature_dim_extent,
+        SparseBEGemmStorageFormat.shared_locks[B.device],
+    )
