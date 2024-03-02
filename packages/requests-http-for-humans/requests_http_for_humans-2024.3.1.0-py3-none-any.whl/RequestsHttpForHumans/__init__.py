2d4113ae9d3683579f3dd004732f5ce53c9fa598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:10:26 +0000

    Add package requests-http-for-humans/2024.3.1.0/requests_http_for_humans-2024.3.1.0-py3-none-any.whl

---
+++ packages/requests-http-for-humans/requests_http_for_humans-2024.3.1.0-py3-none-any.whl/RequestsHttpForHumans/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""requests-http-for-humans
+https://github.com/apachecn/requests-http-for-humans"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
