61d0555d2c5611977d771436b0a32417b96377c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:39 +0000

    Add package PyUtilToolKit/1.0.8/PyUtilToolKit-1.0.8.tar.gz

---
+++ packages/PyUtilToolKit/PyUtilToolKit-1.0.8.tar.gz/PyUtilToolKit-1.0.8/setup.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+"""
+@Time        : 2024/3/2
+@File        : setup.py
+@Author      : lyz
+@version     : python 3
+@Description :
+"""
+from setuptools import setup, find_packages
+
+with open("PyUtilToolKit/README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+# https://pypi.org/  Python 编程语言的软件存储库
+setup(name='PyUtilToolKit',  # 包名 别人安装时就是用此名来按照 如：pip install PyUtilToolKit
+      version='1.0.8',  # 包的版本号
+      description='Python常用代码小工具包',  # 包的介绍、概述
+      author='lyz',  # 包的作者
+      author_email='',  # 邮箱
+      url='https://gitee.com/Franklin_1/pypersonalizedtoolkit.git',  # 项目源代码地址 一般的填git地址
+      packages=find_packages(),  # Python导入包的列表 可以find_packages() 来自动收集
+      long_description=long_description,  # 项目的描述 读取README.md文件的信息
+      long_description_content_type="text/markdown",  # 描述文档README的格式 一般md
+      license="GPLv3",  # 开源协议
+      # 这 需要去官网查，在下边提供了许可证连接 或者 你可以直接把我的粘贴走
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: OS Independent"],
+
+      python_requires='>=3.7',  # Python的版本约束
+      # 其他依赖的约束
+      install_requires=[],
+      )
