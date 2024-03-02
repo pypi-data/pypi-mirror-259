ea89cd17d68c37f2ea2ea6e55b29709e0c12f699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:04:43 +0000

    Add package oliver-codegen/0.1.5/oliver_codegen-0.1.5.tar.gz

---
+++ packages/oliver-codegen/oliver_codegen-0.1.5.tar.gz/oliver_codegen-0.1.5/oliver_codegen/util/file_folder_util.py
@@ -0,0 +1,21 @@
+import os
+
+
+def handle_read_only_error(func, path, _):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+    """
+    import stat
+    # Is the error an access error?
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
