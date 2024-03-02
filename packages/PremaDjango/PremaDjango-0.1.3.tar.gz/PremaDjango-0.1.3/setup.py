10270c7e36e782d7fd769970eeb8d78014ec04a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:26 +0000

    Add package PremaDjango/0.1.3/PremaDjango-0.1.3.tar.gz

---
+++ packages/PremaDjango/PremaDjango-0.1.3.tar.gz/PremaDjango-0.1.3/setup.py
@@ -0,0 +1,26 @@
+"""PremaDjango setup.py."""
+
+from setuptools import find_packages, setup
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="PremaDjango",
+    version="0.1.3",
+    # use_scm_version=True,
+    # setup_requires=['setuptools_scm'],
+    packages=find_packages(),
+    install_requires=["Django", "setuptools_scm"],
+    author="Premanath",
+    author_email="talamarlapremanath143@gmail.com",
+    description="My Short Description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/prema1432/premadjango/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+    ],
+    license="MIT",
+)
