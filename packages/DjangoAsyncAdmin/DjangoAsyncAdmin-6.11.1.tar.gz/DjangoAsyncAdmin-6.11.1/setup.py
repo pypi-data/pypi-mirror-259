8acf51d4fdf0cf1f64ce907f13157a35b59fb391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:45 +0000

    Add package DjangoAsyncAdmin/6.11.1/DjangoAsyncAdmin-6.11.1.tar.gz

---
+++ packages/DjangoAsyncAdmin/DjangoAsyncAdmin-6.11.1.tar.gz/DjangoAsyncAdmin-6.11.1/setup.py
@@ -0,0 +1,12 @@
+# _*_ codign:utf8 _*_
+"""====================================
+@Author:Sadam·Sadik
+@Email：1903249375@qq.com
+@Date：2023/10/10
+@Software: PyCharm
+@disc:
+======================================="""
+from setuptools import setup, find_packages
+
+requires = ['django>=2.1', 'django-simpleui>=2022.11.30', 'django-import-export', 'requests', 'rsa', 'psutil']
+setup(packages=find_packages(exclude=[]), install_requires=requires)
