a3bb6d86e26a2c52a550dd4fade236bf739f4734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:06:03 +0000

    Add package rang-kaifa-zidonghua-xilie-zhuanlan/2024.3.2.0/rang_kaifa_zidonghua_xilie_zhuanlan-2024.3.2.0-py3-none-any.whl

---
+++ packages/rang-kaifa-zidonghua-xilie-zhuanlan/rang_kaifa_zidonghua_xilie_zhuanlan-2024.3.2.0-py3-none-any.whl/RangKaifaZidonghuaXilieZhuanlan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""rang-kaifa-zidonghua-xilie-zhuanlan
+https://github.com/apachecn/rang-kaifa-zidonghua-xilie-zhuanlan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
