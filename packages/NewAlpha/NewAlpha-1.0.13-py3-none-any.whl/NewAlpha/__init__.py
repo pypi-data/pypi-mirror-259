cdff25ff67f99125339333ea61ec7401410e5ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:10:24 +0000

    Add package NewAlpha/1.0.13/NewAlpha-1.0.13-py3-none-any.whl

---
+++ packages/NewAlpha/NewAlpha-1.0.13-py3-none-any.whl/NewAlpha/__init__.py
@@ -0,0 +1,6 @@
+#Object Classes
+from NewAlpha import AlphaClient, AlphaSwitch
+#AlphaSwitch methods
+from NewAlpha import switchSetup, startLog, stopLog, getNewestLog, getFullLog, handleTraffic
+#AlphaClient methods
+from NewAlpha import clientSetup, bridge, responseFlag, registerSwitch, request, encode_format, frozenResponse, dynamicResponse, confirmationResponse
