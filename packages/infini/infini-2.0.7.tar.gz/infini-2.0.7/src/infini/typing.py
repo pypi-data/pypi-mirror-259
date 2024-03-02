993f09e358bea38dc2e9bcb9e85e331912d5f5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:31 +0000

    Add package infini/2.0.7/infini-2.0.7.tar.gz

---
+++ packages/infini/infini-2.0.7.tar.gz/infini-2.0.7/src/infini/typing.py
@@ -0,0 +1,27 @@
+from typing import (
+    Dict as Dict,
+    List as List,
+    Any as Any,
+    Optional as Optional,
+    Generic as Generic,
+    Callable as Callable,
+    Literal as Literal,
+    Sequence as Sequence,
+    Generator as Generator,
+    overload as overload,
+    TypeVar,
+    TypedDict,
+    Union,
+)
+from types import ModuleType as ModuleType, GeneratorType as GeneratorType
+from . import router, input, output
+
+T = TypeVar("T")
+Stream = Union["input.Input[Any]", "output.Output"]
+OutputGenerator = Generator["output.Output", Any, None]
+
+
+class RouterType(TypedDict):
+    priority: int
+    router: router.Router
+    handler: Callable[["input.Input"], Union[Stream, OutputGenerator]]
