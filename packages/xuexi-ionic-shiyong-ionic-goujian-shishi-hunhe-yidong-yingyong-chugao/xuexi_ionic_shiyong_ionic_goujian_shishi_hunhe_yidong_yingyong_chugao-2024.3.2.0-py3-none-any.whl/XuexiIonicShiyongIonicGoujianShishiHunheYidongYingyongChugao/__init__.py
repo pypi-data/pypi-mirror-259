e67e791b53d3319601336639ef28dc1d3d6b33e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:05:29 +0000

    Add package xuexi-ionic-shiyong-ionic-goujian-shishi-hunhe-yidong-yingyong-chugao/2024.3.2.0/xuexi_ionic_shiyong_ionic_goujian_shishi_hunhe_yidong_yingyong_chugao-2024.3.2.0-py3-none-any.whl

---
+++ packages/xuexi-ionic-shiyong-ionic-goujian-shishi-hunhe-yidong-yingyong-chugao/xuexi_ionic_shiyong_ionic_goujian_shishi_hunhe_yidong_yingyong_chugao-2024.3.2.0-py3-none-any.whl/XuexiIonicShiyongIonicGoujianShishiHunheYidongYingyongChugao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xuexi-ionic-shiyong-ionic-goujian-shishi-hunhe-yidong-yingyong-chugao
+https://github.com/apachecn/xuexi-ionic-shiyong-ionic-goujian-shishi-hunhe-yidong-yingyong-chugao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
