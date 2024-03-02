e9c0c955363cf801e94973a9911cb9f9f388aed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:42 +0000

    Add package yong-jersey-goujian-restful-fuwu/2024.3.2.0/yong_jersey_goujian_restful_fuwu-2024.3.2.0-py3-none-any.whl

---
+++ packages/yong-jersey-goujian-restful-fuwu/yong_jersey_goujian_restful_fuwu-2024.3.2.0-py3-none-any.whl/YongJerseyGoujianRestfulFuwu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yong-jersey-goujian-restful-fuwu
+https://github.com/apachecn/yong-jersey-goujian-restful-fuwu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
