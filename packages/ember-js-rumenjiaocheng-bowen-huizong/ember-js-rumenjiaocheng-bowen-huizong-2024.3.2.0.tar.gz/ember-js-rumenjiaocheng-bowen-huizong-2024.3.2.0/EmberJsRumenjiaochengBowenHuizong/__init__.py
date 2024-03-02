894e8527981dd6520fc1f10dc6c80958bc5a2140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:47 +0000

    Add package ember-js-rumenjiaocheng-bowen-huizong/2024.3.2.0/ember-js-rumenjiaocheng-bowen-huizong-2024.3.2.0.tar.gz

---
+++ packages/ember-js-rumenjiaocheng-bowen-huizong/ember-js-rumenjiaocheng-bowen-huizong-2024.3.2.0.tar.gz/ember-js-rumenjiaocheng-bowen-huizong-2024.3.2.0/EmberJsRumenjiaochengBowenHuizong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ember-js-rumenjiaocheng-bowen-huizong
+https://github.com/apachecn/ember-js-rumenjiaocheng-bowen-huizong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
