b2265fa24efeabcc13959276d9f8d0063ab7c1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:34 +0000

    Add package struts-user-guide-simplified-chinese/2024.3.2.0/struts-user-guide-simplified-chinese-2024.3.2.0.tar.gz

---
+++ packages/struts-user-guide-simplified-chinese/struts-user-guide-simplified-chinese-2024.3.2.0.tar.gz/struts-user-guide-simplified-chinese-2024.3.2.0/StrutsUserGuideSimplifiedChinese/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""struts-user-guide-simplified-chinese
+https://github.com/apachecn/struts-user-guide-simplified-chinese"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
