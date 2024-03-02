c8ae2084cae49e526d623fba828bf7d18a37ef06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:43 +0000

    Add package jiqi-xuexi-shizhan-apachecn/2024.3.2.0/jiqi_xuexi_shizhan_apachecn-2024.3.2.0-py3-none-any.whl

---
+++ packages/jiqi-xuexi-shizhan-apachecn/jiqi_xuexi_shizhan_apachecn-2024.3.2.0-py3-none-any.whl/JiqiXuexiShizhanApachecn/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiqi-xuexi-shizhan-apachecn
+https://github.com/apachecn/jiqi-xuexi-shizhan-apachecn"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
