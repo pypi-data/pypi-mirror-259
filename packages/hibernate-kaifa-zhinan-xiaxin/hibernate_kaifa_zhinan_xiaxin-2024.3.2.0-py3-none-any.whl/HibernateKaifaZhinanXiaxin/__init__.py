81f30af8407eadcd89f522a3b35941f593c05711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:38 +0000

    Add package hibernate-kaifa-zhinan-xiaxin/2024.3.2.0/hibernate_kaifa_zhinan_xiaxin-2024.3.2.0-py3-none-any.whl

---
+++ packages/hibernate-kaifa-zhinan-xiaxin/hibernate_kaifa_zhinan_xiaxin-2024.3.2.0-py3-none-any.whl/HibernateKaifaZhinanXiaxin/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hibernate-kaifa-zhinan-xiaxin
+https://github.com/apachecn/hibernate-kaifa-zhinan-xiaxin"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
