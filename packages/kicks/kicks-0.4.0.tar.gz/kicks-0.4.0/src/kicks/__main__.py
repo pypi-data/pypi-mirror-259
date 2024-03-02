bc352cbc4d59899b70418e90f88845bc4fbad216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:49 +0000

    Add package kicks/0.4.0/kicks-0.4.0.tar.gz

---
+++ packages/kicks/kicks-0.4.0.tar.gz/kicks-0.4.0/src/kicks/__main__.py
@@ -0,0 +1,40 @@
+import os
+import rich
+import subprocess
+from rich.console import Console
+from rich.table import Table
+
+CONSOLE = Console()
+
+
+OP="_kicks"
+OD=os.makedirs(OP, exist_ok=True)
+
+t = Table()
+t.add_column("#")
+t.add_column("File")
+t.add_row("1", "firefox windows 64")
+t.add_row("2", "chrome windows 64")
+
+CONSOLE.print(t)
+
+x = CONSOLE.input('# ')
+
+if x == "1":
+    outfile = os.path.join(OP, "firefox-setup.exe")
+    cmd = 'curl -C - -L -o "%s" "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"' % outfile
+    subprocess.call(cmd, shell=True)
+    run_confirm = CONSOLE.input('run it? type "run" to execute or anything else to exit # ')
+    if run_confirm == "run":
+        subprocess.call(outfile, shell=True)
+
+if x == "2":
+    outfile = os.path.join(OP, "chrome.zip")
+    outzipdir = os.path.join(OP, "chrome")
+    cmd = 'curl -C - -L -o "%s" "https://dl.google.com/chrome/install/GoogleChromeEnterpriseBundle64.zip"' % outfile
+    subprocess.call(cmd, shell=True)
+    run_confirm = CONSOLE.input('run it? type "run" to execute or anything else to exit # ')
+    if run_confirm == "run":
+        subprocess.call("""powershell -Command Expand-Archive -Path '%s' -DestinationPath '%s'""" % (outfile, outzipdir), shell=True)
+        subprocess.call("""%s""" % os.path.join(outzipdir, "Installers", "GoogleChromeStandaloneEnterprise64.msi"), shell=True)
+        #subprocess.call("""explorer %s""" % (outzipdir), shell=True)
