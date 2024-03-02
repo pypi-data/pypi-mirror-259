e6b7fc3bd7d831a0691e9f189cc072f878b59edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:30 +0000

    Add package zgdvvVisfsdfs/0.0.0/zgdvvVisfsdfs-0.0.0.tar.gz

---
+++ packages/zgdvvVisfsdfs/zgdvvVisfsdfs-0.0.0.tar.gz/zgdvvVisfsdfs-0.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zgdvvVisfsdfs",  # Package name
+    version="0.0.0",  # Package version
+    description="parallel, efficient and ensemble analysis of Rao's Q spectral diversity using remote sensing datasets.",
+    author="Mohammad Reza Fathi",
+    author_email="mmadrzfathi@gmail.com",
+    packages=find_packages(),  # Automatically finds packages in the directory
+    install_requires=[  # List of required dependencies
+        "tk",
+        "ipywidgets",
+        "xarray",
+        "rioxarray",
+        "rasterio",
+        "numpy",
+        "spyndex",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "ray",
+        "tqdm",
+    ],
+)
