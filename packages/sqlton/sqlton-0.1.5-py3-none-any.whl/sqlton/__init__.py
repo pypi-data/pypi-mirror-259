21cb0291295619c0d2980b440b5df2c2a5e32e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:56 +0000

    Add package sqlton/0.1.5/sqlton-0.1.5-py3-none-any.whl

---
+++ packages/sqlton/sqlton-0.1.5-py3-none-any.whl/sqlton/__init__.py
@@ -0,0 +1,10 @@
+from sqlton.parser import Lexer, Parser
+
+def parse(statement):
+    lexer = Lexer()
+
+    tokens = lexer.tokenize(statement)
+        
+    parser = Parser()
+    
+    return parser.parse(tokens)
