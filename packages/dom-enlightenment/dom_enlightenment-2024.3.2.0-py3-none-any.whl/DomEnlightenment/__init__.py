1c6d9912b1af1142f9199b9db42b0803b62695a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:05:01 +0000

    Add package dom-enlightenment/2024.3.2.0/dom_enlightenment-2024.3.2.0-py3-none-any.whl

---
+++ packages/dom-enlightenment/dom_enlightenment-2024.3.2.0-py3-none-any.whl/DomEnlightenment/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dom-enlightenment
+https://github.com/apachecn/dom-enlightenment"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
