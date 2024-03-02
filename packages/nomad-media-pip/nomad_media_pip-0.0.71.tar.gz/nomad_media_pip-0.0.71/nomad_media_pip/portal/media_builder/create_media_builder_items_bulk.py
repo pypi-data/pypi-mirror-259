c509d4010d538315547b1b64eb586d1b75d08353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:11 +0000

    Add package nomad-media-pip/0.0.71/nomad_media_pip-0.0.71.tar.gz

---
+++ packages/nomad-media-pip/nomad_media_pip-0.0.71.tar.gz/nomad_media_pip-0.0.71/nomad_media_pip/portal/media_builder/create_media_builder_items_bulk.py
@@ -0,0 +1,30 @@
+from exceptions.api_exception_handler import _api_exception_handler
+
+import requests, json
+
+def _create_media_builder_items_bulk(AUTH_TOKEN, URL, MEDIA_BUILDER_ID, MEDIA_BUILDER_ITEMS, DEBUG):
+
+	API_URL = f"{URL}/mediaBuilder/{MEDIA_BUILDER_ID}/items/bulk"
+
+	HEADERS = {
+		"Content-Type": "application/json",
+		"Authorization": f"Bearer {AUTH_TOKEN}"
+	}
+
+	MEDIA_BUILDER_ITEMS["mediaBuilderId"] = MEDIA_BUILDER_ID
+
+	BODY = MEDIA_BUILDER_ITEMS
+	
+
+	if DEBUG:
+		print(f"API_URL: {API_URL}\nMETHOD: POST\nBODY: {json.dumps(BODY)}")
+
+	try:
+		RESPONSE = requests.post(API_URL, headers=HEADERS, data=json.dumps(BODY))
+
+		if not RESPONSE.ok:
+			raise Exception()
+
+		return RESPONSE.json()
+	except:
+		_api_exception_handler(RESPONSE, "Create Media Builder Items Bulk Failed")
