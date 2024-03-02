63be14d1ab4dd549a978c43bbfdd17dd8c459905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:49 +0000

    Add package formatforge/0.1.0/formatforge-0.1.0.tar.gz

---
+++ packages/formatforge/formatforge-0.1.0.tar.gz/formatforge-0.1.0/formatforge/main.py
@@ -0,0 +1,28 @@
+import typer
+from formatforge.uppercase import convert_keywords_to_uppercase
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    dbt-lint is a linter for dbt projects.
+    """
+
+
+@app.command()
+def hello():
+    """
+    Say hello.
+    """
+    print("Hello")
+
+
+@app.command()
+def uppercase(sql_file: str):
+    """
+    Lints a SQL file to ensure all keywords are uppercase.
+    """
+    convert_keywords_to_uppercase(sql_file)
+    return f"Converted {sql_file} to uppercase."
