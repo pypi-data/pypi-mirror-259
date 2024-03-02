125a8645105449ce2ff4748d8b5ff2ffd09b505b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:05:00 +0000

    Add package aliyun-logger/0.2/aliyun_logger-0.2.tar.gz

---
+++ packages/aliyun-logger/aliyun_logger-0.2.tar.gz/aliyun_logger-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aliyun_logger',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'aliyun-log-python-sdk',
+        'enum34'
+    ],
+    author='Yuxiang Zhang',
+    author_email='zhangyuxiang@53zaixian.com',
+    description='Python package for 53 Aliyun Logger',
+    url='https://github.com/your_username/aliyun-logger',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
