aa93e1d6f9195ebf4a3f36ca39034f8c055cf6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:13 +0000

    Add package a-second-course-in-algorithms-lecture-notes-stanford-cs261/2024.3.1.0/a_second_course_in_algorithms_lecture_notes_stanford_cs261-2024.3.1.0-py3-none-any.whl

---
+++ packages/a-second-course-in-algorithms-lecture-notes-stanford-cs261/a_second_course_in_algorithms_lecture_notes_stanford_cs261-2024.3.1.0-py3-none-any.whl/ASecondCourseInAlgorithmsLectureNotesStanfordCs261/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""a-second-course-in-algorithms-lecture-notes-stanford-cs261
+https://github.com/apachecn/a-second-course-in-algorithms-lecture-notes-stanford-cs261"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
