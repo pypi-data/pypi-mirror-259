5c0acab10cd27d428bf452f772fc6d642cf09583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:15 +0000

    Add package fei-jiandu-tezheng-xuexi-yu-shendu-xuexi-zhongwen-jiaocheng-ufldl/2024.3.2.0/fei_jiandu_tezheng_xuexi_yu_shendu_xuexi_zhongwen_jiaocheng_ufldl-2024.3.2.0-py3-none-any.whl

---
+++ packages/fei-jiandu-tezheng-xuexi-yu-shendu-xuexi-zhongwen-jiaocheng-ufldl/fei_jiandu_tezheng_xuexi_yu_shendu_xuexi_zhongwen_jiaocheng_ufldl-2024.3.2.0-py3-none-any.whl/FeiJianduTezhengXuexiYuShenduXuexiZhongwenJiaochengUfldl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fei-jiandu-tezheng-xuexi-yu-shendu-xuexi-zhongwen-jiaocheng-ufldl
+https://github.com/apachecn/fei-jiandu-tezheng-xuexi-yu-shendu-xuexi-zhongwen-jiaocheng-ufldl"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
