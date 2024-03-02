450a835f958f22523adbf95dad6a77688cc6d5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:59 +0000

    Add package shangzhao-zi-de-jiemi-rumen-jiaoxue/2024.3.2.0/shangzhao_zi_de_jiemi_rumen_jiaoxue-2024.3.2.0-py3-none-any.whl

---
+++ packages/shangzhao-zi-de-jiemi-rumen-jiaoxue/shangzhao_zi_de_jiemi_rumen_jiaoxue-2024.3.2.0-py3-none-any.whl/ShangzhaoZiDeJiemiRumenJiaoxue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shangzhao-zi-de-jiemi-rumen-jiaoxue
+https://github.com/apachecn/shangzhao-zi-de-jiemi-rumen-jiaoxue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
