0dc2efeee8b3a595294bc46058d2b437bd035d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:58 +0000

    Add package wutong-de-jiqi-xuexi-diyiji/2024.3.2.0/wutong_de_jiqi_xuexi_diyiji-2024.3.2.0-py3-none-any.whl

---
+++ packages/wutong-de-jiqi-xuexi-diyiji/wutong_de_jiqi_xuexi_diyiji-2024.3.2.0-py3-none-any.whl/WutongDeJiqiXuexiDiyiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wutong-de-jiqi-xuexi-diyiji
+https://github.com/apachecn/wutong-de-jiqi-xuexi-diyiji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
