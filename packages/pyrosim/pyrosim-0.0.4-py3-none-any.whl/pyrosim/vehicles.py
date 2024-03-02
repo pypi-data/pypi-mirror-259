067a78c1088feb31ded2a2fca996c20071679bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:10:28 +0000

    Add package pyrosim/0.0.4/pyrosim-0.0.4-py3-none-any.whl

---
+++ packages/pyrosim/pyrosim-0.0.4-py3-none-any.whl/pyrosim/vehicles.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2024 - Intelligentica (Morocco)
+Email: a.benmhamed@intelligentica.net
+Website: https://intelligentica.net
+"""
+
+
+class Vehicle(object):
+    """
+    Representation of a vehicle object
+    """
+    pass
+
+
+class Truck(Vehicle):
+    """
+    Representation of a truck object
+    """
+    pass
+
+
+class Drone(Vehicle):
+    """
+    Representation of a drone object
+    """
+    pass
+
+
+class Moto(Vehicle):
+    """
+    Representation of a moto object
+    """
+    pass
+
+
+class Fleet(object):
+    """
+    Representation of a fleet of vehicles
+    """
+
