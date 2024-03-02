84ab5366612826ee9122b92cef3affbb2dd0c6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:15 +0000

    Add package compiler-design-lecture-notes-cmu-15-411/2024.3.2.0/compiler_design_lecture_notes_cmu_15_411-2024.3.2.0-py3-none-any.whl

---
+++ packages/compiler-design-lecture-notes-cmu-15-411/compiler_design_lecture_notes_cmu_15_411-2024.3.2.0-py3-none-any.whl/CompilerDesignLectureNotesCmu15411/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""compiler-design-lecture-notes-cmu-15-411
+https://github.com/apachecn/compiler-design-lecture-notes-cmu-15-411"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
