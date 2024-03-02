6857024f1e64f71663e0a71cebb9339b5b5f97c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:50 +0000

    Add package NearBeach/0.31.3/nearbeach-0.31.3.tar.gz

---
+++ packages/NearBeach/nearbeach-0.31.3.tar.gz/nearbeach-0.31.3/src/js/components/modules/sub_modules/MiscModule.vue
@@ -0,0 +1,23 @@
+<template>
+	<list-tags-module></list-tags-module>
+
+	<hr>
+
+	<list-public-links></list-public-links>
+</template>
+
+<script>
+// JavaScript Libraries
+import ListPublicLinks from "./ListPublicLinks.vue";
+import ListTagsModule from "./ListTagsModule.vue";
+
+export default {
+	name: "MiscModule",
+	components: {
+		ListPublicLinks,
+		ListTagsModule,
+	},
+};
+</script>
+
+<style scoped></style>
