e476be877293fd3442290f68096214d68b2e0392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:41 +0000

    Add package cool-seq-tool/0.4.0.dev3/cool_seq_tool-0.4.0.dev3-py3-none-any.whl

---
+++ packages/cool-seq-tool/cool_seq_tool-0.4.0.dev3-py3-none-any.whl/cool_seq_tool/__init__.py
@@ -0,0 +1,14 @@
+"""The cool_seq_tool package"""
+import logging
+from pathlib import Path
+
+APP_ROOT = Path(__file__).resolve().parents[0]
+
+logging.basicConfig(
+    filename="cool_seq_tool.log",
+    format="[%(asctime)s] - %(name)s - %(levelname)s : %(message)s",
+)
+logger = logging.getLogger("cool_seq_tool")
+logger.setLevel(logging.DEBUG)
+
+LOG_FN = "cool_seq_tool.log"
