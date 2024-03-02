38727bc6fa63745f877fbc211045e28b68bdaf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:47 +0000

    Add package datazone/0.0.2/datazone-0.0.2-py3-none-any.whl

---
+++ packages/datazone/datazone-0.0.2-py3-none-any.whl/datazone/models/settings.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import Dict, Optional
+
+from pydantic import BaseModel
+
+
+class Profile(BaseModel):
+    username: str
+    password: str
+    server_endpoint: str
+    token: Optional[str] = None
+    is_default: Optional[bool] = False
+    last_login: Optional[datetime] = None
+
+    @property
+    def token_url(self):
+        return f"{self.server_endpoint}/token"
+
+
+class Settings(BaseModel):
+    profiles: Dict[str, Profile] = {}
