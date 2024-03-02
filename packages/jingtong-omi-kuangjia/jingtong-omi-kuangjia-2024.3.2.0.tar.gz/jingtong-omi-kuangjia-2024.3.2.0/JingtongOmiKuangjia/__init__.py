8b9ceb64fac00b9ef5db232cc4328d98064b4752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:42 +0000

    Add package jingtong-omi-kuangjia/2024.3.2.0/jingtong-omi-kuangjia-2024.3.2.0.tar.gz

---
+++ packages/jingtong-omi-kuangjia/jingtong-omi-kuangjia-2024.3.2.0.tar.gz/jingtong-omi-kuangjia-2024.3.2.0/JingtongOmiKuangjia/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingtong-omi-kuangjia
+https://github.com/apachecn/jingtong-omi-kuangjia"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
