2fc2e91e46b6866bcb3999900cc456f61182ea9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:06:08 +0000

    Add package openobd-protocol/0.0.27/openobd_protocol-0.0.27-py3-none-any.whl

---
+++ packages/openobd-protocol/openobd_protocol-0.0.27-py3-none-any.whl/openobd_protocol/CanServices_pb2.pyi
@@ -0,0 +1,12 @@
+from openobd_protocol import Authentication_pb2 as _Authentication_pb2
+from openobd_protocol import BasicResponse_pb2 as _BasicResponse_pb2
+from openobd_protocol import ModuleConfiguration_pb2 as _ModuleConfiguration_pb2
+from openobd_protocol import BusConfiguration_pb2 as _BusConfiguration_pb2
+from openobd_protocol import ObdMessage_pb2 as _ObdMessage_pb2
+from openobd_protocol import Status_pb2 as _Status_pb2
+from openobd_protocol.ISOTP import IsotpMessage_pb2 as _IsotpMessage_pb2
+from openobd_protocol.ISOTP import IsotpChannel_pb2 as _IsotpChannel_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
