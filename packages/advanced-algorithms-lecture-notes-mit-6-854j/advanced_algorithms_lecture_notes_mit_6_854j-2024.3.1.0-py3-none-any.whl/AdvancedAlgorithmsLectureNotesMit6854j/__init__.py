1988cf8813bd39261d64388625a22bb6c5f70730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:19 +0000

    Add package advanced-algorithms-lecture-notes-mit-6-854j/2024.3.1.0/advanced_algorithms_lecture_notes_mit_6_854j-2024.3.1.0-py3-none-any.whl

---
+++ packages/advanced-algorithms-lecture-notes-mit-6-854j/advanced_algorithms_lecture_notes_mit_6_854j-2024.3.1.0-py3-none-any.whl/AdvancedAlgorithmsLectureNotesMit6854j/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""advanced-algorithms-lecture-notes-mit-6-854j
+https://github.com/apachecn/advanced-algorithms-lecture-notes-mit-6-854j"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
