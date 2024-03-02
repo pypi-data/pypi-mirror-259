ea315bc626af808e444f9c7b0a91c44b74f04683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:10:28 +0000

    Add package pyrosim/0.0.4/pyrosim-0.0.4.tar.gz

---
+++ packages/pyrosim/pyrosim-0.0.4.tar.gz/pyrosim-0.0.4/README.md
@@ -0,0 +1,3 @@
+# pyrosim
+
+description
---
+++ packages/pyrosim/pyrosim-0.0.4.tar.gz/pyrosim-0.0.4/pyrosim/locations.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2024 - Intelligentica (Morocco)
+Email: a.benmhamed@intelligentica.net
+Website: https://intelligentica.net
+"""
