ffa821f10640d3445c783b296df4d2d781394582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:32 +0000

    Add package yiqi-xue-node-js/2024.3.2.0/yiqi_xue_node_js-2024.3.2.0-py3-none-any.whl

---
+++ packages/yiqi-xue-node-js/yiqi_xue_node_js-2024.3.2.0-py3-none-any.whl/YiqiXueNodeJs/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yiqi-xue-node-js
+https://github.com/apachecn/yiqi-xue-node-js"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
