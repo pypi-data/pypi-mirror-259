29b860a5c7574c248b10acaadffbeba83e346d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:55 +0000

    Add package algorithms-jeff-erickson-uiuc-cs473-573/2024.3.1.0/algorithms_jeff_erickson_uiuc_cs473_573-2024.3.1.0-py3-none-any.whl

---
+++ packages/algorithms-jeff-erickson-uiuc-cs473-573/algorithms_jeff_erickson_uiuc_cs473_573-2024.3.1.0-py3-none-any.whl/AlgorithmsJeffEricksonUiucCs473573/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""algorithms-jeff-erickson-uiuc-cs473-573
+https://github.com/apachecn/algorithms-jeff-erickson-uiuc-cs473-573"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
