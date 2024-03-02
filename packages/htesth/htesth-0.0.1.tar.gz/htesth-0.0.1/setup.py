d1ec1546778f862df014fd122eeb24403c4a5041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:05:59 +0000

    Add package htesth/0.0.1/htesth-0.0.1.tar.gz

---
+++ packages/htesth/htesth-0.0.1.tar.gz/htesth-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='htesth',
+    version='0.0.1',
+    packages=find_packages(),
+    author='melong',
+    description='This is a test package',
+    python_requires='~=3.6',
+)
