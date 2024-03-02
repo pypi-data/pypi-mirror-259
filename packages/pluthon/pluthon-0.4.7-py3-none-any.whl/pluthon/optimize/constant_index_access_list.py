a11c7269db7644d5290b6c300b2a4378ad4b1242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:04:43 +0000

    Add package pluthon/0.4.7/pluthon-0.4.7-py3-none-any.whl

---
+++ packages/pluthon/pluthon-0.4.7-py3-none-any.whl/pluthon/optimize/constant_index_access_list.py
@@ -0,0 +1,24 @@
+from pluthon.pluthon_ast import Integer
+from pluthon.pluthon_sugar import (
+    IndexAccessList,
+    ConstantIndexAccessList,
+    NthField,
+    ConstantNthField,
+)
+from pluthon.util import NodeTransformer
+
+
+class IndexAccessOptimizer(NodeTransformer):
+    """
+    Replaces IndexAccesses to constants with ConstantIndexAccesses
+    """
+
+    def visit_IndexAccessList(self, node: IndexAccessList):
+        if isinstance(node.i, Integer):
+            return ConstantIndexAccessList(node.l, node.i.x)
+        return node
+
+    def visit_NthField(self, node: NthField):
+        if isinstance(node.n, Integer):
+            return ConstantNthField(node.d, node.n.x)
+        return node
