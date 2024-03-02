316b07c20aa2111fef8cfc7c742fb0f81654f468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:16 +0000

    Add package pythagoras/0.8.1/pythagoras-0.8.1.tar.gz

---
+++ packages/pythagoras/pythagoras-0.8.1.tar.gz/pythagoras-0.8.1/pythagoras/__main__.py
@@ -0,0 +1,6 @@
+# from pythagoras._05_mission_control.global_state_management import _clean_global_state
+# import pythagoras as pth
+#
+# _clean_global_state()
+# pth.initialize("QQQQQQQQQQQQQQQQQQQQQQQQQQQQ")
+# assert False
