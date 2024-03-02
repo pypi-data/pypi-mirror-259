cf2c050ade6d85a25f41b3fdb0fae09f2f09c2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:51 +0000

    Add package java-api-banben-de-vert-x-core-shouce/2024.3.2.0/java_api_banben_de_vert_x_core_shouce-2024.3.2.0-py3-none-any.whl

---
+++ packages/java-api-banben-de-vert-x-core-shouce/java_api_banben_de_vert_x_core_shouce-2024.3.2.0-py3-none-any.whl/JavaApiBanbenDeVertXCoreShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-api-banben-de-vert-x-core-shouce
+https://github.com/apachecn/java-api-banben-de-vert-x-core-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
