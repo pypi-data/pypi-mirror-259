25b75bb9379b0b9210044848538c76872ddc9cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:12 +0000

    Add package hgraph/0.1.17/hgraph-0.1.17.tar.gz

---
+++ packages/hgraph/hgraph-0.1.17.tar.gz/hgraph-0.1.17/tests/python/unit/hgraph/_impl/_runtime/test_graph_runner.py
@@ -0,0 +1,29 @@
+from hgraph import graph, run_graph, compute_node, TS, TIME_SERIES_TYPE
+from hgraph._runtime._evaluation_engine import EvaluationMode
+from hgraph.nodes import const, print_
+from hgraph.nodes._print import debug_print
+
+
+def test_hello_world():
+
+    @graph
+    def hello_world():
+        c = const("Hello World")
+        print_(c)
+
+    run_graph(hello_world, run_mode=EvaluationMode.SIMULATION)
+
+
+def test_compute_node():
+
+    @compute_node
+    def tick(ts: TIME_SERIES_TYPE) -> TS[bool]:
+        return True
+
+    @graph
+    def hello_world():
+        c = const(1)
+        t = tick(c)
+        debug_print("t", t)
+
+    run_graph(hello_world, run_mode=EvaluationMode.SIMULATION)
