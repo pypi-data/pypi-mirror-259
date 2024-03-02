7e9b546f03eab4515ffab5254820593f9d0872ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:31 +0000

    Add package secodadk/0.0.4/secodadk-0.0.4-py3-none-any.whl

---
+++ packages/secodadk/secodadk-0.0.4-py3-none-any.whl/secodadk/strats/core.py
@@ -0,0 +1,40 @@
+import abc
+from typing import Optional, Any
+
+from httpx import Response
+
+from secodadk.ingestion_models import Resource, DeclaredLineage
+
+
+class IngestResourcesStrategy(abc.ABC):
+    @abc.abstractmethod
+    def declare_resource(self, resource: Resource):
+        ...
+
+    @abc.abstractmethod
+    def declare_lineage(self, lineage: DeclaredLineage):
+        ...
+
+    @abc.abstractmethod
+    def finalize(self):
+        ...
+
+
+class NetworkCallStrategy(abc.ABC):
+    @abc.abstractmethod
+    def make_request(
+        self,
+        method: str,
+        url: str,
+        *,
+        params: Optional[dict] = None,
+        headers: Optional[dict] = None,
+        data: Optional[dict] = None,
+        json: Optional[Any] = None,
+        verify: bool | str = True,
+        follow_redirects: bool = False,
+    ) -> Response:
+        ...
+
+    def finalize(self):
+        pass
