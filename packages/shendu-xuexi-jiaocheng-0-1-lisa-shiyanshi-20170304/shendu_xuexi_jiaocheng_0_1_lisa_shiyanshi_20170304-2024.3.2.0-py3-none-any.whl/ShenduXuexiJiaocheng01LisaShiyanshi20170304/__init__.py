a9bf028309f15d6355be2c15e0ce1bddf59a00d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:59 +0000

    Add package shendu-xuexi-jiaocheng-0-1-lisa-shiyanshi-20170304/2024.3.2.0/shendu_xuexi_jiaocheng_0_1_lisa_shiyanshi_20170304-2024.3.2.0-py3-none-any.whl

---
+++ packages/shendu-xuexi-jiaocheng-0-1-lisa-shiyanshi-20170304/shendu_xuexi_jiaocheng_0_1_lisa_shiyanshi_20170304-2024.3.2.0-py3-none-any.whl/ShenduXuexiJiaocheng01LisaShiyanshi20170304/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shendu-xuexi-jiaocheng-0-1-lisa-shiyanshi-20170304
+https://github.com/apachecn/shendu-xuexi-jiaocheng-0-1-lisa-shiyanshi-20170304"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
