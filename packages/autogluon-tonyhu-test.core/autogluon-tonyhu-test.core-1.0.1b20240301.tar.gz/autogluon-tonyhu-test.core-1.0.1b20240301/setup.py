f34f39611420b6b80ba93b86aeabb8fb7edd2262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:12 +0000

    Add package autogluon-tonyhu-test.core/1.0.1b20240301/autogluon-tonyhu-test.core-1.0.1b20240301.tar.gz

---
+++ packages/autogluon-tonyhu-test.core/autogluon-tonyhu-test.core-1.0.1b20240301.tar.gz/autogluon-tonyhu-test.core-1.0.1b20240301/setup.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+###########################
+# This code block is a HACK (!), but is necessary to avoid code duplication. Do NOT alter these lines.
+import importlib.util
+import os
+import platform
+import sys
+
+from setuptools import setup
+
+filepath = os.path.abspath(os.path.dirname(__file__))
+filepath_import = os.path.join(filepath, "..", "core", "src", "autogluon-tonyhu-test", "core", "_setup_utils.py")
+spec = importlib.util.spec_from_file_location("ag_min_dependencies", filepath_import)
+ag = importlib.util.module_from_spec(spec)
+# Identical to `from autogluon.core import _setup_utils as ag`, but works without `autogluon.core` being installed.
+spec.loader.exec_module(ag)
+###########################
+
+version = ag.load_version_file()
+version = ag.update_version(version)
+
+submodule = "core"
+install_requires = (
+    [
+        # version ranges added in ag.get_dependency_version_ranges()
+        "numpy",
+        "scipy",
+        "scikit-learn",
+        "networkx",
+        "pandas",
+        "tqdm",
+        "requests",
+        "matplotlib",
+        "boto3",
+        f"autogluon-tonyhu-test.common=={version}",
+    ]
+    if not ag.LITE_MODE
+    else [
+        # version ranges added in ag.get_dependency_version_ranges()
+        "numpy",
+        "scipy",
+        "scikit-learn",
+        "pandas",
+        "tqdm",
+        "matplotlib",
+        f"{ag.PACKAGE_NAME}.common=={version}",
+    ]
+)
+
+# Check for Windows and Python 3.11
+is_windows_and_py311 = platform.system().lower() == "windows" and sys.version_info.major == 3 and sys.version_info.minor == 11
+if not is_windows_and_py311:
+    extras_require = {
+        "ray": [
+            "ray[default]>=2.6.3,<2.7",
+        ],
+        "raytune": [
+            "ray[default,tune]>=2.6.3,<2.7",
+            # TODO: consider alternatives as hyperopt is not actively maintained.
+            "hyperopt>=0.2.7,<0.2.8",  # This is needed for the bayes search to work.
+            # 'GPy>=1.10.0,<1.11.0'  # TODO: Enable this once PBT/PB2 are supported by ray lightning
+        ],
+    }
+else:
+    # we don't install ray and raytune on Windows with Python 3.11 due to issue https://github.com/autogluon/autogluon/issues/3807
+    extras_require = {}
+
+tests_require = [
+    "pytest",
+    "types-requests",
+    "types-setuptools",
+    "pytest-mypy",
+]
+
+all_requires = []
+
+for extra_package in ["ray", "raytune"]:
+    if extra_package in extras_require:
+        all_requires += extras_require[extra_package]
+tests_require = list(set(tests_require))
+all_requires = list(set(all_requires))
+extras_require["tests"] = tests_require
+extras_require["all"] = all_requires
+
+install_requires = ag.get_dependency_version_ranges(install_requires)
+
+if __name__ == "__main__":
+    ag.create_version_file(version=version, submodule=submodule)
+    setup_args = ag.default_setup_args(version=version, submodule=submodule)
+    setup(
+        install_requires=install_requires,
+        extras_require=extras_require,
+        **setup_args,
+    )
