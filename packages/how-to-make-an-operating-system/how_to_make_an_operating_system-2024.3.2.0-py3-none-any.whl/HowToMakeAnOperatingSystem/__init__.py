63fa310206d73afd36aa97ee9925fde365aaa827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:10 +0000

    Add package how-to-make-an-operating-system/2024.3.2.0/how_to_make_an_operating_system-2024.3.2.0-py3-none-any.whl

---
+++ packages/how-to-make-an-operating-system/how_to_make_an_operating_system-2024.3.2.0-py3-none-any.whl/HowToMakeAnOperatingSystem/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""how-to-make-an-operating-system
+https://github.com/apachecn/how-to-make-an-operating-system"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
