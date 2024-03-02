5c7797622b4f69ae0e7432ef4b5d8cfd81b529ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:43 +0000

    Add package jisuanjichengxu-de-siwei-luoji-java-miaoshu/2024.3.2.0/jisuanjichengxu_de_siwei_luoji_java_miaoshu-2024.3.2.0-py3-none-any.whl

---
+++ packages/jisuanjichengxu-de-siwei-luoji-java-miaoshu/jisuanjichengxu_de_siwei_luoji_java_miaoshu-2024.3.2.0-py3-none-any.whl/JisuanjichengxuDeSiweiLuojiJavaMiaoshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jisuanjichengxu-de-siwei-luoji-java-miaoshu
+https://github.com/apachecn/jisuanjichengxu-de-siwei-luoji-java-miaoshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
