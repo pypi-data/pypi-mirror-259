3ed0281855328349df1f746aeb0bc2e7869318e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:11 +0000

    Add package gaodengshuxue-ccp-maths-165/2024.3.2.0/gaodengshuxue_ccp_maths_165-2024.3.2.0-py3-none-any.whl

---
+++ packages/gaodengshuxue-ccp-maths-165/gaodengshuxue_ccp_maths_165-2024.3.2.0-py3-none-any.whl/GaodengshuxueCcpMaths165/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gaodengshuxue-ccp-maths-165
+https://github.com/apachecn/gaodengshuxue-ccp-maths-165"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
