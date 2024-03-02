3e6a987fea599c98652024de49d7396513e715e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:06 +0000

    Add package anquan-dangan-dierqi/2024.3.2.0/anquan_dangan_dierqi-2024.3.2.0-py3-none-any.whl

---
+++ packages/anquan-dangan-dierqi/anquan_dangan_dierqi-2024.3.2.0-py3-none-any.whl/AnquanDanganDierqi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""anquan-dangan-dierqi
+https://github.com/apachecn/anquan-dangan-dierqi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
