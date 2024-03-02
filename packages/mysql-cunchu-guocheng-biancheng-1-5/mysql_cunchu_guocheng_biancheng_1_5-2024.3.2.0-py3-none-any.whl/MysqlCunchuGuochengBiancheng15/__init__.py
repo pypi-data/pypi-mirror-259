ff5c155cba977930ade0fd567228352967e961c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:42 +0000

    Add package mysql-cunchu-guocheng-biancheng-1-5/2024.3.2.0/mysql_cunchu_guocheng_biancheng_1_5-2024.3.2.0-py3-none-any.whl

---
+++ packages/mysql-cunchu-guocheng-biancheng-1-5/mysql_cunchu_guocheng_biancheng_1_5-2024.3.2.0-py3-none-any.whl/MysqlCunchuGuochengBiancheng15/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mysql-cunchu-guocheng-biancheng-1-5
+https://github.com/apachecn/mysql-cunchu-guocheng-biancheng-1-5"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
