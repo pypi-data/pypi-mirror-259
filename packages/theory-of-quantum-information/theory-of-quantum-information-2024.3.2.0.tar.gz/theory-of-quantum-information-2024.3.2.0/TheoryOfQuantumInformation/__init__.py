abb3154b8fa2de58488b7ae7cd532075fca1638f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:09 +0000

    Add package theory-of-quantum-information/2024.3.2.0/theory-of-quantum-information-2024.3.2.0.tar.gz

---
+++ packages/theory-of-quantum-information/theory-of-quantum-information-2024.3.2.0.tar.gz/theory-of-quantum-information-2024.3.2.0/TheoryOfQuantumInformation/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""theory-of-quantum-information
+https://github.com/apachecn/theory-of-quantum-information"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
