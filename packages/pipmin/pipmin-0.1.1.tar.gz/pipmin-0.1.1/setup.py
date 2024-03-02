b2bb31f0242b6a010c8bad9b828135933626c2cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:53 +0000

    Add package pipmin/0.1.1/pipmin-0.1.1.tar.gz

---
+++ packages/pipmin/pipmin-0.1.1.tar.gz/pipmin-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name='pipmin',
+    version='0.1.1',
+    author='Harshit Sharma',
+    author_email='harshit158@gmail.com',
+    description='Package to generate trimmed requirements.txt',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pipmin=trim:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    python_requires='>=3.9.0',
+)
