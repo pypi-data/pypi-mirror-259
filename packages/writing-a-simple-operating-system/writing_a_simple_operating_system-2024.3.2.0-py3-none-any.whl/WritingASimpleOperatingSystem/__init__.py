e7d62fa2bbafaf9988ac935ba16081fab3c2d061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:12 +0000

    Add package writing-a-simple-operating-system/2024.3.2.0/writing_a_simple_operating_system-2024.3.2.0-py3-none-any.whl

---
+++ packages/writing-a-simple-operating-system/writing_a_simple_operating_system-2024.3.2.0-py3-none-any.whl/WritingASimpleOperatingSystem/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""writing-a-simple-operating-system
+https://github.com/apachecn/writing-a-simple-operating-system"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
