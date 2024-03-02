127cffe2ebc6965c329302bb56e818064ec11977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:06:08 +0000

    Add package tg-flask-sse-common/1.0.8/tg_flask_sse_common-1.0.8.tar.gz

---
+++ packages/tg-flask-sse-common/tg_flask_sse_common-1.0.8.tar.gz/tg_flask_sse_common-1.0.8/tg_flask_sse_common/sse_cache.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    sse_cache.py
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    sse消息推送和订阅
+
+    :author: Tangshimin
+    :copyright: (c) 2024, Tungee
+    :date created: 2024-01-29
+
+"""
+import json
+from datetime import datetime
+
+
+class SseChannelMapCache(object):
+    """
+    节点sse连接信息缓存
+    用于过渡一个频道下多个用户的sse连接信息
+    channel : [111, 222] - set
+    """
+    def __init__(self, redis_client, key_prefix):
+        self.redis = redis_client
+        self.prefix = key_prefix + ':sse:'
+        self.expire = 60 * 60 * 12
+
+    def key(self, channel):
+        return self.prefix + 'channel_map:' + channel
+
+    def add(self, channel, connect_id):
+        self.redis.sadd(self.key(channel), connect_id)
+        self.redis.expire(self.key(channel), self.expire)
+
+    def get(self, channel):
+        data = self.redis.smembers(self.key(channel))
+        if data is None:
+            return []
+
+        for i in range(len(data)):
+            if isinstance(data[i], bytes):
+                data[i] = data[i].decode('utf-8')
+
+    def delete(self, channel):
+        self.redis.srem(self.key(channel))
+
+
+class SseConnectStatsCache(object):
+    """
+    节点sse连接状态信息统计缓存
+    用于统计每个节点下的所有channel-sse连接状态信息，用于查看节点下所有用户的连接状态
+    info : {
+        'channel': '122121',
+        'connect_time': '2024-01-29 12:00:00',
+        'extra': {},
+    }
+    """
+    def __init__(self, redis_client, key_prefix):
+        self.redis = redis_client
+        self.tail = datetime.now().strftime('%Y%m%d%H')
+        self.prefix = key_prefix + ':sse:'
+        self.expire = 60 * 60 * 12
+
+    def key(self):
+        return self.prefix + 'connect_stats_info:' + self.tail
+
+    def add(self, channel, info):
+        info = json.dumps(info)
+        self.redis.hset(self.key(), channel, info)
+        self.redis.expire(self.key(), self.expire)
+
+    def get(self, channel):
+        data = self.redis.hget(self.key(), channel)
+        if data is None:
+            return {}
+
+        if isinstance(data, bytes):
+            data = data.decode('utf-8')
+
+        return json.loads(data)
+
+    def get_all(self):
+        datas = self.redis.hgetall(self.key())
+        if datas is None:
+            return {}
+
+        for key, value in datas.items():
+            if isinstance(key, bytes):
+                key = key.decode('utf-8')
+            if isinstance(value, bytes):
+                datas[key] = json.loads(value.decode('utf-8'))
+
+        return datas
+
+    def delete(self, channel):
+        self.redis.hdel(self.key(), channel)
+
+
+class SseEventMessageCache(object):
+    """
+    sse消息缓存
+    """
+    def __init__(self, redis_client, key_prefix):
+        self.redis = redis_client
+        self.tail = datetime.now().strftime('%Y%m%d%H')
+        self.prefix = key_prefix + ':sse:'
+        self.expire = 60 * 60 * 12
+
+    def pub_key(self):
+        return self.prefix + 'pub_event_message:' + self.tail
+
+    def sub_key(self):
+        return self.prefix + 'sub_event_message:' + self.tail
+
+    def add_pub_message(self, message):
+        message = json.dumps(message)
+        self.redis.lpush(self.pub_key(), message)
+        self.redis.expire(self.pub_key(), self.expire)
+
+    def add_sub_message(self, message):
+        message = json.dumps(message)
+        self.redis.lpush(self.sub_key(), message)
+        self.redis.expire(self.sub_key(), self.expire)
+
+    def get_pub_all(self):
+        message = self.redis.lrange(self.pub_key(), 0, -1)
+        if message is None:
+            return []
+
+        for i in range(len(message)):
+            if isinstance(message[i], bytes):
+                message[i] = json.loads(message[i].decode('utf-8'))
+
+        return message
+
+    def get_sub_all(self):
+        message = self.redis.lrange(self.sub_key(), 0, -1)
+        if message is None:
+            return []
+
+        for i in range(len(message)):
+            if isinstance(message[i], bytes):
+                message[i] = json.loads(message[i].decode('utf-8'))
+
+        return message
