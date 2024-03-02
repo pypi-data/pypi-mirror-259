69e6ebf696cda7f1fc2d69289448a4c3ed32ef2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:09 +0000

    Add package scml/0.7.0/scml-0.7.0.tar.gz

---
+++ packages/scml/scml-0.7.0.tar.gz/scml-0.7.0/src/scml/std/agent.py
@@ -0,0 +1,10 @@
+from scml.oneshot.agent import EndingNegotiator, OneShotAgent, OneShotSyncAgent
+
+__all__ = [
+    "StdAgent",
+    "StdSyncAgent",
+    "EndingNegotiator",
+]
+
+StdAgent = OneShotAgent
+StdSyncAgent = OneShotSyncAgent
