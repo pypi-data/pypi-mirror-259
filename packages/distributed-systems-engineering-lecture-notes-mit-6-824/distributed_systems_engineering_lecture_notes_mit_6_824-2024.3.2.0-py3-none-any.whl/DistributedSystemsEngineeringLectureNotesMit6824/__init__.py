8392dfd3010c46c31435345ae83a95729003ce74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:50 +0000

    Add package distributed-systems-engineering-lecture-notes-mit-6-824/2024.3.2.0/distributed_systems_engineering_lecture_notes_mit_6_824-2024.3.2.0-py3-none-any.whl

---
+++ packages/distributed-systems-engineering-lecture-notes-mit-6-824/distributed_systems_engineering_lecture_notes_mit_6_824-2024.3.2.0-py3-none-any.whl/DistributedSystemsEngineeringLectureNotesMit6824/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""distributed-systems-engineering-lecture-notes-mit-6-824
+https://github.com/apachecn/distributed-systems-engineering-lecture-notes-mit-6-824"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
