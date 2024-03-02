39776523bbfe142e9d428e43d462e5439356622e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:50 +0000

    Add package discrete-stochastic-processes-lecture-notes-mit-6-262/2024.3.2.0/discrete_stochastic_processes_lecture_notes_mit_6_262-2024.3.2.0-py3-none-any.whl

---
+++ packages/discrete-stochastic-processes-lecture-notes-mit-6-262/discrete_stochastic_processes_lecture_notes_mit_6_262-2024.3.2.0-py3-none-any.whl/DiscreteStochasticProcessesLectureNotesMit6262/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""discrete-stochastic-processes-lecture-notes-mit-6-262
+https://github.com/apachecn/discrete-stochastic-processes-lecture-notes-mit-6-262"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
