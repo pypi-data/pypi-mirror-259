382c2fc3f2d4d8ae1a824a6b3287006cc0f2378f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:43 +0000

    Add package PyUtilToolKit/1.0.5/PyUtilToolKit-1.0.5-py3-none-any.whl

---
+++ packages/PyUtilToolKit/PyUtilToolKit-1.0.5-py3-none-any.whl/PyUtilToolKitDev/database/database_design/database_insert.py
@@ -0,0 +1,67 @@
+# -*- coding:utf-8 -*-
+"""
+# File       : database_insert.py
+# Time       ：2023/1/21
+# Author     ：lyz
+# version    ：python 3
+# Description：
+"""
+import datetime
+import gc
+
+from faker import Faker
+
+from PyUtilToolKit.database.database_design.database_base import DbOperation
+'''
+sql语句   根据实际情况修改语句
+'''
+sql = "insert into mysql_test_database_2023_1_18.test1(_random_int,_address,_ssn,_company,_credit_card_full,_name,_phone_number," \
+      "_ascii_company_email,_domain_name,_chrome,_text,_password,_date_object,_profile) values(%s,%s,%s,%s,%s,%s" \
+      ",%s,%s,%s,%s,%s,%s,%s,%s)"
+f = Faker(locale='zh_CN')
+# "insert into table1(_varchar,_number,_float,_time,_DECIMAL) values(%s,%s,%s,%s,%s)"
+def func_insert(class_feature,sql,flag=0):
+    '''
+    插入函数
+    :param class_featuree:实例化数据库类
+    :param flag:记录次数
+    :param sql:插入语句
+    '''
+    startTime = datetime.datetime.now()
+    for i in range(1000000):  # 1.设置插入次数
+        data_list_all = []  # 存储所有字段值
+        startTime1 = datetime.datetime.now()    # 每一次插入用时 起始
+        for n in range(10):  # 2.批量插入数量
+            data_list = []  # 存储字段值
+            data_list.extend((int(f.int()),f.address(),f.ssn(),f.company(),f.credit_card_full()
+                ,f.name(),int(f.phone_number()),f.ascii_company_email(),f.domain_name()
+                ,f.chrome(),f.text(),f.password(),f.date_object(),str(f.profile())))   # 添加字段值
+            data_list_all.append(data_list) # 添加所有字段值
+            flag = flag + 1 # 总数
+        else:
+            # 3.插入语句
+            # class_feature.insert_data("insert into table1(_varchar,_number,_float,_time,_DECIMAL) values(%s,%s,%s,%s,%s)",
+            #                data_list_all)
+            class_feature.insert_data(sql,data_list_all)
+            print("总数量：{}".format(flag))
+            del data_list   # 删除变量
+            gc.collect()    # 回收空间
+            endTime1 = datetime.datetime.now()  # 每一次插入用时 结束
+        print('第{}轮新增完毕，耗时：'.format(i + 1), endTime1 - startTime1)
+        endTime = datetime.datetime.now()
+        print('已耗时:', endTime - startTime, '\n')
+        # if do.select_data("select _bigint from table5 where _bigint = {}".format(num3)):
+        #     print("数量：{}".format(flag))
+        #     flag = flag + 1
+        # print("数量：{}".format(flag))
+        del data_list_all   # 删除变量
+        gc.collect()    # 回收空间
+    else:
+        print('新增完毕')
+    endTime = datetime.datetime.now()   # 设置结束插入时间
+    print('总耗时:', endTime - startTime)
+
+
+if __name__ == '__main__':
+    do = DbOperation('localhost','mysql_test_database_2023_1_18')
+    func_insert(do,sql)
---
+++ packages/PyUtilToolKit/PyUtilToolKit-1.0.5-py3-none-any.whl/PyUtilToolKitDev/login_base.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+# File       : login_base.py
+# Time       ：2023/10/21
+# Author     ：lyz
+# version    ：python 3
+# Description：
+"""
+from datetime import datetime
+from yiXie_1_config.hostUrl.hostUrl_login import *
+from yiXie_1_config.tokenConfig.token_manage import WriteYaml
+from PyUtilToolKit.log import Info
+import requests
+
+from yiXie_3_func.xuanHuMini_func.woDe_func.geRenXinXi_func import GeRenXinXi
+
+
+class SaasLogin:
+    def __init__(self):
+        self.session = requests.session()
+    def saaslogin(self, user, pwd, plant=None):
+        """
+        用户登录       适用于 后台/公司/监理     "houTai","firm","jianLi"
+        :param user: 获取接口用户名
+        :param pwd: 获取密码
+        :param plant: 平台（后台/公司/监理）
+        :return: 返回响应体
+        """
+        data_login = {"accountName": user, "password": pwd}
+        response = self.session.post(url=userAccountLogin, data=data_login, verify=False)   # 登录请求
+        param = {"callback": "{}".format(orgg)}
+        requests.get(url=container, params=param, headers=self.session.cookies)   # 登录重定向请求
+        response.raise_for_status()  # 如果请求不成功，抛出异常
+        token(response, plant) if plant is not None else True       # 如果 plant存在，获取token
+        Info.log("Token过期时间:{}".format(datetime.fromtimestamp(response.json()["data"]["expireDate"] / 1000)))
+        Info.log(f"Login success! {userAccountLogin} & {container}")
+        tokenValue = response.json()["data"]["tokenValue"]      # 获取登录接口响应体中的token值
+        self.session.headers.update({"token": tokenValue})      # 在session中添加token
+        return self.session
+
+# class YykWebLogin:
+#     def __init__(self):
+#         self.session = requests.session()
+#     def yykweblogin(self, user, pwd, plant=None):
+#         """
+#         用户登录       适用于 云药客web        "yykWeb"                       # 需要优化，只是将上面复制下来了
+#         :param user: 获取接口用户名
+#         :param pwd: 获取密码
+#         :param plant: 平台（云药客web）
+#         :return: 返回响应体
+#         """
+#         url = HostEnv().host_login("user") + login_url["login"]  # host_("xxx") 在 config.py中配置
+#         data_login = {"accountName": user, "password": pwd}
+#         response = self.session.post(url, data=data_login)
+#         response.raise_for_status()  # 如果请求不成功，抛出异常
+#         token(response, plant) if plant is not None else True  # 如果 plant存在，获取token
+#         Info.log("Token过期时间:{}".format(datetime.fromtimestamp(response.json()["data"]["expireDate"] / 1000)))
+#         # Info.log(f"Url {userAccountLogin} & {container} login success!")
+#         tokenValue = response.json()["data"]["tokenValue"]  # 获取登录接口响应体中的token值
+#         self.session.headers.update({"token": tokenValue})  # 在session中添加token
+#         return self.session
+class XuanHuLogin:
+    def __init__(self):
+        self.session = requests.session()
+    def xuanHuMiniLogin(self, user, pwd, plant=None):
+        """
+        用户登录       适用于 悬壶小程序            "xuanHuMini"
+        :param user: 获取接口用户名
+        :param pwd: 获取密码
+        :param plant: 平台（悬壶）
+        :return: 返回响应体
+        """
+        data_login = {"unionId": user, "signature": pwd}
+        response = self.session.post(url=userWechatMiniLogin, data=data_login, verify=False)
+        response.raise_for_status()  # 如果请求不成功，抛出异常
+        token(response, plant) if plant is not None else True  # 如果 plant存在，获取token
+        Info.log("Token过期时间:{}".format(datetime.fromtimestamp(response.json()["data"]["expireDate"] / 1000)))
+        Info.log(f"Login success! {userWechatMiniLogin}")
+        tokenValue = response.json()["data"]["tokenValue"]  # 获取登录接口响应体中的token值
+        self.session.headers.update({"token": tokenValue})  # 在session中添加token
+        return self.session
+
+class YykAppLogin:
+    def __init__(self):
+        self.session = requests.session()
+    # def yykapplogin(self, user, pwd, plant=None):
+    #     """
+    #     用户登录       适用于 云药客app       "yykApp"                        # 需要优化，只是将上面复制下来了
+    #     :param user: 获取接口用户名
+    #     :param pwd: 获取密码
+    #     :param plant: 平台（云药客app）
+    #     :return: 返回响应体
+    #     """
+    #     url = HostEnv().host_login("user") + login_url["login"]  # host_("xxx") 在 config.py中配置
+    #     data_login = {  # body 参数
+    #         "accountName": user,
+    #         "password": pwd
+    #     }
+    #     response = self.session.post(url, data=data_login)
+    #     response.raise_for_status()  # 如果请求不成功，抛出异常
+    #     token(response, plant) if plant is not None else True  # 如果 plant存在，获取token
+    #     Info.log("Token过期时间:{}".format(datetime.fromtimestamp(response.json()["data"]["expireDate"] / 1000)))
+    #     # Info.log(f"Url {userWechatMiniLogin} login success!")
+    #     tokenValue = response.json()["data"]["tokenValue"]  # 获取登录接口响应体中的token值
+    #     self.session.headers.update({"token": tokenValue})  # 在session中添加token
+    #     return self.session
+class TimedTaskLogin:
+    def __init__(self):
+        self.session = requests.session()
+    def timedTaskLogin(self, user, pwd):
+        """
+        用户登录       适用于 定时任务     "timedTask"
+        :param user: 获取接口用户名
+        :param pwd:  获取密码
+        :param plant: 平台 定时任务
+        :return: 返回响应体
+        """
+        data_login = {"userName": user, "password": pwd}
+        response = self.session.post(url=timedTaskLogin, data=data_login, verify=False)
+        requests.get(url=timedTaskToLogin, headers=self.session.cookies)
+        response.raise_for_status()     # 如果请求不成功，抛出异常
+        Info.log(f"Login success! {timedTaskLogin} & {timedTaskToLogin}")
+        return self.session
+
+def token(res, plant):
+    '''
+    将登录的token写入yaml文件中
+    :param res: 返回值
+    :param plant: 后台？公司？监理？等...
+    :return:
+    '''
+    if (plant == "houTai" or plant == "firm" or plant == "jianLi" or plant == "houTai" or plant == "yykWeb"
+            or plant == "xuanHuMini" or plant == "xuanHuWeb" or plant == "yykApp" or plant == "timedTask"):
+        res_token = res.json()["data"]["tokenValue"]  # 获取 token
+        WriteYaml(plant, res_token)  # 将 token 写入yaml文件
+        Info.log(f'token_{plant}值写入yaml文件，成功！')
+    else:
+        raise Exception('请输入:正确名称!')
+
+if __name__ == '__main__':
+    a = XuanHuLogin().xuanHuMiniLogin("obGrgv2jyS-Z48-US8Gnhem_4kSI","4117C1A5E2B35F193F3990361EDAB27E")
+    GeRenXinXi(a).user_base_info()
