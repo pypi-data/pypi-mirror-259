e7f5ed9d268121fd9c3018b270a3af46839eb991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:25 +0000

    Add package c-yuyan-jichujiaocheng-lellansin/2024.3.2.0/c_yuyan_jichujiaocheng_lellansin-2024.3.2.0-py3-none-any.whl

---
+++ packages/c-yuyan-jichujiaocheng-lellansin/c_yuyan_jichujiaocheng_lellansin-2024.3.2.0-py3-none-any.whl/CYuyanJichujiaochengLellansin/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""c-yuyan-jichujiaocheng-lellansin
+https://github.com/apachecn/c-yuyan-jichujiaocheng-lellansin"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
