2046b23461a510c134adc89b359a32a58b34449a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:30 +0000

    Add package coincurve/19.0.1/coincurve-19.0.1.tar.gz

---
+++ packages/coincurve/coincurve-19.0.1.tar.gz/coincurve-19.0.1/setup_support.py
@@ -0,0 +1,97 @@
+import glob
+import os
+import shutil
+import subprocess
+from contextlib import contextmanager, suppress
+from tempfile import mkdtemp
+
+
+@contextmanager
+def workdir():
+    cwd = os.getcwd()
+    tmpdir = mkdtemp()
+    os.chdir(tmpdir)
+    try:
+        yield
+    finally:
+        os.chdir(cwd)
+        shutil.rmtree(tmpdir)
+
+
+@contextmanager
+def redirect(stdchannel, dest_filename):
+    oldstdchannel = os.dup(stdchannel.fileno())
+    dest_file = open(dest_filename, 'w')
+    os.dup2(dest_file.fileno(), stdchannel.fileno())
+    try:
+        yield
+    finally:
+        if oldstdchannel is not None:
+            os.dup2(oldstdchannel, stdchannel.fileno())
+        if dest_file is not None:
+            dest_file.close()
+
+
+def absolute(*paths):
+    op = os.path
+    return op.realpath(op.abspath(op.join(op.dirname(__file__), *paths)))
+
+
+def build_flags(library, type_, path):
+    """Return separated build flags from pkg-config output"""
+
+    pkg_config_path = [path]
+    if 'PKG_CONFIG_PATH' in os.environ:
+        pkg_config_path.append(os.environ['PKG_CONFIG_PATH'])
+    if 'LIB_DIR' in os.environ:
+        pkg_config_path.append(os.environ['LIB_DIR'])
+        pkg_config_path.append(os.path.join(os.environ['LIB_DIR'], 'pkgconfig'))
+
+    options = {'I': '--cflags-only-I', 'L': '--libs-only-L', 'l': '--libs-only-l'}
+    env = dict(os.environ, PKG_CONFIG_PATH=':'.join(pkg_config_path))
+    flags = subprocess.check_output(['pkg-config', '--static', options[type_], library], env=env)  # noqa S603
+    flags = list(flags.decode('UTF-8').split())
+
+    return [flag.strip(f'-{type_}') for flag in flags]
+
+
+def _find_lib():
+    if 'COINCURVE_IGNORE_SYSTEM_LIB' in os.environ:
+        return False
+
+    from cffi import FFI
+
+    try:
+        subprocess.check_output(['pkg-config', '--exists', 'libsecp256k1'])  # noqa S603
+
+        includes = subprocess.check_output(['pkg-config', '--cflags-only-I', 'libsecp256k1'])  # noqa S603
+        includes = includes.strip().decode('utf-8')
+
+        return os.path.exists(os.path.join(includes[2:], 'secp256k1_ecdh.h'))
+
+    except (OSError, subprocess.CalledProcessError):
+        if 'LIB_DIR' in os.environ:
+            for path in glob.glob(os.path.join(os.environ['LIB_DIR'], '*secp256k1*')):
+                with suppress(OSError):
+                    FFI().dlopen(path)
+                    return True
+        # We couldn't locate libsecp256k1, so we'll use the bundled one
+        return False
+
+
+_has_system_lib = None
+
+
+def has_system_lib():
+    global _has_system_lib
+    if _has_system_lib is None:
+        _has_system_lib = _find_lib()
+    return _has_system_lib
+
+
+def detect_dll():
+    here = os.path.dirname(os.path.abspath(__file__))
+    for fn in os.listdir(os.path.join(here, 'coincurve')):
+        if fn.endswith('.dll'):
+            return True
+    return False
