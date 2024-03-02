2ebdeb38a0a4e7cb9093e454de0d0b29db49d301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:53 +0000

    Add package tansuo-es2016-yu-es2017/2024.3.2.0/tansuo-es2016-yu-es2017-2024.3.2.0.tar.gz

---
+++ packages/tansuo-es2016-yu-es2017/tansuo-es2016-yu-es2017-2024.3.2.0.tar.gz/tansuo-es2016-yu-es2017-2024.3.2.0/TansuoEs2016YuEs2017/README.md
@@ -0,0 +1,27 @@
+# 探索 ES2016 与 ES2017
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/tansuo-es2016-yu-es2017
+docker run -tid -p <port>:80 apachecn0/tansuo-es2016-yu-es2017
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install tansuo-es2016-yu-es2017
+tansuo-es2016-yu-es2017 <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g tansuo-eserlingyiliu-yu-eserlingyiqi
+tansuo-eserlingyiliu-yu-eserlingyiqi <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/tansuo-es2016-yu-es2017/tansuo-es2016-yu-es2017-2024.3.2.0.tar.gz/tansuo-es2016-yu-es2017-2024.3.2.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import TansuoEs2016YuEs2017
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('TansuoEs2016YuEs2017'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="tansuo-es2016-yu-es2017",
+    version=TansuoEs2016YuEs2017.__version__,
+    url="https://github.com/apachecn/tansuo-es2016-yu-es2017",
+    author=TansuoEs2016YuEs2017.__author__,
+    author_email=TansuoEs2016YuEs2017.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="探索 ES2016 与 ES2017",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "tansuo-es2016-yu-es2017=TansuoEs2016YuEs2017.__main__:main",
+            "TansuoEs2016YuEs2017=TansuoEs2016YuEs2017.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
