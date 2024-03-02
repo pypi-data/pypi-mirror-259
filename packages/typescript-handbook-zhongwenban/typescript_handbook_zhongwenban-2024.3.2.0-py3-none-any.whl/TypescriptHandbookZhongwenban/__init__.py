7b9f41468fa165988df1c6048f62363e62b78ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:20 +0000

    Add package typescript-handbook-zhongwenban/2024.3.2.0/typescript_handbook_zhongwenban-2024.3.2.0-py3-none-any.whl

---
+++ packages/typescript-handbook-zhongwenban/typescript_handbook_zhongwenban-2024.3.2.0-py3-none-any.whl/TypescriptHandbookZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""typescript-handbook-zhongwenban
+https://github.com/apachecn/typescript-handbook-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
