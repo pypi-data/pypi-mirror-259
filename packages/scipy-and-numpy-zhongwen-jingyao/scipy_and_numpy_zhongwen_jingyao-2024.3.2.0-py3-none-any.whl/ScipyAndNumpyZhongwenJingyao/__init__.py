ca5833304e42ccd2bb17f4cda8541a49fe3020fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:56 +0000

    Add package scipy-and-numpy-zhongwen-jingyao/2024.3.2.0/scipy_and_numpy_zhongwen_jingyao-2024.3.2.0-py3-none-any.whl

---
+++ packages/scipy-and-numpy-zhongwen-jingyao/scipy_and_numpy_zhongwen_jingyao-2024.3.2.0-py3-none-any.whl/ScipyAndNumpyZhongwenJingyao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""scipy-and-numpy-zhongwen-jingyao
+https://github.com/apachecn/scipy-and-numpy-zhongwen-jingyao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
