e77640d028612c32bc772c158a844f1545086798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:50 +0000

    Add package street-fighting-mathematics-lecture-notes-mit-6-009/2024.3.2.0/street_fighting_mathematics_lecture_notes_mit_6_009-2024.3.2.0-py3-none-any.whl

---
+++ packages/street-fighting-mathematics-lecture-notes-mit-6-009/street_fighting_mathematics_lecture_notes_mit_6_009-2024.3.2.0-py3-none-any.whl/StreetFightingMathematicsLectureNotesMit6009/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""street-fighting-mathematics-lecture-notes-mit-6-009
+https://github.com/apachecn/street-fighting-mathematics-lecture-notes-mit-6-009"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
