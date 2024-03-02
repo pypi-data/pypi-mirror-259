97607ef1ae4020f814ce19f62b63382441be6acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:39 +0000

    Add package visualtorch/0.2.1/visualtorch-0.2.1-py3-none-any.whl

---
+++ packages/visualtorch/visualtorch-0.2.1-py3-none-any.whl/visualtorch/__init__.py
@@ -0,0 +1,9 @@
+"""Modules for pytorch model visualization."""
+
+# Copyright (C) 2024 Willy Fitra Hendria
+# SPDX-License-Identifier: MIT
+
+from visualtorch.layered import layered_view
+from visualtorch.graph import graph_view
+
+__all__ = ["layered_view", "graph_view"]
