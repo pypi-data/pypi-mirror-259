f6bfb6544cad4138760cdac14ce55b5bc6d3ff71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:13 +0000

    Add package programming-practice-tutorials-kaist-cs109/2024.3.2.0/programming_practice_tutorials_kaist_cs109-2024.3.2.0-py3-none-any.whl

---
+++ packages/programming-practice-tutorials-kaist-cs109/programming_practice_tutorials_kaist_cs109-2024.3.2.0-py3-none-any.whl/ProgrammingPracticeTutorialsKaistCs109/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""programming-practice-tutorials-kaist-cs109
+https://github.com/apachecn/programming-practice-tutorials-kaist-cs109"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
