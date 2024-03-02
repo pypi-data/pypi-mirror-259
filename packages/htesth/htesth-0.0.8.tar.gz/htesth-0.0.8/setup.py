6288454b32dc05166ca5ce4e74553d17b0cc666e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:36 +0000

    Add package htesth/0.0.8/htesth-0.0.8.tar.gz

---
+++ packages/htesth/htesth-0.0.8.tar.gz/htesth-0.0.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='htesth',
+    version='0.0.8',
+    packages=find_packages(where="src"),
+    author='melong',
+    description='This is a test package',
+    python_requires='~=3.6',
+)
---
+++ packages/htesth/htesth-0.0.8.tar.gz/htesth-0.0.8/src/__init__.py
@@ -0,0 +1,5 @@
+from .main import main
+from .htesth import htest
+
+def say_hello(name: str):
+    print(f'Hello {name}!')
