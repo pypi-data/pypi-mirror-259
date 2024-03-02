349d54b3bf580c851f0f195d090572df6b7e091a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:09 +0000

    Add package programming-languages-and-logics-lecture-notes-cornell-cs4110/2024.3.2.0/programming-languages-and-logics-lecture-notes-cornell-cs4110-2024.3.2.0.tar.gz

---
+++ packages/programming-languages-and-logics-lecture-notes-cornell-cs4110/programming-languages-and-logics-lecture-notes-cornell-cs4110-2024.3.2.0.tar.gz/programming-languages-and-logics-lecture-notes-cornell-cs4110-2024.3.2.0/ProgrammingLanguagesAndLogicsLectureNotesCornellCs4110/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""programming-languages-and-logics-lecture-notes-cornell-cs4110
+https://github.com/apachecn/programming-languages-and-logics-lecture-notes-cornell-cs4110"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
