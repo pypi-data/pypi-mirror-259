03504ec1ec5b897d8357ae4e9ca901e1e1516060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:53 +0000

    Add package openctp-ctpopt/3.6.3/openctp_ctpopt-3.6.3-cp311-cp311-win_amd64.whl

---
+++ packages/openctp-ctpopt/openctp_ctpopt-3.6.3-cp311-cp311-win_amd64.whl/openctp_ctpopt/__init__.py
@@ -0,0 +1,14 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_5_2():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'openctp_ctpopt.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_5_2()
+del _delvewheel_patch_1_5_2
+# end delvewheel patch
+
+from . import thostmduserapi as mdapi
+from . import thosttraderapi as tdapi
