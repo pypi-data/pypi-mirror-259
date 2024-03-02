c92968e8d968b363f3a05886019b91a68c5fb2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:38 +0000

    Add package deep-residual-learning-for-image-recognition-qiyue-zaixian-dl-fanyi-zu-2017-4/2024.3.2.0/deep_residual_learning_for_image_recognition_qiyue_zaixian_dl_fanyi_zu_2017_4-2024.3.2.0-py3-none-any.whl

---
+++ packages/deep-residual-learning-for-image-recognition-qiyue-zaixian-dl-fanyi-zu-2017-4/deep_residual_learning_for_image_recognition_qiyue_zaixian_dl_fanyi_zu_2017_4-2024.3.2.0-py3-none-any.whl/DeepResidualLearningForImageRecognitionQiyueZaixianDlFanyiZu20174/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""deep-residual-learning-for-image-recognition-qiyue-zaixian-dl-fanyi-zu-2017-4
+https://github.com/apachecn/deep-residual-learning-for-image-recognition-qiyue-zaixian-dl-fanyi-zu-2017-4"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
