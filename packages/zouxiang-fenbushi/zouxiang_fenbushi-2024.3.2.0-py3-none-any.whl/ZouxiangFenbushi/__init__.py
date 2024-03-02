1b35f892787925faa6d8ab4e0d9eacbc1493fc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:05:54 +0000

    Add package zouxiang-fenbushi/2024.3.2.0/zouxiang_fenbushi-2024.3.2.0-py3-none-any.whl

---
+++ packages/zouxiang-fenbushi/zouxiang_fenbushi-2024.3.2.0-py3-none-any.whl/ZouxiangFenbushi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zouxiang-fenbushi
+https://github.com/apachecn/zouxiang-fenbushi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
