67d1c3a3befaa2d0af99e38ea897c9a2dba1c08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:19 +0000

    Add package weixin-gongzhong-pingtai-kaifa-wendang/2024.3.2.0/weixin-gongzhong-pingtai-kaifa-wendang-2024.3.2.0.tar.gz

---
+++ packages/weixin-gongzhong-pingtai-kaifa-wendang/weixin-gongzhong-pingtai-kaifa-wendang-2024.3.2.0.tar.gz/weixin-gongzhong-pingtai-kaifa-wendang-2024.3.2.0/WeixinGongzhongPingtaiKaifaWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""weixin-gongzhong-pingtai-kaifa-wendang
+https://github.com/apachecn/weixin-gongzhong-pingtai-kaifa-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
