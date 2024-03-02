1fe92f390e895a4d9d13287e28c8e622ea92afef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:19 +0000

    Add package KMIS/0.2.7/KMIS-0.2.7.tar.gz

---
+++ packages/KMIS/KMIS-0.2.7.tar.gz/KMIS-0.2.7/bikmis/__init__.py
@@ -0,0 +1,17 @@
+#######################
+# BI library for KMIS #
+#######################
+
+import datetime
+
+import traceback
+import sys
+import pandas as pd
+import time
+
+
+
+
+
+def calc(num1, num2):
+    return num1 + num2
