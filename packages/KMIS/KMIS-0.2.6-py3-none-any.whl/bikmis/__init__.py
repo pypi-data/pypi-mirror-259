2c357a79859217aea3a6946f9ff577548c3120fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:13 +0000

    Add package KMIS/0.2.6/KMIS-0.2.6-py3-none-any.whl

---
+++ packages/KMIS/KMIS-0.2.6-py3-none-any.whl/bikmis/__init__.py
@@ -0,0 +1,18 @@
+#######################
+# BI library for KMIS #
+#######################
+
+import datetime
+
+import traceback
+import sys
+import pandas as pd
+import time
+import pandera as pa
+import pyspark.sql.types as T
+
+
+
+
+def calc(num1, num2):
+    return num1 + num2
