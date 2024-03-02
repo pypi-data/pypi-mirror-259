b5f70343b75e5b88476ee453c2c56b6a5c3a4d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:42 +0000

    Add package loudong-liyong-de-yishu-gongji-javascript-yinqing/2024.3.2.0/loudong-liyong-de-yishu-gongji-javascript-yinqing-2024.3.2.0.tar.gz

---
+++ packages/loudong-liyong-de-yishu-gongji-javascript-yinqing/loudong-liyong-de-yishu-gongji-javascript-yinqing-2024.3.2.0.tar.gz/loudong-liyong-de-yishu-gongji-javascript-yinqing-2024.3.2.0/LoudongLiyongDeYishuGongjiJavascriptYinqing/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""loudong-liyong-de-yishu-gongji-javascript-yinqing
+https://github.com/apachecn/loudong-liyong-de-yishu-gongji-javascript-yinqing"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
