2f75eb45060a8e5a2d71774774cd115c15319ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:50 +0000

    Add package git-community-book-zhongwenban/2024.3.2.0/git_community_book_zhongwenban-2024.3.2.0-py3-none-any.whl

---
+++ packages/git-community-book-zhongwenban/git_community_book_zhongwenban-2024.3.2.0-py3-none-any.whl/GitCommunityBookZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""git-community-book-zhongwenban
+https://github.com/apachecn/git-community-book-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
