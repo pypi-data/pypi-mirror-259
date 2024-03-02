8efb2b81b0fb7a3e71bc8e5881c11d6624b2f54f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:45 +0000

    Add package sql-for-web-nerds/2024.3.2.0/sql_for_web_nerds-2024.3.2.0-py3-none-any.whl

---
+++ packages/sql-for-web-nerds/sql_for_web_nerds-2024.3.2.0-py3-none-any.whl/SqlForWebNerds/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sql-for-web-nerds
+https://github.com/apachecn/sql-for-web-nerds"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
