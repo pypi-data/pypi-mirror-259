0917f190f3542d0c4117695492f107e40663d786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:55 +0000

    Add package introduction-to-electric-power-systems-lecture-notes-mit-6-061/2024.3.2.0/introduction_to_electric_power_systems_lecture_notes_mit_6_061-2024.3.2.0-py3-none-any.whl

---
+++ packages/introduction-to-electric-power-systems-lecture-notes-mit-6-061/introduction_to_electric_power_systems_lecture_notes_mit_6_061-2024.3.2.0-py3-none-any.whl/IntroductionToElectricPowerSystemsLectureNotesMit6061/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""introduction-to-electric-power-systems-lecture-notes-mit-6-061
+https://github.com/apachecn/introduction-to-electric-power-systems-lecture-notes-mit-6-061"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
