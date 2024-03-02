5f7027cc071cb1a107236f66e6155932b9167efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:50 +0000

    Add package yingyong-gailv-tongji-jichu-ji-suanfa-yujiangsheng/2024.3.2.0/yingyong_gailv_tongji_jichu_ji_suanfa_yujiangsheng-2024.3.2.0-py3-none-any.whl

---
+++ packages/yingyong-gailv-tongji-jichu-ji-suanfa-yujiangsheng/yingyong_gailv_tongji_jichu_ji_suanfa_yujiangsheng-2024.3.2.0-py3-none-any.whl/YingyongGailvTongjiJichuJiSuanfaYujiangsheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyong-gailv-tongji-jichu-ji-suanfa-yujiangsheng
+https://github.com/apachecn/yingyong-gailv-tongji-jichu-ji-suanfa-yujiangsheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
