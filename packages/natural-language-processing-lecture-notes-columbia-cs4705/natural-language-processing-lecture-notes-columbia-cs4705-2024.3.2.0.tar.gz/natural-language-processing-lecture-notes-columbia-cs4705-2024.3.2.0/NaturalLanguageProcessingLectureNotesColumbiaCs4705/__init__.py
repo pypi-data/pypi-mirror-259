4dd1f84e7c57df9b936024c28d5d280a9f0a505d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:41 +0000

    Add package natural-language-processing-lecture-notes-columbia-cs4705/2024.3.2.0/natural-language-processing-lecture-notes-columbia-cs4705-2024.3.2.0.tar.gz

---
+++ packages/natural-language-processing-lecture-notes-columbia-cs4705/natural-language-processing-lecture-notes-columbia-cs4705-2024.3.2.0.tar.gz/natural-language-processing-lecture-notes-columbia-cs4705-2024.3.2.0/NaturalLanguageProcessingLectureNotesColumbiaCs4705/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""natural-language-processing-lecture-notes-columbia-cs4705
+https://github.com/apachecn/natural-language-processing-lecture-notes-columbia-cs4705"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
