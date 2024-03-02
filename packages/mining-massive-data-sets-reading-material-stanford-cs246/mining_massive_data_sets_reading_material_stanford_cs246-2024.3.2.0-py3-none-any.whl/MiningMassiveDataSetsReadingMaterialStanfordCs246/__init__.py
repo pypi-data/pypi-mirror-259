189c9044a5ab0a5303bd17bdbb424ed1c48ed6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:15 +0000

    Add package mining-massive-data-sets-reading-material-stanford-cs246/2024.3.2.0/mining_massive_data_sets_reading_material_stanford_cs246-2024.3.2.0-py3-none-any.whl

---
+++ packages/mining-massive-data-sets-reading-material-stanford-cs246/mining_massive_data_sets_reading_material_stanford_cs246-2024.3.2.0-py3-none-any.whl/MiningMassiveDataSetsReadingMaterialStanfordCs246/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mining-massive-data-sets-reading-material-stanford-cs246
+https://github.com/apachecn/mining-massive-data-sets-reading-material-stanford-cs246"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
