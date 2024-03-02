f784584467ebe72464dde5ab62577e6c1da89b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:16 +0000

    Add package KMIS/0.2.3/KMIS-0.2.3.tar.gz

---
+++ packages/KMIS/KMIS-0.2.3.tar.gz/KMIS-0.2.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup, extension
+
+with open("bikmis/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="KMIS",
+    version="0.2.3",
+    description="My own tester",
+    package_dir={"": "bikmis"},
+    packages=find_packages(where="bikmis"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/KMIS-BI/0.1.0/",
+    author="marast",
+    author_email="havlicek.mara@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["bson >= 0.5.10"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+)
