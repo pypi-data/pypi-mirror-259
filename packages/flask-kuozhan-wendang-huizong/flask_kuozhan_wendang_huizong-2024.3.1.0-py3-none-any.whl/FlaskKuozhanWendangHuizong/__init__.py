c937aa13f4708233a7f5e431254911c1d98538ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:04:49 +0000

    Add package flask-kuozhan-wendang-huizong/2024.3.1.0/flask_kuozhan_wendang_huizong-2024.3.1.0-py3-none-any.whl

---
+++ packages/flask-kuozhan-wendang-huizong/flask_kuozhan_wendang_huizong-2024.3.1.0-py3-none-any.whl/FlaskKuozhanWendangHuizong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""flask-kuozhan-wendang-huizong
+https://github.com/apachecn/flask-kuozhan-wendang-huizong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
