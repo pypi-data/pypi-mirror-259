b654b78ecf336477fd44dd97a075b9ab7eaaafff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:44 +0000

    Add package gitoptim/0.1.5/gitoptim-0.1.5-py3-none-any.whl

---
+++ packages/gitoptim/gitoptim-0.1.5-py3-none-any.whl/gitoptim/services/gitlab_api.py
@@ -0,0 +1,13 @@
+import gitlab
+
+from gitoptim.utils import EnvironmentVariables
+
+
+class GitlabAPIFactory:
+    @staticmethod
+    def with_job_token():
+        base_url = EnvironmentVariables().gitlab_server_url.split(":")[1]
+        return gitlab.Gitlab(
+            base_url,
+            job_token=EnvironmentVariables().job_token,
+        )
