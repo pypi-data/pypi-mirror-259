dc6deee00df0b0b852f2244242c6406b27cda9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:36 +0000

    Add package CTDProcessingPackage/1.7.4/CTDProcessingPackage-1.7.4.tar.gz

---
+++ packages/CTDProcessingPackage/CTDProcessingPackage-1.7.4.tar.gz/CTDProcessingPackage-1.7.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CTDProcessingPackage',
+    version='1.7.4',
+    packages=['ctdmodule'],
+    url='',
+    license='',
+    author='Rafael S. Bittencourt',
+    author_email='rafael.bittencourt.2002@gmail.com',
+    description='Package for pre-processing CTD data'
+)
