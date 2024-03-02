6aa0e91fb5889a37305fab24dc17f5ac5f38326a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:47 +0000

    Add package information-and-entropy-mit-6-050j/2024.3.2.0/information-and-entropy-mit-6-050j-2024.3.2.0.tar.gz

---
+++ packages/information-and-entropy-mit-6-050j/information-and-entropy-mit-6-050j-2024.3.2.0.tar.gz/information-and-entropy-mit-6-050j-2024.3.2.0/InformationAndEntropyMit6050j/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""information-and-entropy-mit-6-050j
+https://github.com/apachecn/information-and-entropy-mit-6-050j"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
