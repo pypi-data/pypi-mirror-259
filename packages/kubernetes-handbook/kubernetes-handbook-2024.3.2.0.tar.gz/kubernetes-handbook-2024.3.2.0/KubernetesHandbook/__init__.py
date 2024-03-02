5668b963181c03312f08329082bc3e6a77cc8f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:49 +0000

    Add package kubernetes-handbook/2024.3.2.0/kubernetes-handbook-2024.3.2.0.tar.gz

---
+++ packages/kubernetes-handbook/kubernetes-handbook-2024.3.2.0.tar.gz/kubernetes-handbook-2024.3.2.0/KubernetesHandbook/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kubernetes-handbook
+https://github.com/apachecn/kubernetes-handbook"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
