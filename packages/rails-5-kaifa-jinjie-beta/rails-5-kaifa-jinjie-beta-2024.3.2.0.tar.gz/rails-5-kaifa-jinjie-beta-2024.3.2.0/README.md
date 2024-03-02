0f2e776ea129af41d122e3874470bd470042b7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:38 +0000

    Add package rails-5-kaifa-jinjie-beta/2024.3.2.0/rails-5-kaifa-jinjie-beta-2024.3.2.0.tar.gz

---
+++ packages/rails-5-kaifa-jinjie-beta/rails-5-kaifa-jinjie-beta-2024.3.2.0.tar.gz/rails-5-kaifa-jinjie-beta-2024.3.2.0/README.md
@@ -0,0 +1,27 @@
+# Rails 5 开发进阶(Beta)
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/rails-5-kaifa-jinjie-beta
+docker run -tid -p <port>:80 apachecn0/rails-5-kaifa-jinjie-beta
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install rails-5-kaifa-jinjie-beta
+rails-5-kaifa-jinjie-beta <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g rails-5-kaifa-jinjie-beta
+rails-5-kaifa-jinjie-beta <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/rails-5-kaifa-jinjie-beta/rails-5-kaifa-jinjie-beta-2024.3.2.0.tar.gz/rails-5-kaifa-jinjie-beta-2024.3.2.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Rails5KaifaJinjieBeta
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('Rails5KaifaJinjieBeta'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="rails-5-kaifa-jinjie-beta",
+    version=Rails5KaifaJinjieBeta.__version__,
+    url="https://github.com/apachecn/rails-5-kaifa-jinjie-beta",
+    author=Rails5KaifaJinjieBeta.__author__,
+    author_email=Rails5KaifaJinjieBeta.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="Rails 5 开发进阶(Beta)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "rails-5-kaifa-jinjie-beta=Rails5KaifaJinjieBeta.__main__:main",
+            "Rails5KaifaJinjieBeta=Rails5KaifaJinjieBeta.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
