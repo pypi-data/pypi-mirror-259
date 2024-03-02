f99178e735b10a35ec31703b252600d01b741e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:12 +0000

    Add package hadoop-biji-pennywong/2024.3.2.0/hadoop_biji_pennywong-2024.3.2.0-py3-none-any.whl

---
+++ packages/hadoop-biji-pennywong/hadoop_biji_pennywong-2024.3.2.0-py3-none-any.whl/HadoopBijiPennywong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hadoop-biji-pennywong
+https://github.com/apachecn/hadoop-biji-pennywong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
