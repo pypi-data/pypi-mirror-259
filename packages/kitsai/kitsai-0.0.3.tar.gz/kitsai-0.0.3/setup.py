e290e5eb433c86aa4219289f9a7ca6b5122aed7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:18 +0000

    Add package kitsai/0.0.3/kitsai-0.0.3.tar.gz

---
+++ packages/kitsai/kitsai-0.0.3.tar.gz/kitsai-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name="kitsai",
+    version="0.0.3",
+    author="Blaise",
+    author_email="iahispano0@gmail.com",
+    description="Unofficial package to easily interact with the Kits.AI API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Attribution-NonCommercial 4.0 International",
+    packages=find_packages(),
+    package_data={"kitsai": ["*/*.py", "*/*/*.py"]},
+    url="https://github.com/blaise-tk/kitsai",
+    keywords=["python", "audio", "vc", "ai", "rvc", "kits"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
