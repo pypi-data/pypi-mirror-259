fe21941ca8860f085aaacde64602bf4614f83426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:41 +0000

    Add package design-and-analysis-of-algorithms-lecture-notes-mit-6-046j/2024.3.2.0/design_and_analysis_of_algorithms_lecture_notes_mit_6_046j-2024.3.2.0-py3-none-any.whl

---
+++ packages/design-and-analysis-of-algorithms-lecture-notes-mit-6-046j/design_and_analysis_of_algorithms_lecture_notes_mit_6_046j-2024.3.2.0-py3-none-any.whl/DesignAndAnalysisOfAlgorithmsLectureNotesMit6046j/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""design-and-analysis-of-algorithms-lecture-notes-mit-6-046j
+https://github.com/apachecn/design-and-analysis-of-algorithms-lecture-notes-mit-6-046j"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
