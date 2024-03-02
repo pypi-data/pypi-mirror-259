b5a4925f26698f922f9b44fe3584855c0de45a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:59 +0000

    Add package office-jiqiao-1000-li/2024.3.2.0/office_jiqiao_1000_li-2024.3.2.0-py3-none-any.whl

---
+++ packages/office-jiqiao-1000-li/office_jiqiao_1000_li-2024.3.2.0-py3-none-any.whl/OfficeJiqiao1000Li/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""office-jiqiao-1000-li
+https://github.com/apachecn/office-jiqiao-1000-li"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
