4a4f1303742a342aaefddfa05cf622d7d0c393d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:06:03 +0000

    Add package java-bingfa-de-yishu-infoq/2024.3.2.0/java-bingfa-de-yishu-infoq-2024.3.2.0.tar.gz

---
+++ packages/java-bingfa-de-yishu-infoq/java-bingfa-de-yishu-infoq-2024.3.2.0.tar.gz/java-bingfa-de-yishu-infoq-2024.3.2.0/JavaBingfaDeYishuInfoq/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-bingfa-de-yishu-infoq
+https://github.com/apachecn/java-bingfa-de-yishu-infoq"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
