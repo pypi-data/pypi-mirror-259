dbae3c49a66de1f9f7fdb0699359f726f4247ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:12 +0000

    Add package 10lineEDA/0.1.0/10lineEDA-0.1.0.tar.gz

---
+++ packages/10lineEDA/10lineEDA-0.1.0.tar.gz/10lineEDA-0.1.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="10lineEDA",
+    version="0.1.0",
+    author="may_b_prince",
+    author_email="princekminati@gmail.com",
+    packages=setuptools.find_packages(),
+)
