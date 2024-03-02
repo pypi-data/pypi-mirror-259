0b9e87bcee1853f5b438e3b750c1e371788f9950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:15 +0000

    Add package overcoming-intractability-in-machine-learning-lecture-notes-princeton-cos598d/2024.3.2.0/overcoming_intractability_in_machine_learning_lecture_notes_princeton_cos598d-2024.3.2.0-py3-none-any.whl

---
+++ packages/overcoming-intractability-in-machine-learning-lecture-notes-princeton-cos598d/overcoming_intractability_in_machine_learning_lecture_notes_princeton_cos598d-2024.3.2.0-py3-none-any.whl/OvercomingIntractabilityInMachineLearningLectureNotesPrincetonCos598d/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""overcoming-intractability-in-machine-learning-lecture-notes-princeton-cos598d
+https://github.com/apachecn/overcoming-intractability-in-machine-learning-lecture-notes-princeton-cos598d"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
