39e273ff91efa7fc3b2e2bd91f618bc853332dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:38 +0000

    Add package webdriver-auto-update/1.2.1/webdriver_auto_update-1.2.1.tar.gz

---
+++ packages/webdriver-auto-update/webdriver_auto_update-1.2.1.tar.gz/webdriver_auto_update-1.2.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='webdriver_auto_update',
+    version='1.2.1',
+    description='A tool for managing ChromeDriver downloads and updates',
+    author='Rony Khong',
+    author_email='ronykhong77@gmail.com',
+    url='https://github.com/competencytestlvl/webdriver_auto_update',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.9',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=[
+        'auto',
+        'browser',
+        'chrome',
+        'driver',
+        'download',
+        'selenium',
+        'update',
+        'web',
+        'webdriver'],
+    install_requires=[
+        'requests',
+        'wget'],
+)
