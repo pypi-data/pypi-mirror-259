0b32b437fc3a8a1cdfe30bf71b0bc6b5eb6364fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 07:04:50 +0000

    Add package euporie/2.8.0/euporie-2.8.0.tar.gz

---
+++ packages/euporie/euporie-2.8.0.tar.gz/euporie-2.8.0/euporie/core/convert/utils.py
@@ -0,0 +1,91 @@
+"""Utility functions for format converters."""
+
+from __future__ import annotations
+
+import asyncio
+import logging
+import subprocess  # S404 - Security implications have been considered
+import tempfile
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
+log = logging.getLogger(__name__)
+
+
+async def call_subproc(
+    data: str | bytes,
+    cmd: list[Any],
+    use_tempfile: bool = False,
+    suffix: str = "",
+) -> bytes:
+    """Call the command as a subprocess and return it's output as bytes.
+
+    Args:
+        data: The data to pass to the subprocess
+        cmd: The command and arguments to call
+        use_tempfile: If True, the command saves its output to a file, not stdout
+        suffix: Suffix for the temporary file name
+
+    Returns:
+        The data printed to standard out by the subprocess.
+
+    """
+    # Convert all command arguments to strings
+    cmd = list(map(str, cmd))
+
+    # Convert data to bytes
+    if isinstance(data, str):
+        data = data.encode()
+
+    if use_tempfile:
+        # If the command cannot read from stdin, create a temporary file to pass to
+        # the command
+        tfile = tempfile.NamedTemporaryFile(delete=False, suffix=suffix)
+        tfile.write(data)
+        tfile.close()
+        cmd.append(tfile.name)
+        stdinput = None
+    else:
+        stdinput = data
+
+    log.debug("Running external command `%s`", cmd)
+    error: Exception | None = None
+    try:
+        proc = await asyncio.create_subprocess_exec(
+            *cmd,
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.DEVNULL,
+        )
+        output_bytes, _ = await proc.communicate(stdinput)
+    except FileNotFoundError as error_:
+        log.error("Could not run external command `%s`", cmd)
+        error = error_
+    except subprocess.CalledProcessError as error_:
+        log.error("There was an error while running external command `%s`", cmd)
+        error = error_
+    finally:
+        if error is not None:
+            # Generate an output stating there was an error
+            output_bytes = (
+                b"\x1b[33m"  # Set fg to yellow
+                b"\xee\x82\xb6"  # Draw left pill side
+                b"\x1b[43m\x1b[30m"  # Set fg to black, bg to yellow
+                b"\xe2\x9a\xa0"  # Draw warning symbol
+                b" Rendering Error"
+                b"\x1b[33m\x1b[49m"  # Set fg to yellow, reset bg
+                b"\xee\x82\xb4"  # Draw right pill side
+                b"\x1b[n"  # Reset style
+            )
+
+        # TODO Log any stderr
+
+        # Clean up any temporary file
+        if use_tempfile:
+            tfile.close()
+            Path(tfile.name).unlink()
+
+    return output_bytes
