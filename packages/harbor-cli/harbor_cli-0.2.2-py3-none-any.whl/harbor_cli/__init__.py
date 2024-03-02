44173f70df09d604290e7cf34f3de59cda6c952d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:10:17 +0000

    Add package harbor-cli/0.2.2/harbor_cli-0.2.2-py3-none-any.whl

---
+++ packages/harbor-cli/harbor_cli-0.2.2-py3-none-any.whl/harbor_cli/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present University of Oslo <pederhan@uio.no>
+#
+# SPDX-License-Identifier: MIT
+from __future__ import annotations
+
+from . import logs  # noreorder # configure logger first as side-effect
+from . import *
