5f354c539c579ef4a22a695631a44a018f85015b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:06:01 +0000

    Add package tutorialspoint-java-jishu-jiaocheng-20160127-fix1/2024.3.2.0/tutorialspoint_java_jishu_jiaocheng_20160127_fix1-2024.3.2.0-py3-none-any.whl

---
+++ packages/tutorialspoint-java-jishu-jiaocheng-20160127-fix1/tutorialspoint_java_jishu_jiaocheng_20160127_fix1-2024.3.2.0-py3-none-any.whl/TutorialspointJavaJishuJiaocheng20160127Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tutorialspoint-java-jishu-jiaocheng-20160127-fix1
+https://github.com/apachecn/tutorialspoint-java-jishu-jiaocheng-20160127-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
