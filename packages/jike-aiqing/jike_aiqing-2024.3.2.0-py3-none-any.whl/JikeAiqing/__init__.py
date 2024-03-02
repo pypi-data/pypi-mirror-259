4456bb72269f38ecd504625e6a2dc183375d1d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:19 +0000

    Add package jike-aiqing/2024.3.2.0/jike_aiqing-2024.3.2.0-py3-none-any.whl

---
+++ packages/jike-aiqing/jike_aiqing-2024.3.2.0-py3-none-any.whl/JikeAiqing/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jike-aiqing
+https://github.com/apachecn/jike-aiqing"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
