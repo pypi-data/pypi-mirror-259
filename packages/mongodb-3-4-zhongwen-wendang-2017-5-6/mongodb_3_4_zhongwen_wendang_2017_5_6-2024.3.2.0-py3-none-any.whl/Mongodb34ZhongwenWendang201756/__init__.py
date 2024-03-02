3f9e90b603a11d024f3f9d6e8810567d7f4591da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:37 +0000

    Add package mongodb-3-4-zhongwen-wendang-2017-5-6/2024.3.2.0/mongodb_3_4_zhongwen_wendang_2017_5_6-2024.3.2.0-py3-none-any.whl

---
+++ packages/mongodb-3-4-zhongwen-wendang-2017-5-6/mongodb_3_4_zhongwen_wendang_2017_5_6-2024.3.2.0-py3-none-any.whl/Mongodb34ZhongwenWendang201756/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mongodb-3-4-zhongwen-wendang-2017-5-6
+https://github.com/apachecn/mongodb-3-4-zhongwen-wendang-2017-5-6"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
