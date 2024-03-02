0d5cb68df506b2f77991c26090577bfe0ae908e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:53 +0000

    Add package electromagnetics-and-applications-lecture-notes-mit-6-013/2024.3.2.0/electromagnetics_and_applications_lecture_notes_mit_6_013-2024.3.2.0-py3-none-any.whl

---
+++ packages/electromagnetics-and-applications-lecture-notes-mit-6-013/electromagnetics_and_applications_lecture_notes_mit_6_013-2024.3.2.0-py3-none-any.whl/ElectromagneticsAndApplicationsLectureNotesMit6013/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""electromagnetics-and-applications-lecture-notes-mit-6-013
+https://github.com/apachecn/electromagnetics-and-applications-lecture-notes-mit-6-013"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
