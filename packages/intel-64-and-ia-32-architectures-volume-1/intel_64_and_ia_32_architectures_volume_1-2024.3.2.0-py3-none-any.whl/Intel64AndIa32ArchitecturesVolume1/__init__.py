c816765b2313810eb4fb8d878e1fd0593e710a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:53 +0000

    Add package intel-64-and-ia-32-architectures-volume-1/2024.3.2.0/intel_64_and_ia_32_architectures_volume_1-2024.3.2.0-py3-none-any.whl

---
+++ packages/intel-64-and-ia-32-architectures-volume-1/intel_64_and_ia_32_architectures_volume_1-2024.3.2.0-py3-none-any.whl/Intel64AndIa32ArchitecturesVolume1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""intel-64-and-ia-32-architectures-volume-1
+https://github.com/apachecn/intel-64-and-ia-32-architectures-volume-1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
