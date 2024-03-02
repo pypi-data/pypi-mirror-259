bc536ae39209887edc251f682b338ddc0f2d242b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:33 +0000

    Add package namespacelib/0.1.0/namespacelib-0.1.0.tar.gz

---
+++ packages/namespacelib/namespacelib-0.1.0.tar.gz/namespacelib-0.1.0/tests/test_namespaces.py
@@ -0,0 +1,33 @@
+import unittest
+
+import rdflib
+
+import namespacelib
+
+
+class TestNamespaces(unittest.TestCase):
+
+    def test_m4i(self):
+        self.assertIsInstance(namespacelib.M4I.Tool, rdflib.URIRef)
+        self.assertEqual(str(namespacelib.M4I.Tool),
+                         'http://w3id.org/nfdi4ing/metadata4ing#Tool')
+
+        with self.assertRaises(AttributeError):
+            namespacelib.M4I.Invalid
+
+    def test_qudt_unit(self):
+        self.assertIsInstance(namespacelib.QUDT_UNIT.M_PER_SEC, rdflib.URIRef)
+        self.assertEqual(str(namespacelib.QUDT_UNIT.M_PER_SEC),
+                         'http://qudt.org/vocab/unit/M-PER-SEC')
+        with self.assertRaises(AttributeError):
+            namespacelib.QUDT_UNIT.METER
+
+    def test_qudt_kind(self):
+        self.assertIsInstance(namespacelib.QUDT_KIND.Mass, rdflib.URIRef)
+        self.assertEqual(str(namespacelib.QUDT_KIND.Mass),
+                         'http://qudt.org/vocab/quantitykind/Mass')
+
+    def test_rdflib(self):
+        self.assertIsInstance(rdflib.PROV.Agent, rdflib.URIRef)
+        self.assertEqual(str(rdflib.PROV.Agent),
+                         "http://www.w3.org/ns/prov#Agent")
