09e5899404acebe310ba19409bec2b1cd4f01879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:24 +0000

    Add package shiyanlou-python-jiaocheng-heji-20160905-fix1/2024.3.2.0/shiyanlou_python_jiaocheng_heji_20160905_fix1-2024.3.2.0-py3-none-any.whl

---
+++ packages/shiyanlou-python-jiaocheng-heji-20160905-fix1/shiyanlou_python_jiaocheng_heji_20160905_fix1-2024.3.2.0-py3-none-any.whl/ShiyanlouPythonJiaochengHeji20160905Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shiyanlou-python-jiaocheng-heji-20160905-fix1
+https://github.com/apachecn/shiyanlou-python-jiaocheng-heji-20160905-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
