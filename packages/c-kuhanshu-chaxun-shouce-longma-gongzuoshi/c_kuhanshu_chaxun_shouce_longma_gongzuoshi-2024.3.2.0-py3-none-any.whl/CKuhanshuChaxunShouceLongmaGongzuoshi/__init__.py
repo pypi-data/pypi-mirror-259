a89ed842ae419a93eb40d26954d4005f727c95db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:23 +0000

    Add package c-kuhanshu-chaxun-shouce-longma-gongzuoshi/2024.3.2.0/c_kuhanshu_chaxun_shouce_longma_gongzuoshi-2024.3.2.0-py3-none-any.whl

---
+++ packages/c-kuhanshu-chaxun-shouce-longma-gongzuoshi/c_kuhanshu_chaxun_shouce_longma_gongzuoshi-2024.3.2.0-py3-none-any.whl/CKuhanshuChaxunShouceLongmaGongzuoshi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""c-kuhanshu-chaxun-shouce-longma-gongzuoshi
+https://github.com/apachecn/c-kuhanshu-chaxun-shouce-longma-gongzuoshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
