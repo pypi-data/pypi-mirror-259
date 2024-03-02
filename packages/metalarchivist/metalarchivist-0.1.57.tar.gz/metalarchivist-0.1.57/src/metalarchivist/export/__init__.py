8230fd9fc7e58bf9448a83815ea1e14be0ae2c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:48 +0000

    Add package metalarchivist/0.1.57/metalarchivist-0.1.57.tar.gz

---
+++ packages/metalarchivist/metalarchivist-0.1.57.tar.gz/metalarchivist-0.1.57/src/metalarchivist/export/__init__.py
@@ -0,0 +1,9 @@
+
+from .album import Album, MetalArchives
+from .band import Band
+from .genre import GenreBands as Genre
+
+
+__all__ = ['Album', 'MetalArchives', 
+           'Band',
+           'Genre']
