86d4909e42cd2fd664bd02ddb3d31b0cf4581adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:05 +0000

    Add package java-bianma-guifan/2024.3.2.0/java-bianma-guifan-2024.3.2.0.tar.gz

---
+++ packages/java-bianma-guifan/java-bianma-guifan-2024.3.2.0.tar.gz/java-bianma-guifan-2024.3.2.0/JavaBianmaGuifan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-bianma-guifan
+https://github.com/apachecn/java-bianma-guifan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
