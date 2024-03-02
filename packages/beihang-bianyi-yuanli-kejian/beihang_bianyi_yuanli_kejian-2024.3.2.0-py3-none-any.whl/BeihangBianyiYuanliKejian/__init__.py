b8bb72cd60079d274d3bbe1312d362301da2960c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:16 +0000

    Add package beihang-bianyi-yuanli-kejian/2024.3.2.0/beihang_bianyi_yuanli_kejian-2024.3.2.0-py3-none-any.whl

---
+++ packages/beihang-bianyi-yuanli-kejian/beihang_bianyi_yuanli_kejian-2024.3.2.0-py3-none-any.whl/BeihangBianyiYuanliKejian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""beihang-bianyi-yuanli-kejian
+https://github.com/apachecn/beihang-bianyi-yuanli-kejian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
