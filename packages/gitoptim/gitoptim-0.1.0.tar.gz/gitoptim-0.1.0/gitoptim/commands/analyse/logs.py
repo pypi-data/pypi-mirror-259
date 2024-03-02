2ff85990e4964af1b48d2d804a56f1fd92d1577a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:44 +0000

    Add package gitoptim/0.1.0/gitoptim-0.1.0.tar.gz

---
+++ packages/gitoptim/gitoptim-0.1.0.tar.gz/gitoptim-0.1.0/gitoptim/commands/analyse/logs.py
@@ -0,0 +1,27 @@
+from typing import Annotated
+
+import rich
+import typer
+
+from gitoptim.utils import EnvironmentVariables
+
+
+def command(
+        after_last_tag: Annotated[
+            bool, typer.Option("--after_last_tag",
+                               help="Include only the logs that appear after the last execution of `gitoptim tag`. "
+                                    "Includes all logs if `gitoptim tag` was never used.")] = False,
+        analyse_logs_trigger_id: Annotated[
+            int, typer.Option(help="Needs to be provided if not set in environment variables.")] = None,
+):
+    """
+    Analyze logs from the current job.
+
+    Includes only the logs that appear before execution of this command.
+    """
+
+    rich.print(EnvironmentVariables().job_id)
+    rich.print(after_last_tag)
+    rich.print(analyse_logs_trigger_id)
+
+    rich.print("Running job logs analysis...")
