b5e49c438a54f2491a2f3396a7e9f5d169139c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:38 +0000

    Add package deep-learning-methods-and-application/2024.3.2.0/deep-learning-methods-and-application-2024.3.2.0.tar.gz

---
+++ packages/deep-learning-methods-and-application/deep-learning-methods-and-application-2024.3.2.0.tar.gz/deep-learning-methods-and-application-2024.3.2.0/DeepLearningMethodsAndApplication/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""deep-learning-methods-and-application
+https://github.com/apachecn/deep-learning-methods-and-application"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
