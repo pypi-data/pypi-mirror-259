866588e8111fade45a514a0e107b43efaa12bb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:10:23 +0000

    Add package c-chengxusheji-kecheng-xuexi-xisai-jiaoyu/2024.3.2.0/c-chengxusheji-kecheng-xuexi-xisai-jiaoyu-2024.3.2.0.tar.gz

---
+++ packages/c-chengxusheji-kecheng-xuexi-xisai-jiaoyu/c-chengxusheji-kecheng-xuexi-xisai-jiaoyu-2024.3.2.0.tar.gz/c-chengxusheji-kecheng-xuexi-xisai-jiaoyu-2024.3.2.0/CChengxushejiKechengXuexiXisaiJiaoyu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""c-chengxusheji-kecheng-xuexi-xisai-jiaoyu
+https://github.com/apachecn/c-chengxusheji-kecheng-xuexi-xisai-jiaoyu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
