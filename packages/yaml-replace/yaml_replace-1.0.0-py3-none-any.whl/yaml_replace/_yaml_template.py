7177baf41d46c910367357c1c274d87c48b2d35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:40 +0000

    Add package yaml-replace/1.0.0/yaml_replace-1.0.0-py3-none-any.whl

---
+++ packages/yaml-replace/yaml_replace-1.0.0-py3-none-any.whl/yaml_replace/_yaml_template.py
@@ -0,0 +1,20 @@
+import yaml
+from jinja2 import Template
+
+
+class YAMLTemplate:
+    def __init__(self, yaml_string: str):
+        self.yaml_string = yaml_string
+        self.template = Template(
+            yaml_string,
+            variable_start_string="${{",
+            variable_end_string="}}",
+        )
+
+    def render(self, substitutions: dict) -> dict:
+        return yaml.safe_load(self.template.render(substitutions))
+
+    @staticmethod
+    def from_file(file_path: str):
+        with open(file_path, "r") as file:
+            return YAMLTemplate(file.read())
