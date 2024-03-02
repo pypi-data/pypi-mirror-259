ca0d2b3065d3d83a35eaf9131502d3cebc62544c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:20 +0000

    Add package ember-js-cankao-wendang/2024.3.2.0/ember-js-cankao-wendang-2024.3.2.0.tar.gz

---
+++ packages/ember-js-cankao-wendang/ember-js-cankao-wendang-2024.3.2.0.tar.gz/ember-js-cankao-wendang-2024.3.2.0/EmberJsCankaoWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ember-js-cankao-wendang
+https://github.com/apachecn/ember-js-cankao-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
