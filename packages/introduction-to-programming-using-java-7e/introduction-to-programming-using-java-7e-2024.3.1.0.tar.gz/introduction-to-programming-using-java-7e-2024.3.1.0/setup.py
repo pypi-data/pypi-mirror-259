3ea23ead5e55fb640ae9c294e21a337100e5e022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:06:07 +0000

    Add package introduction-to-programming-using-java-7e/2024.3.1.0/introduction-to-programming-using-java-7e-2024.3.1.0.tar.gz

---
+++ packages/introduction-to-programming-using-java-7e/introduction-to-programming-using-java-7e-2024.3.1.0.tar.gz/introduction-to-programming-using-java-7e-2024.3.1.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import IntroductionToProgrammingUsingJava7e
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('IntroductionToProgrammingUsingJava7e'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="introduction-to-programming-using-java-7e",
+    version=IntroductionToProgrammingUsingJava7e.__version__,
+    url="https://github.com/apachecn/introduction-to-programming-using-java-7e",
+    author=IntroductionToProgrammingUsingJava7e.__author__,
+    author_email=IntroductionToProgrammingUsingJava7e.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="Introduction to Programming Using Java 7e",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "introduction-to-programming-using-java-7e=IntroductionToProgrammingUsingJava7e.__main__:main",
+            "IntroductionToProgrammingUsingJava7e=IntroductionToProgrammingUsingJava7e.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
