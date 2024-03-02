be60bc2ab9b206c0b0883eb88f2d95d7823725ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:04:57 +0000

    Add package learn-c-the-hard-way/2024.3.1.0/learn_c_the_hard_way-2024.3.1.0-py3-none-any.whl

---
+++ packages/learn-c-the-hard-way/learn_c_the_hard_way-2024.3.1.0-py3-none-any.whl/LearnCTheHardWay/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""learn-c-the-hard-way
+https://github.com/apachecn/learn-c-the-hard-way"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
