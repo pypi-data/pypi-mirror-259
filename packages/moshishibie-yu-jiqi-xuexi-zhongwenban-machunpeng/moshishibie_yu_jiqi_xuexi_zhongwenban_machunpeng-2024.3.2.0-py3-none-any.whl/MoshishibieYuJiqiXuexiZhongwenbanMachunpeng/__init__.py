71bf403996a55f3c8c0ae184e20743edae34cb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:20 +0000

    Add package moshishibie-yu-jiqi-xuexi-zhongwenban-machunpeng/2024.3.2.0/moshishibie_yu_jiqi_xuexi_zhongwenban_machunpeng-2024.3.2.0-py3-none-any.whl

---
+++ packages/moshishibie-yu-jiqi-xuexi-zhongwenban-machunpeng/moshishibie_yu_jiqi_xuexi_zhongwenban_machunpeng-2024.3.2.0-py3-none-any.whl/MoshishibieYuJiqiXuexiZhongwenbanMachunpeng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""moshishibie-yu-jiqi-xuexi-zhongwenban-machunpeng
+https://github.com/apachecn/moshishibie-yu-jiqi-xuexi-zhongwenban-machunpeng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
