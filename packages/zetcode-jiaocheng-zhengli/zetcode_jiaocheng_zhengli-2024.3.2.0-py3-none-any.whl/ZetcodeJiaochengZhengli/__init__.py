7ca89dabc92be01f880f87620059e024029bda06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:45 +0000

    Add package zetcode-jiaocheng-zhengli/2024.3.2.0/zetcode_jiaocheng_zhengli-2024.3.2.0-py3-none-any.whl

---
+++ packages/zetcode-jiaocheng-zhengli/zetcode_jiaocheng_zhengli-2024.3.2.0-py3-none-any.whl/ZetcodeJiaochengZhengli/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zetcode-jiaocheng-zhengli
+https://github.com/apachecn/zetcode-jiaocheng-zhengli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
