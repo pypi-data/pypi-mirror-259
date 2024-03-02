c1c8a4f7333088d57c0cbca6ce0fa24290b884aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:43 +0000

    Add package altdss/0.2.1/altdss-0.2.1.tar.gz

---
+++ packages/altdss/altdss-0.2.1.tar.gz/altdss-0.2.1/docs/changelog.md
@@ -0,0 +1,90 @@
+# AltDSS-Python changelog
+
+Remember that changes in our alternative OpenDSS engine, currently known as AltDSS/DSS C-API, are always
+relevant. See [DSS C-API's repository](https://github.com/dss-extensions/dss_capi/) for more information.
+
+## 0.2.1
+
+- Meter/Generator/etc.: Remove the previous workaround for issue with the DSS C-API headers related to `RegisterValues` -- now working without it. Add test.
+
+## 0.2.0
+
+Version incremented to 0.2.0 due to the number of changes in the function/property names. The engine is also updated to DSS C-API 0.14.2.
+
+- PDElement: 
+    - Fix `ParentPDElement`
+    - Fix and update names:
+        - `pctNorm` to `pctNormal`
+        - `pctEmerg` to `pctEmergency`
+    - Batch: add `pctNormal` and `pctEmergency`
+    - Add/complement docstrings
+
+- Bus class: 
+    - Fix errors in `ZSC0`, `ZSC1`, `Voltages`
+    - Batch: fix `len(bus_batch)`
+    - Fix element batches (PCElements, PDElements)
+    - Tweak tracking when buses are redefined but there are still live objects
+    - Complement docstrings
+    - Update names of several items:
+        - `Voc` to `VOC`
+        - `Isc` to `ISC`
+        - `YscMatrix` to `YSC`
+        - `ZscMatrix` to `ZSC`
+        - `Zsc012Matrix` to `ZSC012`
+        - `Zsc0` to `ZSC0`
+        - `Zsc1` to `ZSC1`
+        - `ZscRefresh` to `ZSCRefresh`
+
+- AltDSS (main class): adjust capitalization of a few items:
+    - `NodeVmagByPhase` to `NodeVMagByPhase`
+    - `NodeVmagPUByPhase` to `NodeVMagPUByPhase`
+    - `BusVmag` to `BusVMag`
+    - `BusVmagPu` to `BusVMagPU`
+
+
+## 0.1.3
+
+- Meter/Generator/etc.: Add workaround for issue with the DSS C-API headers related to `RegisterValues`.
+- Monitor: fix issue with `Channel` returning the whole data instead of the channel data.
+
+## 0.1.2
+
+- Fix `SetterFlags`: import from backend instead of defining a new enum here.
+- Batch:
+    - Implement `__slots__` to constrain the attributes and avoid accidental names.
+    - `EnergyMeter.Loads`: make it a function to avoid issues with uninitialized meters (such as new meters).
+    - Minor quality of life changes, such as returning zero instead of error for some empty batches.
+
+- Known issue: reading `pctEmerg` or `pctNorm` for uninitialized PDElements (missing solution for the circuit) will crash. This is already fixed in the engine and will be included in the next backend update.
+
+
+## 0.1.1
+
+- Batch: fix setter for single object
+- Line: adjust Conductors to allow any conductor data object (WireData, CNData, TSData).
+- LineGeometry: rename "Wire" to "Conductors", follow the changes in Line.
+- Depend on DSS-Python 0.15.2 for the updated backend.
+- Add tests to ensure the following issues don't happen again:
+    - Bus: Fix typo in `puVMagAngle` (rename it from `puVmagAngle`), and `Voc`.
+    - Meters: Fix typo in call to `DoReliabilityCalc`
+    - Circuit: Fix typo in `NumCircuitElements`
+    - PCE: Found a function not implemented in the engine. Fixed there.
+
+## 0.1.0
+
+First version. Still depends on DSS-Python for the backend, but otherwise can be used for a lot of common operations by itself.
+
+Highlights compared to the previous Obj API on DSS-Python:
+
+- The method `find()` (and `__getitem__`) now assume 0-based numeric indices. This is more appropriate to Python, e.g. `altdss.Load[0]` now returns the first load.
+- Besides the previous DSS property functions, new API methods to allow extracting data and manipulating objects through generalized versions of the classic (COM-like) API.
+- Introduce extra batch types for special classes
+- Introduce non-uniform batches
+- Introduce dynamic batches, avoiding redundant copies
+- Special handling of EnergyMeter and MeterSections
+- Object lifetime: implement synchronization of circuit state and pointers, invalidating dead pointer instead of crashing
+
+
+ ## 0.0.1
+
+ Published to PyPI to reserve the name.
---
+++ packages/altdss/altdss-0.2.1.tar.gz/altdss-0.2.1/tests/test_alt.py
@@ -0,0 +1,234 @@
+try:
+    from ._settings import BASE_DIR, WIN32
+except ImportError:
+    from _settings import BASE_DIR, WIN32
+
+
+import numpy
+numpy.set_printoptions(linewidth=999)
+import numpy.testing as nptest
+from altdss import altdss, AltDSS
+from altdss.CircuitElement import CircuitElementMixin, ElementHasRegistersMixin
+from altdss.PDElement import PDElementMixin
+from altdss.PCElement import PCElementMixin
+from altdss.LoadShape import LoadShapeObjMixin
+from altdss.Monitor import MonitorObjMixin
+from altdss.Transformer import TransformerObjMixin
+from altdss.EnergyMeter import EnergyMeterObjMixin
+
+altdss(f'redirect "{BASE_DIR}/Version8/Distrib/IEEETestCases/13Bus/IEEE13Nodeckt.dss"')
+
+for objset in [altdss.Element, altdss.PDElement, altdss.PCElement]:
+    print(objset.IsIsolated())
+    (objset.Currents())
+    (objset.CurrentsMagAng())
+    (objset.SeqCurrents())
+    (objset.ComplexSeqCurrents())
+    (objset.Voltages())
+    (objset.VoltagesMagAng())
+    (objset.SeqVoltages())
+    (objset.ComplexSeqVoltages())
+    print(objset.HasOCPDevice())
+    print(objset.HasSwitchControl())
+    print(objset.OCPDeviceType())
+    print(objset.HasVoltControl())
+    print(objset.Losses())
+    print(objset.TotalPowers())
+    print(objset.SeqPowers())
+    print(objset.NumPhases())
+
+altdss(f'redirect "{BASE_DIR}/Version8/Distrib/IEEETestCases/LVTestCase/Master.dss"')
+
+errors = []
+
+dss = altdss.to_dss_python()
+for pass_num in (0, 1):
+    if pass_num == 1:
+        altdss('solve mode=daily')
+
+    for cname in dss.Classes:
+        cls = getattr(altdss, cname)
+        if not len(cls):
+            continue
+
+        print('=' * 40)
+        c = cls[0]
+        for k in dir(c):
+            if k.lower() in c._cls_prop_idx:
+                print(f'{c.FullName()}.{k}={getattr(c, k)}')
+
+        # DSSObj
+        print('Name', c.Name)
+        print('FullName', c.FullName())
+
+        if isinstance(c, CircuitElementMixin):
+            print('DisplayName', c.DisplayName)
+            for funcname in [
+                'Handle',
+                'NumConductors',
+                'NumPhases',
+                'NumTerminals',
+                'NumControllers',
+                'OCPDevice',
+                'OCPDeviceType',
+                'OCPDeviceIndex',
+                'IsIsolated',
+                'HasOCPDevice',
+                'HasSwitchControl',
+                'HasVoltControl',
+                'NodeOrder',
+                'NodeRef',
+                'ComplexSeqVoltages',
+                'ComplexSeqCurrents',
+                'Currents',
+                'Voltages',
+                'Losses',
+                'PhaseLosses',
+                'Powers',
+                'SeqVoltages',
+                'Residuals',
+                'YPrim',
+                'VoltagesMagAng',
+                'TotalPowers',
+            ]:
+                if isinstance(c, EnergyMeterObjMixin) and funcname == 'Losses':
+                    continue
+
+                try:
+                    print(funcname, getattr(c, funcname)())
+                except:
+                    errors.append((c.FullName(), funcname))
+                    continue
+
+
+        if isinstance(c, PCElementMixin):
+            for funcname in [
+                'VariableNames',
+                'VariableValues',
+                'VariablesDict',
+                'EnergyMeter',
+                'EnergyMeterName',
+            ]:
+                try:
+                    print(funcname, getattr(c, funcname)())
+                except:
+                    errors.append((c.FullName(), funcname))
+                    continue
+
+        if isinstance(c, PDElementMixin):
+            for funcname in [
+                'EnergyMeter',
+                'EnergyMeterName',
+                'IsShunt',
+                'AccumulatedL',
+                'Lambda',
+                'NumCustomers',
+                'ParentPDElement',
+                'TotalCustomers',
+                'FromTerminal',
+                'TotalMiles',
+                'SectionID',
+                'pctNormal',
+                'pctEmergency',
+            ]:
+                try:
+                    print(funcname, getattr(c, funcname)())
+                except:
+                    errors.append((c.FullName(), funcname))
+                    continue
+
+
+        if isinstance(c, LoadShapeObjMixin):
+            mult_f64 = c.PMult
+            c.UseFloat32()
+            mult_f32 = c.PMult
+            nptest.assert_allclose(mult_f32, mult_f64)
+
+
+        if isinstance(c, TransformerObjMixin):
+            for funcname, *params in [
+                ('WindingCurrents',),
+                ('WindingVoltages', 1),
+                ('LossesByType',),
+            ]:
+                try:
+                    print(funcname, getattr(c, funcname)(*params))
+                except:
+                    errors.append((c.FullName(), funcname))
+                    continue
+
+
+        if isinstance(c, MonitorObjMixin):
+            for funcname in [
+                'Header',
+                'ByteStream',
+                'FileName',
+                'SampleCount',
+                'NumChannels',
+                'RecordSize',
+                'dblFreq',
+                'dblHour',
+                'AsMatrix',
+                'ToDataFrame'
+            ]:
+                try:
+                    print(funcname, getattr(c, funcname)())
+                except:
+                    errors.append((c.FullName(), funcname))
+                    continue
+
+            df = c.ToDataFrame()
+            header = c.Header()
+            for num in range(c.NumChannels()):
+                assert all(df[header[num]] == c.Channel(num + 1)), 'Monitor should match series from DataFrame'
+
+        if isinstance(c, EnergyMeterObjMixin):
+            print('CalcCurrent', getattr(c, 'CalcCurrent'))
+            print('AllocFactors', getattr(c, 'AllocFactors'))
+            for funcname in [
+                'TotalCustomers',
+                'NumEndElements',
+                'NumSections',
+                # 'Sections',
+                # 'ZonePCEs',
+                # 'EndElements',
+                # 'Branches',
+                # 'Loads',
+                # 'Sequence',
+            ]:
+                try:
+                    print(funcname, getattr(c, funcname)())
+                except:
+                    errors.append((c.FullName(), funcname))
+                    continue
+
+
+        if isinstance(c, ElementHasRegistersMixin):
+            for funcname in [
+                'RegisterNames',
+                'RegisterValues',
+                'RegistersDict',
+            ]:
+                try:
+                    print(funcname, getattr(c, funcname)())
+                except:
+                    errors.append((c.FullName(), funcname))
+                    raise
+                    continue
+
+
+
+print('=' * 40)
+print('=' * 40)
+print('ERRORS:', len(errors))
+print('=' * 40)
+for error in errors:
+    print(*error)
+
+print('=' * 40)
+print('=' * 40)
+
+
+# m1 = altdss.EnergyMeter[0]
+# print(m1, m1.NumEndElements())
+# print(m1, m1.CalcCurrent)
