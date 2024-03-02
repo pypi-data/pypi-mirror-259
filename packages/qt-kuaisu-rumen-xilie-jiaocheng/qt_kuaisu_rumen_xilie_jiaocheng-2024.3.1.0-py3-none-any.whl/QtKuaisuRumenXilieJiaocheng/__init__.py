33f9dad9633eaeb11c713e3c7ad5708ff0cfe061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:09:19 +0000

    Add package qt-kuaisu-rumen-xilie-jiaocheng/2024.3.1.0/qt_kuaisu_rumen_xilie_jiaocheng-2024.3.1.0-py3-none-any.whl

---
+++ packages/qt-kuaisu-rumen-xilie-jiaocheng/qt_kuaisu_rumen_xilie_jiaocheng-2024.3.1.0-py3-none-any.whl/QtKuaisuRumenXilieJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qt-kuaisu-rumen-xilie-jiaocheng
+https://github.com/apachecn/qt-kuaisu-rumen-xilie-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
