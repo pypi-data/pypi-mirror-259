2c09736cecfae8f661c9040a954f994c26d42345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:16 +0000

    Add package shejimoshi-baojiao-bu-baohui/2024.3.2.0/shejimoshi_baojiao_bu_baohui-2024.3.2.0-py3-none-any.whl

---
+++ packages/shejimoshi-baojiao-bu-baohui/shejimoshi_baojiao_bu_baohui-2024.3.2.0-py3-none-any.whl/ShejimoshiBaojiaoBuBaohui/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shejimoshi-baojiao-bu-baohui
+https://github.com/apachecn/shejimoshi-baojiao-bu-baohui"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
