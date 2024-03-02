019f30e83abe4b10db1d9da8e5b7e54898ad66c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:56 +0000

    Add package sitanfu-cs229-jiqi-xuexi-kecheng-biji/2024.3.2.0/sitanfu_cs229_jiqi_xuexi_kecheng_biji-2024.3.2.0-py3-none-any.whl

---
+++ packages/sitanfu-cs229-jiqi-xuexi-kecheng-biji/sitanfu_cs229_jiqi_xuexi_kecheng_biji-2024.3.2.0-py3-none-any.whl/SitanfuCs229JiqiXuexiKechengBiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sitanfu-cs229-jiqi-xuexi-kecheng-biji
+https://github.com/apachecn/sitanfu-cs229-jiqi-xuexi-kecheng-biji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
