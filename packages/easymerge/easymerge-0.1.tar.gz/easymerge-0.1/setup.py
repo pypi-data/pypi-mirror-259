9c69216af32c12119e0360cb578b93d8d74f0b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:58 +0000

    Add package easymerge/0.1/easymerge-0.1.tar.gz

---
+++ packages/easymerge/easymerge-0.1.tar.gz/easymerge-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='easymerge',
+    description='A simple Python library for merging dataframes accommodating variations in language, spelling, and typos. ',
+    author='Ethan Mai',
+    author_email='iam.ethanmai@gmail.com',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=requirements
+)
+
