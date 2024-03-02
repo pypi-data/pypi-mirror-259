a1eea34a157e963601a43ffe901fce901baa0fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:07 +0000

    Add package the-beginner-s-guide-to-idapython-zhongwenban/2024.3.2.0/the_beginner_s_guide_to_idapython_zhongwenban-2024.3.2.0-py3-none-any.whl

---
+++ packages/the-beginner-s-guide-to-idapython-zhongwenban/the_beginner_s_guide_to_idapython_zhongwenban-2024.3.2.0-py3-none-any.whl/TheBeginnerSGuideToIdapythonZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""the-beginner-s-guide-to-idapython-zhongwenban
+https://github.com/apachecn/the-beginner-s-guide-to-idapython-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
