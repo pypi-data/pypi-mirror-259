e4f339e541092a78e1fe421c0e308e193bbc0656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:19 +0000

    Add package tushuo-shejimoshi/2024.3.2.0/tushuo-shejimoshi-2024.3.2.0.tar.gz

---
+++ packages/tushuo-shejimoshi/tushuo-shejimoshi-2024.3.2.0.tar.gz/tushuo-shejimoshi-2024.3.2.0/TushuoShejimoshi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tushuo-shejimoshi
+https://github.com/apachecn/tushuo-shejimoshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
