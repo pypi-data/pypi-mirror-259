b0c90af8744314aefba6e01b1cf70f5a3ba61760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:57 +0000

    Add package ruby-yuanma-jiedu/2024.3.2.0/ruby_yuanma_jiedu-2024.3.2.0-py3-none-any.whl

---
+++ packages/ruby-yuanma-jiedu/ruby_yuanma_jiedu-2024.3.2.0-py3-none-any.whl/RubyYuanmaJiedu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ruby-yuanma-jiedu
+https://github.com/apachecn/ruby-yuanma-jiedu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
