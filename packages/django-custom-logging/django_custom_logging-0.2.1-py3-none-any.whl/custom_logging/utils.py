f4e8f33acf6e7264f7dee349bf87731d441fb5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:35 +0000

    Add package django-custom-logging/0.2.1/django_custom_logging-0.2.1-py3-none-any.whl

---
+++ packages/django-custom-logging/django_custom_logging-0.2.1-py3-none-any.whl/custom_logging/utils.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2021-present Seonghyeon Cho <seonghyeoncho96@gmail.com>
+# SPDX-License-Identifier: MIT
+from functools import reduce
+
+
+class NoDefaultProvided:
+    """dummy class for getattrd"""
+
+    pass
+
+
+def getattrd(obj, name, default=NoDefaultProvided):
+    """
+    Same as getattr(), but allows dot notation lookup
+    Discussed in: http://stackoverflow.com/questions/11975781
+    """
+    try:
+        return reduce(getattr, name.split("."), obj)
+    except AttributeError:
+        if default != NoDefaultProvided:
+            return default
+        raise
+
+
+def setattrd(obj, name, val):
+    pre, _, post = name.rpartition(".")
+    return setattr(getattrd(obj, pre, None) if pre else obj, post, val)
