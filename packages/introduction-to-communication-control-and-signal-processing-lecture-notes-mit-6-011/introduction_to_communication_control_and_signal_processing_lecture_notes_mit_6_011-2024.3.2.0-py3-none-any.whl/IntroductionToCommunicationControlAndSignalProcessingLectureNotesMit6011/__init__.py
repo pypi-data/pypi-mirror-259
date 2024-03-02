a7ee2dbe9fc0f1b198bc82a0b27143354481a3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:49 +0000

    Add package introduction-to-communication-control-and-signal-processing-lecture-notes-mit-6-011/2024.3.2.0/introduction_to_communication_control_and_signal_processing_lecture_notes_mit_6_011-2024.3.2.0-py3-none-any.whl

---
+++ packages/introduction-to-communication-control-and-signal-processing-lecture-notes-mit-6-011/introduction_to_communication_control_and_signal_processing_lecture_notes_mit_6_011-2024.3.2.0-py3-none-any.whl/IntroductionToCommunicationControlAndSignalProcessingLectureNotesMit6011/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""introduction-to-communication-control-and-signal-processing-lecture-notes-mit-6-011
+https://github.com/apachecn/introduction-to-communication-control-and-signal-processing-lecture-notes-mit-6-011"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
