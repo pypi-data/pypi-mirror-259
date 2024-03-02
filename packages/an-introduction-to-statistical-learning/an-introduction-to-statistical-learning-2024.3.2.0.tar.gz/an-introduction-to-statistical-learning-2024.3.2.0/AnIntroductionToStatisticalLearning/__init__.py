e114257bd7fb56946331220869ff87ad7a3969fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:35 +0000

    Add package an-introduction-to-statistical-learning/2024.3.2.0/an-introduction-to-statistical-learning-2024.3.2.0.tar.gz

---
+++ packages/an-introduction-to-statistical-learning/an-introduction-to-statistical-learning-2024.3.2.0.tar.gz/an-introduction-to-statistical-learning-2024.3.2.0/AnIntroductionToStatisticalLearning/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""an-introduction-to-statistical-learning
+https://github.com/apachecn/an-introduction-to-statistical-learning"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
