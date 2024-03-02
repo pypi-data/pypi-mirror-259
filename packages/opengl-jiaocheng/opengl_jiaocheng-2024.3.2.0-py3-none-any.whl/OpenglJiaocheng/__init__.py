d7e951744e9fdd280bc74f73c313e1d3d8b83566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:40 +0000

    Add package opengl-jiaocheng/2024.3.2.0/opengl_jiaocheng-2024.3.2.0-py3-none-any.whl

---
+++ packages/opengl-jiaocheng/opengl_jiaocheng-2024.3.2.0-py3-none-any.whl/OpenglJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""opengl-jiaocheng
+https://github.com/apachecn/opengl-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
