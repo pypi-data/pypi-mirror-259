5757a0d9849c004719f50a5b7547a884ee0426bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:36 +0000

    Add package mysql-jiaocheng-c-yuyan-zhongwenwang/2024.3.2.0/mysql_jiaocheng_c_yuyan_zhongwenwang-2024.3.2.0-py3-none-any.whl

---
+++ packages/mysql-jiaocheng-c-yuyan-zhongwenwang/mysql_jiaocheng_c_yuyan_zhongwenwang-2024.3.2.0-py3-none-any.whl/MysqlJiaochengCYuyanZhongwenwang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mysql-jiaocheng-c-yuyan-zhongwenwang
+https://github.com/apachecn/mysql-jiaocheng-c-yuyan-zhongwenwang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
