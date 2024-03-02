4bd5da38325742e5d73f0d73e52dc2e075316c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:05:53 +0000

    Add package linux-inside-zhongwenban/2024.3.1.0/linux_inside_zhongwenban-2024.3.1.0-py3-none-any.whl

---
+++ packages/linux-inside-zhongwenban/linux_inside_zhongwenban-2024.3.1.0-py3-none-any.whl/LinuxInsideZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""linux-inside-zhongwenban
+https://github.com/apachecn/linux-inside-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
