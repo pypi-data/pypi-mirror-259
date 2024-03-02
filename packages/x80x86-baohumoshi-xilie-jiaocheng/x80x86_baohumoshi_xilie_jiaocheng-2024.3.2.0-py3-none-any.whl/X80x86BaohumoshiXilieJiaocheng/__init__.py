c0d976aa996452dea745908779abe5475b5c3a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:39 +0000

    Add package x80x86-baohumoshi-xilie-jiaocheng/2024.3.2.0/x80x86_baohumoshi_xilie_jiaocheng-2024.3.2.0-py3-none-any.whl

---
+++ packages/x80x86-baohumoshi-xilie-jiaocheng/x80x86_baohumoshi_xilie_jiaocheng-2024.3.2.0-py3-none-any.whl/X80x86BaohumoshiXilieJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""x80x86-baohumoshi-xilie-jiaocheng
+https://github.com/apachecn/x80x86-baohumoshi-xilie-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
