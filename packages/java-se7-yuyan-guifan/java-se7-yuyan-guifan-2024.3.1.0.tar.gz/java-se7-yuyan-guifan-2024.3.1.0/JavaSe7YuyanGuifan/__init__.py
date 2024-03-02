940f06e86372b0d65769ec722458686151d984fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:05:52 +0000

    Add package java-se7-yuyan-guifan/2024.3.1.0/java-se7-yuyan-guifan-2024.3.1.0.tar.gz

---
+++ packages/java-se7-yuyan-guifan/java-se7-yuyan-guifan-2024.3.1.0.tar.gz/java-se7-yuyan-guifan-2024.3.1.0/JavaSe7YuyanGuifan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-se7-yuyan-guifan
+https://github.com/apachecn/java-se7-yuyan-guifan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
