f598727141f3b719be1e7158df6bcbbd983633a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:10 +0000

    Add package mmda2/0.1/mmda2-0.1.tar.gz

---
+++ packages/mmda2/mmda2-0.1.tar.gz/mmda2-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup , find_packages
+
+
+setup(
+    name = 'mmda2',
+    version=0.1,
+    description='student managmnet',
+    long_description=open('README.txt').read() + '/n/n',
+    url='',
+    author='mmda2',
+    author_email='mohammadafshar0083@gmail.com',
+    license='MIT',
+    keywords='managments',
+    packages=find_packages()
+
+
+
+
+)
