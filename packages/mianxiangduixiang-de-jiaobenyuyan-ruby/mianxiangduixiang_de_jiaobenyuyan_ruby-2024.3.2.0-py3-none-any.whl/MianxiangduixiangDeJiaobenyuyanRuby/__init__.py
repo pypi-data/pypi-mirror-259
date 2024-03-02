3e6aa53efae54e26a2ce39a012c30b5cd4755489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:12 +0000

    Add package mianxiangduixiang-de-jiaobenyuyan-ruby/2024.3.2.0/mianxiangduixiang_de_jiaobenyuyan_ruby-2024.3.2.0-py3-none-any.whl

---
+++ packages/mianxiangduixiang-de-jiaobenyuyan-ruby/mianxiangduixiang_de_jiaobenyuyan_ruby-2024.3.2.0-py3-none-any.whl/MianxiangduixiangDeJiaobenyuyanRuby/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mianxiangduixiang-de-jiaobenyuyan-ruby
+https://github.com/apachecn/mianxiangduixiang-de-jiaobenyuyan-ruby"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
