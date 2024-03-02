2171dd8880094d6e6e98f3b48aac585a031e038d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:06:17 +0000

    Add package html-5-1-specification/2024.3.1.0/html_5_1_specification-2024.3.1.0-py3-none-any.whl

---
+++ packages/html-5-1-specification/html_5_1_specification-2024.3.1.0-py3-none-any.whl/Html51Specification/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""html-5-1-specification
+https://github.com/apachecn/html-5-1-specification"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
