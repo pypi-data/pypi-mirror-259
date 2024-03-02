1c83d980c818c468caf7160fdf1bb5792078c2fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:49 +0000

    Add package shujujiegou-jiu-gai-zheyang-xue/2024.3.2.0/shujujiegou_jiu_gai_zheyang_xue-2024.3.2.0-py3-none-any.whl

---
+++ packages/shujujiegou-jiu-gai-zheyang-xue/shujujiegou_jiu_gai_zheyang_xue-2024.3.2.0-py3-none-any.whl/ShujujiegouJiuGaiZheyangXue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shujujiegou-jiu-gai-zheyang-xue
+https://github.com/apachecn/shujujiegou-jiu-gai-zheyang-xue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
