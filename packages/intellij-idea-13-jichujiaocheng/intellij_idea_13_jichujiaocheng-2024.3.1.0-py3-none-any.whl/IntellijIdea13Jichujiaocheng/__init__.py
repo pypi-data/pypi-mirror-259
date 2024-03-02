2c62dff221a4058c51edf6e5ab1736e1c6060378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:05:56 +0000

    Add package intellij-idea-13-jichujiaocheng/2024.3.1.0/intellij_idea_13_jichujiaocheng-2024.3.1.0-py3-none-any.whl

---
+++ packages/intellij-idea-13-jichujiaocheng/intellij_idea_13_jichujiaocheng-2024.3.1.0-py3-none-any.whl/IntellijIdea13Jichujiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""intellij-idea-13-jichujiaocheng
+https://github.com/apachecn/intellij-idea-13-jichujiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
