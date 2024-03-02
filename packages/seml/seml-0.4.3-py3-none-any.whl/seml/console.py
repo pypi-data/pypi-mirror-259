077abe9f9bc03af8ff6da36e6e8ca8a616c1e826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:04:59 +0000

    Add package seml/0.4.3/seml-0.4.3-py3-none-any.whl

---
+++ packages/seml/seml-0.4.3-py3-none-any.whl/seml/console.py
@@ -0,0 +1,70 @@
+import functools
+import os
+from typing import Sequence
+
+import rich
+import rich.box
+import rich.table
+from rich.console import Console
+from rich.padding import Padding
+from rich.rule import Rule
+
+
+try:
+    terminal_width = os.get_terminal_size().columns
+except OSError:
+    from seml.settings import SETTINGS
+
+    terminal_width = SETTINGS.EXPERIMENT.TERMINAL_WIDTH
+
+console = Console(
+    width=terminal_width,
+)
+
+Table = functools.partial(
+    rich.table.Table,
+    collapse_padding=True,
+    show_lines=False,
+    show_edge=False,
+    box=rich.box.SIMPLE,
+    row_styles=['none', 'dim'],
+    padding=(
+        0,
+        0,
+    ),
+    highlight=True,
+)
+
+
+def Heading(text: str):
+    return Padding(Rule(text, style='red'), pad=(1, 0, 0, 0))
+
+
+def list_items(items: Sequence[str]):
+    """
+    Print a list of items in columns, using as many columns as possible
+    while keeping the items readable. This will look similar to bash autcompletition suggestions.
+
+    Parameters
+    ----------
+    items : Sequence[str]
+        The items to print.
+    """
+    # Calculate available width
+    available_width = console.width
+
+    # Determine the number of columns based on available width
+    max_len = max(len(s) for s in items)
+    num_columns = max(1, available_width // (max_len + 1))
+
+    # Create a table with the calculated number of columns
+    table = rich.table.Table.grid(expand=True, pad_edge=True)
+    for i in range(num_columns):
+        table.add_column()
+
+    # Add suggestions to the table, distributing them across columns
+    for i in range(0, len(items), num_columns):
+        table.add_row(*items[i : i + num_columns])
+
+    # Print the table
+    console.print(table)
