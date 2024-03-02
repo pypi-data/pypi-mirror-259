3f1b272a2bef1dd8b451d698f0fb95dee34c803d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:15 +0000

    Add package shangchuan-gongji-zongjie/2024.3.2.0/shangchuan-gongji-zongjie-2024.3.2.0.tar.gz

---
+++ packages/shangchuan-gongji-zongjie/shangchuan-gongji-zongjie-2024.3.2.0.tar.gz/shangchuan-gongji-zongjie-2024.3.2.0/ShangchuanGongjiZongjie/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shangchuan-gongji-zongjie
+https://github.com/apachecn/shangchuan-gongji-zongjie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
