9c1c07e85f5fc9fae04558c18fbc04a367f2b9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:12 +0000

    Add package bitnet/0.1.2/bitnet-0.1.2.tar.gz

---
+++ packages/bitnet/bitnet-0.1.2.tar.gz/bitnet-0.1.2/bitnet/replace_hf.py
@@ -0,0 +1,44 @@
+from torch import nn
+
+from bitnet.bitlinear import BitLinear
+
+
+def replace_linears_in_hf(
+    model,
+):
+    """
+    Replaces all instances of nn.Linear in the given model with BitLinear15b.
+
+    Args:
+        model (nn.Module): The model to modify.
+
+    Returns:
+        None
+    """
+    for name, module in model.named_children():
+        if isinstance(module, nn.Linear):
+            # Replace the nn.Linear with BitLinear matching in features and and out_features, and add it to the model
+            setattr(
+                model,
+                name,
+                BitLinear(
+                    in_features=module.in_features,
+                    out_features=module.out_features,
+                    bias=module.bias is not None,
+                ),
+            )
+        else:
+            # Recursively apply to child modules
+            replace_linears_in_hf(module)
+
+
+# # Example usage:
+# # Load a model from Hugging Face's Transformers
+# model_name = "bert-base-uncased"  # Example model
+# model = AutoModel.from_pretrained(model_name)
+
+# # Replace its Linear layers with BitLinear
+# replace_linears_in_hf(model)
+
+# # Now, `model` has its Linear layers replaced with BitLinear
+# print(model)
