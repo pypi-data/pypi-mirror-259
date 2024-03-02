c9e7fa8ebc1c91899e3de3e1fdb1790ec412d46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:55 +0000

    Add package introduction-to-nanoelectronics-mit-6-701/2024.3.2.0/introduction_to_nanoelectronics_mit_6_701-2024.3.2.0-py3-none-any.whl

---
+++ packages/introduction-to-nanoelectronics-mit-6-701/introduction_to_nanoelectronics_mit_6_701-2024.3.2.0-py3-none-any.whl/IntroductionToNanoelectronicsMit6701/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""introduction-to-nanoelectronics-mit-6-701
+https://github.com/apachecn/introduction-to-nanoelectronics-mit-6-701"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
