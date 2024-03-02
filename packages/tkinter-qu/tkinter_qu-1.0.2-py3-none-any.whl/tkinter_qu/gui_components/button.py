b807f0bb96fd43d2e7a970091f89960ba4d0bc43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 14:05:28 +0000

    Add package tkinter-qu/1.0.2/tkinter_qu-1.0.2-py3-none-any.whl

---
+++ packages/tkinter-qu/tkinter_qu-1.0.2-py3-none-any.whl/tkinter_qu/gui_components/button.py
@@ -0,0 +1,17 @@
+from tkinter import Button as TkButton
+from tkinter_qu.gui_components.component import Component
+from tkinter_qu.base.colors import *
+
+
+class Button(TkButton, Component):
+    """A button for any application (adds some helper methods from Component to an existing TkButton)"""
+
+    def __init__(self, window_type, text, font, background_color=white, text_color=black):
+        """Initializes a button"""
+
+        super().__init__(window_type, text=text, font=font, bg=background_color, fg=text_color)
+
+    def set_command(self, command):
+        """Sets the command that is called when the button is clicked"""
+
+        self.config(command=command)
