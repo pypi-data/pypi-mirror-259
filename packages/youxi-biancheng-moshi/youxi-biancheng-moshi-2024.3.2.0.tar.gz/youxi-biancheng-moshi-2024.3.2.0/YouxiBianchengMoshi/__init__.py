892dc42c205ab616dc212aca06365d34f4d56d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:43 +0000

    Add package youxi-biancheng-moshi/2024.3.2.0/youxi-biancheng-moshi-2024.3.2.0.tar.gz

---
+++ packages/youxi-biancheng-moshi/youxi-biancheng-moshi-2024.3.2.0.tar.gz/youxi-biancheng-moshi-2024.3.2.0/YouxiBianchengMoshi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""youxi-biancheng-moshi
+https://github.com/apachecn/youxi-biancheng-moshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
