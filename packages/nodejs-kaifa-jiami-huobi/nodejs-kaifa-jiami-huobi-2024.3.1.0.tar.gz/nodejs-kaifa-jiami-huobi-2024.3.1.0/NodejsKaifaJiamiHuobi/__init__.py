aabc43522003269439bf15b14762d68795cefb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 03:05:13 +0000

    Add package nodejs-kaifa-jiami-huobi/2024.3.1.0/nodejs-kaifa-jiami-huobi-2024.3.1.0.tar.gz

---
+++ packages/nodejs-kaifa-jiami-huobi/nodejs-kaifa-jiami-huobi-2024.3.1.0.tar.gz/nodejs-kaifa-jiami-huobi-2024.3.1.0/NodejsKaifaJiamiHuobi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nodejs-kaifa-jiami-huobi
+https://github.com/apachecn/nodejs-kaifa-jiami-huobi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.1.0"
