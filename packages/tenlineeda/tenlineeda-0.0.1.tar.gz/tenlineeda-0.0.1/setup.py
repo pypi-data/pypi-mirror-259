f4d7aaef970d9cf435ed6efd8082560eaa4abb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:30 +0000

    Add package tenlineeda/0.0.1/tenlineeda-0.0.1.tar.gz

---
+++ packages/tenlineeda/tenlineeda-0.0.1.tar.gz/tenlineeda-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="tenlineeda",
+    version="0.0.1",
+    author="may_b_prince",
+    author_email="princekminati@gmail.com",
+    description="I'm tring to making EDA a bit faster for data scientist",
+    long_description="This Library has only one function called <summery>, it will generate your EDA and tells you where you have to dig deeper",
+    packages=setuptools.find_packages(),
+    
+)
