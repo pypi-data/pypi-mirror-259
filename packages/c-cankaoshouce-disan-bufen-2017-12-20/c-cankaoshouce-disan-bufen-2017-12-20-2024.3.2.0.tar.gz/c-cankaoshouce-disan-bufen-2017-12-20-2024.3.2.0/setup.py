d75d89533e2574eee23ca4a38359a214a96be5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 04:10:19 +0000

    Add package c-cankaoshouce-disan-bufen-2017-12-20/2024.3.2.0/c-cankaoshouce-disan-bufen-2017-12-20-2024.3.2.0.tar.gz

---
+++ packages/c-cankaoshouce-disan-bufen-2017-12-20/c-cankaoshouce-disan-bufen-2017-12-20-2024.3.2.0.tar.gz/c-cankaoshouce-disan-bufen-2017-12-20-2024.3.2.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import CCankaoshouceDisanBufen20171220
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('CCankaoshouceDisanBufen20171220'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="c-cankaoshouce-disan-bufen-2017-12-20",
+    version=CCankaoshouceDisanBufen20171220.__version__,
+    url="https://github.com/apachecn/c-cankaoshouce-disan-bufen-2017-12-20",
+    author=CCankaoshouceDisanBufen20171220.__author__,
+    author_email=CCankaoshouceDisanBufen20171220.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="C++ 参考手册 第三部分 2017.12.20",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "c-cankaoshouce-disan-bufen-2017-12-20=CCankaoshouceDisanBufen20171220.__main__:main",
+            "CCankaoshouceDisanBufen20171220=CCankaoshouceDisanBufen20171220.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
