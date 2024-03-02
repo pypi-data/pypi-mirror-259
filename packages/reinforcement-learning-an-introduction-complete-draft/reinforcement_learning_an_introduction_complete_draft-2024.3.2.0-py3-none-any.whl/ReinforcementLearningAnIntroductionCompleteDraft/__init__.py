7a7f2ba8bd56b0d219247484cab1c7a28c93bce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:47 +0000

    Add package reinforcement-learning-an-introduction-complete-draft/2024.3.2.0/reinforcement_learning_an_introduction_complete_draft-2024.3.2.0-py3-none-any.whl

---
+++ packages/reinforcement-learning-an-introduction-complete-draft/reinforcement_learning_an_introduction_complete_draft-2024.3.2.0-py3-none-any.whl/ReinforcementLearningAnIntroductionCompleteDraft/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""reinforcement-learning-an-introduction-complete-draft
+https://github.com/apachecn/reinforcement-learning-an-introduction-complete-draft"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
