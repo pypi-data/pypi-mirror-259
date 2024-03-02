c8d0c8ca18a07deb8b0e8f7ea5d72c6129bccaa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 05:04:21 +0000

    Add package YOLOv8-Explainer/0.0.5/YOLOv8_Explainer-0.0.5.tar.gz

---
+++ packages/YOLOv8-Explainer/YOLOv8_Explainer-0.0.5.tar.gz/YOLOv8_Explainer-0.0.5/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+with open("./README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="YOLOv8_Explainer",
+    version="0.0.05",
+    description="Python packages that enable XAI methods for YOLOv8",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Spritan/YOLOv8_Explainer",
+    # Github="https://github.com/Spritan/YOLOv8_Explainer",
+    author="Spritan",
+    author_email="proyashpsb@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Education",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "grad-cam==1.4.8",
+        "ultralytics",
+        "Pillow",
+        "tqdm",
+        "torch",
+        "matplotlib",
+    ],
+    extras_require={
+        "dev": ["twine>=4.0.2"],
+    },
+    project_urls={
+        'Homepage': 'https://spritan.github.io/YOLOv8_Explainer/',  # Homepage URL
+        'Source': 'https://github.com/Spritan/YOLOv8_Explainer',  # GitHub URL
+    },
+)
