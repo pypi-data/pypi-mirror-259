d891d59f66148af80df12154ecc13f922c81bf0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:31 +0000

    Add package benbanfa-xue-linux-zhongwenban/2024.3.2.0/benbanfa_xue_linux_zhongwenban-2024.3.2.0-py3-none-any.whl

---
+++ packages/benbanfa-xue-linux-zhongwenban/benbanfa_xue_linux_zhongwenban-2024.3.2.0-py3-none-any.whl/BenbanfaXueLinuxZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""benbanfa-xue-linux-zhongwenban
+https://github.com/apachecn/benbanfa-xue-linux-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
