a46def03b3808278dacd1aa26d1e5ebaccf5f8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:34 +0000

    Add package ruby-on-rails-zhinan/2024.3.2.0/ruby-on-rails-zhinan-2024.3.2.0.tar.gz

---
+++ packages/ruby-on-rails-zhinan/ruby-on-rails-zhinan-2024.3.2.0.tar.gz/ruby-on-rails-zhinan-2024.3.2.0/RubyOnRailsZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ruby-on-rails-zhinan
+https://github.com/apachecn/ruby-on-rails-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
