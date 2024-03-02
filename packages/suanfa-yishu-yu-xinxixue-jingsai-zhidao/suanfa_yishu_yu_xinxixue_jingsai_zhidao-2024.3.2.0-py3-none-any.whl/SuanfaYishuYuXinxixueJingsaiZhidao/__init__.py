ed17e6d9ab33aecd4f8cb3adad752876264918c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 15:07:52 +0000

    Add package suanfa-yishu-yu-xinxixue-jingsai-zhidao/2024.3.2.0/suanfa_yishu_yu_xinxixue_jingsai_zhidao-2024.3.2.0-py3-none-any.whl

---
+++ packages/suanfa-yishu-yu-xinxixue-jingsai-zhidao/suanfa_yishu_yu_xinxixue_jingsai_zhidao-2024.3.2.0-py3-none-any.whl/SuanfaYishuYuXinxixueJingsaiZhidao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""suanfa-yishu-yu-xinxixue-jingsai-zhidao
+https://github.com/apachecn/suanfa-yishu-yu-xinxixue-jingsai-zhidao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
