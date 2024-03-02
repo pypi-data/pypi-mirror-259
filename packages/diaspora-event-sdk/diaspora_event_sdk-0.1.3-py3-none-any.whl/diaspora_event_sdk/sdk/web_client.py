ba47f9efa2d30cf992b6a3c1a580e4b9ab039f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:06:03 +0000

    Add package diaspora-event-sdk/0.1.3/diaspora_event_sdk-0.1.3-py3-none-any.whl

---
+++ packages/diaspora-event-sdk/diaspora_event_sdk-0.1.3-py3-none-any.whl/diaspora_event_sdk/sdk/web_client.py
@@ -0,0 +1,67 @@
+from typing import Optional
+
+import globus_sdk
+from diaspora_event_sdk.sdk.utils.uuid_like import UUID_LIKE_T
+
+from ._environments import TOKEN_EXCHANGE
+
+
+class WebClient(globus_sdk.BaseClient):
+    def __init__(
+        self,
+        *,
+        environment: Optional[str] = None,
+        base_url: Optional[str] = None,
+        app_name: Optional[str] = None,
+        **kwargs,
+    ):
+        if base_url is None:
+            base_url = TOKEN_EXCHANGE
+
+        super().__init__(environment=environment, base_url=base_url, **kwargs)
+
+        self._user_app_name = None
+        self.user_app_name = app_name
+
+    def create_key(self, subject: UUID_LIKE_T) -> globus_sdk.GlobusHTTPResponse:
+        return self.post("/v1/create_key", headers={"Subject": str(subject)})
+
+    def list_topics(self, subject: UUID_LIKE_T) -> globus_sdk.GlobusHTTPResponse:
+        return self.get("/v1/list_topics", headers={"Subject": str(subject)})
+
+    def register_topic(
+        self, subject: UUID_LIKE_T, topic: str
+    ) -> globus_sdk.GlobusHTTPResponse:
+        return self.post(
+            "/v1/register_topic", headers={"Subject": str(subject), "Topic": topic}
+        )
+
+    def unregister_topic(
+        self, subject: UUID_LIKE_T, topic: str
+    ) -> globus_sdk.GlobusHTTPResponse:
+        return self.post(
+            "/v1/unregister_topic", headers={"Subject": str(subject), "Topic": topic}
+        )
+
+    def list_functions(self, subject: UUID_LIKE_T) -> globus_sdk.GlobusHTTPResponse:
+        return self.get("/v1/list_functions", headers={"Subject": str(subject)})
+
+    def register_function(
+        self, subject: UUID_LIKE_T, topic: str, function: str,
+        function_runtime: str, function_handler: str, function_body: str
+    ) -> globus_sdk.GlobusHTTPResponse:
+        return self.post(
+            "/v1/register_function",
+            headers={"Subject": str(subject), "Topic": topic,
+                     "Function": function},
+            data={"function_runtime": function_runtime,
+                  "function_handler": function_handler,
+                  "function_body": function_body}
+        )
+
+    def unregister_function(
+        self, subject: UUID_LIKE_T, topic: str, function: str
+    ) -> globus_sdk.GlobusHTTPResponse:
+        return self.post(
+            "/v1/unregister_function", headers={"Subject": str(subject), "Topic": topic, "Function": function}
+        )
