61884087bfae12fba71643909a37b56b5834035c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:20 +0000

    Add package daima-neng-you-duonan-jiandan-de-wangye-daima-shu/2024.3.2.0/daima_neng_you_duonan_jiandan_de_wangye_daima_shu-2024.3.2.0-py3-none-any.whl

---
+++ packages/daima-neng-you-duonan-jiandan-de-wangye-daima-shu/daima_neng_you_duonan_jiandan_de_wangye_daima_shu-2024.3.2.0-py3-none-any.whl/DaimaNengYouDuonanJiandanDeWangyeDaimaShu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""daima-neng-you-duonan-jiandan-de-wangye-daima-shu
+https://github.com/apachecn/daima-neng-you-duonan-jiandan-de-wangye-daima-shu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
