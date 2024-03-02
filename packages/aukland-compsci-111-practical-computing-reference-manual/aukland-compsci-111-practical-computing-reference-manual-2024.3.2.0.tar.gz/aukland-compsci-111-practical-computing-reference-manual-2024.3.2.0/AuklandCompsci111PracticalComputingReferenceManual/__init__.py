2fcd44b78f8241e02e82c861b85ceaf5952b50d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:05:13 +0000

    Add package aukland-compsci-111-practical-computing-reference-manual/2024.3.2.0/aukland-compsci-111-practical-computing-reference-manual-2024.3.2.0.tar.gz

---
+++ packages/aukland-compsci-111-practical-computing-reference-manual/aukland-compsci-111-practical-computing-reference-manual-2024.3.2.0.tar.gz/aukland-compsci-111-practical-computing-reference-manual-2024.3.2.0/AuklandCompsci111PracticalComputingReferenceManual/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aukland-compsci-111-practical-computing-reference-manual
+https://github.com/apachecn/aukland-compsci-111-practical-computing-reference-manual"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
