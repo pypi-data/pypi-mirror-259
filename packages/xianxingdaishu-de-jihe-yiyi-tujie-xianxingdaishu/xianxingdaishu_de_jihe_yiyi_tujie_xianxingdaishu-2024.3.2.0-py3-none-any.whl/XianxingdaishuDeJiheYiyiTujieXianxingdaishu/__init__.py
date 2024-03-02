ca6ca0c0fa2a407f7ef2c53f1d7224a3171cf424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:39 +0000

    Add package xianxingdaishu-de-jihe-yiyi-tujie-xianxingdaishu/2024.3.2.0/xianxingdaishu_de_jihe_yiyi_tujie_xianxingdaishu-2024.3.2.0-py3-none-any.whl

---
+++ packages/xianxingdaishu-de-jihe-yiyi-tujie-xianxingdaishu/xianxingdaishu_de_jihe_yiyi_tujie_xianxingdaishu-2024.3.2.0-py3-none-any.whl/XianxingdaishuDeJiheYiyiTujieXianxingdaishu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xianxingdaishu-de-jihe-yiyi-tujie-xianxingdaishu
+https://github.com/apachecn/xianxingdaishu-de-jihe-yiyi-tujie-xianxingdaishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
