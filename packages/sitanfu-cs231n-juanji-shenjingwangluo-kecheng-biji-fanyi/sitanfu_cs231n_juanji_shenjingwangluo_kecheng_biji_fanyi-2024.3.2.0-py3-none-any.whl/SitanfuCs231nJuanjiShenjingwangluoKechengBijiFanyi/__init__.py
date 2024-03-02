33465e594a7e3fdd16a7a5c8c0e810a0ee413798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 06:06:06 +0000

    Add package sitanfu-cs231n-juanji-shenjingwangluo-kecheng-biji-fanyi/2024.3.2.0/sitanfu_cs231n_juanji_shenjingwangluo_kecheng_biji_fanyi-2024.3.2.0-py3-none-any.whl

---
+++ packages/sitanfu-cs231n-juanji-shenjingwangluo-kecheng-biji-fanyi/sitanfu_cs231n_juanji_shenjingwangluo_kecheng_biji_fanyi-2024.3.2.0-py3-none-any.whl/SitanfuCs231nJuanjiShenjingwangluoKechengBijiFanyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sitanfu-cs231n-juanji-shenjingwangluo-kecheng-biji-fanyi
+https://github.com/apachecn/sitanfu-cs231n-juanji-shenjingwangluo-kecheng-biji-fanyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
