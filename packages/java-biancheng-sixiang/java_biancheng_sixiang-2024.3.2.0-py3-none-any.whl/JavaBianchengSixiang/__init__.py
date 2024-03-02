35df7a76385460397a81b8ec744ccf89d708ceec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:14 +0000

    Add package java-biancheng-sixiang/2024.3.2.0/java_biancheng_sixiang-2024.3.2.0-py3-none-any.whl

---
+++ packages/java-biancheng-sixiang/java_biancheng_sixiang-2024.3.2.0-py3-none-any.whl/JavaBianchengSixiang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-biancheng-sixiang
+https://github.com/apachecn/java-biancheng-sixiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
