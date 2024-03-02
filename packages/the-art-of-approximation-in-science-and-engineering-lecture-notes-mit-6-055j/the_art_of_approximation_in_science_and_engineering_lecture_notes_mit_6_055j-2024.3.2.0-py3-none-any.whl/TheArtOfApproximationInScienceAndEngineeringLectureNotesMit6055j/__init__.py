9c4e075b33bbf03d93370637da84f6a01778d031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-02 16:05:06 +0000

    Add package the-art-of-approximation-in-science-and-engineering-lecture-notes-mit-6-055j/2024.3.2.0/the_art_of_approximation_in_science_and_engineering_lecture_notes_mit_6_055j-2024.3.2.0-py3-none-any.whl

---
+++ packages/the-art-of-approximation-in-science-and-engineering-lecture-notes-mit-6-055j/the_art_of_approximation_in_science_and_engineering_lecture_notes_mit_6_055j-2024.3.2.0-py3-none-any.whl/TheArtOfApproximationInScienceAndEngineeringLectureNotesMit6055j/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""the-art-of-approximation-in-science-and-engineering-lecture-notes-mit-6-055j
+https://github.com/apachecn/the-art-of-approximation-in-science-and-engineering-lecture-notes-mit-6-055j"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
