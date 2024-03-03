74583f08a361a1dac78ae0ad27dbd12d40d942a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:07:07 +0000

    Add package operating-systems-and-middleware-supporting-controlled-interaction/2024.3.2.0/operating_systems_and_middleware_supporting_controlled_interaction-2024.3.2.0-py3-none-any.whl

---
+++ packages/operating-systems-and-middleware-supporting-controlled-interaction/operating_systems_and_middleware_supporting_controlled_interaction-2024.3.2.0-py3-none-any.whl/OperatingSystemsAndMiddlewareSupportingControlledInteraction/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""operating-systems-and-middleware-supporting-controlled-interaction
+https://github.com/apachecn/operating-systems-and-middleware-supporting-controlled-interaction"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
