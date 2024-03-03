3ece35923b28f3ff741969457577da99709d8d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:06:46 +0000

    Add package qukuai-lian-jishu-zhinan-0-9-0/2024.3.2.0/qukuai_lian_jishu_zhinan_0_9_0-2024.3.2.0-py3-none-any.whl

---
+++ packages/qukuai-lian-jishu-zhinan-0-9-0/qukuai_lian_jishu_zhinan_0_9_0-2024.3.2.0-py3-none-any.whl/QukuaiLianJishuZhinan090/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qukuai-lian-jishu-zhinan-0-9-0
+https://github.com/apachecn/qukuai-lian-jishu-zhinan-0-9-0"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
