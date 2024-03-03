245e11ead8da8d9fc70ae6b3b60c42daf3217656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:07:08 +0000

    Add package scprotein/1.1.1/scprotein-1.1.1-py3-none-any.whl

---
+++ packages/scprotein/scprotein-1.1.1-py3-none-any.whl/scprotein/__init__.py
@@ -0,0 +1,4 @@
+from . import peptide_uncertainty_estimation
+from .model import *
+from .utils import *
+from .prototype_loss import *
---
+++ packages/scprotein/scprotein-1.1.1-py3-none-any.whl/scprotein/peptide_uncertainty_estimation/__init__.py
@@ -0,0 +1,7 @@
+from .multi_task_heteroscedastic_regression_loss import *
+from .multi_task_heteroscedastic_regression_model import *
+from .peptide_uncertainty_utils import *
+
+
+
+
