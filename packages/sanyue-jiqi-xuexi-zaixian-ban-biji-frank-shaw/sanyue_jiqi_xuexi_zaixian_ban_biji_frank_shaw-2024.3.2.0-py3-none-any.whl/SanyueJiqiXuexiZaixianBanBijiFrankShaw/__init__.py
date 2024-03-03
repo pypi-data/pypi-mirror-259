b9a7a694517e8518e264fe3eca9b606540c6d656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:05:20 +0000

    Add package sanyue-jiqi-xuexi-zaixian-ban-biji-frank-shaw/2024.3.2.0/sanyue_jiqi_xuexi_zaixian_ban_biji_frank_shaw-2024.3.2.0-py3-none-any.whl

---
+++ packages/sanyue-jiqi-xuexi-zaixian-ban-biji-frank-shaw/sanyue_jiqi_xuexi_zaixian_ban_biji_frank_shaw-2024.3.2.0-py3-none-any.whl/SanyueJiqiXuexiZaixianBanBijiFrankShaw/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sanyue-jiqi-xuexi-zaixian-ban-biji-frank-shaw
+https://github.com/apachecn/sanyue-jiqi-xuexi-zaixian-ban-biji-frank-shaw"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
