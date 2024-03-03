179e7ba7e561b3db3a429bdb6e82bed759abe5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:07:05 +0000

    Add package acb/0.5.8/acb-0.5.8-py3-none-any.whl

---
+++ packages/acb/acb-0.5.8-py3-none-any.whl/acb/adapters/sql/mysql.py
@@ -0,0 +1,27 @@
+import typing as t
+
+from acb.config import Config
+from acb.depends import depends
+from sqlalchemy.pool import NullPool
+from ._base import SqlBase
+from ._base import SqlBaseSettings
+
+
+class SqlSettings(SqlBaseSettings):
+    _driver: str = "mysql+mysqldb"
+    _async_driver: str = "mysql+asyncmy"
+
+    @depends.inject
+    def __init__(self, config: Config = depends(), **values: t.Any) -> None:
+        super().__init__(config, **values)
+        self.poolclass = NullPool
+        self.pool_pre_ping = True
+        self.engine_kwargs = self.engine_kwargs | dict(  # type: ignore
+            poolclass=self.poolclass, pool_pre_ping=self.pool_pre_ping
+        )
+
+
+class Sql(SqlBase): ...
+
+
+depends.set(Sql)
