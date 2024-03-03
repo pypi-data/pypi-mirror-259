7d7b0069d4e3ddbfb7f4f631a8931d8e692b37d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:05:18 +0000

    Add package thermopack/2.2.1/thermopack-2.2.1-py3-none-win_amd64.whl

---
+++ packages/thermopack/thermopack-2.2.1-py3-none-win_amd64.whl/thermopack/platform_specifics.py
@@ -0,0 +1,17 @@
+# Module for platform specific stuff. Automatically generated.
+# Timestamp : 2024-03-02T11:34:55.999267
+
+
+DIFFERENTIAL_RETURN_MODE = 'v2'
+
+
+def get_platform_specifics():
+    pf_specifics = {}
+    pf_specifics["os_id"] = "win"
+    pf_specifics["prefix"] = ""
+    pf_specifics["module"] = "_mp_"
+    pf_specifics["postfix"] = "_"
+    pf_specifics["postfix_no_module"] = "_"
+    pf_specifics["dyn_lib"] = "thermopack.dll"
+    pf_specifics["diff_return_mode"] = "v2"
+    return pf_specifics
