be234be8c2750f5f096e48af2086317b5659f473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:07:32 +0000

    Add package gitoptim/0.1.0.dev7/gitoptim-0.1.0.dev7.tar.gz

---
+++ packages/gitoptim/gitoptim-0.1.0.dev7.tar.gz/gitoptim-0.1.0.dev7/gitoptim/utils/cli.py
@@ -0,0 +1,13 @@
+import sys
+import functools
+from rich.console import Console
+
+SUBCOMMAND_NAMES = ["analyse"]
+
+
+def should_callback_execute():
+    return "--help" not in sys.argv and sys.argv[-1] not in SUBCOMMAND_NAMES
+
+
+console = Console()
+error_console = Console(stderr=True, style="bold red")
