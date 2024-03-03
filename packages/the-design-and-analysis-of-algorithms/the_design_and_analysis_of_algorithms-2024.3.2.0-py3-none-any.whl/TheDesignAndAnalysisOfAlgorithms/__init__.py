a76e30503707a2ffa34b6d3de51869f96c8a1156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:05:42 +0000

    Add package the-design-and-analysis-of-algorithms/2024.3.2.0/the_design_and_analysis_of_algorithms-2024.3.2.0-py3-none-any.whl

---
+++ packages/the-design-and-analysis-of-algorithms/the_design_and_analysis_of_algorithms-2024.3.2.0-py3-none-any.whl/TheDesignAndAnalysisOfAlgorithms/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""the-design-and-analysis-of-algorithms
+https://github.com/apachecn/the-design-and-analysis-of-algorithms"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
