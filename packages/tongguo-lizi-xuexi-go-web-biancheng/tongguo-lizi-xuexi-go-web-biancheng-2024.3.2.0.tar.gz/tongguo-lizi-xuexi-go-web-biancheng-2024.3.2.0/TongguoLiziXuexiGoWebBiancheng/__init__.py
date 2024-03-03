e1dbe82557accc41f6fd14d175ce0b11018bedd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:07:32 +0000

    Add package tongguo-lizi-xuexi-go-web-biancheng/2024.3.2.0/tongguo-lizi-xuexi-go-web-biancheng-2024.3.2.0.tar.gz

---
+++ packages/tongguo-lizi-xuexi-go-web-biancheng/tongguo-lizi-xuexi-go-web-biancheng-2024.3.2.0.tar.gz/tongguo-lizi-xuexi-go-web-biancheng-2024.3.2.0/TongguoLiziXuexiGoWebBiancheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tongguo-lizi-xuexi-go-web-biancheng
+https://github.com/apachecn/tongguo-lizi-xuexi-go-web-biancheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
