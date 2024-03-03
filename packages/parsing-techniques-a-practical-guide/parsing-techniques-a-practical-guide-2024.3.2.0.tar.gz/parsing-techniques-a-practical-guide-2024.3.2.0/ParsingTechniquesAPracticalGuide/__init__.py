cdcff48007e11a4f3d8f78afb4d6ce91dc22cf8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:06:56 +0000

    Add package parsing-techniques-a-practical-guide/2024.3.2.0/parsing-techniques-a-practical-guide-2024.3.2.0.tar.gz

---
+++ packages/parsing-techniques-a-practical-guide/parsing-techniques-a-practical-guide-2024.3.2.0.tar.gz/parsing-techniques-a-practical-guide-2024.3.2.0/ParsingTechniquesAPracticalGuide/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""parsing-techniques-a-practical-guide
+https://github.com/apachecn/parsing-techniques-a-practical-guide"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
