0849709381b87f809940a0f487616fd2fece7ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:07:19 +0000

    Add package tiny-python-es6-notebook/2024.3.2.0/tiny_python_es6_notebook-2024.3.2.0-py3-none-any.whl

---
+++ packages/tiny-python-es6-notebook/tiny_python_es6_notebook-2024.3.2.0-py3-none-any.whl/TinyPythonEs6Notebook/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tiny-python-es6-notebook
+https://github.com/apachecn/tiny-python-es6-notebook"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
