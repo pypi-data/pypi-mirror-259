10714ae05bd9e9502e399ed911b26483b4ff8a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:05:30 +0000

    Add package jiagoushi-zhi-lu-58-shenjian/2024.3.2.0/jiagoushi_zhi_lu_58_shenjian-2024.3.2.0-py3-none-any.whl

---
+++ packages/jiagoushi-zhi-lu-58-shenjian/jiagoushi_zhi_lu_58_shenjian-2024.3.2.0-py3-none-any.whl/JiagoushiZhiLu58Shenjian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiagoushi-zhi-lu-58-shenjian
+https://github.com/apachecn/jiagoushi-zhi-lu-58-shenjian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
