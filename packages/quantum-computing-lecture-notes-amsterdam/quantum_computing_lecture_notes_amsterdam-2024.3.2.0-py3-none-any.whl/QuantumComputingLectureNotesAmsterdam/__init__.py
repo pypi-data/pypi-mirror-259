1b9cec41e89597cd9311a4be064b0ccd864a3dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:06:45 +0000

    Add package quantum-computing-lecture-notes-amsterdam/2024.3.2.0/quantum_computing_lecture_notes_amsterdam-2024.3.2.0-py3-none-any.whl

---
+++ packages/quantum-computing-lecture-notes-amsterdam/quantum_computing_lecture_notes_amsterdam-2024.3.2.0-py3-none-any.whl/QuantumComputingLectureNotesAmsterdam/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""quantum-computing-lecture-notes-amsterdam
+https://github.com/apachecn/quantum-computing-lecture-notes-amsterdam"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
