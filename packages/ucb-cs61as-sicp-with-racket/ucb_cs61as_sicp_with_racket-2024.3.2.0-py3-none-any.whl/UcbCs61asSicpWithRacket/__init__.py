e501bbeeb076ee781deed035c3495da1ea1c6747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:07:25 +0000

    Add package ucb-cs61as-sicp-with-racket/2024.3.2.0/ucb_cs61as_sicp_with_racket-2024.3.2.0-py3-none-any.whl

---
+++ packages/ucb-cs61as-sicp-with-racket/ucb_cs61as_sicp_with_racket-2024.3.2.0-py3-none-any.whl/UcbCs61asSicpWithRacket/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ucb-cs61as-sicp-with-racket
+https://github.com/apachecn/ucb-cs61as-sicp-with-racket"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
