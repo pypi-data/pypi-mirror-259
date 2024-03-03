090b1584fbf1906e151b41582339b88a8d80bb87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:07:34 +0000

    Add package zhichi-xiangliang-ji-tongsu-daolun-lijie-svm-de-sanceng-jingjie-latex-ban/2024.3.2.0/zhichi_xiangliang_ji_tongsu_daolun_lijie_svm_de_sanceng_jingjie_latex_ban-2024.3.2.0-py3-none-any.whl

---
+++ packages/zhichi-xiangliang-ji-tongsu-daolun-lijie-svm-de-sanceng-jingjie-latex-ban/zhichi_xiangliang_ji_tongsu_daolun_lijie_svm_de_sanceng_jingjie_latex_ban-2024.3.2.0-py3-none-any.whl/ZhichiXiangliangJiTongsuDaolunLijieSvmDeSancengJingjieLatexBan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhichi-xiangliang-ji-tongsu-daolun-lijie-svm-de-sanceng-jingjie-latex-ban
+https://github.com/apachecn/zhichi-xiangliang-ji-tongsu-daolun-lijie-svm-de-sanceng-jingjie-latex-ban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
