0923810acd7a4162baba18b104e813d403913a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:05:53 +0000

    Add package sitanfu-cs183b-yc-chuangye-ke-wenziban-chuangjian/2024.3.2.0/sitanfu-cs183b-yc-chuangye-ke-wenziban-chuangjian-2024.3.2.0.tar.gz

---
+++ packages/sitanfu-cs183b-yc-chuangye-ke-wenziban-chuangjian/sitanfu-cs183b-yc-chuangye-ke-wenziban-chuangjian-2024.3.2.0.tar.gz/sitanfu-cs183b-yc-chuangye-ke-wenziban-chuangjian-2024.3.2.0/SitanfuCs183bYcChuangyeKeWenzibanChuangjian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sitanfu-cs183b-yc-chuangye-ke-wenziban-chuangjian
+https://github.com/apachecn/sitanfu-cs183b-yc-chuangye-ke-wenziban-chuangjian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
