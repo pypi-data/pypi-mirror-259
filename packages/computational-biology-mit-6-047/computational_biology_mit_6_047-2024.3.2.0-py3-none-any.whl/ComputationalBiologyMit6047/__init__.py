bb8488d503e18c62d778175dd5735c0d8d7c50a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:05:16 +0000

    Add package computational-biology-mit-6-047/2024.3.2.0/computational_biology_mit_6_047-2024.3.2.0-py3-none-any.whl

---
+++ packages/computational-biology-mit-6-047/computational_biology_mit_6_047-2024.3.2.0-py3-none-any.whl/ComputationalBiologyMit6047/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""computational-biology-mit-6-047
+https://github.com/apachecn/computational-biology-mit-6-047"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
