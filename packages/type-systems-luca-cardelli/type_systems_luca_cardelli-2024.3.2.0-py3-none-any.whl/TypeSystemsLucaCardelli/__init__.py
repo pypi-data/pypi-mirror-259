1316520e67b53385e75e8c96801bbd99af497525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:07:24 +0000

    Add package type-systems-luca-cardelli/2024.3.2.0/type_systems_luca_cardelli-2024.3.2.0-py3-none-any.whl

---
+++ packages/type-systems-luca-cardelli/type_systems_luca_cardelli-2024.3.2.0-py3-none-any.whl/TypeSystemsLucaCardelli/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""type-systems-luca-cardelli
+https://github.com/apachecn/type-systems-luca-cardelli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
