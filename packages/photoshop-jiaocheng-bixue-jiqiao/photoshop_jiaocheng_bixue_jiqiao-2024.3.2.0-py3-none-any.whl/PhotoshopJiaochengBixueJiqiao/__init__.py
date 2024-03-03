f7bf1423f30b9ae003893ef01a8d09c6eb16c8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:06:15 +0000

    Add package photoshop-jiaocheng-bixue-jiqiao/2024.3.2.0/photoshop_jiaocheng_bixue_jiqiao-2024.3.2.0-py3-none-any.whl

---
+++ packages/photoshop-jiaocheng-bixue-jiqiao/photoshop_jiaocheng_bixue_jiqiao-2024.3.2.0-py3-none-any.whl/PhotoshopJiaochengBixueJiqiao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""photoshop-jiaocheng-bixue-jiqiao
+https://github.com/apachecn/photoshop-jiaocheng-bixue-jiqiao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.2.0"
